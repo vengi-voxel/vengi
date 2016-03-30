f74f29550276a5b4adf7cdc7088bb7af30b5f1a0
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-03-30 17:25:36 +0200

    silence some polyvox warnings

--- src/modules/core/Common.h
+++ src/modules/core/Common.h
@@ -33,3 +33,28 @@ inline T clamp(T a, T low, T high) {
 //#define core_assert(condition) SDL_assert(condition)
 #define core_assert(condition) assert(condition)
 #endif
+
+#define DIAG_STR(s) #s
+#define DIAG_JOINSTR(x,y) DIAG_STR(x ## y)
+#ifdef _MSC_VER
+#define DIAG_DO_PRAGMA(x) __pragma (#x)
+#define DIAG_PRAGMA(compiler,x) DIAG_DO_PRAGMA(warning(x))
+#else
+#define DIAG_DO_PRAGMA(x) _Pragma (#x)
+#define DIAG_PRAGMA(compiler,x) DIAG_DO_PRAGMA(compiler diagnostic x)
+#endif
+#if defined(__clang__)
+# define DISABLE_WARNING(gcc_unused,clang_option,msvc_unused) DIAG_PRAGMA(clang,push) DIAG_PRAGMA(clang,ignored DIAG_JOINSTR(-W,clang_option))
+# define ENABLE_WARNING(gcc_unused,clang_option,msvc_unused) DIAG_PRAGMA(clang,pop)
+#elif defined(_MSC_VER)
+# define DISABLE_WARNING(gcc_unused,clang_unused,msvc_errorcode) DIAG_PRAGMA(msvc,push) DIAG_DO_PRAGMA(warning(disable:##msvc_errorcode))
+# define ENABLE_WARNING(gcc_unused,clang_unused,msvc_errorcode) DIAG_PRAGMA(msvc,pop)
+#elif defined(__GNUC__)
+#if ((__GNUC__ * 100) + __GNUC_MINOR__) >= 406
+# define DISABLE_WARNING(gcc_option,clang_unused,msvc_unused) DIAG_PRAGMA(GCC,push) DIAG_PRAGMA(GCC,ignored DIAG_JOINSTR(-W,gcc_option))
+# define ENABLE_WARNING(gcc_option,clang_unused,msvc_unused) DIAG_PRAGMA(GCC,pop)
+#else
+# define DISABLE_WARNING(gcc_option,clang_unused,msvc_unused) DIAG_PRAGMA(GCC,ignored DIAG_JOINSTR(-W,gcc_option))
+# define ENABLE_WARNING(gcc_option,clang_option,msvc_unused) DIAG_PRAGMA(GCC,warning DIAG_JOINSTR(-W,gcc_option))
+#endif
+#endif
--- src/modules/voxel/WorldData.h
+++ src/modules/voxel/WorldData.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "core/Common.h"
+
+DISABLE_WARNING(unused-but-set-variable,unused-but-set-variable,42)
 #include <PolyVox/PagedVolume.h>
 #include "Voxel.h"
 
