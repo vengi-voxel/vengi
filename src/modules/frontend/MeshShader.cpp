50eb7aa7e7f9e0e1e0509553e4f759e675a79401
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-04-04 20:14:57 +0200

    FRONTEND: load a shader even if some uniforms or attributes are not found
    during development it is possible that some uniforms/attributes
    are optimized out of the shader - it annoys the shit out of me
    that the client doesn't start then

--- src/modules/frontend/MeshShader.cpp
+++ src/modules/frontend/MeshShader.cpp
@@ -9,39 +9,30 @@ bool MeshShader::init() {
 
 	if (!hasAttribute("a_pos")) {
 		Log::error("no attribute a_pos found");
-		return false;
 	}
 	if (!hasAttribute("a_texcoords")) {
 		Log::error("no attribute a_texcoords found");
-		return false;
 	}
 	if (!hasAttribute("a_norm")) {
 		Log::error("no attribute a_norm found");
-		return false;
 	}
 	if (!hasUniform("u_projection")) {
 		Log::error("no uniform u_projection found");
-		return false;
 	}
 	if (!hasUniform("u_model")) {
 		Log::error("no uniform u_model found");
-		return false;
 	}
 	if (!hasUniform("u_view")) {
 		Log::error("no uniform u_view found");
-		return false;
 	}
 	if (!hasUniform("u_texture")) {
 		Log::error("no uniform u_texture found");
-		return false;
 	}
 	if (!hasUniform("u_viewdistance")) {
 		Log::error("no uniform u_viewdistance found");
-		return false;
 	}
 	if (!hasUniform("u_fogrange")) {
 		Log::error("no uniform u_fogrange found");
-		return false;
 	}
 
 	return true;
