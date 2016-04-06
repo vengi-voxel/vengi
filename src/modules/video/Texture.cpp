d7c7b778a63728f9ae8c9f47f8d94f180da3f76b
mgerhardy <martin.gerhardy@gmail.com>
2016-04-06 20:44:54 +0200

    VIDEO: fixed black voxels when we cross the 0,0 border

--- src/modules/video/Texture.cpp
+++ src/modules/video/Texture.cpp
@@ -32,8 +32,8 @@ void Texture::upload(const uint8_t* data, int width, int height, int depth) {
 	glTexImage2D(GL_TEXTURE_2D, 0, mode, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, (const void*)data);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
 	_state = io::IOSTATE_LOADED;
 	GL_checkError();
 }
