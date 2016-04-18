60555dbeb2424937e8baf37d63259d83180b9002
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-04-18 20:21:19 +0200

    NETWORK: fixed compile errors and warnings

--- src/modules/network/IMsgProtocolHandler.h
+++ src/modules/network/IMsgProtocolHandler.h
@@ -22,7 +22,7 @@ public:
 	virtual void execute(ENetPeer* peer, const void* message) override {
 		auto* attachment = getAttachment<ATTACHMENTTYPE>(peer);
 		if (_needsAttachment && attachment == nullptr) {
-			Log::error("No attachment yet for a message that needs one");
+			::Log::error("No attachment yet for a message that needs one");
 			return;
 		}
 		const auto* msg = getMsg<MSGTYPE>(message);
