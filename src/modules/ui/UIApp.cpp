32ef79cab9cf87a5b410154b8dfa5638feddf9f7
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-04-26 20:24:29 +0200

    UI; comment for tb::TBSystem::RescheduleTimer
    this is not really needed, as we are not event driven, but our core loop is updated each frame

--- src/modules/ui/UIApp.cpp
+++ src/modules/ui/UIApp.cpp
@@ -389,6 +389,12 @@ void UIApp::afterUI() {
 
 }
 
+/**
+ * Called when the need to call TBMessageHandler::ProcessMessages has changed due to changes in the
+ * message queue. fire_time is the new time is needs to be called.
+ * It may be 0 which means that ProcessMessages should be called asap (but NOT from this call!)
+ * It may also be TB_NOT_SOON which means that ProcessMessages doesn't need to be called.
+ */
 void tb::TBSystem::RescheduleTimer(double fireTime) {
 #if 0
 	static double setFireTime = -1;
