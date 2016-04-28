13518e97d88fc69c4e1a7fafa32c726ddef70467
mgerhardy <martin.gerhardy@gmail.com>
2016-04-28 22:13:40 +0200

    VOXEL: added test for generating/extraction chunks

--- src/modules/voxel/tests/WorldTest.cpp
+++ src/modules/voxel/tests/WorldTest.cpp
@@ -1,10 +1,38 @@
 #include "AbstractVoxelTest.h"
 #include "voxel/World.h"
+#include <chrono>
 
 namespace voxel {
 
 class WorldTest: public AbstractVoxelTest {
 };
+
+TEST_F(WorldTest, DISABLED_testExtraction) {
+	World world;
+	int expected = 0;
+	for (int i = 0; i < 1024; ++i) {
+		const glm::ivec3 pos { i, 0, i };
+		if (world.scheduleMeshExtraction(pos))
+			++expected;
+	}
+
+	int extracted = 0;
+	auto start = std::chrono::high_resolution_clock::now();
+	for (;;) {
+		DecodedMeshData meshData;
+		while (!world.pop(meshData)) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+			auto end = std::chrono::high_resolution_clock::now();
+			std::chrono::duration<double, std::milli> elapsed = end - start;
+			const double millis = elapsed.count();
+			ASSERT_LT(120 * 1000, millis);
+		}
+		++extracted;
+		if (extracted == expected) {
+			break;
+		}
+	}
+}
 
 TEST_F(WorldTest, testChunkAndGridPos) {
 	World world;
