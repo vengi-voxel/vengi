e9b11a7c3b6ecc3a2a133f02ce8222c51fc699c6
mgerhardy <martin.gerhardy@gmail.com>
2016-05-10 20:06:16 +0200

    VOXEL: call allowReExtraction for dirty positions

--- src/modules/voxel/World.cpp
+++ src/modules/voxel/World.cpp
@@ -42,6 +42,9 @@ bool World::Pager::pageIn(const Region& region, PagedVolume::Chunk* chunk) {
 #endif
 	{
 		_world.create(ctx);
+		for (const glm::ivec3& pos : ctx.dirty) {
+			_world.allowReExtraction(pos);
+		}
 	}
 	return true;
 }
