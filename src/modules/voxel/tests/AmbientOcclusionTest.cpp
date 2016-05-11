db1c4e63be01011cd0623c5826291f4b784aa710
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-05-11 20:31:12 +0200

    VOXEL: ao unittest

---
+++ src/modules/voxel/tests/AmbientOcclusionTest.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file
+ */
+
+#include "AbstractVoxelTest.h"
+#include "voxel/World.h"
+
+namespace voxel {
+
+class AmbientOcclusionTest: public AbstractVoxelTest {
+};
+
+TEST_F(AmbientOcclusionTest, testFoo) {
+}
+
+}
