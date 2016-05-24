219d6f88fd6ffb4e9772fb8d9b9471f9b7c31319
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-05-24 20:25:14 +0200

    Revert "VOXEL: use the normalized noise functions"
    This reverts commit 4c40636afdbe643dede828240b49c97ff15f5240.
    
    no terrain was generated

--- src/modules/voxel/generator/WorldGenerator.cpp
+++ src/modules/voxel/generator/WorldGenerator.cpp
@@ -33,7 +33,7 @@ void WorldGenerator::createWorld(WorldContext& worldCtx, TerrainContext& ctx, Bi
 	for (int z = lowerZ; z < lowerZ + depth; ++z) {
 		for (int x = lowerX; x < lowerX + width; ++x) {
 			const glm::vec2 noisePos2d(noiseSeedOffsetX + x, noiseSeedOffsetZ + z);
-			const float landscapeNoise = noise::Simplex::Noise2DClamped(noisePos2d, worldCtx.landscapeNoiseOctaves,
+			const float landscapeNoise = noise::Simplex::Noise2D(noisePos2d, worldCtx.landscapeNoiseOctaves,
 					worldCtx.landscapeNoisePersistence, worldCtx.landscapeNoiseFrequency, worldCtx.landscapeNoiseAmplitude);
 			const float noiseNormalized = noise::norm(landscapeNoise);
 			const float mountainNoise = noise::Simplex::Noise2D(noisePos2d, worldCtx.mountainNoiseOctaves,
@@ -41,7 +41,7 @@ void WorldGenerator::createWorld(WorldContext& worldCtx, TerrainContext& ctx, Bi
 			const float mountainNoiseNormalized = noise::norm(mountainNoise);
 			const float mountainMultiplier = mountainNoiseNormalized * (mountainNoiseNormalized + 0.5f);
 			const float n = glm::clamp(noiseNormalized * mountainMultiplier, 0.0f, 1.0f);
-			const int ni = n * (MAX_TERRAIN_HEIGHT - 1);
+			 const int ni = n * (MAX_TERRAIN_HEIGHT - 1);
 			if (plainTerrainBool) {
 				for (int y = 0; y < ni; ++y) {
 					const Voxel& voxel = biomManager.getVoxelType(x, y, z, false);
@@ -54,7 +54,7 @@ void WorldGenerator::createWorld(WorldContext& worldCtx, TerrainContext& ctx, Bi
 				for (int y = ni - 1; y >= 1; --y) {
 					const glm::vec3 noisePos3d(noisePos2d.x, y, noisePos2d.y);
 					const float noiseVal = noise::norm(
-							noise::Simplex::Noise3DClamped(noisePos3d, worldCtx.caveNoiseOctaves, worldCtx.caveNoisePersistence,
+							noise::Simplex::Noise3D(noisePos3d, worldCtx.caveNoiseOctaves, worldCtx.caveNoisePersistence,
 									worldCtx.caveNoiseFrequency, worldCtx.caveNoiseAmplitude));
 					const float finalDensity = noiseNormalized + noiseVal;
 					if (finalDensity > worldCtx.caveDensityThreshold) {
