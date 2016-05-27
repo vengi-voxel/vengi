60226e7bcf42dddf86403fbd36f5344e4ddc24c8
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-05-27 20:04:12 +0200

    NOISETOOL: started to add 3d noise support

--- src/tools/noisetool/ui/NoiseParametersWindow.h
+++ src/tools/noisetool/ui/NoiseParametersWindow.h
@@ -10,13 +10,23 @@
 
 class NoiseParametersWindow: public ui::Window {
 private:
+	void make3DNoise(bool append, float amplitude, float frequency, int octaves, float persistence) {
+		tb::TBStr idStr;
+		idStr.SetFormatted("3d-%f-%f-%i-%f", amplitude, frequency, octaves, persistence);
+		cleanup(idStr);
+		const int height = 768;
+		const int width = 1024;
+		const int components = 4;
+		uint8_t buffer[width * height * components];
+		const glm::vec3 pos = {0, 0, 0};
+		noise::Simplex::Noise3D(pos, octaves, persistence, frequency, amplitude);
+		addImage(idStr, append, buffer, width, height);
+	}
+
 	void make2DNoise(bool append, bool gray, bool seamless, bool alpha, float amplitude, float frequency, int octaves, float persistence) {
 		tb::TBStr idStr;
-		idStr.SetFormatted("%i-%f-%f-%i-%f", gray ? 1 : 0, amplitude, frequency, octaves, persistence);
-		tb::TBBitmapFragment *existingFragment = tb::g_tb_skin->GetFragmentManager()->GetFragment(tb::TBID(idStr.CStr()));
-		if (existingFragment != nullptr) {
-			tb::g_tb_skin->GetFragmentManager()->FreeFragment(existingFragment);
-		}
+		idStr.SetFormatted("2d-%i-%i-%i-%f-%f-%i-%f", gray ? 1 : 0, seamless ? 1 : 0, alpha ? 1 : 0, amplitude, frequency, octaves, persistence);
+		cleanup(idStr);
 		const int height = 768;
 		const int width = seamless ? height : 1024;
 		core_assert(!seamless || width == height);
@@ -40,6 +50,17 @@ private:
 				buffer[i] = 255;
 			}
 		}
+		addImage(idStr, append, buffer, width, height);
+	}
+
+	void cleanup(const tb::TBStr& idStr) {
+		tb::TBBitmapFragment *existingFragment = tb::g_tb_skin->GetFragmentManager()->GetFragment(tb::TBID(idStr.CStr()));
+		if (existingFragment != nullptr) {
+			tb::g_tb_skin->GetFragmentManager()->FreeFragment(existingFragment);
+		}
+	}
+
+	void addImage(const tb::TBStr& idStr, bool append, uint8_t* buffer, int width, int height) {
 		tb::TBLayout* layout = GetWidgetByIDAndType<tb::TBLayout>("imagelayout");
 		if (layout == nullptr) {
 			Log::error("could not find layout node");
@@ -67,8 +88,6 @@ private:
 		imageWidget->SetImage(image);
 		layout->AddChild(imageWidget, tb::WIDGET_Z_TOP);
 		layout->OnInflateChild(imageWidget);
-		Log::info("seamless: %i, gray: %i, amplitude: %f, freq: %f, oct: %i, persist: %f",
-				seamless ? 1 : 0, gray ? 1 : 0, amplitude, frequency, octaves, persistence);
 	}
 
 public:
@@ -86,9 +105,16 @@ public:
 			const bool append = isToggled("append");
 			const bool alpha = isToggled("alpha");
 			const bool seamless = isToggled("seamless");
+			const bool threedimensional = isToggled("threedimensional");
 			const int octaves = enableoctaves ? getInt("octaves") : 1;
 			const float persistence = enableoctaves ? getFloat("persistence") : 1.0f;
-			make2DNoise(append, gray, seamless, alpha, amplitude, frequency, octaves, persistence);
+			Log::info("seamless: %i, gray: %i, amplitude: %f, freq: %f, oct: %i, persist: %f",
+					seamless ? 1 : 0, gray ? 1 : 0, amplitude, frequency, octaves, persistence);
+			if (threedimensional) {
+				make3DNoise(append, amplitude, frequency, octaves, persistence);
+			} else {
+				make2DNoise(append, gray, seamless, alpha, amplitude, frequency, octaves, persistence);
+			}
 			return true;
 		}
 		if ((ev.type == tb::EVENT_TYPE_CLICK && ev.target->GetID() == TBIDC("remove")) || ev.special_key == tb::TB_KEY_DELETE) {
