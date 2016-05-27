6e2894a6e16b2163671a109a77a8a809f1a50dfd
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-05-27 20:42:43 +0200

    TESTS: log level for tests should be warn

--- src/modules/core/tests/AbstractTest.cpp
+++ src/modules/core/tests/AbstractTest.cpp
@@ -4,10 +4,12 @@
 
 #include "AbstractTest.h"
 #include "core/Log.h"
+#include "core/Var.h"
 
 namespace core {
 
 void AbstractTest::SetUp() {
+	core::Var::get(cfg::CoreLogLevel, std::to_string(SDL_LOG_PRIORITY_WARN));
 	Log::init();
 	const core::EventBusPtr eventBus(new core::EventBus());
 	const io::FilesystemPtr filesystem(new io::Filesystem());
