8cdf3a0ab01c32e1f0be0108509ef5d7a5fbdde4
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-06-02 20:57:57 +0200

    VOXEL: generate mushrooms

--- src/modules/voxel/generator/PlantGenerator.cpp
+++ src/modules/voxel/generator/PlantGenerator.cpp
@@ -1,5 +1,4 @@
 #include "PlantGenerator.h"
-#include "voxel/polyvox/RawVolume.h"
 #include "voxel/polyvox/CubicSurfaceExtractor.h"
 #include "voxel/IsQuadNeeded.h"
 
@@ -22,6 +21,79 @@ void PlantGenerator::shutdown() {
 	}
 }
 
+void PlantGenerator::createFlower(int size, glm::ivec3 pos, RawVolume& volume) const {
+	const Voxel stalk = createVoxel(Grass1);
+	for (int i = 0; i < size - 2; ++i) {
+		volume.setVoxel(pos, stalk);
+		++pos.y;
+	}
+	volume.setVoxel(pos, createVoxel(Leaves10));
+	--pos.x;
+	volume.setVoxel(pos, stalk);
+	--pos.z;
+	++pos.x;
+	volume.setVoxel(pos, stalk);
+	pos.z += 2;
+	volume.setVoxel(pos, stalk);
+	--pos.z;
+	++pos.x;
+	volume.setVoxel(pos, stalk);
+}
+
+void PlantGenerator::createGrass(int size, glm::ivec3 pos, RawVolume& volume) const {
+	// TODO: use noise
+	const Voxel stalk = createVoxel(Grass1);
+	glm::ivec3 p = pos;
+	for (int i = 0; i < size; ++i) {
+		volume.setVoxel(p, stalk);
+		++p.y;
+	}
+	p.y = pos.y;
+	p.x -= size / 2 - 1;
+	for (int i = 0; i < size - 2; ++i) {
+		volume.setVoxel(p, stalk);
+		++p.y;
+	}
+
+	p.y = pos.y;
+	p.x = pos.x;
+	p.x += size / 2 - 1;
+	for (int i = 0; i < size - 2; ++i) {
+		volume.setVoxel(p, stalk);
+		++p.y;
+	}
+}
+
+void PlantGenerator::createMushroom(int size, glm::ivec3 pos, RawVolume& volume) const {
+	const Voxel stalk = createVoxel(Grass1);
+	for (int i = 0; i < 3; ++i) {
+		volume.setVoxel(pos, stalk);
+		++pos.y;
+	}
+	const int height = size - 3;
+	const double minRadius = size / 2.0;
+	const double heightFactor = height / (height - 1.0) / 2.0;
+	for (int y = 0; y <= height; ++y) {
+		const double percent = y / heightFactor;
+		const double circleRadius = glm::pow(minRadius, 2.0) - glm::pow(percent, 2.0);
+		const glm::ivec3 planePos(pos.x, pos.y + y, pos.z);
+		const int radius = height / 2;
+		const double ratio = radius / minRadius;
+
+		const Voxel voxel = createVoxel(Leaves10);
+		for (int z = -radius; z <= radius; ++z) {
+			for (int x = -radius; x <= radius; ++x) {
+				const double distance = glm::pow(x / ratio, 2.0) + glm::pow(z / ratio, 2.0);
+				if (distance > circleRadius) {
+					continue;
+				}
+				const glm::ivec3 pos(planePos.x + x, planePos.y, planePos.z + z);
+				volume.setVoxel(pos, voxel);
+			}
+		}
+	}
+}
+
 bool PlantGenerator::generatePlant(int size, PlantType type, Mesh *result) {
 	const Region region(0, 0, 0, size, size, size);
 	RawVolume volume(region);
@@ -29,27 +101,15 @@ bool PlantGenerator::generatePlant(int size, PlantType type, Mesh *result) {
 	glm::ivec3 pos = region.getCentre();
 	pos.y = 0;
 	switch (type) {
-	case Flower: {
-		const Voxel stalk = createVoxel(Grass1);
-		for (int i = 0; i < size - 2; ++i) {
-			volume.setVoxel(pos, stalk);
-			++pos.y;
-		}
-		volume.setVoxel(pos, createVoxel(Leaves10));
-		--pos.x;
-		volume.setVoxel(pos, stalk);
-		--pos.z;
-		++pos.x;
-		volume.setVoxel(pos, stalk);
-		pos.z += 2;
-		volume.setVoxel(pos, stalk);
-		--pos.z;
-		++pos.x;
-		volume.setVoxel(pos, stalk);
-		}
+	case Flower:
+		createFlower(size, pos, volume);
 		break;
 	case Mushroom:
-		return false;
+		createMushroom(size, pos, volume);
+		break;
+	case Grass:
+		createGrass(size, pos, volume);
+		break;
 	default:
 		return false;
 	}
@@ -66,7 +126,19 @@ Mesh* PlantGenerator::getMesh(PlantType type) {
 void PlantGenerator::generateAll() {
 	for (int t = 0; t < MaxPlantTypes; ++t) {
 		_meshes[t] = new Mesh(1000, 1000);
-		generatePlant(5, (PlantType)t, _meshes[t]);
+		int size;
+		switch (t) {
+		case Grass:
+			size = 10;
+			break;
+		case Mushroom:
+			size = 7;
+			break;
+		default:
+			size = 5;
+			break;
+		}
+		generatePlant(size, (PlantType)t, _meshes[t]);
 	}
 }
 
--- src/modules/voxel/generator/PlantGenerator.h
+++ src/modules/voxel/generator/PlantGenerator.h
@@ -1,12 +1,14 @@
 #pragma once
 
 #include "voxel/polyvox/Mesh.h"
+#include "voxel/polyvox/RawVolume.h"
 
 namespace voxel {
 
 enum PlantType {
 	Flower,
 	Mushroom,
+	Grass,
 
 	MaxPlantTypes
 };
@@ -14,6 +16,11 @@ enum PlantType {
 class PlantGenerator {
 private:
 	Mesh* _meshes[MaxPlantTypes];
+
+	void createFlower(int size, glm::ivec3 pos, RawVolume& volume) const;
+	void createGrass(int size, glm::ivec3 pos, RawVolume& volume) const;
+	void createMushroom(int size, glm::ivec3 pos, RawVolume& volume) const;
+
 public:
 	PlantGenerator();
 	~PlantGenerator();
