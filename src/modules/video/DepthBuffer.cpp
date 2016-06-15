ce6fbb8eb9c545908e9c76a3cccfc5684f76b2f6
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-06-15 20:59:39 +0200

    VIDEO: set GL_TEXTURE_COMPARE_MODE, to GL_NONE
    as we are not using a shadow sampler - but a normal sampler in the shader

--- src/modules/video/DepthBuffer.cpp
+++ src/modules/video/DepthBuffer.cpp
@@ -44,6 +44,7 @@ bool DepthBuffer::init(int width, int height) {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
 	GLfloat borderColor[] = { 1.0, 1.0, 1.0, 1.0 };
 	glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_MODE, GL_NONE);
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, _depthTexture, 0);
 	glDrawBuffer(GL_NONE);
 	glReadBuffer(GL_NONE);
