a97c85c345fbe282863ae8203460411f4e315314
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-06-27 20:50:26 +0200

    FRONTEND: fixed WorldRenderer compilation

--- src/modules/frontend/WorldRenderer.h
+++ src/modules/frontend/WorldRenderer.h
@@ -12,7 +12,7 @@
 #include "video/VertexBuffer.h"
 #include "video/GBuffer.h"
 #include "video/DepthBuffer.h"
-#include "frontend/ShadowmapRenderShader.h"
+#include "Shaders.h"
 #include "video/GLMeshData.h"
 #include "core/GLM.h"
 #include "core/Var.h"
@@ -74,7 +74,7 @@ private:
 	video::VertexBuffer _fullscreenQuad;
 	video::VertexBuffer _texturedFullscreenQuad;
 	video::DepthBuffer _depthBuffer;
-	frontend::ShadowmapRenderShader _shadowMapRenderShader;
+	shader::ShadowmapRenderShader _shadowMapRenderShader;
 
 	voxel::PlantGenerator _plantGenerator;
 
