f230a32b426308f736752dc45a5a5bd688758f56
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-06-30 20:27:22 +0200

    TESTCORE: new module for the visual tests

---
+++ src/modules/testcore/TestApp.cpp
@@ -0,0 +1,75 @@
+#include "TestApp.h"
+#include "core/AppModule.h"
+#include "video/GLDebug.h"
+#include "video/ScopedViewPort.h"
+#include "core/Color.h"
+#include "core/Command.h"
+#include "frontend/Movement.h"
+
+TestApp::TestApp(io::FilesystemPtr filesystem, core::EventBusPtr eventBus) :
+		Super(filesystem, eventBus, 21000) {
+	init("engine", "test");
+}
+
+TestApp::~TestApp() {
+	core::Command::unregisterCommand("+move_right");
+	core::Command::unregisterCommand("+move_left");
+	core::Command::unregisterCommand("+move_upt");
+	core::Command::unregisterCommand("+move_down");
+}
+
+core::AppState TestApp::onInit() {
+	const core::AppState state = Super::onInit();
+
+	GLDebug::enable(GLDebug::Medium);
+
+	_axis.init();
+
+	_camera.init(_width, _height);
+	_camera.setPosition(glm::vec3(50.0f, 50.0f, 0.0f));
+	_camera.lookAt(glm::vec3(0.0f));
+
+	registerMoveCmd("+move_right", MOVERIGHT);
+	registerMoveCmd("+move_left", MOVELEFT);
+	registerMoveCmd("+move_forward", MOVEFORWARD);
+	registerMoveCmd("+move_backward", MOVEBACKWARD);
+
+	const glm::vec4& color = ::core::Color::Red;
+	glClearColor(color.r, color.g, color.b, color.a);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glEnable(GL_CULL_FACE);
+	glDepthMask(GL_TRUE);
+
+	return state;
+}
+
+core::AppState TestApp::onRunning() {
+	const core::AppState state = Super::onRunning();
+	if (state == core::AppState::Cleanup) {
+		return state;
+	}
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	_camera.setFarPlane(500.0f);
+	_camera.setFieldOfView(45.0f);
+	_camera.setAspectRatio(_aspect);
+	const bool left = _moveMask & MOVELEFT;
+	const bool right = _moveMask & MOVERIGHT;
+	const bool forward = _moveMask & MOVEFORWARD;
+	const bool backward = _moveMask & MOVEBACKWARD;
+	_camera.updatePosition(_deltaFrame, left, right, forward, backward);
+	_camera.update();
+
+	doRender();
+
+	_axis.render(_camera);
+
+	return state;
+}
+
+core::AppState TestApp::onCleanup() {
+	_axis.shutdown();
+	return Super::onCleanup();
+}
---
+++ src/modules/testcore/TestApp.h
@@ -0,0 +1,29 @@
+/**
+ * @file
+ */
+
+#pragma once
+
+#include "video/WindowedApp.h"
+#include "video/Mesh.h"
+#include "video/Camera.h"
+#include "frontend/Axis.h"
+
+class TestApp: public video::WindowedApp {
+private:
+	using Super = video::WindowedApp;
+	video::Camera _camera;
+	frontend::Axis _axis;
+	uint8_t _moveMask = 0;
+
+protected:
+	virtual void doRender() = 0;
+
+public:
+	TestApp(io::FilesystemPtr filesystem, core::EventBusPtr eventBus);
+	virtual ~TestApp();
+
+	core::AppState onInit() override;
+	core::AppState onRunning() override;
+	core::AppState onCleanup() override;
+};
