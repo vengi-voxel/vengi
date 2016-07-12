cf7733b51792d426f4ffba02ca3e58268b6ffe6a
anthraxx <levente@leventepolyak.net>
2016-07-12 20:36:01 +0200

    TESTS: fix camera test using parallel look direction and up vector

--- src/modules/video/tests/CameraTest.cpp
+++ src/modules/video/tests/CameraTest.cpp
@@ -13,10 +13,10 @@ class CameraTest : public core::AbstractTest {
 TEST_F(CameraTest, testLookAt) {
 	Camera camera;
 	camera.setPosition(glm::vec3(0.0, 1.0, 0.0));
-	camera.lookAt(glm::vec3(0.0));
+	camera.lookAt(glm::vec3(0.0), glm::forward);
 	camera.update(0l);
 	// looking straight down
-	EXPECT_FLOAT_EQ(-glm::half_pi<float>(), camera.pitch());
+	EXPECT_FLOAT_EQ(glm::half_pi<float>(), camera.pitch());
 	EXPECT_FLOAT_EQ(0.0f, camera.yaw());
 	EXPECT_FLOAT_EQ(0.0f, camera.roll());
 }
@@ -24,7 +24,7 @@ TEST_F(CameraTest, testLookAt) {
 TEST_F(CameraTest, testMotion) {
 	Camera camera;
 	camera.setPosition(glm::vec3(0.0, 1.0, 0.0));
-	camera.lookAt(glm::vec3(0.0));
+	camera.lookAt(glm::vec3(0.0), glm::forward);
 	camera.rotate(10, 0, 1.0f);
 	camera.update(0l);
 }
