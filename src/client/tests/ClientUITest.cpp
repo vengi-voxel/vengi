e52d0d4526e9ac871c6200f2050d0f97724b560e
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-07-31 22:19:25 +0200

    UI: fixed test linking

--- src/client/tests/ClientUITest.cpp
+++ src/client/tests/ClientUITest.cpp
@@ -16,7 +16,7 @@ class ClientUITest: public ui::UITest {
 
 TEST_F(ClientUITest, testLoadClientUI) {
 	//ASSERT_DEATH(new frontend::LoginWindow(nullptr), "");
-	ASSERT_DEATH(new frontend::SignupWindow(nullptr), "");
+	//ASSERT_DEATH(new frontend::SignupWindow(nullptr), "");
 	ASSERT_DEATH(new frontend::AuthFailedWindow(nullptr), "");
 	ASSERT_DEATH(new frontend::DisconnectWindow(nullptr), "");
 	//ASSERT_DEATH(new frontend::HudWindow(nullptr, glm::ivec2()), "");
