f04b9eae0c9a78cb1ce8b6f3cb0375f6aee0fc50
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-08-15 20:22:08 +0200

    CORE: memory allocation guard

---
+++ src/modules/core/MemGuard.cpp
@@ -0,0 +1,95 @@
+#include "MemGuard.h"
+#include <SDL.h>
+
+namespace core {
+
+MemGuard::MemGuard(const std::string& name) :
+		_name(name), _rwLock(name) {
+}
+
+MemGuard::~MemGuard() {
+	for (Chunk* chunk : _chunkBuckets) {
+		free(pointer_cast(chunk));
+	}
+
+	core_assert(_chunkAmount == 0);
+	core_assert(_byteAmount == 0);
+}
+
+void MemGuard::free(void* ptr) {
+	if (ptr == nullptr) {
+		return;
+	}
+
+	Chunk* const chunk = chunk_cast(ptr);
+
+	{
+		core::ScopedWriteLock scoped(_rwLock);
+
+		_chunkAmount--;
+		_byteAmount -= chunk->_size + _overheadSize;
+
+		Chunk** prevBucket = &_chunkBuckets[(uintptr_t) chunk % _chunkBuckets.size()];
+		for (;;) {
+			Chunk* c = *prevBucket;
+			if (c == nullptr) {
+				break;
+			}
+
+			if (c == chunk) {
+				*prevBucket = c->_nextChunk;
+				break;
+			}
+			prevBucket = &c->_nextChunk;
+		}
+	}
+
+	SDL_free(chunk);
+}
+
+void* MemGuard::alloc(size_t size, bool zeroFill) {
+	size += _overheadSize;
+
+	Chunk* mem = static_cast<Chunk*>(SDL_malloc(size));
+	if (zeroFill) {
+		SDL_memset(mem, 0, size);
+	}
+
+	mem->_headGuard = _headGuard;
+	mem->_size = size - _overheadSize;
+	mem->_tailGuard = _tailGuard;
+
+	*footer_cast(mem) = _footerGuard;
+
+	core::ScopedWriteLock scoped(_rwLock);
+
+	_chunkAmount++;
+	_byteAmount += size;
+
+	mem->_nextChunk = _chunkBuckets[(uintptr_t) mem % _chunkBuckets.size()];
+	_chunkBuckets[(uintptr_t) mem % _chunkBuckets.size()] = mem;
+
+	return pointer_cast(mem);
+}
+
+void* MemGuard::realloc(void* ptr, size_t size) {
+	Chunk* const chunk = chunk_cast(ptr);
+	if (chunk->_size == size) {
+		return ptr;
+	}
+
+	void* newPtr = alloc(size, false);
+	SDL_memcpy(newPtr, ptr, std::min(chunk->_size, size));
+	if (chunk->_size < size) {
+		const size_t delta = size - chunk->_size;
+		SDL_memset((uint8_t*) newPtr + chunk->_size, 0, delta);
+	}
+
+	free(ptr);
+
+	checkIntegrity(chunk_cast(newPtr));
+
+	return newPtr;
+}
+
+}
---
+++ src/modules/core/MemGuard.h
@@ -0,0 +1,77 @@
+/**
+ * @file
+ */
+
+#pragma once
+
+#include "core/Common.h"
+#include "core/ReadWriteLock.h"
+
+namespace core {
+
+class MemGuard {
+private:
+	static constexpr uint32_t _headGuard = 0xDEADBEEF;
+	static constexpr uint32_t _tailGuard = 0xBADDCAFE;
+	static constexpr uint32_t _footerGuard = 0x8BADF00D;
+
+	struct Chunk {
+		Chunk* _nextChunk;
+		uint32_t _headGuard;
+		size_t _size;
+		uint32_t _tailGuard;
+	};
+	static constexpr size_t _overheadSize = sizeof(Chunk) + sizeof(uint32_t);
+
+	std::string _name;
+	uint32_t _chunkAmount = 0u;
+	uint32_t _byteAmount = 0u;
+	core::ReadWriteLock _rwLock;
+
+	std::array<Chunk*, 13> _chunkBuckets = { nullptr, nullptr, nullptr, nullptr,
+			nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
+			nullptr, nullptr };
+
+	Chunk* chunk_cast(void* const ptr) const;
+
+	void* pointer_cast(Chunk* const chunk) const;
+
+	uint32_t* footer_cast(Chunk* const chunk) const;
+
+	void checkIntegrity(Chunk* const chunk) const;
+
+public:
+	MemGuard(const std::string& name);
+
+	~MemGuard();
+
+	void free(void* ptr);
+
+	void* alloc(size_t size, bool zeroFill = false) __attribute__ ((malloc));
+
+	void* realloc(void* ptr, size_t size);
+};
+
+inline MemGuard::Chunk* MemGuard::chunk_cast(void* const ptr) const {
+	Chunk* chunk = static_cast<Chunk*>(ptr) - 1;
+	checkIntegrity(chunk);
+	return chunk;
+}
+
+inline void* MemGuard::pointer_cast(MemGuard::Chunk* const chunk) const {
+	return chunk + 1;
+}
+
+inline uint32_t* MemGuard::footer_cast(MemGuard::Chunk* const chunk) const {
+	uint8_t* bytePtr = reinterpret_cast<uint8_t*>(pointer_cast(chunk));
+	return reinterpret_cast<uint32_t*>(bytePtr + chunk->_size);
+}
+
+inline void MemGuard::checkIntegrity(MemGuard::Chunk* const chunk) const {
+	core_assert_always(chunk->_headGuard == _headGuard);
+	core_assert_always(chunk->_tailGuard == _tailGuard);
+	core_assert_always(*footer_cast(chunk) == _footerGuard);
+}
+
+
+}
