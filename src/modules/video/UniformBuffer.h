0c0f7ed89017f842a6bbf28f8a5c7400d3129074
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-08-29 20:26:37 +0200

    VIDEO: doxygen

--- src/modules/video/UniformBuffer.h
+++ src/modules/video/UniformBuffer.h
@@ -5,6 +5,21 @@
 
 namespace video {
 
+/**
+ * @brief A Buffer Object that is used to store uniform data for a shader program.
+ *
+ * Uniform buffers have several uses.
+ *
+ * Switching between uniform buffer bindings is typically faster than switching dozens of uniforms in a program.
+ * Therefore, uniform buffers can be used to quickly change between different sets of uniform data for different
+ * objects that share the same program.
+ *
+ * Also, uniform buffer objects can typically store more data than non-buffered uniforms. So they can be used to
+ * store and access larger blocks of data than unbuffered uniform values.
+ *
+ * Lastly, they can be used to share information between different programs. So modifying a single buffer can
+ * effectively allow uniforms in multiple programs to be updated.
+ */
 class UniformBuffer {
 private:
 	GLuint _handle;
