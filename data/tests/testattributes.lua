78049f7e4e9044a1d1c85a4d7e371520016d8b5d
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-09-06 20:33:04 +0200

    DATA: added missing test data

---
+++ data/tests/testattributes.lua
@@ -0,0 +1,5 @@
+function init()
+	local test1 = attrib.createContainer("test1")
+	test1:absolute("ATTACKRANGE", 2.0)
+	test1:register()
+end
---
+++ data/tests/testattributes_fail.lua
@@ -0,0 +1,5 @@
+function init()
+	local test1 = attrib.createContainer("test1")
+	test1:absolute("FOO", 2.0)
+	test1:register()
+end
