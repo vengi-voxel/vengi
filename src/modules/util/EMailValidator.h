6a52fe72c7913b8a18bf3032ec0901a581d8c731
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-09-09 20:35:51 +0200

    UTIL: limit valid email length

--- src/modules/util/EMailValidator.h
+++ src/modules/util/EMailValidator.h
@@ -6,6 +6,9 @@
 namespace util {
 
 static inline bool isValidEmail(const std::string& email) {
+	if (email.size() > 256) {
+		return false;
+	}
 	static std::regex r("^[_a-z0-9-]+(\\.[_a-z0-9-]+)*@[a-z0-9-]+(\\.[a-z0-9-]+)*(\\.[a-z]{2,4})$", std::regex::extended | std::regex_constants::icase);
 	return std::regex_search(email, r);
 }
