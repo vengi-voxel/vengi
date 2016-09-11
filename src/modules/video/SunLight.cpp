719656cca7f4091abce003840cfcaee7118c9144
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-09-11 10:33:36 +0200

    VIDEO: reversed the logic

--- src/modules/video/SunLight.cpp
+++ src/modules/video/SunLight.cpp
@@ -7,12 +7,12 @@ void SunLight::SunCamera::updateSun(long deltaFrame, const core::RectFloat& bbox
 	_dirty |= DIRTY_PERSPECTIVE;
 	updateOrientation();
 	updateViewMatrix();
-	// normalize the opengl depth from [-1, 1] to [0, 1]
-	static const glm::mat4 normalizeDepth = glm::scale(glm::translate(glm::mat4(), glm::backward), glm::vec3(1.0f, 1.0f, 0.5f));
-	if (mode == DepthBufferMode::DEPTH || mode == DepthBufferMode::DEPTH_CMP) {
-		_projectionMatrix = glm::ortho(bbox.getMinX(), bbox.getMaxX(), bbox.getMinZ(), bbox.getMaxZ(), nearPlane(), farPlane());
-	} else {
+	if (mode == DepthBufferMode::RGBA) {
+		// normalize the opengl depth from [-1, 1] to [0, 1]
+		static const glm::mat4 normalizeDepth = glm::scale(glm::translate(glm::mat4(), glm::backward), glm::vec3(1.0f, 1.0f, 0.5f));
 		_projectionMatrix = normalizeDepth * glm::ortho(bbox.getMinX(), bbox.getMaxX(), bbox.getMinZ(), bbox.getMaxZ(), nearPlane(), farPlane());
+	} else {
+		_projectionMatrix = glm::ortho(bbox.getMinX(), bbox.getMaxX(), bbox.getMinZ(), bbox.getMaxZ(), nearPlane(), farPlane());
 	}
 	updateFrustumPlanes();
 	updateFrustumVertices();
