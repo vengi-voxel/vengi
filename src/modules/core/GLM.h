ef52be831dd8a87da841b1ada123db1346694c02
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-09-16 20:46:12 +0200

    CORE: improved glm includes

--- src/modules/core/GLM.h
+++ src/modules/core/GLM.h
@@ -5,9 +5,15 @@
 #define GLM_SWIZZLE // TODO: deprecated - remove me someday
 
 #include <glm/glm.hpp>
+
 #include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
 #include <glm/vec4.hpp>
+
+#include <glm/exponential.hpp>
+#include <glm/common.hpp>
+#include <glm/trigonometric.hpp>
+
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/matrix_access.hpp>
 #include <glm/gtc/type_ptr.hpp>
@@ -15,13 +21,13 @@
 #include <glm/gtc/random.hpp>
 #include <glm/gtc/noise.hpp>
 #include <glm/gtc/epsilon.hpp>
+
 #include <glm/gtx/string_cast.hpp>
 #include <glm/gtx/euler_angles.hpp>
 #include <glm/gtx/compatibility.hpp>
 #include <glm/gtx/norm.hpp>
 #include <glm/gtx/rotate_vector.hpp>
-#include <glm/detail/func_common.hpp>
-#include <glm/detail/func_trigonometric.hpp>
+
 #include <limits>
 #include <cmath>
 
