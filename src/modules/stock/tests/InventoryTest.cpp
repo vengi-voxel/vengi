9a9ef305892efec683d8c13025061097cd569428
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-09-22 20:34:23 +0200

    STOCK: added more inventory tests

--- src/modules/stock/tests/InventoryTest.cpp
+++ src/modules/stock/tests/InventoryTest.cpp
@@ -42,17 +42,32 @@ public:
 	}
 };
 
-TEST_F(InventoryTest, testAdd) {
-	ASSERT_FALSE(_inv.add(_containerId, _item1, 0, 0)) << "Could place item to invalid container position";
-	ASSERT_FALSE(_container->hasItemOfType(_itemData1->type()));
+TEST_F(InventoryTest, testAddValidLocation) {
 	ASSERT_TRUE(_inv.add(_containerId, _item1, 1, 1)) << "Could not place item to valid container position";
 	ASSERT_TRUE(_container->hasItemOfType(_itemData1->type()));
 	ASSERT_EQ(15, _container->free());
+}
+
+TEST_F(InventoryTest, testAddInvalidLocation) {
+	ASSERT_FALSE(_inv.add(_containerId, _item1, 0, 0)) << "Could place item to invalid container position";
+	ASSERT_FALSE(_container->hasItemOfType(_itemData1->type()));
+	ASSERT_EQ(17, _container->free());
+}
+
+TEST_F(InventoryTest, testAddAndRemove) {
+	ASSERT_TRUE(_inv.add(_containerId, _item1, 1, 1)) << "Could not place item to valid container position";
+	ASSERT_TRUE(_container->hasItemOfType(_itemData1->type()));
+	ASSERT_EQ(15, _container->free());
+	ASSERT_EQ(_item1, _inv.remove(_containerId, 1, 2)) << "Could not remove item from container 1, 2 - even though the item with a size of two should also occupy this field";
+	ASSERT_EQ(17, _container->free());
+}
+
+TEST_F(InventoryTest, testRemoveFromInvalidLocation) {
+	ASSERT_TRUE(_inv.add(_containerId, _item1, 1, 1)) << "Could not place item to valid container position";
 	ASSERT_EQ(nullptr, _inv.remove(_containerId, 3, 3)) << "Removed item from invalid position 3, 3";
 	ASSERT_EQ(nullptr, _inv.remove(_containerId, 3, 1)) << "Removed item from invalid position 3, 1";
 	ASSERT_EQ(nullptr, _inv.remove(_containerId, 2, 1)) << "Removed item from invalid position 2, 1";
-	ASSERT_NE(nullptr, _inv.remove(_containerId, 1, 2)) << "Could not remove item from container 1, 2 - even though the item with a size of two should also occupy this field";
-	ASSERT_EQ(17, _container->free());
+	ASSERT_EQ(15, _container->free());
 }
 
 }
