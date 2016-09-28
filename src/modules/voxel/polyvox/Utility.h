ff048cd99263654ce15d7da8d40d6de868dd777a
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-09-28 20:37:46 +0200

    VOXEL: use glm::isPowerOfTwo and converted asserts

--- src/modules/voxel/polyvox/Utility.h
+++ src/modules/voxel/polyvox/Utility.h
@@ -9,24 +9,13 @@
 
 namespace voxel {
 
-inline bool isPowerOf2(uint32_t uInput) {
-	if (uInput == 0)
-		return false;
-	return (uInput & (uInput - 1)) == 0;
-}
-
 /**
  * @note: this function only works for inputs which are a power of two and not zero
  * If this is not the case then the output is undefined.
  */
 inline uint8_t logBase2(uint32_t uInput) {
-	//Release mode validation
-	if (uInput == 0) {
-		core_assert_msg(false, "Cannot compute the log of zero.");
-	}
-	if (!isPowerOf2(uInput)) {
-		core_assert_msg(false, "Input must be a power of two in order to compute the log.");
-	}
+	core_assert_msg(uInput != 0, "Cannot compute the log of zero.");
+	core_assert_msg(glm::isPowerOfTwo(uInput), "Input must be a power of two in order to compute the log.");
 
 	uint32_t uResult = 0;
 	while ((uInput >> uResult) != 0) {
