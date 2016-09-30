3aaef393311822b55b4597baa3322a6208a39871
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-09-30 20:52:21 +0200

    CORE: removed default parameter

--- src/modules/core/Trace.cpp
+++ src/modules/core/Trace.cpp
@@ -11,7 +11,7 @@ namespace core {
 static void rmtInputHandler(const char* text, void* context) {
 	Log::info("typed '%s' to console", text);
 	if (core::Command::execute(text) <= 0) {
-		const VarPtr& var = core::Var::get(text, nullptr);
+		const VarPtr& var = core::Var::get(text);
 		if (var) {
 			core_trace_msg(var->strVal().c_str());
 			return;
