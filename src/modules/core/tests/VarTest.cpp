f2c43c1185635fa7a8785a40635959fe5484ba37
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-10-19 20:52:39 +0200

    CORE: converted VarTest to AbstractTest to shutdown the static var instances
    ... and to be able to use the repeat feature of gtest

--- src/modules/core/tests/VarTest.cpp
+++ src/modules/core/tests/VarTest.cpp
@@ -2,12 +2,15 @@
  * @file
  */
 
-#include <gtest/gtest.h>
+#include "AbstractTest.h"
 #include "core/Var.h"
 
 namespace core {
 
-TEST(VarTest, testChange) {
+class VarTest: public AbstractTest {
+};
+
+TEST_F(VarTest, testChange) {
 	const VarPtr& v = Var::get("test", "nonsense");
 	ASSERT_EQ("nonsense", v->strVal());
 	v->setVal("1");
@@ -15,18 +18,18 @@ TEST(VarTest, testChange) {
 	ASSERT_EQ(1, v->intVal());
 }
 
-TEST(VarTest, testFlags) {
+TEST_F(VarTest, testFlags) {
 	const VarPtr& v = Var::get("test", "nonsense", CV_READONLY);
 	ASSERT_EQ(CV_READONLY, v->getFlags());
 }
 
-TEST(VarTest, testFlagsOverride) {
+TEST_F(VarTest, testFlagsOverride) {
 	const VarPtr& v = Var::get("test", "nonsense");
 	Var::get("test", "nonsense", CV_READONLY);
 	ASSERT_EQ(CV_READONLY, v->getFlags());
 }
 
-TEST(VarTest, testDirty) {
+TEST_F(VarTest, testDirty) {
 	const VarPtr& v = Var::get("test", "nonsense");
 	v->setVal("reasonable");
 	ASSERT_TRUE(v->isDirty());
