a0413c14fe6207ce0c804b2025d1751f54ff1038
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-10-20 20:29:15 +0200

    CORE: extracted threadpool code into cpp/h file

---
+++ src/modules/core/ThreadPool.cpp
@@ -0,0 +1,47 @@
+/**
+ * @file
+ */
+
+#include "ThreadPool.h"
+#include "String.h"
+#include "Trace.h"
+
+namespace core {
+
+ThreadPool::ThreadPool(size_t threads, const char *name) :
+		_stop(false) {
+	_workers.reserve(threads);
+	if (name == nullptr)
+		name = "ThreadPool";
+	for (size_t i = 0; i < threads; ++i) {
+		_workers.emplace_back([this, name, i] {
+			const std::string n = core::string::format("%s-%i", name, (int)i);
+			core_trace_thread(n.c_str());
+			for (;;) {
+				std::function<void()> task;
+				{
+					std::unique_lock<std::mutex> lock(this->_queueMutex);
+					this->_condition.wait(lock, [this] {
+						return this->_stop || !this->_tasks.empty();
+					});
+					if (this->_stop && this->_tasks.empty()) {
+						return;
+					}
+					task = std::move(this->_tasks.front());
+					this->_tasks.pop();
+				}
+
+				task();
+			}
+		});
+	}
+}
+
+ThreadPool::~ThreadPool() {
+	_stop = true;
+	_condition.notify_all();
+	for (std::thread &worker : _workers)
+		worker.join();
+}
+
+}
--- src/modules/core/ThreadPool.h
+++ src/modules/core/ThreadPool.h
@@ -64,36 +64,6 @@ private:
 	std::atomic_bool _stop;
 };
 
-// the constructor just launches some amount of workers
-inline ThreadPool::ThreadPool(size_t threads, const char *name) :
-		_stop(false) {
-	_workers.reserve(threads);
-	if (name == nullptr)
-		name = "ThreadPool";
-	for (size_t i = 0; i < threads; ++i) {
-		_workers.emplace_back([this, name, i] {
-			const std::string n = core::string::format("%s-%i", name, (int)i);
-			core_trace_thread(n.c_str());
-			for (;;) {
-				std::function<void()> task;
-				{
-					std::unique_lock<std::mutex> lock(this->_queueMutex);
-					this->_condition.wait(lock, [this] {
-						return this->_stop || !this->_tasks.empty();
-					});
-					if (this->_stop && this->_tasks.empty()) {
-						return;
-					}
-					task = std::move(this->_tasks.front());
-					this->_tasks.pop();
-				}
-
-				task();
-			}
-		});
-	}
-}
-
 // add new work item to the pool
 template<class F, class ... Args>
 auto ThreadPool::enqueue(F&& f, Args&&... args)
@@ -109,14 +79,6 @@ auto ThreadPool::enqueue(F&& f, Args&&... args)
 		_condition.notify_one();
 	}
 	return res;
-}
-
-// the destructor joins all threads
-inline ThreadPool::~ThreadPool() {
-	_stop = true;
-	_condition.notify_all();
-	for (std::thread &worker : _workers)
-		worker.join();
 }
 
 }
