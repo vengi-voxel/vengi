a5ef6c9e6f5eadb3695f3ff751a40533e130b00b
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-10-23 20:41:49 +0200

    IO: windows implementations - untested

--- src/modules/io/FilesystemWindows.cpp
+++ src/modules/io/FilesystemWindows.cpp
@@ -5,16 +5,63 @@
 
 namespace io {
 
-bool Filesystem::createDir(const std::string& dir) const {
-	WCHAR wpath[1024];
-	const int len = MultiByteToWideChar(CP_UTF8, 0, dir.c_str(), -1, wpath, SDL_arraysize(wpath) - 1);
-	wpath[len] = 0;
-	_wmkdir(wpath);
-	return true;
+bool Filesystem::createDir(const std::string& directory) const {
+	if (directory.empty()) {
+		return false;
+	}
+	const bool bCD = CreateDirectory(directory.c_str(), nullptr);
+	if (bCD) {
+		return true;
+	}
+
+	DWORD dwLastError = GetLastError();
+	switch (dwLastError) {
+	case ERROR_ALREADY_EXISTS:
+		return true;
+	case ERROR_PATH_NOT_FOUND: {
+		std::size_t sep = directory.rfind('/');
+		if (sep == std::string::npos) {
+			sep = directory.rfind('\\');
+		}
+		if (sep == std::string::npos) {
+			return false;
+		}
+		const std::string szPrev = directory.substr(0, sep);
+		if (CreateDirectory(szPrev, nullptr)) {
+			const bool bSuccess = CreateDirectory(directory.c_str(), nullptr) != 0;
+			if (bSuccess) {
+				return true;
+			}
+			return GetLastError() == ERROR_ALREADY_EXISTS;
+		}
+		return false;
+	}
+	default:
+		return false;
+	}
 }
 
 bool Filesystem::list(const std::string& directory, std::vector<DirEntry>& entities, const std::string& filter) const {
-	return false;
+	std::string search(directory + "\\*");
+
+	struct _finddata_t findinfo;
+	const int dir = _findfirst(search.c_str(), &findinfo);
+	if (dir == -1) {
+		return false;
+	}
+
+	do {
+		if (findinfo.attrib & _A_SUBDIR) {
+			if (strcmp(findinfo.name, ".") && strcmp(findinfo.name, "..")) {
+				entities.push_back(DirEntry{findinfo.name, DirEntry::Type::dir});
+			}
+		} else {
+			entities.push_back(DirEntry{findinfo.name, DirEntry::Type::file});
+		}
+	} while (_findnext(dir, &findinfo) != -1);
+
+	_findclose(dir);
+	return true;
 }
 
 }
