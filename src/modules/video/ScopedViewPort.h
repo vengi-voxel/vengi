509f0d2d4a1cfcb751f7cce18050cccff10a4da7
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-10-26 20:16:51 +0200

    VIDEO: fixed missing pragma once for ScopedViewPort

--- src/modules/video/ScopedViewPort.h
+++ src/modules/video/ScopedViewPort.h
@@ -1,11 +1,19 @@
+#pragma once
+
 #include "GLFunc.h"
 
 namespace video {
 
+/**
+ * @brief Restore the previous viewport after leaving the scope of the object
+ */
 class ScopedViewPort {
 private:
 	GLint _viewport[4];
 public:
+	/**
+	 * @note Keep in mind that opengl y starts from below - these are no screen coordinates, but opengl coordinates.
+	 */
 	ScopedViewPort(int x, int y, int w, int h) {
 		glGetIntegerv(GL_VIEWPORT, _viewport);
 		glViewport(x, y, w, h);
