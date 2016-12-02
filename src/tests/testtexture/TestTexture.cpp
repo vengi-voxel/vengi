a2802f1777fb58c0c947ccad0b02d9d733e6c24c
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-12-02 20:07:18 +0100

    TESTTEXTURE: minor cleanup

--- src/tests/testtexture/TestTexture.cpp
+++ src/tests/testtexture/TestTexture.cpp
@@ -64,9 +64,9 @@ core::AppState TestTexture::onCleanup() {
 }
 
 int main(int argc, char *argv[]) {
-	const core::EventBusPtr eventBus = std::make_shared<core::EventBus>();
-	const io::FilesystemPtr filesystem = std::make_shared<io::Filesystem>();
-	const core::TimeProviderPtr timeProvider = std::make_shared<core::TimeProvider>();
+	const core::EventBusPtr& eventBus = std::make_shared<core::EventBus>();
+	const io::FilesystemPtr& filesystem = std::make_shared<io::Filesystem>();
+	const core::TimeProviderPtr& timeProvider = std::make_shared<core::TimeProvider>();
 	TestTexture app(filesystem, eventBus, timeProvider);
 	return app.startMainLoop(argc, argv);
 
