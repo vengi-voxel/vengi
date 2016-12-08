fd6c0960984e21792bccbbf4b012912701836b86
Martin Gerhardy <martin.gerhardy@gmail.com>
2016-12-08 20:58:00 +0100

    CORE: added console colors to log

--- src/modules/core/Log.cpp
+++ src/modules/core/Log.cpp
@@ -7,6 +7,23 @@
 #include <cstring>
 #include <SDL.h>
 
+#ifdef __LINUX__
+#define ANSI_COLOR_RESET "\033[0m"
+#define ANSI_COLOR_RED "\033[31m"
+#define ANSI_COLOR_GREEN "\033[32m"
+#define ANSI_COLOR_YELLOW "\033[33m"
+#define ANSI_COLOR_BLUE "\033[34m"
+#define ANSI_COLOR_CYAN "\033[36m"
+#else
+#define ANSI_COLOR_RESET ""
+#define ANSI_COLOR_RED ""
+#define ANSI_COLOR_GREEN ""
+#define ANSI_COLOR_YELLOW ""
+#define ANSI_COLOR_BLUE ""
+#define ANSI_COLOR_CYAN ""
+#endif
+
+
 static constexpr int bufSize = 1024;
 static SDL_LogPriority _logLevel = SDL_LOG_PRIORITY_INFO;
 
@@ -24,7 +41,7 @@ void Log::trace(const char* msg, ...) {
 	char buf[bufSize];
 	SDL_vsnprintf(buf, sizeof(buf), msg, args);
 	buf[sizeof(buf) - 1] = '\0';
-	SDL_LogVerbose(SDL_LOG_CATEGORY_APPLICATION, "%s\n", buf);
+	SDL_LogVerbose(SDL_LOG_CATEGORY_APPLICATION, ANSI_COLOR_CYAN "%s" ANSI_COLOR_RESET "\n", buf);
 	va_end(args);
 }
 
@@ -37,7 +54,7 @@ void Log::debug(const char* msg, ...) {
 	char buf[bufSize];
 	SDL_vsnprintf(buf, sizeof(buf), msg, args);
 	buf[sizeof(buf) - 1] = '\0';
-	SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "%s\n", buf);
+	SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, ANSI_COLOR_BLUE "%s" ANSI_COLOR_RESET "\n", buf);
 	va_end(args);
 }
 
@@ -50,7 +67,7 @@ void Log::info(const char* msg, ...) {
 	char buf[bufSize];
 	SDL_vsnprintf(buf, sizeof(buf), msg, args);
 	buf[sizeof(buf) - 1] = '\0';
-	SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "%s\n", buf);
+	SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, ANSI_COLOR_GREEN "%s" ANSI_COLOR_RESET "\n", buf);
 	va_end(args);
 }
 
@@ -63,7 +80,7 @@ void Log::warn(const char* msg, ...) {
 	char buf[bufSize];
 	SDL_vsnprintf(buf, sizeof(buf), msg, args);
 	buf[sizeof(buf) - 1] = '\0';
-	SDL_LogWarn(SDL_LOG_CATEGORY_APPLICATION, "%s\n", buf);
+	SDL_LogWarn(SDL_LOG_CATEGORY_APPLICATION, ANSI_COLOR_YELLOW "%s" ANSI_COLOR_RESET "\n", buf);
 	va_end(args);
 }
 
@@ -76,6 +93,6 @@ void Log::error(const char* msg, ...) {
 	char buf[bufSize];
 	SDL_vsnprintf(buf, sizeof(buf), msg, args);
 	buf[sizeof(buf) - 1] = '\0';
-	SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "%s\n", buf);
+	SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, ANSI_COLOR_RED "%s" ANSI_COLOR_RESET "\n", buf);
 	va_end(args);
 }
