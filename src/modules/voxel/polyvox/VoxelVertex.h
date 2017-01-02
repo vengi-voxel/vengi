a1ae52612ea3296aeefb056f0a33c56e13982cad
Martin Gerhardy <martin.gerhardy@gmail.com>
2017-01-02 20:32:16 +0100

    VOXEL: changed size of VoxelVertex to 16 bytes, but use full int range
    ... for the position
    
    this will allow us to merge chunk buffers across byte boundaries

--- src/modules/voxel/polyvox/VoxelVertex.h
+++ src/modules/voxel/polyvox/VoxelVertex.h
@@ -15,13 +15,13 @@ namespace voxel {
  * as well as color and material information.
  */
 struct VoxelVertex {
-	glm::u8vec3 position;
+	glm::ivec3 position;
 	uint8_t ambientOcclusion;
 	uint8_t colorIndex;
 	// currently we only need to know whether it's water, or not.
 	VoxelType material;
-	uint8_t padding[2];
+	uint8_t padding[1];
 };
-static_assert(sizeof(VoxelVertex) == 8, "Unexpected size of the vertex struct");
+static_assert(sizeof(VoxelVertex) == 16, "Unexpected size of the vertex struct");
 
 }
