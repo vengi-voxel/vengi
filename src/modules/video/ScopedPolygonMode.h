3c6da17d673eff68b4cccf5a3c4587692c0e9110
Martin Gerhardy <martin.gerhardy@gmail.com>
2017-01-09 20:20:49 +0100

    VIDEO: fixed invalid enum in ScopedPolygonMode

--- src/modules/video/ScopedPolygonMode.h
+++ src/modules/video/ScopedPolygonMode.h
@@ -11,24 +11,15 @@ namespace video {
 
 class ScopedPolygonMode {
 private:
-	GLint _polygonMode[2] = {GL_NONE, GL_NONE};
 	GLint _polygonOffsetMode = GL_NONE;
 public:
 	inline ScopedPolygonMode(const video::PolygonMode mode) {
-		if (mode == video::PolygonMode::Solid) {
-			return;
-		}
-		glGetIntegerv(GL_POLYGON_MODE, _polygonMode);
 		glPolygonMode(GL_FRONT_AND_BACK, std::enum_value(mode));
 		GL_checkError();
 	}
 
-	inline ScopedPolygonMode(const video::PolygonMode mode, const glm::vec2& offset) {
-		if (mode == video::PolygonMode::Solid) {
-			return;
-		}
-		glGetIntegerv(GL_POLYGON_MODE, _polygonMode);
-		glPolygonMode(GL_FRONT_AND_BACK, std::enum_value(mode));
+	inline ScopedPolygonMode(const video::PolygonMode mode, const glm::vec2& offset) :
+			ScopedPolygonMode(mode) {
 		if (mode == video::PolygonMode::Points) {
 			_polygonOffsetMode = GL_POLYGON_OFFSET_POINT;
 		} else if (mode == video::PolygonMode::WireFrame) {
@@ -38,29 +29,20 @@ public:
 		}
 		if (_polygonOffsetMode != GL_NONE) {
 			glEnable(_polygonOffsetMode);
+			GL_checkError();
 			glPolygonOffset(offset.x, offset.y);
+			GL_checkError();
 		}
-		GL_checkError();
 	}
 
 	inline ~ScopedPolygonMode() {
-		if (_polygonMode[0] == GL_NONE) {
-			return;
-		}
-
 		if (_polygonOffsetMode != GL_NONE) {
 			glDisable(_polygonOffsetMode);
 			GL_checkError();
 		}
 
-		if (_polygonMode[0] == _polygonMode[1]) {
-			glPolygonMode(GL_FRONT_AND_BACK, _polygonMode[0]);
-			GL_checkError();
-		} else {
-			glPolygonMode(GL_FRONT, _polygonMode[0]);
-			glPolygonMode(GL_BACK, _polygonMode[1]);
-			GL_checkError();
-		}
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		GL_checkError();
 	}
 };
 
