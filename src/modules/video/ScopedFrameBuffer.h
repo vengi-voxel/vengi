44dabb73f6e66183b0ad2a2a30b879c689304559
Martin Gerhardy <martin.gerhardy@gmail.com>
2017-01-16 20:53:08 +0100

    VIDEO: don't reset old framebuffer, but 0 in ScopedFrameBuffer

--- src/modules/video/ScopedFrameBuffer.h
+++ src/modules/video/ScopedFrameBuffer.h
@@ -5,35 +5,18 @@
 namespace video {
 
 class ScopedFrameBuffer {
-private:
-	GLint _handle;
 public:
-	ScopedFrameBuffer() {
-		glGetIntegerv(GL_FRAMEBUFFER_BINDING, &_handle);
-		GL_checkError();
-	}
-
 	explicit ScopedFrameBuffer(const FrameBuffer& fbo) :
 			ScopedFrameBuffer(fbo._fbo) {
 	}
 
-	explicit ScopedFrameBuffer(GLint bindHandle) {
-		glGetIntegerv(GL_FRAMEBUFFER_BINDING, &_handle);
-		GL_checkError();
-		// check for double bind
-		if (_handle == bindHandle) {
-			_handle = -1;
-			return;
-		}
+	explicit ScopedFrameBuffer(Id bindHandle) {
 		glBindFramebuffer(GL_FRAMEBUFFER, bindHandle);
 		GL_checkError();
 	}
 
 	~ScopedFrameBuffer() {
-		if (_handle != -1) {
-			glBindFramebuffer(GL_FRAMEBUFFER, _handle);
-			GL_checkError();
-		}
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 	}
 };
 
