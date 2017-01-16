798164d79ee8950835fc605640b5924dfff13448
Martin Gerhardy <martin.gerhardy@gmail.com>
2017-01-16 20:25:41 +0100

    VIDEO: moved framebuffer setup in GLRenderer

--- src/modules/video/FrameBuffer.cpp
+++ src/modules/video/FrameBuffer.cpp
@@ -18,56 +18,7 @@ FrameBuffer::~FrameBuffer() {
 
 bool FrameBuffer::init(const glm::ivec2& dimension) {
 	_dimension = dimension;
-
-	_fbo = video::genFramebuffer();
-	ScopedFrameBuffer scopedFrameBuffer(_fbo);
-
-	_texture = video::genTexture();
-	video::bindTexture(video::TextureUnit::Zero, TextureType::Texture2D, _texture);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, dimension.x, dimension.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, _texture, 0);
-
-	_depth = video::genRenderbuffer();
-	video::bindRenderbuffer(_depth);
-	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, dimension.x, dimension.y);
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, _depth);
-
-	video::checkError();
-
-	const GLenum drawBuffers[] = { GL_COLOR_ATTACHMENT0 };
-	glDrawBuffers(1, drawBuffers);
-
-	const GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
-	if (status != GL_FRAMEBUFFER_COMPLETE) {
-		switch (status) {
-		case GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT:
-			Log::error("FB error, incomplete attachment");
-			break;
-		case GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:
-			Log::error("FB error, incomplete missing attachment");
-			break;
-		case GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER:
-			Log::error("FB error, incomplete draw buffer");
-			break;
-		case GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER:
-			Log::error("FB error, incomplete read buffer");
-			break;
-		case GL_FRAMEBUFFER_UNSUPPORTED:
-			Log::error("FB error, framebuffer unsupported");
-			break;
-		default:
-			Log::error("FB error, status: %i", (int)status);
-			break;
-		}
-		return false;
-	}
-
-	return true;
+	return video::setupFramebuffer(_fbo, _texture, _depth, dimension);
 }
 
 void FrameBuffer::shutdown() {
--- src/modules/video/gl/GLRenderer.h
+++ src/modules/video/gl/GLRenderer.h
@@ -638,6 +638,57 @@ inline void setupDepthCompareTexture(TextureUnit unit, video::TextureType type, 
 	checkError();
 }
 
+inline bool setupFramebuffer(Id& _fbo, Id& _texture, Id& _depth, const glm::ivec2& dimension) {
+	_fbo = genFramebuffer();
+	bindFramebuffer(video::FrameBufferMode::Default, _fbo);
+	_texture = genTexture();
+	bindTexture(video::TextureUnit::Upload, TextureType::Texture2D, _texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, dimension.x, dimension.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, _texture, 0);
+
+	_depth = genRenderbuffer();
+	bindRenderbuffer(_depth);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, dimension.x, dimension.y);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, _depth);
+
+	checkError();
+
+	const GLenum drawBuffers[] = { GL_COLOR_ATTACHMENT0 };
+	glDrawBuffers(1, drawBuffers);
+
+	const GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+	if (status != GL_FRAMEBUFFER_COMPLETE) {
+		switch (status) {
+		case GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT:
+			Log::error("FB error, incomplete attachment");
+			break;
+		case GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:
+			Log::error("FB error, incomplete missing attachment");
+			break;
+		case GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER:
+			Log::error("FB error, incomplete draw buffer");
+			break;
+		case GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER:
+			Log::error("FB error, incomplete read buffer");
+			break;
+		case GL_FRAMEBUFFER_UNSUPPORTED:
+			Log::error("FB error, framebuffer unsupported");
+			break;
+		default:
+			Log::error("FB error, status: %i", (int)status);
+			break;
+		}
+		return false;
+	}
+
+	return true;
+}
+
 inline void setupTexture(video::TextureType type, video::TextureWrap wrap) {
 	const GLenum glType = _priv::TextureTypes[std::enum_value(type)];
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
