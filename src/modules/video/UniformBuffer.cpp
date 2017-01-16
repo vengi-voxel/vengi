0e25c7fba43a434b70c258c12141e56222145c19
Martin Gerhardy <martin.gerhardy@gmail.com>
2017-01-16 20:25:34 +0100

    VIDEO: use bindBuffer for UniformBuffer class

--- src/modules/video/UniformBuffer.cpp
+++ src/modules/video/UniformBuffer.cpp
@@ -21,9 +21,9 @@ void* UniformBuffer::lock(BufferLockMode mode) {
 	if (_handle == video::InvalidId) {
 		return nullptr;
 	}
-	glBindBuffer(GL_UNIFORM_BUFFER, _handle);
+	video::bindBuffer(VertexBufferType::UniformBuffer, _handle);
 	void* result = glMapBufferRange(GL_UNIFORM_BUFFER, 0, (GLsizeiptr)_size, std::enum_value(mode));
-	glBindBuffer(GL_UNIFORM_BUFFER, 0);
+	video::bindBuffer(VertexBufferType::UniformBuffer, InvalidId);
 	return result;
 }
 
@@ -31,9 +31,9 @@ void UniformBuffer::unlock() {
 	if (_handle == video::InvalidId) {
 		return;
 	}
-	glBindBuffer(GL_UNIFORM_BUFFER, _handle);
+	video::bindBuffer(VertexBufferType::UniformBuffer, _handle);
 	glUnmapBuffer(GL_UNIFORM_BUFFER);
-	glBindBuffer(GL_UNIFORM_BUFFER, 0);
+	video::bindBuffer(VertexBufferType::UniformBuffer, InvalidId);
 }
 
 
@@ -44,9 +44,9 @@ void UniformBuffer::create(size_t size, const void *data) {
 	_size = size;
 	glGenBuffers(1, &_handle);
 	if (data != nullptr) {
-		glBindBuffer(GL_UNIFORM_BUFFER, _handle);
+		video::bindBuffer(VertexBufferType::UniformBuffer, _handle);
 		glBufferData(GL_UNIFORM_BUFFER, (GLsizeiptr)_size, data, GL_STATIC_DRAW);
-		glBindBuffer(GL_UNIFORM_BUFFER, 0);
+		video::bindBuffer(VertexBufferType::UniformBuffer, InvalidId);
 	}
 }
 
