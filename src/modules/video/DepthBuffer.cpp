5f15b60ac494b5aae583dc96e5078874a82c7a90
Martin Gerhardy <martin.gerhardy@gmail.com>
2017-01-17 20:28:24 +0100

    VIDEO: test asserts - might not work as default fb is 0, too (afaik)

--- src/modules/video/DepthBuffer.cpp
+++ src/modules/video/DepthBuffer.cpp
@@ -54,6 +54,7 @@ bool DepthBuffer::init(const glm::ivec2& dimension, DepthBufferMode mode, int te
 bool DepthBuffer::bind() {
 	video::getViewport(_oldViewport[0], _oldViewport[1], _oldViewport[2], _oldViewport[3]);
 	_oldFramebuffer = video::bindFramebuffer(video::FrameBufferMode::Default, _fbo);
+	core_assert(_oldFramebuffer != InvalidId);
 	video::viewport(0, 0, _depthTexture.width(), _depthTexture.height());
 	return true;
 }
@@ -68,6 +69,7 @@ bool DepthBuffer::bindTexture(int textureIndex) {
 
 void DepthBuffer::unbind() {
 	video::viewport(_oldViewport[0], _oldViewport[1], _oldViewport[2], _oldViewport[3]);
+	core_assert(_oldFramebuffer != InvalidId);
 	video::bindFramebuffer(video::FrameBufferMode::Default, _oldFramebuffer);
 	_oldFramebuffer = video::InvalidId;
 }
