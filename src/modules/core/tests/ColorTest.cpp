dd04e1ea63b17f7df6a5b7c6702ae051b5334361
Martin Gerhardy <martin.gerhardy@gmail.com>
2017-01-26 22:10:38 +0100

    CORE: extended color test

--- src/modules/core/tests/ColorTest.cpp
+++ src/modules/core/tests/ColorTest.cpp
@@ -14,6 +14,12 @@ TEST(ColorTest, testRGBA) {
 	EXPECT_EQ(0x99, color.g);
 	EXPECT_EQ(0x66, color.b);
 	EXPECT_EQ(0xff, color.a);
+
+	const glm::vec4 fcolor = core::Color::fromRGBA(color.rgba);
+	EXPECT_FLOAT_EQ(1.0f, fcolor.a);
+	EXPECT_FLOAT_EQ(color.b / (float)core::Color::magnitude, fcolor.b);
+	EXPECT_FLOAT_EQ(color.g / (float)core::Color::magnitude, fcolor.g);
+	EXPECT_FLOAT_EQ(1.0f, fcolor.r);
 }
 
 }
