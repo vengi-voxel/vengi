c0b27586377485fb674ba078a37036307852cf26
Martin Gerhardy <martin.gerhardy@gmail.com>
2017-01-29 13:39:36 +0100

    NOISE: doxygen

--- src/modules/noise/SphereNoise.cpp
+++ src/modules/noise/SphereNoise.cpp
@@ -2,6 +2,12 @@
 
 namespace noise {
 
+/**
+ * @brief Transforms the latitude and longitude unit sphere coordinates into the
+ * cartesian coordinates and uses those as input for the noise function.
+ * @param[in] latitude Given in degrees - must be [-90,90]
+ * @param[in] longitude Given in degrees - must be [-180,180]
+ */
 float sphereNoise(float longitude, float latitude) {
 	const float r = glm::cos(glm::radians(latitude));
 	glm::vec3 pos(glm::uninitialize);
