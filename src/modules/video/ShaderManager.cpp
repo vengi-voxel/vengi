f18a0958e5009006dbd3e3805936d0a683c4ef8e
Martin Gerhardy <martin.gerhardy@gmail.com>
2017-02-14 20:09:08 +0100

    VIDEO: extract into cpp file

---
+++ src/modules/video/ShaderManager.cpp
@@ -0,0 +1,42 @@
+#include "ShaderManager.h"
+#include "core/Var.h"
+#include "core/Log.h"
+
+namespace video {
+
+void ShaderManager::registerShader(Shader* shader) {
+	_shaders.push_back(shader);
+}
+
+void ShaderManager::unregisterShader(Shader* shader) {
+	auto i = std::find(_shaders.begin(), _shaders.end(), shader);
+	if (i == _shaders.end()) {
+		return;
+	}
+	_shaders.erase(i);
+}
+
+void ShaderManager::update() {
+	bool refreshShaders = core::Var::check([&] (const core::VarPtr& var) {
+		if (!var->isDirty()) {
+			return false;
+		}
+		if ((var->getFlags() & core::CV_SHADER) == 0) {
+			return false;
+		}
+		var->markClean();
+		return true;
+	});
+
+	if (!refreshShaders) {
+		return;
+	}
+
+	Log::debug("Reload shaders");
+	Shaders copy = _shaders;
+	for (Shader* shader : copy) {
+		shader->reload();
+	}
+}
+
+}
--- src/modules/video/ShaderManager.h
+++ src/modules/video/ShaderManager.h
@@ -2,8 +2,6 @@
 
 #include "Shader.h"
 #include <vector>
-#include "core/Var.h"
-#include "core/Log.h"
 
 namespace video {
 
@@ -12,41 +10,13 @@ private:
 	typedef std::vector<Shader*> Shaders;
 	Shaders _shaders;
 public:
-	void registerShader(Shader* shader) {
-		_shaders.push_back(shader);
-	}
-
-	void unregisterShader(Shader* shader) {
-		auto i = std::find(_shaders.begin(), _shaders.end(), shader);
-		if (i == _shaders.end())
-			return;
-		_shaders.erase(i);
-	}
+	void registerShader(Shader* shader);
+	void unregisterShader(Shader* shader);
 
 	/**
 	 * @brief Checks whether a shader var was changed, and recompile all shaders if needed.
 	 */
-	void update() {
-		bool refreshShaders = core::Var::check([&] (const core::VarPtr& var) {
-			if (!var->isDirty()) {
-				return false;
-			}
-			if ((var->getFlags() & core::CV_SHADER) == 0) {
-				return false;
-			}
-			var->markClean();
-			return true;
-		});
-
-		if (!refreshShaders)
-			return;
-
-		Log::debug("Reload shaders");
-		Shaders copy = _shaders;
-		for (Shader* shader : copy) {
-			shader->reload();
-		}
-	}
+	void update();
 };
 
 }
