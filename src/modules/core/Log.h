c20acb897e5a382da0434874cc21bc8eaa7ef319
Martin Gerhardy <martin.gerhardy@gmail.com>
2017-03-07 22:04:09 +0100

    CORE: add printf format checks for msvc

--- src/modules/core/Log.h
+++ src/modules/core/Log.h
@@ -7,12 +7,27 @@
 #include <cstdio>
 #include <memory>
 
+#ifndef __GNUC__
+#define __attribute__(x)
+#endif
+
+#ifdef _MSC_VER
+#include <sal.h>
+#if _MSC_VER >= 1400
+#define CORE_FORMAT_STRING _Printf_format_string_
+#else
+#define CORE_FORMAT_STRING __format_string
+#endif
+#else
+#define CORE_FORMAT_STRING
+#endif
+
 class Log {
 public:
 	static void init();
-	static void trace(const char* msg, ...) __attribute__((format(printf, 1, 2)));
-	static void debug(const char* msg, ...) __attribute__((format(printf, 1, 2)));
-	static void info(const char* msg, ...) __attribute__((format(printf, 1, 2)));
-	static void warn(const char* msg, ...) __attribute__((format(printf, 1, 2)));
-	static void error(const char* msg, ...) __attribute__((format(printf, 1, 2)));
+	static void trace(CORE_FORMAT_STRING const char* msg, ...) __attribute__((format(printf, 1, 2)));
+	static void debug(CORE_FORMAT_STRING const char* msg, ...) __attribute__((format(printf, 1, 2)));
+	static void info(CORE_FORMAT_STRING const char* msg, ...) __attribute__((format(printf, 1, 2)));
+	static void warn(CORE_FORMAT_STRING const char* msg, ...) __attribute__((format(printf, 1, 2)));
+	static void error(CORE_FORMAT_STRING const char* msg, ...) __attribute__((format(printf, 1, 2)));
 };
--- src/modules/ui/UIApp.h
+++ src/modules/ui/UIApp.h
@@ -32,8 +32,8 @@ protected:
 	virtual void OnWidgetFocusChanged(tb::TBWidget *widget, bool focused) override;
 
 	bool invokeKey(int key, tb::SPECIAL_KEY special, tb::MODIFIER_KEYS mod, bool down);
-	void showStr(int x, int y, const glm::vec4& color, const char *fmt, ...) __attribute__((format(printf, 5, 6)));
-	void enqueueShowStr(int x, const glm::vec4& color, const char *fmt, ...) __attribute__((format(printf, 4, 5)));
+	void showStr(int x, int y, const glm::vec4& color, CORE_FORMAT_STRING const char *fmt, ...) __attribute__((format(printf, 5, 6)));
+	void enqueueShowStr(int x, const glm::vec4& color, CORE_FORMAT_STRING const char *fmt, ...) __attribute__((format(printf, 4, 5)));
 
 	tb::MODIFIER_KEYS getModifierKeys() const;
 public:
