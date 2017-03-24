49293e0466524166f00bd96d097d3221ec9b96dd
Martin Gerhardy <martin.gerhardy@gmail.com>
2017-03-24 18:34:21 +0100

    VOXEL: doxygen for RandomVoxel

--- src/modules/voxel/MaterialColor.h
+++ src/modules/voxel/MaterialColor.h
@@ -33,32 +33,41 @@ extern Voxel createRandomColorVoxel(VoxelType type, core::Random& random);
  */
 extern Voxel createColorVoxel(VoxelType type, uint32_t colorIndex);
 
+/**
+ * @brief Helper class to pick a random colored @c VoxelType
+ */
 class RandomVoxel {
 private:
 	const MaterialColorIndices& indices;
-	const core::Random& random;
-	const VoxelType type;
+	const core::Random& _random;
+	const VoxelType _type;
 	int _sameCount;
 	mutable int _amount = 1;
 	mutable uint8_t _currentIndex = 0u;
 public:
-	RandomVoxel(VoxelType _type, const core::Random& _random, int sameCount = 3) :
-			indices(getMaterialIndices(_type)), random(_random), type(_type), _sameCount(sameCount) {
+	/**
+	 * @param type The VoxelType to pick the color index for
+	 * @param random @c core::Random instance
+	 * @param sameCount The amount of Voxel instances that are returned as the same
+	 * color before a possible change in the color index is evaluated.
+	 */
+	RandomVoxel(VoxelType type, const core::Random& random, int sameCount = 3) :
+			indices(getMaterialIndices(type)), _random(random), _type(type), _sameCount(sameCount) {
 	}
 
 	inline operator Voxel() const {
 		if (indices.size() == 1) {
-			return Voxel(type, indices.front());
+			return Voxel(_type, indices.front());
 		}
 		if (_amount == 1) {
-			auto i = random.randomElement(indices.begin(), indices.end());
+			auto i = _random.randomElement(indices.begin(), indices.end());
 			_currentIndex = *i;
 		}
 		++_amount;
 		if (_amount >= _sameCount) {
 			_amount = 1;
 		}
-		return Voxel(type, _currentIndex);
+		return Voxel(_type, _currentIndex);
 	}
 };
 
