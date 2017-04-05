62be3f8688ad69a2e8282580b37399b5113f2510
Martin Gerhardy <martin.gerhardy@gmail.com>
2017-04-05 22:49:42 +0200

    VOXEL: extracted paged volume wrapper code into own cpp file

---
+++ src/modules/voxel/polyvox/PagedVolumeWrapper.cpp
@@ -0,0 +1,104 @@
+/**
+ * @file
+ */
+
+#include "PagedVolumeWrapper.h"
+#include "core/Common.h"
+
+namespace voxel {
+
+PagedVolumeWrapper::Sampler::Sampler(const PagedVolumeWrapper* volume) :
+		Super(volume->getVolume()), _chunk(volume->_chunk) {
+}
+
+PagedVolumeWrapper::Sampler::Sampler(const PagedVolumeWrapper& volume) :
+		Super(volume.getVolume()), _chunk(volume._chunk) {
+}
+
+void PagedVolumeWrapper::Sampler::setPosition(int32_t xPos, int32_t yPos, int32_t zPos) {
+	_xPosInVolume = xPos;
+	_yPosInVolume = yPos;
+	_zPosInVolume = zPos;
+
+	// Then we update the voxel pointer
+	const int32_t xChunk = _xPosInVolume >> _volume->_chunkSideLengthPower;
+	const int32_t yChunk = _yPosInVolume >> _volume->_chunkSideLengthPower;
+	const int32_t zChunk = _zPosInVolume >> _volume->_chunkSideLengthPower;
+
+	_xPosInChunk = static_cast<uint16_t>(_xPosInVolume - (xChunk << _volume->_chunkSideLengthPower));
+	_yPosInChunk = static_cast<uint16_t>(_yPosInVolume - (yChunk << _volume->_chunkSideLengthPower));
+	_zPosInChunk = static_cast<uint16_t>(_zPosInVolume - (zChunk << _volume->_chunkSideLengthPower));
+	const uint32_t voxelIndexInChunk = morton256_x[_xPosInChunk] | morton256_y[_yPosInChunk] | morton256_z[_zPosInChunk];
+
+	PagedVolume::Chunk* currentChunk;
+	const glm::ivec3& p = _chunk->_chunkSpacePosition;
+	if (p.x == xChunk && p.y == yChunk && p.z == zChunk) {
+		currentChunk = _chunk;
+	} else {
+		PagedVolume::VolumeLockGuard scopedLock(_volume->_lock);
+		currentChunk = _volume->getChunk(xChunk, yChunk, zChunk);
+	}
+
+	_currentVoxel = currentChunk->_data + voxelIndexInChunk;
+}
+
+PagedVolumeWrapper::PagedVolumeWrapper(PagedVolume* voxelStorage, PagedVolume::Chunk* chunk, const Region& region) :
+		_pagedVolume(voxelStorage), _chunk(chunk), _region(region) {
+	if (_chunk != nullptr) {
+		_validRegion = _chunk->getRegion();
+	}
+}
+
+const Voxel& PagedVolumeWrapper::getVoxel(int x, int y, int z) const {
+	if (_validRegion.containsPoint(x, y, z)) {
+		core_assert(_chunk != nullptr);
+		const int relX = x - _validRegion.getLowerX();
+		const int relY = y - _validRegion.getLowerY();
+		const int relZ = z - _validRegion.getLowerZ();
+		return _chunk->getVoxel(relX, relY, relZ);
+	}
+	core_assert(_pagedVolume != nullptr);
+	return _pagedVolume->getVoxel(x, y, z);
+}
+
+bool PagedVolumeWrapper::setVoxel(int x, int y, int z, const Voxel& voxel) {
+	if (_validRegion.containsPoint(x, y, z)) {
+		core_assert(_chunk != nullptr);
+		const int relX = x - _validRegion.getLowerX();
+		const int relY = y - _validRegion.getLowerY();
+		const int relZ = z - _validRegion.getLowerZ();
+		_chunk->setVoxel(relX, relY, relZ, voxel);
+		return true;
+	}
+	core_assert(_pagedVolume != nullptr);
+	_pagedVolume->setVoxel(x, y, z, voxel);
+	return true;
+}
+
+bool PagedVolumeWrapper::setVoxels(int x, int y, int z, int nx, int nz, const Voxel* voxels, int amount) {
+	for (int j = 0; j < nx; ++j) {
+		for (int k = 0; k < nz; ++k) {
+			const int fx = x + j;
+			const int fz = z + k;
+			int left = amount;
+			if (_validRegion.containsPoint(fx, y, fz)) {
+				// first part goes into the chunk
+				const int h = _validRegion.getHeightInVoxels();
+				_chunk->setVoxels(fx - _validRegion.getLowerX(), y - _validRegion.getLowerY(), fz - _validRegion.getLowerZ(), voxels, std::min(h, left));
+				left -= h;
+				if (left > 0) {
+					// everything else goes into the volume
+					core_assert(_pagedVolume != nullptr);
+					_pagedVolume->setVoxels(fx, y + h, fz, 1, 1, voxels + h, left);
+				}
+			} else {
+				// TODO: add region/chunk support here, too
+				core_assert(_pagedVolume != nullptr);
+				_pagedVolume->setVoxels(fx, y, fz, 1, 1, voxels, left);
+			}
+		}
+	}
+	return true;
+}
+
+}
--- src/modules/voxel/polyvox/PagedVolumeWrapper.h
+++ src/modules/voxel/polyvox/PagedVolumeWrapper.h
@@ -4,9 +4,9 @@
 
 #pragma once
 
-#include "core/Common.h"
 #include "voxel/polyvox/Region.h"
 #include "voxel/polyvox/PagedVolume.h"
+#include <glm/vec3.hpp>
 
 namespace voxel {
 
@@ -25,140 +25,75 @@ public:
 		using Super = PagedVolume::Sampler;
 		PagedVolume::Chunk* _chunk;
 	public:
-		Sampler(const PagedVolumeWrapper* volume) : Super(volume->getVolume()), _chunk(volume->_chunk) {}
+		Sampler(const PagedVolumeWrapper* volume);
+		Sampler(const PagedVolumeWrapper& volume);
 
-		Sampler(const PagedVolumeWrapper& volume) : Super(volume.getVolume()), _chunk(volume._chunk) {};
-
-		void setPosition(int32_t xPos, int32_t yPos, int32_t zPos) override {
-			_xPosInVolume = xPos;
-			_yPosInVolume = yPos;
-			_zPosInVolume = zPos;
-
-			// Then we update the voxel pointer
-			const int32_t xChunk = _xPosInVolume >> _volume->_chunkSideLengthPower;
-			const int32_t yChunk = _yPosInVolume >> _volume->_chunkSideLengthPower;
-			const int32_t zChunk = _zPosInVolume >> _volume->_chunkSideLengthPower;
-
-			_xPosInChunk = static_cast<uint16_t>(_xPosInVolume - (xChunk << _volume->_chunkSideLengthPower));
-			_yPosInChunk = static_cast<uint16_t>(_yPosInVolume - (yChunk << _volume->_chunkSideLengthPower));
-			_zPosInChunk = static_cast<uint16_t>(_zPosInVolume - (zChunk << _volume->_chunkSideLengthPower));
-			const uint32_t voxelIndexInChunk = morton256_x[_xPosInChunk] | morton256_y[_yPosInChunk] | morton256_z[_zPosInChunk];
-
-			PagedVolume::Chunk* currentChunk;
-			const glm::ivec3& p = _chunk->_chunkSpacePosition;
-			if (p.x == xChunk && p.y == yChunk && p.z == zChunk) {
-				currentChunk = _chunk;
-			} else {
-				PagedVolume::VolumeLockGuard scopedLock(_volume->_lock);
-				currentChunk = _volume->getChunk(xChunk, yChunk, zChunk);
-			}
-
-			_currentVoxel = currentChunk->_data + voxelIndexInChunk;
-		}
+		void setPosition(int32_t xPos, int32_t yPos, int32_t zPos) override;
 	};
 
-	PagedVolumeWrapper(PagedVolume* voxelStorage, PagedVolume::Chunk* chunk, const Region& region) :
-			_pagedVolume(voxelStorage), _chunk(chunk), _region(region) {
-		if (_chunk != nullptr) {
-			_validRegion = _chunk->getRegion();
-		}
-	}
+	PagedVolumeWrapper(PagedVolume* voxelStorage, PagedVolume::Chunk* chunk, const Region& region);
 
-	inline const Region& getRegion() const {
-		return _region;
-	}
+	operator PagedVolume& () const;
+	operator const PagedVolume& () const;
+	operator PagedVolume* () const;
+	operator const PagedVolume* () const;
 
-	inline operator PagedVolume& () const {
-		return *_pagedVolume;
-	}
+	PagedVolume::Chunk* getChunk() const;
+	PagedVolume* getVolume() const;
+	const Region& getRegion() const;
 
-	inline operator const PagedVolume& () const {
-		return *_pagedVolume;
-	}
+	const Voxel& getVoxel(const glm::ivec3& pos) const;
+	const Voxel& getVoxel(int x, int y, int z) const;
 
-	inline operator PagedVolume* () const {
-		return _pagedVolume;
-	}
-
-	inline operator const PagedVolume* () const {
-		return _pagedVolume;
-	}
-
-	inline PagedVolume::Chunk* getChunk() const {
-		return _chunk;
-	}
-
-	inline PagedVolume* getVolume() const {
-		return _pagedVolume;
-	}
-
-	inline bool setVoxel(const glm::ivec3& pos, const Voxel& voxel) {
-		return setVoxel(pos.x, pos.y, pos.z, voxel);
-	}
-
-	inline const Voxel& getVoxel(const glm::ivec3& pos) const {
-		return getVoxel(pos.x, pos.y, pos.z);
-	}
-
-	inline const Voxel& getVoxel(int x, int y, int z) const {
-		if (_validRegion.containsPoint(x, y, z)) {
-			core_assert(_chunk != nullptr);
-			const int relX = x - _validRegion.getLowerX();
-			const int relY = y - _validRegion.getLowerY();
-			const int relZ = z - _validRegion.getLowerZ();
-			return _chunk->getVoxel(relX, relY, relZ);
-		}
-		core_assert(_pagedVolume != nullptr);
-		return _pagedVolume->getVoxel(x, y, z);
-	}
-
-	inline bool setVoxel(int x, int y, int z, const Voxel& voxel) {
-		if (_validRegion.containsPoint(x, y, z)) {
-			core_assert(_chunk != nullptr);
-			const int relX = x - _validRegion.getLowerX();
-			const int relY = y - _validRegion.getLowerY();
-			const int relZ = z - _validRegion.getLowerZ();
-			_chunk->setVoxel(relX, relY, relZ, voxel);
-			return true;
-		}
-		core_assert(_pagedVolume != nullptr);
-		_pagedVolume->setVoxel(x, y, z, voxel);
-		return true;
-	}
-
-	inline bool setVoxels(int x, int z, const Voxel* voxels, int amount) {
-		return setVoxels(x, 0, z, 1, 1, voxels, amount);
-	}
-
-	inline bool setVoxels(int x, int y, int z, int nx, int nz, const Voxel* voxels, int amount) {
-		for (int j = 0; j < nx; ++j) {
-			for (int k = 0; k < nz; ++k) {
-				const int fx = x + j;
-				const int fz = z + k;
-				int left = amount;
-				if (_validRegion.containsPoint(fx, y, fz)) {
-					// first part goes into the chunk
-					const int h = _validRegion.getHeightInVoxels();
-					_chunk->setVoxels(fx - _validRegion.getLowerX(), y - _validRegion.getLowerY(), fz - _validRegion.getLowerZ(), voxels, std::min(h, left));
-					left -= h;
-					if (left > 0) {
-						// everything else goes into the volume
-						core_assert(_pagedVolume != nullptr);
-						_pagedVolume->setVoxels(fx, y + h, fz, 1, 1, voxels + h, left);
-					}
-				} else {
-					// TODO: add region/chunk support here, too
-					core_assert(_pagedVolume != nullptr);
-					_pagedVolume->setVoxels(fx, y, fz, 1, 1, voxels, left);
-				}
-			}
-		}
-		return true;
-	}
-
-	inline bool setVoxels(int x, int y, int z, const Voxel* voxels, int amount) {
-		return setVoxels(x, y, z, 1, 1, voxels, amount);
-	}
+	bool setVoxel(const glm::ivec3& pos, const Voxel& voxel);
+	bool setVoxel(int x, int y, int z, const Voxel& voxel);
+	bool setVoxels(int x, int z, const Voxel* voxels, int amount);
+	bool setVoxels(int x, int y, int z, int nx, int nz, const Voxel* voxels, int amount);
+	bool setVoxels(int x, int y, int z, const Voxel* voxels, int amount);
 };
+
+inline const Region& PagedVolumeWrapper::getRegion() const {
+	return _region;
+}
+
+inline PagedVolumeWrapper::operator PagedVolume& () const {
+	return *_pagedVolume;
+}
+
+inline PagedVolumeWrapper::operator const PagedVolume& () const {
+	return *_pagedVolume;
+}
+
+inline PagedVolumeWrapper::operator PagedVolume* () const {
+	return _pagedVolume;
+}
+
+inline PagedVolumeWrapper::operator const PagedVolume* () const {
+	return _pagedVolume;
+}
+
+inline PagedVolume::Chunk* PagedVolumeWrapper::getChunk() const {
+	return _chunk;
+}
+
+inline PagedVolume* PagedVolumeWrapper::getVolume() const {
+	return _pagedVolume;
+}
+
+inline bool PagedVolumeWrapper::setVoxel(const glm::ivec3& pos, const Voxel& voxel) {
+	return setVoxel(pos.x, pos.y, pos.z, voxel);
+}
+
+inline const Voxel& PagedVolumeWrapper::getVoxel(const glm::ivec3& pos) const {
+	return getVoxel(pos.x, pos.y, pos.z);
+}
+
+inline bool PagedVolumeWrapper::setVoxels(int x, int z, const Voxel* voxels, int amount) {
+	return setVoxels(x, 0, z, 1, 1, voxels, amount);
+}
+
+inline bool PagedVolumeWrapper::setVoxels(int x, int y, int z, const Voxel* voxels, int amount) {
+	return setVoxels(x, y, z, 1, 1, voxels, amount);
+}
 
 }
