90deb8dfcfc1937a45cafb6e86f5b881e91a4baa
Martin Gerhardy <martin.gerhardy@gmail.com>
2017-05-08 14:24:05 +0200

    TESTTEMPLATE: fixed TestTemplateApp ctor

--- src/tests/testtemplate/TestTemplate.cpp
+++ src/tests/testtemplate/TestTemplate.cpp
@@ -21,6 +21,7 @@ void TestTemplate::doRender() {
 int main(int argc, char *argv[]) {
 	const core::EventBusPtr eventBus = std::make_shared<core::EventBus>();
 	const io::FilesystemPtr filesystem = std::make_shared<io::Filesystem>();
-	TestTemplate app(filesystem, eventBus);
+	const core::TimeProviderPtr timeProvider = std::make_shared<core::TimeProvider>();
+	TestTemplate app(filesystem, eventBus, timeProvider);
 	return app.startMainLoop(argc, argv);
 }
