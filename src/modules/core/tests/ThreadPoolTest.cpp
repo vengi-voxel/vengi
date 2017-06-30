cd0fe742dd181602359bce90ab411df18c332318
Martin Gerhardy <martin.gerhardy@gmail.com>
2017-06-30 17:02:12 +0200

    CORE: updated threadpool test

--- src/modules/core/tests/ThreadPoolTest.cpp
+++ src/modules/core/tests/ThreadPoolTest.cpp
@@ -14,24 +14,32 @@ public:
 	ThreadPoolTest() :
 		_count(0), _executed(false) {
 	}
+
+	void SetUp() override {
+		AbstractTest::SetUp();
+		_count = 0;
+		_executed = false;
+	}
 };
 
 TEST_F(ThreadPoolTest, testPush) {
 	core::ThreadPool pool(1);
-	auto future = pool.enqueue([this] () {_executed = true;});
+	auto future = pool.enqueue([this] () {
+		_executed = true;
+	});
 	future.get();
 	ASSERT_TRUE(_executed) << "Thread wasn't executed";
 }
 
 TEST_F(ThreadPoolTest, testMultiplePush) {
 	const int x = 1000;
-	{
-		core::ThreadPool pool(2);
-		for (int i = 0; i < x; ++i) {
-			pool.enqueue([this] () {_count++;});
-		}
-		pool.shutdown(true);
+	core::ThreadPool pool(2);
+	for (int i = 0; i < x; ++i) {
+		pool.enqueue([this] () {
+			_count++;
+		});
 	}
+	pool.shutdown(true);
 	ASSERT_EQ(x, _count) << "Not all threads were executed";
 }
 
