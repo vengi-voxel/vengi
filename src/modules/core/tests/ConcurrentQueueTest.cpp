1965a9fc604cacbabab8c6b1ffd05b8d90d6e057
Martin Gerhardy <martin.gerhardy@gmail.com>
2017-07-03 06:41:22 +0200

    CORE: added ConcurrentQueueTest

---
+++ src/modules/core/tests/ConcurrentQueueTest.cpp
@@ -0,0 +1,86 @@
+/**
+ * @file
+ */
+
+#include "core/tests/AbstractTest.h"
+#include "core/ConcurrentQueue.h"
+#include <thread>
+
+namespace core {
+
+class ConcurrentQueueTest : public core::AbstractTest {
+};
+
+TEST_F(ConcurrentQueueTest, testPushPop) {
+	core::ConcurrentQueue<int> queue;
+	const int n = 1000;
+	for (int i = 0; i < n; ++i) {
+		queue.push(i);
+	}
+	ASSERT_EQ((int)queue.size(), n);
+	for (int i = n - 1; i >= 0; --i) {
+		int v;
+		ASSERT_TRUE(queue.pop(v));
+		ASSERT_EQ(i, v);
+	}
+}
+
+TEST_F(ConcurrentQueueTest, testPushWaitAndPop) {
+	core::ConcurrentQueue<int> queue;
+	const int n = 1000;
+	for (int i = 0; i < n; ++i) {
+		queue.push(i);
+	}
+	ASSERT_EQ((int)queue.size(), n);
+	for (int i = n - 1; i >= 0; --i) {
+		int v;
+		ASSERT_TRUE(queue.waitAndPop(v));
+		ASSERT_EQ(i, v);
+	}
+}
+
+TEST_F(ConcurrentQueueTest, testPushWaitAndPopConcurrent) {
+	core::ConcurrentQueue<int> queue;
+	const int n = 1000;
+	std::thread thread([&] () {
+		for (int i = 0; i < n; ++i) {
+			queue.push(i);
+		}
+	});
+	for (int i = n - 1; i >= 0; --i) {
+		int v;
+		ASSERT_TRUE(queue.waitAndPop(v));
+	}
+	thread.join();
+}
+
+TEST_F(ConcurrentQueueTest, testPushWaitAndPopMultipleThreads) {
+	core::ConcurrentQueue<int> queue;
+	const int n = 1000;
+	std::thread threadPush([&] () {
+		for (int i = 0; i < n; ++i) {
+			queue.push(i);
+		}
+	});
+	std::thread threadPop([&] () {
+		for (int i = n - 1; i >= 0; --i) {
+			int v;
+			ASSERT_TRUE(queue.waitAndPop(v));
+		}
+	});
+	threadPush.join();
+	threadPop.join();
+}
+
+TEST_F(ConcurrentQueueTest, testAbortWait) {
+	core::ConcurrentQueue<int> queue;
+	std::thread threadWait([&] () {
+		int v;
+		ASSERT_FALSE(queue.waitAndPop(v));
+	});
+	std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	queue.abortWait();
+	threadWait.join();
+}
+
+}
