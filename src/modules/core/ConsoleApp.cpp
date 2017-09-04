e659965e8575aa2bc291289648a037596396b9bb
Martin Gerhardy <martin.gerhardy@gmail.com>
2017-09-04 20:12:55 +0200

    CORE: log level commands

--- src/modules/core/ConsoleApp.cpp
+++ src/modules/core/ConsoleApp.cpp
@@ -16,10 +16,30 @@ ConsoleApp::~ConsoleApp() {
 AppState ConsoleApp::onConstruct() {
 	const core::AppState state = Super::onConstruct();
 
-	registerArg("--verbose").setShort("-v").setDescription("Change log level to trace");
-	const bool isVerbose = hasArg("--verbose");
-	if (isVerbose) {
+	registerArg("--trace").setDescription("Change log level to trace");
+	registerArg("--debug").setDescription("Change log level to debug");
+	registerArg("--info").setDescription("Change log level to info");
+	registerArg("--warn").setDescription("Change log level to warn");
+	registerArg("--error").setDescription("Change log level to error");
+
+	bool changed = false;
+	if (hasArg("--trace")) {
 		core::Var::getSafe(cfg::CoreLogLevel)->setVal(SDL_LOG_PRIORITY_VERBOSE);
+		changed = true;
+	} else if (hasArg("--debug")) {
+		core::Var::getSafe(cfg::CoreLogLevel)->setVal(SDL_LOG_PRIORITY_DEBUG);
+		changed = true;
+	} else if (hasArg("--info")) {
+		core::Var::getSafe(cfg::CoreLogLevel)->setVal(SDL_LOG_PRIORITY_INFO);
+		changed = true;
+	} else if (hasArg("--warn")) {
+		core::Var::getSafe(cfg::CoreLogLevel)->setVal(SDL_LOG_PRIORITY_WARN);
+		changed = true;
+	} else if (hasArg("--error")) {
+		core::Var::getSafe(cfg::CoreLogLevel)->setVal(SDL_LOG_PRIORITY_ERROR);
+		changed = true;
+	}
+	if (changed) {
 		Log::init();
 	}
 	return state;
