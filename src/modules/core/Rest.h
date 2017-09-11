f9a7cc94d0756af79744cc174903a74f27911f3f
Martin Gerhardy <martin.gerhardy@gmail.com>
2017-09-11 17:17:31 +0200

    CORE: extended http status codes

--- src/modules/core/Rest.h
+++ src/modules/core/Rest.h
@@ -17,7 +17,18 @@ core::rest::Response post(const std::string& url, const core::json& json = {});
 
 enum StatusCode {
 	OK = 200,
+	Created = 201,
+	Accepted = 202,
+	BadRequest = 400,
+	Unauthorized = 401,
+	Forbidden = 403,
 	NotFound = 404,
+	InternalServerError = 500,
+	NotImplemented = 501,
+	BadGateway = 502,
+	ServiceUnavailable = 503,
+	GatewayTimeout = 504,
+	HttpVersionNotSupported = 505,
 
 	Unknown = -1
 };
