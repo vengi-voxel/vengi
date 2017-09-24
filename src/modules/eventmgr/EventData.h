6ea41f7205f5af0dc1c03f975dc0943a74e22eef
Martin Gerhardy <martin.gerhardy@gmail.com>
2017-09-24 21:00:28 +0200

    EVENTMGR: use EventType from protocol

---
+++ src/modules/eventmgr/EventData.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace eventmgr {
+
+struct EventData {
+};
+
+}
--- src/modules/eventmgr/EventMgr.h
+++ src/modules/eventmgr/EventMgr.h
@@ -2,11 +2,16 @@
  * @file
  */
 
+#include "EventData.h"
+#include "Shared_generated.h"
 #include <memory>
+#include <unordered_map>
 
 namespace eventmgr {
 
 class EventMgr {
+private:
+	std::unordered_map<network::EventType, EventData> _events;
 public:
 	EventMgr();
 
