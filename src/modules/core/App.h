e65ee66c99fa1b653a86db1dc493f31ecf794a17
Martin Gerhardy <martin.gerhardy@gmail.com>
2017-10-08 20:53:07 +0200

    CORE: msvc compile fix

--- src/modules/core/App.h
+++ src/modules/core/App.h
@@ -249,7 +249,7 @@ inline void App::ProfilerCPU::enter() {
 
 inline void App::ProfilerCPU::leave() {
 	const double time = core::TimeProvider::systemNanos() - _stamp;
-	_max = std::max(_max, time);
+	_max = (std::max)(_max, time);
 	_min = (std::min)(_min, time);
 	_avg = _avg * 0.5 + time * 0.5;
 	_samples[_sampleCount & (_maxSampleCount - 1)] = time;
