26994f77ce62a71f3363ed7cb351d394150d2ca1
Martin Gerhardy <martin.gerhardy@gmail.com>
2017-10-08 20:22:24 +0200

    CORE: fixed msvc compilation

--- src/modules/core/Concurrency.h
+++ src/modules/core/Concurrency.h
@@ -10,11 +10,11 @@
 namespace core {
 
 inline uint32_t cpus() {
-	return std::max(1u, std::thread::hardware_concurrency());
+	return (std::max)(1u, std::thread::hardware_concurrency());
 }
 
 inline uint32_t halfcpus() {
-	return std::max(1u, std::thread::hardware_concurrency() / 2u);
+	return (std::max)(1u, std::thread::hardware_concurrency() / 2u);
 }
 
 }
