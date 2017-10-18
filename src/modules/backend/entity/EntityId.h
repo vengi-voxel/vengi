2425f9b4f6ac4d84ca6f6112a13d45e7d046ddf2
Martin Gerhardy <martin.gerhardy@gmail.com>
2017-10-18 18:15:33 +0200

    BACKEND: easier printing of EntityIds

--- src/modules/backend/entity/EntityId.h
+++ src/modules/backend/entity/EntityId.h
@@ -5,9 +5,11 @@
 #pragma once
 
 #include <cstdint>
+#include <cinttypes>
 
 namespace backend {
 
+#define PRIEntId PRId64
 typedef int64_t EntityId;
 constexpr EntityId EntityIdNone = (EntityId)0;
 
