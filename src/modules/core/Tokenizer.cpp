vengi-voxel/vengi

commit aed941736707ae7af483d901a2e5f0968515c816

ALL: fixed valgrind reports

src/modules/core/Tokenizer.cpp
@@ -138,21 +138,27 @@ bool Tokenizer::skipComments(const char **s, bool skipWhitespace) {
 	if (next == '*') {
 		int l = 0;
 		_len -= 2;
+		if (_len < 0) {
+			return false;
+		}
 		*s += 2;
 		const char* data = *s;
 		while (data[l] != '\0' && data[l] != '*' && data[l + 1] != '\0' && data[l + 1] != '/') {
 			++l;
 		}
-		*s += l + 2;
 		_len -= l + 2;
+		if (_len < 0) {
+			return false;
+		}
+		*s += l + 2;
 		skip(s, skipWhitespace);
 		return true;
 	} else if (next == '/') {
 		while (**s != '\0' && **s != '\n') {
-			(*s)++;
 			if (--_len <= 0) {
 				return true;
 			}
+			(*s)++;
 		}
 		skip(s, skipWhitespace);
 		return true;
@@ -172,6 +178,9 @@ char Tokenizer::skip(const char **s, bool skipWhitespace) {
 			}
 			const size_t cl = core::utf8::lengthChar(c);
 			_len -= cl;
+			if (_len < 0) {
+				return '\0';
+			}
 			*s += cl;
 		}
 	}

src/modules/backend/loop/ServerLoop.cpp
@@ -180,7 +180,6 @@ void ServerLoop::shutdown() {
 	_network->shutdown();
 	uv_timer_stop(&_poiTimer);
 	uv_timer_stop(&_worldTimer);
-	uv_timer_stop(&_spawnMgrTimer);
 	uv_timer_stop(&_entityStorageTimer);
 	uv_idle_stop(&_idleTimer);
 	uv_tty_reset_mode();

src/modules/backend/loop/ServerLoop.h
@@ -50,7 +50,6 @@ private:
 	uv_loop_t *_loop = nullptr;
 	uv_timer_t _poiTimer;
 	uv_timer_t _worldTimer;
-	uv_timer_t _spawnMgrTimer;
 	uv_timer_t _entityStorageTimer;
 	uv_idle_t _idleTimer;
 

src/modules/core/CMakeLists.txt
@@ -69,6 +69,7 @@ set(TEST_SRCS
 	tests/OctreeTest.cpp
 	tests/VarTest.cpp
 	tests/CommandTest.cpp
+	tests/CommandHandlerTest.cpp
 	tests/MD5Test.cpp
 	tests/SetTest.cpp
 	tests/TokenizerTest.cpp

src/modules/core/Input.cpp
@@ -5,6 +5,7 @@
 #include "Input.h"
 #include "core/Log.h"
 #include "core/command/CommandHandler.h"
+#include <cstring>
 
 namespace core {
 
@@ -33,7 +34,16 @@ void Input::onRead(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf) {
 	if (nread < 0) {
 		return uv_close(reinterpret_cast<uv_handle_t*>(stream), nullptr);
 	}
-	const std::string commandLine(buf->base, buf->len);
+	size_t len;
+	for (len = 0u; len < buf->len; ++len) {
+		if (buf->base[len] == '\0' || buf->base[len] == '\n' || buf->base[len] == '\r') {
+			break;
+		}
+	}
+	if (len == 0u || len == buf->len) {
+		return;
+	}
+	const std::string commandLine(buf->base, len);
 	core::executeCommands(commandLine);
 }
 

src/modules/core/command/CommandHandler.cpp
@@ -8,10 +8,24 @@
 namespace core {
 
 int executeCommands(const std::string& _commandLine) {
-	const std::vector<std::string> commands = core::Tokenizer(_commandLine, ";").tokens();
+	if (_commandLine.empty()) {
+		return 0;
+	}
 	int n = 0;
-	for (const std::string& command : commands) {
-		std::vector<std::string> tokens = core::Tokenizer(core::string::trim(command), " ").tokens();
+	const core::Tokenizer tok(_commandLine, ";");
+	for (const std::string& command : tok.tokens()) {
+		if (command.empty()) {
+			continue;
+		}
+		const std::string_view trimmed = core::string::trim(command);
+		if (trimmed.empty()) {
+			continue;
+		}
+		const core::Tokenizer tokInner(trimmed, " ");
+		if (tokInner.tokens().empty()) {
+			continue;
+		}
+		std::vector<std::string> tokens = tokInner.tokens();
 		const std::string cmd = tokens[0];
 		tokens.erase(tokens.begin());
 		if (core::Command::execute(cmd, tokens)) {

src/modules/core/tests/CommandHandlerTest.cpp
@@ -1,0 +1,22 @@
+/**
+ * @file
+ */
+
+#include "core/tests/AbstractTest.h"
+#include "core/command/CommandHandler.h"
+
+namespace core {
+
+class CommandHandlerTest: public AbstractTest {
+};
+
+TEST_F(CommandHandlerTest, testExecuteCommandline) {
+	EXPECT_EQ(-1, core::executeCommands("test"));
+	EXPECT_EQ(-1, core::executeCommands("test/*foo*/"));
+	EXPECT_EQ(-1, core::executeCommands("test;test;test;;"));
+	EXPECT_EQ(0, core::executeCommands(""));
+	EXPECT_EQ(0, core::executeCommands(";;;"));
+	EXPECT_EQ(0, core::executeCommands("; ; ; "));
+}
+
+}

src/modules/metric/Metric.cpp
@@ -79,7 +79,7 @@ bool Metric::assemble(const char* key, int value, const char* type, const TagMap
 	constexpr int metricSize = 256;
 	char buffer[metricSize];
 	constexpr int tagsSize = 256;
-	char tagsBuffer[tagsSize];
+	char tagsBuffer[tagsSize] = "";
 	int written;
 	switch (_flavor) {
 	case Flavor::Etsy:

commit c82dc8e6f5e753fda4ad158a252786b3796cafa4

CORE: fixed Tokenizer parsing error
introduced with aed941736707ae7af483d901a2e5f0968515c816

src/modules/core/Tokenizer.cpp
@@ -155,10 +155,10 @@ bool Tokenizer::skipComments(const char **s, bool skipWhitespace) {
 		return true;
 	} else if (next == '/') {
 		while (**s != '\0' && **s != '\n') {
+			(*s)++;
 			if (--_len <= 0) {
 				return true;
 			}
-			(*s)++;
 		}
 		skip(s, skipWhitespace);
 		return true;