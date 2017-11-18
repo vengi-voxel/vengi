ce1631386af1c33316cfb61067a504658ad59b45
Martin Gerhardy <martin.gerhardy@gmail.com>
2017-11-18 21:39:45 +0100

    BACKEND: use dedicated test tables
    this is safer for the future changes on the user table and also fixes a unique name constraint

--- src/modules/backend/tests/DatabaseModelTest.cpp
+++ src/modules/backend/tests/DatabaseModelTest.cpp
@@ -3,7 +3,7 @@
  */
 
 #include "persistence/tests/AbstractDatabaseTest.h"
-#include "UserModel.h"
+#include "TestModel.h"
 #include "persistence/ConnectionPool.h"
 #include "persistence/DBHandler.h"
 #include "engine-config.h"
@@ -21,8 +21,8 @@ public:
 		Super::SetUp();
 		_supported = _dbHandler.init();
 		if (_supported) {
-			_dbHandler.dropTable(db::UserModel());
-			ASSERT_TRUE(_dbHandler.createTable(db::UserModel())) << "Could not create table";
+			_dbHandler.dropTable(db::TestModel());
+			ASSERT_TRUE(_dbHandler.createTable(db::TestModel())) << "Could not create table";
 		}
 	}
 
@@ -34,7 +34,7 @@ public:
 	void createUser(const std::string& email, const std::string& password, int64_t& id) {
 		ASSERT_TRUE(_supported);
 		const persistence::Timestamp ts = persistence::Timestamp::now();
-		db::UserModel u;
+		db::TestModel u;
 		EXPECT_EQ(0, u.id());
 		u.setEmail(email);
 		u.setPassword(password);
@@ -42,16 +42,16 @@ public:
 		ASSERT_TRUE(_dbHandler.insert(u));
 		EXPECT_NE(0, u.id());
 
-		db::UserModel u2nd;
-		const db::DBConditionUserEmail emailCond(email);
-		const db::DBConditionUserPassword passwordCond(password);
+		db::TestModel u2nd;
+		const db::DBConditionTestEmail emailCond(email);
+		const db::DBConditionTestPassword passwordCond(password);
 		ASSERT_TRUE(_dbHandler.select(u2nd, persistence::DBConditionMultiple(true, {&emailCond, &passwordCond})));
 		EXPECT_GT(u2nd.registrationdate().seconds(), uint64_t(0));
 		EXPECT_EQ(u2nd.email(), email);
 		ASSERT_EQ(u2nd.id(), u.id());
 
-		db::UserModel u3nd;
-		ASSERT_TRUE(_dbHandler.select(u3nd, db::DBConditionUserId(u.id())));
+		db::TestModel u3nd;
+		ASSERT_TRUE(_dbHandler.select(u3nd, db::DBConditionTestId(u.id())));
 		EXPECT_GT(u3nd.registrationdate().seconds(), uint64_t(0));
 		EXPECT_EQ(u3nd.email(), email);
 		ASSERT_EQ(u3nd.id(), u.id());
@@ -88,7 +88,7 @@ TEST_F(DatabaseModelTest, testSelectAll) {
 		createUser(core::string::format("testSelectAll%i@b.c.d", i), "secret", id);
 	}
 	int count = 0;
-	EXPECT_TRUE(_dbHandler.select(db::UserModel(), persistence::DBConditionOne(), [&] (db::UserModel&& model) {
+	EXPECT_TRUE(_dbHandler.select(db::TestModel(), persistence::DBConditionOne(), [&] (db::TestModel&& model) {
 		++count;
 	}));
 	EXPECT_EQ(count, expected);
@@ -104,8 +104,8 @@ TEST_F(DatabaseModelTest, testSelectByEmail) {
 		createUser(core::string::format("testSelectByEmail%i@b.c.d", i), "secret", id);
 	}
 	int count = 0;
-	const db::DBConditionUserEmail condition("testSelectByEmail1@b.c.d");
-	EXPECT_TRUE(_dbHandler.select(db::UserModel(), condition, [&] (db::UserModel&& model) {
+	const db::DBConditionTestEmail condition("testSelectByEmail1@b.c.d");
+	EXPECT_TRUE(_dbHandler.select(db::TestModel(), condition, [&] (db::TestModel&& model) {
 		++count;
 		ASSERT_EQ(std::string(condition.value(0)), model.email());
 	}));
@@ -122,8 +122,8 @@ TEST_F(DatabaseModelTest, testSelectById) {
 		createUser(core::string::format("testSelectById%i@b.c.d", i), "secret", id);
 	}
 	int count = 0;
-	const db::DBConditionUserId condition(id);
-	EXPECT_TRUE(_dbHandler.select(db::UserModel(), condition, [&] (db::UserModel&& model) {
+	const db::DBConditionTestId condition(id);
+	EXPECT_TRUE(_dbHandler.select(db::TestModel(), condition, [&] (db::TestModel&& model) {
 		++count;
 		ASSERT_EQ(id, model.id());
 	}));
@@ -136,9 +136,9 @@ TEST_F(DatabaseModelTest, testTruncate) {
 	}
 	int64_t id = -1L;
 	createUser("testTruncate@b.c.d", "secret", id);
-	EXPECT_TRUE(_dbHandler.truncate(db::UserModel()));
+	EXPECT_TRUE(_dbHandler.truncate(db::TestModel()));
 	int count = 0;
-	_dbHandler.select(db::UserModel(), persistence::DBConditionOne(), [&] (db::UserModel&& model) {
+	_dbHandler.select(db::TestModel(), persistence::DBConditionOne(), [&] (db::TestModel&& model) {
 		++count;
 	});
 	EXPECT_EQ(count, 0);
@@ -150,9 +150,9 @@ TEST_F(DatabaseModelTest, testDelete) {
 	}
 	int64_t id = -1L;
 	createUser("testDelete@b.c.d", "secret", id);
-	EXPECT_TRUE(_dbHandler.deleteModel(db::UserModel(), db::DBConditionUserId(id)));
+	EXPECT_TRUE(_dbHandler.deleteModel(db::TestModel(), db::DBConditionTestId(id)));
 	int count = 0;
-	_dbHandler.select(db::UserModel(), persistence::DBConditionOne(), [&] (db::UserModel&& model) {
+	_dbHandler.select(db::TestModel(), persistence::DBConditionOne(), [&] (db::TestModel&& model) {
 		++count;
 	});
 	EXPECT_EQ(count, 0);
@@ -165,8 +165,8 @@ TEST_F(DatabaseModelTest, testUpdate) {
 	int64_t id = -1L;
 	createUser("testUpdate@b.c.d", "secret", id);
 	int count = 0;
-	db::UserModel copy;
-	_dbHandler.select(db::UserModel(), db::DBConditionUserId(id), [&] (db::UserModel&& model) {
+	db::TestModel copy;
+	_dbHandler.select(db::TestModel(), db::DBConditionTestId(id), [&] (db::TestModel&& model) {
 		++count;
 		copy = std::move(model);
 	});
@@ -174,7 +174,7 @@ TEST_F(DatabaseModelTest, testUpdate) {
 	ASSERT_EQ("testUpdate@b.c.d", copy.email());
 	copy.setEmail("no@mail.com");
 	_dbHandler.update(copy);
-	_dbHandler.select(db::UserModel(), db::DBConditionUserId(id), [&] (db::UserModel&& model) {
+	_dbHandler.select(db::TestModel(), db::DBConditionTestId(id), [&] (db::TestModel&& model) {
 		++count;
 		ASSERT_EQ(copy.email(), model.email());
 	});
@@ -184,7 +184,7 @@ TEST_F(DatabaseModelTest, testTimestamp) {
 	if (!_supported) {
 		return;
 	}
-	db::UserModel u;
+	db::TestModel u;
 	EXPECT_EQ(0, u.id());
 	u.setEmail("testTimestamp@now.de");
 	u.setPassword("now");
@@ -192,7 +192,7 @@ TEST_F(DatabaseModelTest, testTimestamp) {
 	u.setRegistrationdate(now / 1000UL);
 	ASSERT_TRUE(_dbHandler.insert(u));
 
-	_dbHandler.select(db::UserModel(), db::DBConditionUserId(u.id()), [=] (db::UserModel&& model) {
+	_dbHandler.select(db::TestModel(), db::DBConditionTestId(u.id()), [=] (db::TestModel&& model) {
 		const persistence::Timestamp& ts = model.registrationdate();
 		const persistence::Timestamp tsNow(now / 1000);
 		ASSERT_NEAR(ts.millis(), now, 999) << "db: " << ts.toString() << " now: " << tsNow.toString();
@@ -209,8 +209,8 @@ TEST_F(DatabaseModelTest, testLimitOrderBy) {
 	}
 	const int limit = 2;
 	int count = 0;
-	const persistence::OrderBy orderBy(db::UserModel::f_id(), persistence::Order::DESC, limit);
-	_dbHandler.select(db::UserModel(), orderBy, [&] (db::UserModel&& model) {
+	const persistence::OrderBy orderBy(db::TestModel::f_id(), persistence::Order::DESC, limit);
+	_dbHandler.select(db::TestModel(), orderBy, [&] (db::TestModel&& model) {
 		++count;
 	});
 	ASSERT_EQ(limit, count);
@@ -228,8 +228,8 @@ TEST_F(DatabaseModelTest, testOffsetOrderBy) {
 	const int limit = -1;
 	const int offset = 3;
 	int count = 0;
-	const persistence::OrderBy orderBy(db::UserModel::f_id(), persistence::Order::DESC, limit, offset);
-	_dbHandler.select(db::UserModel(), orderBy, [&] (db::UserModel&& model) {
+	const persistence::OrderBy orderBy(db::TestModel::f_id(), persistence::Order::DESC, limit, offset);
+	_dbHandler.select(db::TestModel(), orderBy, [&] (db::TestModel&& model) {
 		++count;
 	});
 	ASSERT_EQ(n - offset, count);
