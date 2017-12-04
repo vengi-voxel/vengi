cd1155ae060768b52a5cb47e299cc1310c5ca283
Martin Gerhardy <martin.gerhardy@gmail.com>
2017-12-04 17:38:51 +0100

    CORE: fixed rest content-type

--- src/modules/core/Rest.cpp
+++ src/modules/core/Rest.cpp
@@ -8,7 +8,7 @@ namespace rest {
 
 core::rest::Response post(const std::string& url, const core::json& json) {
 	core::rest::Connection conn(core::Var::getSafe(cfg::HTTPBaseURL)->strVal());
-	conn.AppendHeader("Content-Type", "text/json");
+	conn.AppendHeader("Content-Type", "application/json");
 	return conn.post(url, json.dump());
 }
 
