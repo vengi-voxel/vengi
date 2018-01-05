1826d9751f92a18c18da86ed3b7c9293e728b137
Martin Gerhardy <martin.gerhardy@gmail.com>
2018-01-05 18:01:18 +0100

    TESTLUAAI: added calculator demo from love nuklear

--- data/testluaui/ui/testluaui.lua
+++ data/testluaui/ui/testluaui.lua
@@ -1,4 +1,63 @@
 -- An overview of most of the supported widgets.
+-- Simple calculator example
+
+local ops = {'+', '-', '*', '/'}
+local a, b, op = '0'
+
+local function clear()
+	a, b, op = '0'
+end
+
+local function digit(d)
+	if op then
+		if b == nil or b == '0' then
+			b = d
+		else
+			b = b..d
+		end
+	else
+		if a == '0' then
+			a = d
+		else
+			a = a..d
+		end
+	end
+end
+
+local function decimal()
+	if op then
+		b = b or '0'
+		b = b:find('.') and b or b..'.'
+	else
+		a = a:find('.') and a or a..'.'
+	end
+end
+
+local function equals()
+	if not tonumber(b) then
+		return
+	end
+	if op == '+' then
+		a, b, op = tostring(tonumber(a) + tonumber(b))
+	elseif op == '-' then
+		a, b, op = tostring(tonumber(a) - tonumber(b))
+	elseif op == '*' then
+		a, b, op = tostring(tonumber(a) * tonumber(b))
+	elseif op == '/' then
+		a, b, op = tostring(tonumber(a) / tonumber(b))
+	end
+end
+
+local function operator(o)
+	if op then
+		equals()
+	end
+	op = o
+end
+
+local function display()
+	return b or a
+end
 
 local checkA = {value = false}
 local checkB = {value = true}
@@ -102,6 +161,38 @@ function update(dt)
 			end
 			ui.label('Contextual (Right click me)')
 			ui.groupEnd()
+		end
+	end
+	ui.windowEnd()
+
+	if ui.windowBegin('Calculator', 50, 50, 180, 250, 'border', 'movable', 'title') then
+		ui.layoutRow('dynamic', 35, 1)
+		ui.label(display(), 'right')
+		ui.layoutRow('dynamic', 35, 4)
+		for i=1,16 do
+			if i >= 13 and i < 16 then
+				if i == 13 then
+					if ui.button('C') then
+						clear()
+					end
+					if ui.button('0') then
+						digit('0')
+					end
+					if ui.button('=') then
+						equals()
+					end
+				end
+			elseif i % 4 ~= 0 then
+				local d = tostring(math.floor(i / 4) * 3 + (i % 4))
+				if ui.button(d) then
+					digit(d)
+				end
+			else
+				local o = ops[math.floor(i / 4)]
+				if ui.button(o) then
+					operator(o)
+				end
+			end
 		end
 	end
 	ui.windowEnd()
