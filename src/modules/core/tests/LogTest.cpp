a4f011196cfe799ee1aba5fc9b5879e098202eef
Martin Gerhardy <martin.gerhardy@gmail.com>
2018-01-20 11:49:33 +0100

    CORE: fixed logtest

--- src/modules/core/tests/LogTest.cpp
+++ src/modules/core/tests/LogTest.cpp
@@ -13,8 +13,6 @@ class LogTest : public core::AbstractTest {
 TEST_F(LogTest, testLogId) {
 	const auto logid1 = Log::logid("LogTest1");
 	const auto logid2 = Log::logid("LogTest2");
-	ASSERT_EQ(logid1, 0u);
-	ASSERT_EQ(logid2, 0u);
 	ASSERT_NE(logid1, logid2);
 }
 
