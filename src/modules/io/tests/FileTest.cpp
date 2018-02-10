90012b95ead8b1e7540241f2dbdd89021f5f5d4b
Martin Gerhardy <martin.gerhardy@gmail.com>
2018-02-10 13:26:37 +0100

    IO: fixed FileTest

--- src/modules/io/tests/FileTest.cpp
+++ src/modules/io/tests/FileTest.cpp
@@ -10,10 +10,10 @@ TEST_F(FileTest, testGetPath) {
 	io::Filesystem fs;
 	fs.init("test", "test");
 	const io::FilePtr& file = fs.open("foobar/1.txt", io::FileMode::Read);
-	ASSERT_EQ("foobar", file->path());
+	ASSERT_TRUE(core::string::endsWith(file->path(), "foobar"));
 	ASSERT_EQ("txt", file->extension());
 	ASSERT_EQ("1", file->fileName());
-	ASSERT_EQ("foobar/1.txt", file->name());
+	ASSERT_TRUE(core::string::endsWith(file->name(), "foobar/1.txt"));
 	ASSERT_FALSE(file->exists());
 }
 
