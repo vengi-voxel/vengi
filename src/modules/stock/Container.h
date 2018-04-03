e9af75c39401a36240119c4c524314425e5a4d8c
Martin Gerhardy <martin.gerhardy@gmail.com>
2018-04-03 23:14:49 +0200

    STOCK: doxygen

--- src/modules/stock/Container.h
+++ src/modules/stock/Container.h
@@ -13,6 +13,7 @@ class Item;
 typedef std::shared_ptr<Item> ItemPtr;
 
 /**
+ * @brief A container is a collection of items. They are packed into a @c ContainerItem
  * @ingroup Stock
  */
 class Container {
@@ -37,8 +38,18 @@ public:
 	 */
 	size_t itemCount() const;
 
+	/**
+	 * @brief Find a free location in the container to place the given item at
+	 * @param[out] x The x location to place the item
+	 * @param[out] y The y location to place the item
+	 * @return @c true if a free location was found, @c false otherwise
+	 */
 	bool findSpace(const ItemPtr& item, uint8_t& x, uint8_t& y) const;
 
+	/**
+	 * @brief Check whether the given item can be added to the specified location in the container
+	 * @return @c true if the placement would work, @c false if not
+	 */
 	bool canAdd(const ItemPtr& item, uint8_t x, uint8_t y) const;
 
 	bool add(const ItemPtr& item, uint8_t x, uint8_t y);
