3d6c456b6e959259a52c11674fb56ef51a7794a3
Martin Gerhardy <martin.gerhardy@gmail.com>
2018-04-03 22:30:22 +0200

    POI: extended tests

--- src/modules/poi/tests/PoiProviderTest.cpp
+++ src/modules/poi/tests/PoiProviderTest.cpp
@@ -53,4 +53,27 @@ TEST_F(PoiProviderTest, testExpireWithProperPos) {
 	EXPECT_EQ(1u, _poiProvider->count()) << "We should have at least one poi left";
 }
 
+TEST_F(PoiProviderTest, testQueryType) {
+	_poiProvider->add(glm::vec3(0.0), Type::GENERIC);
+	_poiProvider->add(glm::vec3(1.0), Type::GENERIC);
+	_poiProvider->add(glm::vec3(2.0), Type::FIGHT);
+	_poiProvider->add(glm::vec3(3.0), Type::QUEST);
+	_poiProvider->add(glm::vec3(4.0), Type::QUEST);
+	_poiProvider->add(glm::vec3(5.0), Type::QUEST);
+	_poiProvider->add(glm::vec3(6.0), Type::QUEST);
+	const PoiResult& result = _poiProvider->query(Type::FIGHT);
+	EXPECT_TRUE(result.valid);
+	EXPECT_EQ(glm::vec3(2.0), result.pos);
+}
+
+TEST_F(PoiProviderTest, testNoExpire) {
+	_poiProvider->add(glm::vec3(1.0));
+	_poiProvider->add(glm::vec3(2.0));
+	_poiProvider->add(glm::vec3(3.0));
+	EXPECT_EQ(3u, _poiProvider->count());
+	_timeProvider->update(30 * 1000UL);
+	_poiProvider->update(0UL);
+	EXPECT_EQ(3u, _poiProvider->count()) << "We should still have all three left";
+}
+
 }
