ccc52efef903007a41ecd6a8fff573387e340ef4
Martin Gerhardy <martin.gerhardy@gmail.com>
2018-05-01 22:12:46 +0200

    UI: missing translation markers

--- src/modules/ui/turbobadger/Window.cpp
+++ src/modules/ui/turbobadger/Window.cpp
@@ -32,17 +32,18 @@ Window::Window(Window* parent) :
 
 Window::~Window() {
 	RemoveFromParent();
+	core::Singleton<io::EventHandler>::getInstance().removeObserver(this);
 }
 
 tb::TBGenericStringItem* Window::addMenuItem(tb::TBSelectItemSourceList<tb::TBGenericStringItem>& items, const char *text, const char *id) {
 	tb::TBGenericStringItem* item;
 	if (id == nullptr) {
 		const std::string& lowerId = core::string::toLower(text);
-		item = new tb::TBGenericStringItem(text, TBIDC(lowerId.c_str()));
+		item = new tb::TBGenericStringItem(tr(text), TBIDC(lowerId.c_str()));
 		const std::string& iconId = core::App::getInstance()->appname() + "-" + lowerId;
 		item->SetSkinImage(TBIDC(iconId.c_str()));
 	} else {
-		item = new tb::TBGenericStringItem(text, TBIDC(id));
+		item = new tb::TBGenericStringItem(tr(text), TBIDC(id));
 		char buf[128];
 		SDL_snprintf(buf, sizeof(buf), "%s-%s", core::App::getInstance()->appname().c_str(), id);
 		item->SetSkinImage(TBIDC(buf));
