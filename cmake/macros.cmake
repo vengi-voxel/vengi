74cdefe2eee3012862c19a90dff7299a6a149ae7
Martin Gerhardy <martin.gerhardy@gmail.com>
2018-06-26 17:33:30 +0200

    CMAKE: fixed DEBUGGER_COMMAND handling

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -471,12 +471,23 @@ endmacro()
 
 macro(engine_add_debuggger TARGET)
 	add_custom_target(${TARGET}-debug)
-	add_custom_command(TARGET ${TARGET}-debug
-		COMMAND ${DEBUGGER_COMMAND} $<TARGET_FILE:${TARGET}>
-		COMMENT "Starting debugger session for ${TARGET}"
-		WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/${TARGET}
-		DEPENDS ${TARGET}
-	)
+	if (${DEBUGGER} STREQUAL "gdb")
+		add_custom_command(TARGET ${TARGET}-debug
+			COMMAND ${GDB_EXECUTABLE} -ex run --args $<TARGET_FILE:${TARGET}>
+			COMMENT "Starting debugger session for ${TARGET}"
+			WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/${TARGET}
+			DEPENDS ${TARGET}
+		)
+	elseif (${DEBUGGER} STREQUAL "lldb")
+		add_custom_command(TARGET ${TARGET}-debug
+			COMMAND ${LLDB_EXECUTABLE} -b -o run $<TARGET_FILE:${TARGET}>
+			COMMENT "Starting debugger session for ${TARGET}"
+			WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/${TARGET}
+			DEPENDS ${TARGET}
+		)
+	else()
+		message(WARN "Unknown DEBUGGER value - set to gdb or lldb")
+	endif()
 endmacro()
 
 #-------------------------------------------------------------------------------
