07a066f309b2a74178996ec7373ed7e549a19b8b
Martin Gerhardy <martin.gerhardy@gmail.com>
2018-09-09 17:44:04 +0200

    VIDEO: try to create window without multisampling if the first attempt failed

--- src/modules/video/WindowedApp.cpp
+++ src/modules/video/WindowedApp.cpp
@@ -330,11 +330,21 @@ core::AppState WindowedApp::onInit() {
 		Log::info("use fake fullscreen for display %i: %i:%i", displayIndex, width, height);
 	}
 
-	_window = SDL_CreateWindow(_appname.c_str(), SDL_WINDOWPOS_CENTERED_DISPLAY(displayIndex), SDL_WINDOWPOS_CENTERED_DISPLAY(displayIndex), width, height, flags);
+#define CreateWindow() SDL_CreateWindow(_appname.c_str(), SDL_WINDOWPOS_CENTERED_DISPLAY(displayIndex), SDL_WINDOWPOS_CENTERED_DISPLAY(displayIndex), width, height, flags)
+
+	_window = CreateWindow();
 	if (!_window) {
-		sdlCheckError();
-		return core::AppState::InitFailure;
+		Log::warn("Failed to get multisampled window - try to disable it");
+		SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 0);
+		SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 0);
+		_window = CreateWindow();
+		if (!_window) {
+			sdlCheckError();
+			return core::AppState::InitFailure;
+		}
 	}
+
+#undef CreateWindow
 
 	_rendererContext = video::createContext(_window);
 
