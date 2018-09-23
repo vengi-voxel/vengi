b8e22ce8032aae85dc5ca5c1aa86e538ebe6c3a2
Martin Gerhardy <martin.gerhardy@gmail.com>
2018-09-23 21:58:21 +0200

    CMAKE: fixed valgrind targets

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -572,15 +572,27 @@ endmacro()
 macro(engine_add_valgrind TARGET)
 	find_program(VALGRIND_EXECUTABLE NAMES valgrind)
 	if (VALGRIND_EXECUTABLE)
+		add_custom_target(${TARGET}-memcheckxml)
+		add_custom_command(TARGET ${TARGET}-memcheckxml
+			COMMAND
+				${VALGRIND_EXECUTABLE} --xml=yes --xml-file=${CMAKE_CURRENT_BINARY_DIR}/memcheck-${TARGET}.xml
+				--tool=memcheck --leak-check=full --show-reachable=yes
+				--undef-value-errors=yes --track-origins=no --child-silent-after-fork=no
+				--trace-children=no
+				$<TARGET_FILE:${TARGET}>
+			COMMENT "Executing valgrind memcheck and log into ${CMAKE_CURRENT_BINARY_DIR}"
+			WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/${TARGET}
+			DEPENDS ${TARGET}
+		)
 		add_custom_target(${TARGET}-memcheck)
 		add_custom_command(TARGET ${TARGET}-memcheck
 			COMMAND
-				${VALGRIND_EXECUTABLE} --xml=yes --xml-file=${CMAKE_BINARY_DIR}/memcheck-${TARGET}.xml
+				${VALGRIND_EXECUTABLE}
 				--tool=memcheck --leak-check=full --show-reachable=yes
 				--undef-value-errors=yes --track-origins=no --child-silent-after-fork=no
-				--trace-children=no --log-file=$<TARGET_FILE:${TARGET}>.memcheck.log
+				--trace-children=no --log-file=${CMAKE_CURRENT_BINARY_DIR}/memcheck-${TARGET}.log
 				$<TARGET_FILE:${TARGET}>
-			COMMENT "memcheck log for ${TARGET}: $<TARGET_FILE:${TARGET}>.memcheck.log"
+			COMMENT "Executing valgrind memcheck and log into ${CMAKE_CURRENT_BINARY_DIR}"
 			WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/${TARGET}
 			DEPENDS ${TARGET}
 		)
@@ -591,7 +603,7 @@ macro(engine_add_valgrind TARGET)
 				--tool=helgrind --child-silent-after-fork=no
 				--trace-children=no --log-file=$<TARGET_FILE:${TARGET}>.helgrind.log
 				$<TARGET_FILE:${TARGET}>
-			COMMENT "helgrind log for ${TARGET}: $<TARGET_FILE:${TARGET}>.helgrind.log"
+			COMMENT "Executing valgrind helgrind and log into ${CMAKE_CURRENT_BINARY_DIR}"
 			WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/${TARGET}
 			DEPENDS ${TARGET}
 		)
