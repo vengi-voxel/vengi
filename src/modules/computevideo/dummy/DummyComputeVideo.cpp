84b4bd48a0708776c71bd610cbfeb3b937d3a0c4
Martin Gerhardy <martin.gerhardy@gmail.com>
2018-10-07 10:27:23 +0200

    COMPUTEVIDEO: dummy and cuda driver

---
+++ src/modules/computevideo/dummy/DummyComputeVideo.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file
+ *
+ * @ingroup Compute
+ */
+#include "compute/cl/CLCompute.h"
+#include "video/Buffer.h"
+#include "video/Texture.h"
+
+namespace computevideo {
+
+bool init() {
+	return false;
+}
+
+void shutdown() {
+}
+
+compute::Id createBuffer(compute::BufferFlag flags, video::Buffer& buffer, int idx) {
+	return compute::InvalidId;
+}
+
+compute::Id createTexture(compute::BufferFlag flags, video::Texture& texture) {
+	return compute::InvalidId;
+}
+
+bool enqueueAcquire(compute::Id id) {
+	return false;
+}
+
+bool enqueueRelease(compute::Id id) {
+	return false;
+}
+
+}
