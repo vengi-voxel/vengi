575a7b6bd17fe5af634e8b88023962a0259a9b9d
Martin Gerhardy <martin.gerhardy@gmail.com>
2018-11-27 21:26:03 +0100

    VIDEO: changed log level

--- src/modules/video/MeshLUAFunctions.cpp
+++ src/modules/video/MeshLUAFunctions.cpp
@@ -17,7 +17,7 @@ static const char* MeshId = "MESH";
 static int meshlua_addskin(lua_State* l) {
 	Mesh* mesh = lua::LUA::globalData<Mesh>(l, MeshId);
 	const char* skin = luaL_checkstring(l, 1);
-	Log::info("Add skin %s to model '%s'", skin, mesh->filename().c_str());
+	Log::debug("Add skin %s to model '%s'", skin, mesh->filename().c_str());
 	//mesh->addSkin(skin);
 	return 0;
 }
@@ -28,15 +28,13 @@ static int meshlua_addanimation(lua_State* l) {
 	const float startFrame = luaL_checknumber(l, 2);
 	const float endFrame = luaL_checknumber(l, 3);
 	const float fps = luaL_checknumber(l, 4);
-	Log::info("Add animation %s to model '%s' to %f:%f:%f", name, mesh->filename().c_str(), startFrame, endFrame, fps);
-	//mesh->addAnimation(name, startFrame, endFrame, fps);
+	//mesh->addAnimation(name, startFrame, endFrame - startFrame, fps, false);
 	return 0;
 }
 
 static int meshlua_setscale(lua_State* l) {
 	Mesh* mesh = lua::LUA::globalData<Mesh>(l, MeshId);
 	const glm::vec3* scale = clua_get<glm::vec3>(l, 1);
-	Log::info("Set scale for model '%s' to %f:%f:%f", mesh->filename().c_str(), scale->x, scale->y, scale->z);
 	mesh->setScale(*scale);
 	return 0;
 }
