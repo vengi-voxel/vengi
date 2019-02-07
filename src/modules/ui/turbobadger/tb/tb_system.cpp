57f9be0d46dd3587059634f9a329ed167cd191db
Martin Gerhardy <martin.gerhardy@gmail.com>
2019-02-07 19:10:23 +0100

    UI: moved code

--- src/modules/ui/turbobadger/tb/tb_system.cpp
+++ src/modules/ui/turbobadger/tb/tb_system.cpp
@@ -5,12 +5,11 @@
 
 #include "tb_system.h"
 
-#ifdef TB_SYSTEM_SDL
-
 #include "tb_msg.h"
 #include "tb_types.h"
 #include <stdio.h>
-
+#include "core/App.h"
+#include "io/Filesystem.h"
 #include <SDL.h>
 
 #ifdef TB_RUNTIME_DEBUG_INFO
@@ -70,6 +69,59 @@ int TBSystem::GetDPI()
 #endif
 }
 
-} // namespace tb
+void TBClipboard::Empty()
+{
+	SetText("");
+}
 
-#endif // TB_SYSTEM_SDL
+bool TBClipboard::HasText()
+{
+	return SDL_HasClipboardText();
+}
+
+bool TBClipboard::SetText(const char *text)
+{
+	return (0 == SDL_SetClipboardText(text));
+}
+
+bool TBClipboard::GetText(TBStr &text)
+{
+	if (const char *str = SDL_GetClipboardText())
+		return text.Set(str);
+	return false;
+}
+
+class File: public TBFile {
+public:
+	File(const io::FilePtr& file) :
+			_file(file) {
+	}
+
+	virtual long Size() {
+		return _file->length();
+	}
+	virtual size_t Read(void *buf, size_t elemSize, size_t count) {
+		return _file->read(buf, elemSize, count);
+	}
+private:
+	io::FilePtr _file;
+};
+
+// static
+TBFile *TBFile::Open(const char *filename, TBFileMode mode) {
+	io::FilePtr f;
+	switch (mode) {
+	case MODE_READ:
+		f = core::App::getInstance()->filesystem()->open(filename,
+				io::FileMode::Read);
+		break;
+	default:
+		break;
+	}
+	if (!f) {
+		return nullptr;
+	}
+	return new File(f);
+}
+
+} // namespace tb
