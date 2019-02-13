bf948cbe39d384f395440169bf5275f6137a1145
Martin Gerhardy <martin.gerhardy@gmail.com>
2019-02-13 21:48:43 +0100

    AI: fixed missing logging in none-unity builds

--- src/modules/ai/common/Types.h
+++ src/modules/ai/common/Types.h
@@ -5,7 +5,15 @@
 
 #include "core/Assert.h"
 #include "core/Log.h"
+
 #define ai_assert core_assert_msg
+#define ai_log(...) Log::info(__VA_ARGS__)
+#define ai_log_error(...) Log::error(__VA_ARGS__)
+#define ai_log_warn(...) Log::warn(__VA_ARGS__)
+#define ai_log_debug(...) Log::debug(__VA_ARGS__)
+#define ai_log_trace(...) Log::trace(__VA_ARGS__)
+
+#define SIMPLEAI_SKIP_LOG
 
 #include "Log.h"
 #include <string>
