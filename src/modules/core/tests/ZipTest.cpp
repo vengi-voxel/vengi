b24ff2d80563bd789862fe98a7a1509cbf6e8480
Martin Gerhardy <martin.gerhardy@gmail.com>
2019-02-24 20:55:18 +0100

    CORE: fixed format issues

--- src/modules/core/tests/ZipTest.cpp
+++ src/modules/core/tests/ZipTest.cpp
@@ -19,7 +19,6 @@ TEST_F(ZipTest, testCompress) {
 	ASSERT_TRUE(zip::compress(inputBuf, inputBufSize, outputBuf, outputBufSize)) << "Failed to compress buffer";
 }
 
-
 TEST_F(ZipTest, testUncompress) {
 	constexpr size_t inputBufSize = 64;
 	const uint8_t inputBuf[inputBufSize] { 0 };
@@ -27,7 +26,7 @@ TEST_F(ZipTest, testUncompress) {
 	uint8_t outputBuf[outputBufSize];
 	size_t finalSize = 0;
 
-	ASSERT_TRUE(zip::compress(inputBuf, inputBufSize, outputBuf, outputBufSize	, &finalSize)) << "Failed to compress buffer";
+	ASSERT_TRUE(zip::compress(inputBuf, inputBufSize, outputBuf, outputBufSize, &finalSize)) << "Failed to compress buffer";
 	ASSERT_LT(finalSize, inputBufSize) << "No compression - expected the compressed size to be smaller than the input size";
 
 	constexpr size_t outputAfterCompressBufSize = inputBufSize;
