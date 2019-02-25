2f4f7e715437043862b19e1f74911a5c960a5843
Martin Gerhardy <martin.gerhardy@gmail.com>
2019-02-25 15:27:35 +0100

    CORE: simplified zlib code

--- src/modules/core/Zip.h
+++ src/modules/core/Zip.h
@@ -5,47 +5,14 @@
 #pragma once
 
 #include <SDL.h>
-#include <zlib.h>
 
 namespace core {
-
-/**
- * @brief Wrapper around zlib z_stream
- */
-class Zip: public z_stream {
-private:
-	static int printError(const char *context, int ret);
-public:
-	bool uncompress(const uint8_t *inputBuf, size_t inputBufSize,
-			uint8_t* outputBuf, size_t outputBufSize);
-
-	/**
-	 * @brief Compresses the given input buffer and store the result in the given output buffer
-	 * @param[in] inputBuf The buffer to compress
-	 * @param[in] inputBufSize The size of the input buffer
-	 * @param[out] outputBuf The buffer to store the compressed data in
-	 * @param[in] outputBufSize The size of the output buffer
-	 * @param[out] finalBufSize The size that was really used in the output buffer. Can be null.
-	 * @return @c true if the compression was successful, @c false otherwise.
-	 */
-	bool compress(const uint8_t *inputBuf, size_t inputBufSize,
-			uint8_t* outputBuf, size_t outputBufSize, size_t* finalBufSize = nullptr, int compressionLevel = Z_DEFAULT_COMPRESSION);
-};
-
 namespace zip {
 
-inline bool compress(const uint8_t *inputBuf, size_t inputBufSize,
-			uint8_t* outputBuf, size_t outputBufSize, size_t* finalBufSize = nullptr, int compressionLevel = Z_DEFAULT_COMPRESSION) {
-	Zip z;
-	return z.compress(inputBuf, inputBufSize, outputBuf, outputBufSize, finalBufSize, compressionLevel);
-}
-
-inline bool uncompress(const uint8_t *inputBuf, size_t inputBufSize,
-			uint8_t* outputBuf, size_t outputBufSize) {
-	Zip z;
-	return z.uncompress(inputBuf, inputBufSize, outputBuf, outputBufSize);
-}
+extern bool compress(const uint8_t *inputBuf, size_t inputBufSize,
+		uint8_t* outputBuf, size_t outputBufSize, size_t* finalBufSize = nullptr);
+extern bool uncompress(const uint8_t *inputBuf, size_t inputBufSize,
+		uint8_t* outputBuf, size_t outputBufSize, size_t* finalBufSize = nullptr);
 
 }
-
 }
--- src/modules/core/tests/ZipTest.cpp
+++ src/modules/core/tests/ZipTest.cpp
@@ -16,25 +16,31 @@ TEST_F(ZipTest, testCompress) {
 	constexpr size_t outputBufSize = 64;
 	uint8_t outputBuf[outputBufSize];
 
-	ASSERT_TRUE(zip::compress(inputBuf, inputBufSize, outputBuf, outputBufSize)) << "Failed to compress buffer";
+	size_t finalSize = 0;
+	EXPECT_TRUE(zip::compress(inputBuf, inputBufSize, outputBuf, outputBufSize, &finalSize)) << "Failed to compress buffer";
+	EXPECT_LT(finalSize, inputBufSize) << "No compression - expected the compressed size to be smaller than the input size";
 }
 
 TEST_F(ZipTest, testUncompress) {
 	constexpr size_t inputBufSize = 64;
-	const uint8_t inputBuf[inputBufSize] { 0 };
+	uint8_t inputBuf[inputBufSize] {0};
+	for (size_t i = 0u; i < inputBufSize / 2; i += 2) {
+		inputBuf[i + 0] = (uint8_t)i + 1;
+		inputBuf[i + 1] = (uint8_t)i + 1;
+	}
 	constexpr size_t outputBufSize = 64;
 	uint8_t outputBuf[outputBufSize];
 	size_t finalSize = 0;
 
-	ASSERT_TRUE(zip::compress(inputBuf, inputBufSize, outputBuf, outputBufSize, &finalSize)) << "Failed to compress buffer";
-	ASSERT_LT(finalSize, inputBufSize) << "No compression - expected the compressed size to be smaller than the input size";
+	EXPECT_TRUE(zip::compress(inputBuf, inputBufSize, outputBuf, outputBufSize, &finalSize)) << "Failed to compress buffer";
+	EXPECT_LT(finalSize, inputBufSize) << "No compression - expected the compressed size to be smaller than the input size";
 
 	constexpr size_t outputAfterCompressBufSize = inputBufSize;
 	uint8_t outputAfterCompressBuf[outputAfterCompressBufSize] { 0 };
-	ASSERT_TRUE(zip::uncompress(outputBuf, outputBufSize, outputAfterCompressBuf, outputAfterCompressBufSize)) << "Failed to compress buffer";
+	EXPECT_TRUE(zip::uncompress(outputBuf, outputBufSize, outputAfterCompressBuf, outputAfterCompressBufSize)) << "Failed to compress buffer";
 
 	for (size_t i = 0; i < outputAfterCompressBufSize; ++i) {
-		ASSERT_EQ(inputBuf[i], outputAfterCompressBuf[i]);
+		EXPECT_EQ(inputBuf[i], outputAfterCompressBuf[i]);
 	}
 }
 
