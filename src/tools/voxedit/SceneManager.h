e6f2d9c5b070331b49091a3bf44f40e24acbb05c
Martin Gerhardy <martin.gerhardy@gmail.com>
2019-03-06 00:12:26 +0100

    VOXEDIT: fixed left and right movement to work in a more natural way

--- src/tools/voxedit/SceneManager.h
+++ src/tools/voxedit/SceneManager.h
@@ -41,8 +41,8 @@ static constexpr struct Direction {
 	int y;
 	int z;
 } DIRECTIONS[] = {
-	{"left",     -1,  0,  0},
-	{"right",     1,  0,  0},
+	{"left",      1,  0,  0},
+	{"right",    -1,  0,  0},
 	{"up",        0,  1,  0},
 	{"down",      0, -1,  0},
 	{"forward",   0,  0,  1},
