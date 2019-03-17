13e68cb9eee921a812b353feadc65fe09889eec4
Martin Gerhardy <martin.gerhardy@gmail.com>
2019-03-17 22:39:00 +0100

    VOXEL: fixed Region::moveInto for regions having their mins != 0

--- src/modules/voxel/polyvox/Region.cpp
+++ src/modules/voxel/polyvox/Region.cpp
@@ -11,21 +11,22 @@ const Region Region::MaxRegion = Region(std::numeric_limits<int32_t>::min(), std
 
 glm::ivec3 Region::moveInto(int32_t x, int32_t y, int32_t z) const {
 	const glm::ivec3& size = getDimensionsInVoxels();
-	int32_t nx = x - getLowerX();
-	int32_t ny = y - getLowerY();
-	int32_t nz = z - getLowerZ();
-	while (nx < 0) {
-		nx += getWidthInVoxels();
+	const glm::ivec3& mins = getLowerCorner();
+	int32_t nx = x - mins.x;
+	int32_t ny = y - mins.y;
+	int32_t nz = z - mins.z;
+	while (nx < mins.x) {
+		nx += size.x;
 	}
-	while (ny < 0) {
-		ny += getHeightInVoxels();
+	while (ny < mins.y) {
+		ny += size.y;
 	}
-	while (nz < 0) {
-		nz += getDepthInVoxels();
+	while (nz < mins.z) {
+		nz += size.z;
 	}
-	const int32_t ox = nx % size.x;
-	const int32_t oy = ny % size.y;
-	const int32_t oz = nz % size.z;
+	const int32_t ox = mins.x + (nx % size.x);
+	const int32_t oy = mins.y + (ny % size.y);
+	const int32_t oz = mins.z + (nz % size.z);
 	core_assert_msg(containsPoint(ox, oy, oz),
 			"shifted(%i:%i:%i) is outside the valid region for pos(%i:%i:%i), size(%i:%i:%i), mins(%i:%i:%i)",
 			ox, oy, oz, x, y, z, size.x, size.y, size.z, getLowerX(), getLowerY(), getLowerZ());
