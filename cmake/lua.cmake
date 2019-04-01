eef914fabb1ea56e9e32a38a18b9a496bd81a1ee
Martin Gerhardy <martin.gerhardy@gmail.com>
2019-04-01 21:20:53 +0200

    CMAKE: cleanup lua validation target

--- cmake/lua.cmake
+++ cmake/lua.cmake
@@ -2,31 +2,24 @@ macro(check_lua_files TARGET)
 	set(files ${ARGV})
 	list(REMOVE_AT files 0)
 	find_program(LUAC_EXECUTABLE NAMES ${DEFAULT_LUAC_EXECUTABLE})
+	set(ADDITIONAL_DEPENDENCIES)
 	if (LUAC_EXECUTABLE)
 		message(STATUS "${LUAC_EXECUTABLE} found")
-		foreach(_file ${files})
-			get_filename_component(filename ${_file} NAME)
-			string(REGEX REPLACE "[/]" "_" targetname ${_file})
-			add_custom_target(
-				${targetname}
-				COMMAND ${LUAC_EXECUTABLE} -o ${CMAKE_CURRENT_BINARY_DIR}/${filename}.out ${_file}
-				COMMENT "Validate ${_file}"
-				WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/lua/
-			)
-			add_dependencies(${TARGET} ${targetname})
-		endforeach()
 	else()
-		foreach(_file ${files})
-			string(REGEX REPLACE "[/]" "_" targetname ${_file})
-			get_filename_component(filename ${_file} NAME)
-			add_custom_target(
-				${targetname}
-				COMMAND $<TARGET_FILE:luac> -o ${CMAKE_CURRENT_BINARY_DIR}/${filename}.out ${_file}
-				COMMENT "Validate ${_file}"
-				DEPENDS luac
-				WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/lua/
-			)
-			add_dependencies(${TARGET} ${targetname})
-		endforeach()
+		set(LUAC_EXECUTABLE luac)
+		list(APPEND ADDITIONAL_DEPENDENCIES luac)
 	endif()
+	foreach(_file ${files})
+		string(REGEX REPLACE "[/]" "_" targetname ${_file})
+		get_filename_component(filename ${_file} NAME)
+		set(_outfile ${filename}.out)
+		add_custom_command(
+			OUTPUT ${_outfile}
+			COMMAND ${LUAC_EXECUTABLE} -o ${_outfile} ${CMAKE_CURRENT_SOURCE_DIR}/lua/${_file}
+			DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/lua/${_file} ${ADDITIONAL_DEPENDENCIES}
+			COMMENT "Validate ${_file}"
+		)
+		add_custom_target(${TARGET}-${_outfile} DEPENDS ${_outfile})
+		add_dependencies(${TARGET} ${TARGET}-${_outfile})
+	endforeach()
 endmacro()
