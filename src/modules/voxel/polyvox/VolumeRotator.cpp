b526ed17c06b7599a9dc1427da01f92a6259e59c
Martin Gerhardy <martin.gerhardy@gmail.com>
2019-05-01 22:44:06 +0200

    VOXEL: apply small epsilon to rotation mins/maxs

--- src/modules/voxel/polyvox/VolumeRotator.cpp
+++ src/modules/voxel/polyvox/VolumeRotator.cpp
@@ -38,8 +38,9 @@ RawVolume* rotateVolume(const RawVolume* source, const glm::vec3& angles, const 
 	if (increaseSize) {
 		const glm::vec3 rotated1 = glm::rotate(rot, srcRegion.getLowerCornerf() - rotateAround);
 		const glm::vec3 rotated2 = glm::rotate(rot, srcRegion.getUpperCornerf() - rotateAround);
-		const glm::vec3 minsf = glm::min(rotated1, rotated2) + rotateAround + 0.5f;
-		const glm::vec3 maxsf = glm::max(rotated1, rotated2) + rotateAround + 0.5f;
+		const float epsilon = 0.00001f;
+		const glm::vec3 minsf = glm::min(rotated1, rotated2) + rotateAround + epsilon;
+		const glm::vec3 maxsf = glm::max(rotated1, rotated2) + rotateAround + epsilon;
 		destRegion = voxel::Region(glm::ivec3(minsf), glm::ivec3(maxsf));
 	} else {
 		destRegion = srcRegion;
