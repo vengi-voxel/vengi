fab041aedd999f29b350644925e1dc04671c5890
Martin Gerhardy <martin.gerhardy@gmail.com>
2019-05-14 18:59:59 +0200

    VOXEDIT-UTIL: extended LayerManager tests

--- src/tools/voxedit/voxedit-util/tests/LayerManagerTest.cpp
+++ src/tools/voxedit/voxedit-util/tests/LayerManagerTest.cpp
@@ -35,6 +35,16 @@ public:
 		_volumes.push_back(v);
 		return _mgr.addLayer(name, visible, v, region.getCentre());
 	}
+
+	int addLayers(int n) {
+		int cnt = 0;
+		for (int i = 0; i < n; ++i) {
+			if (addLayer("unnamed") >= 0) {
+				++cnt;
+			}
+		}
+		return cnt;
+	}
 };
 
 TEST_F(LayerManagerTest, testValidLayersEmpty) {
@@ -48,8 +58,7 @@ TEST_F(LayerManagerTest, testValidLayersAfterAdd) {
 }
 
 TEST_F(LayerManagerTest, testDeleteLayer) {
-	EXPECT_EQ(0, addLayer("Foobar")) << "Failed to add new layer";
-	EXPECT_EQ(1, addLayer("Foobar2")) << "Failed to add new layer";
+	EXPECT_EQ(2, addLayers(2));
 	EXPECT_EQ(2, _mgr.validLayers()) << "Unexpected amount of valid layers";
 	EXPECT_TRUE(_mgr.deleteLayer(0)) << "Deleting the first layer should work";
 	EXPECT_EQ(1, _mgr.validLayers()) << "Unexpected amount of valid layers";
@@ -62,10 +71,7 @@ TEST_F(LayerManagerTest, testDeleteLastRemainingLayer) {
 }
 
 TEST_F(LayerManagerTest, testMoveAfterDelete) {
-	EXPECT_EQ(0, addLayer("Foobar")) << "Failed to add new layer";
-	EXPECT_EQ(1, addLayer("Foobar2")) << "Failed to add new layer";
-	EXPECT_EQ(2, addLayer("Foobar3")) << "Failed to add new layer";
-	EXPECT_EQ(3, addLayer("Foobar4")) << "Failed to add new layer";
+	EXPECT_EQ(4, addLayers(4));
 
 	EXPECT_EQ(4, _mgr.validLayers()) << "Unexpected amount of valid layers";
 	EXPECT_TRUE(_mgr.deleteLayer(1)) << "Deleting the second layer should work";
@@ -75,6 +81,30 @@ TEST_F(LayerManagerTest, testMoveAfterDelete) {
 	EXPECT_TRUE(_mgr.layer(2).valid) << "The third (new) layer should still be valid";
 	EXPECT_TRUE(_mgr.layer(3).valid) << "The last (untouched) layer should still be valid";
 	EXPECT_EQ(3, _mgr.validLayers()) << "Unexpected amount of valid layers";
+}
+
+TEST_F(LayerManagerTest, testLock) {
+	EXPECT_EQ(2, addLayers(2));
+
+	_mgr.lockLayer(0, true);
+	EXPECT_TRUE(_mgr.isLocked(0)) << "First layer should be locked";
+	EXPECT_FALSE(_mgr.isLocked(1)) << "Second layer should not be locked";
+}
+
+TEST_F(LayerManagerTest, testLockGroupVisit) {
+	EXPECT_EQ(4, addLayers(4));
+
+	for (int i = 0; i < _mgr.validLayers(); ++i) {
+		_mgr.lockLayer(i, true);
+	}
+
+	EXPECT_TRUE(_mgr.deleteLayer(1)) << "Deleting the second layer should work";
+
+	int cnt = 0;
+	_mgr.foreachGroupLayer([&] (int layerId) {
+		++cnt;
+	});
+	EXPECT_EQ(_mgr.validLayers(), cnt) << "Not all lock-group layers were visited";
 }
 
 }
