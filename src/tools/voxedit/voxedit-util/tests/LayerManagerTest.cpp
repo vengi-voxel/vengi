79a2795e5aedb2622955e01b59e52b2907e0801b
Martin Gerhardy <martin.gerhardy@gmail.com>
2019-05-14 18:26:24 +0200

    VOXEDIT-UTIL: exteded tests

--- src/tools/voxedit/voxedit-util/tests/LayerManagerTest.cpp
+++ src/tools/voxedit/voxedit-util/tests/LayerManagerTest.cpp
@@ -31,10 +31,30 @@ TEST_F(LayerManagerTest, testValidLayersEmpty) {
 }
 
 TEST_F(LayerManagerTest, testValidLayersAfterAdd) {
-	ASSERT_EQ(0, _mgr.validLayers());
+	EXPECT_EQ(0, _mgr.validLayers());
 	voxel::RawVolume * v = new voxel::RawVolume(voxel::Region(0, 0));
-	ASSERT_EQ(0, _mgr.addLayer("Foobar", true, v)) << "Failed to add new layer";
-	ASSERT_EQ(1, _mgr.validLayers());
+	EXPECT_EQ(0, _mgr.addLayer("Foobar", true, v)) << "Failed to add new layer";
+	EXPECT_EQ(1, _mgr.validLayers());
+	delete v;
+}
+
+TEST_F(LayerManagerTest, testDeleteLayer) {
+	voxel::RawVolume * v = new voxel::RawVolume(voxel::Region(0, 0));
+	EXPECT_EQ(0, _mgr.addLayer("Foobar", true, v)) << "Failed to add new layer";
+	voxel::RawVolume * v2 = new voxel::RawVolume(voxel::Region(0, 0));
+	EXPECT_EQ(1, _mgr.addLayer("Foobar2", true, v2)) << "Failed to add new layer";
+	EXPECT_EQ(2, _mgr.validLayers());
+	EXPECT_TRUE(_mgr.deleteLayer(0)) << "Deleting the first layer should work";
+	EXPECT_EQ(1, _mgr.validLayers());
+	delete v;
+	delete v2;
+}
+
+TEST_F(LayerManagerTest, testDeleteLastRemainingLayer) {
+	voxel::RawVolume * v = new voxel::RawVolume(voxel::Region(0, 0));
+	EXPECT_EQ(0, _mgr.addLayer("Foobar", true, v)) << "Failed to add new layer";
+	EXPECT_FALSE(_mgr.deleteLayer(0)) << "Deleting the last valid layer should not be supported";
+	EXPECT_EQ(1, _mgr.validLayers());
 	delete v;
 }
 
