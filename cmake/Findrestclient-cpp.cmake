80f739886aa9296ec98f37bb2ea25dae94cd13b0
Martin Gerhardy <martin.gerhardy@gmail.com>
2019-06-05 21:17:24 +0200

    LIBS: search for restclient.h
    osx has another connection.h that is found

--- cmake/Findrestclient-cpp.cmake
+++ cmake/Findrestclient-cpp.cmake
@@ -1,2 +1,2 @@
 include("${PROJECT_SOURCE_DIR}/cmake/macros.cmake")
-engine_find_header_only(restclient-cpp connection.h restclient-cpp "" "")
+engine_find_header_only(restclient-cpp restclient.h restclient-cpp "" "")
