8ae69cd54618b59fa16c7fc801b8cb35961cb537
Martin Gerhardy <martin.gerhardy@gmail.com>
2019-06-23 22:24:44 +0200

    TESTTRAZE: use engine_find macro to fix mosquitto detection on brew

--- cmake/FindMosquitto.cmake
+++ cmake/FindMosquitto.cmake
@@ -1,16 +1,2 @@
-if (NOT MOSQUITTO_INCLUDE_DIR)
-	find_path(MOSQUITTO_INCLUDE_DIR mosquitto.h)
-endif()
-
-if (NOT MOSQUITTO_LIBRARY)
-	find_library(MOSQUITTO_LIBRARY NAMES mosquitto)
-endif()
-
-include(FindPackageHandleStandardArgs)
-
-find_package_handle_standard_args(MOSQUITTO DEFAULT_MSG MOSQUITTO_LIBRARY MOSQUITTO_INCLUDE_DIR)
-
-message(STATUS "mosquitto library: ${MOSQUITTO_LIBRARY}")
-message(STATUS "mosquitto include dir: ${MOSQUITTO_INCLUDE_DIR}")
-set(MOSQUITTO_LIBRARIES ${MOSQUITTO_LIBRARY})
-mark_as_advanced(MOSQUITTO_INCLUDE_DIR MOSQUITTO_LIBRARY)
+include("${ROOT_DIR}/cmake/macros.cmake")
+engine_find(mosquitto mosquitto.h "" "" "")
