376e739a60ebb712f0f9def0c8f04a8fd5aa23a7
Martin Gerhardy <martin.gerhardy@gmail.com>
2019-08-06 19:02:44 +0200

    VIDEO: fixed driver hint typo

--- src/modules/video/DriverHints.cpp
+++ src/modules/video/DriverHints.cpp
@@ -14,7 +14,7 @@
 
 extern "C" {
 	// https://community.amd.com/external-link.jspa?url=http%3A%2F%2Fdeveloper.download.nvidia.com%2Fdevzone%2Fdevcenter%2Fgamegraphics%2Ffiles%2FOptimusRenderingPolicies.pdf
-	EXPORT_SYMBOL int32_t ptimusEnablement = 1;
+	EXPORT_SYMBOL int32_t NvOptimusEnablement = 1;
 	// https://community.amd.com/thread/169965
 	EXPORT_SYMBOL int32_t AmdPowerXpressRequestHighPerformance = 1;
 }
