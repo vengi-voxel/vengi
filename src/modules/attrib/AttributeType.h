9818c1b0f9ba20ead76a52e1c1512efc947ec880
Martin Gerhardy <martin.gerhardy@gmail.com>
2019-09-26 18:09:37 +0200

    ATTRIB: prevent name clash

--- src/modules/attrib/AttributeType.h
+++ src/modules/attrib/AttributeType.h
@@ -13,14 +13,14 @@ namespace attrib {
  * @brief The type of each attribute is directly mapped to a protocol enum here.
  * @ingroup Attributes
  */
-using Type = network::AttribType;
+using Type = ::network::AttribType;
 
 /**
  * @brief Converts a string into the enum value
  * @ingroup Attributes
  */
 inline Type getType(const char* name) {
-	return network::getEnum<Type>(name, network::EnumNamesAttribType());
+	return ::network::getEnum<Type>(name, ::network::EnumNamesAttribType());
 }
 
 /**
