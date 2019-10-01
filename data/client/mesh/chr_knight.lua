f465c370f65fca75cc731a7f3ec51e66dbb55394
Martin Gerhardy <martin.gerhardy@gmail.com>
2019-10-01 20:37:06 +0200

    DATA: cleanup files

--- data/client/mesh/chr_knight.lua
+++
@@ -1,7 +0,0 @@
-function init()
-  mesh.setScale(vec3.new(0.4))
-  -- mesh.addSkin('chr_knight.png')
-  mesh.addAnimation('walk',  93.8, 125.0,  22)
-  mesh.addAnimation('run',   93.8, 125.0,  22)
-  mesh.addAnimation('jump',  25.0,  81.0,  22)
-end
