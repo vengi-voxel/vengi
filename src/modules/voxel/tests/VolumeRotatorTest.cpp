eb8288abc98b36d84f8cd4c8993f1da1a79aeca8
Martin Gerhardy <martin.gerhardy@gmail.com>
2019-10-21 22:55:38 +0200

    VOXEL: fixed (and cleanup) VolumeRotatorTest

--- src/modules/voxel/tests/VolumeRotatorTest.cpp
+++ src/modules/voxel/tests/VolumeRotatorTest.cpp
@@ -8,6 +8,7 @@
 #include "voxel/polyvox/Region.h"
 #include "voxel/polyvox/RawVolume.h"
 #include "voxel/polyvox/Voxel.h"
+#include "math/Axis.h"
 
 namespace voxel {
 
@@ -15,27 +16,6 @@ class VolumeRotatorTest: public core::AbstractTest {
 protected:
 	inline std::string str(const voxel::Region& region) const {
 		return "mins(" + glm::to_string(region.getLowerCorner()) + "), maxs(" + glm::to_string(region.getUpperCorner()) + ")";
-	}
-
-	void rotateY90Steps(int degree) {
-		const voxel::Region region(-2, 8);
-		voxel::RawVolume smallVolume(region);
-		glm::ivec3 pos = region.getCentre();
-		EXPECT_TRUE(smallVolume.setVoxel(pos.x, pos.y++, pos.z, createVoxel(voxel::VoxelType::Rock, 0)));
-		EXPECT_TRUE(smallVolume.setVoxel(pos.x, pos.y++, pos.z, createVoxel(voxel::VoxelType::Grass, 0)));
-		EXPECT_TRUE(smallVolume.setVoxel(pos.x, pos.y++, pos.z, createVoxel(voxel::VoxelType::Sand, 0)));
-
-		voxel::RawVolume* rotated = voxel::rotateVolume(&smallVolume, glm::ivec3(0, degree, 0), voxel::Voxel(), region.getCentref());
-		ASSERT_NE(nullptr, rotated) << "No new volume was returned for the desired rotation";
-		const voxel::Region& rotatedRegion = rotated->region();
-		ASSERT_EQ(rotatedRegion, region) << "Rotating by " << degree << " degree should not increase the size of the volume "
-				<< str(rotatedRegion) << " " << str(region);
-		glm::ivec3 rotPos = rotatedRegion.getCentre();
-		SCOPED_TRACE(std::to_string(degree) + " degree rotation: " + str(rotatedRegion) + " " + str(region));
-		EXPECT_EQ(voxel::VoxelType::Rock, rotated->voxel(rotPos.x, rotPos.y++, rotPos.z).getMaterial());
-		EXPECT_EQ(voxel::VoxelType::Grass, rotated->voxel(rotPos.x, rotPos.y++, rotPos.z).getMaterial());
-		EXPECT_EQ(voxel::VoxelType::Sand, rotated->voxel(rotPos.x, rotPos.y++, rotPos.z).getMaterial());
-		delete rotated;
 	}
 };
 
@@ -50,17 +30,7 @@ TEST_F(VolumeRotatorTest, testRotateAxisY) {
 	EXPECT_EQ(voxel::VoxelType::Rock, rotated->voxel(0, 0, 0).getMaterial());
 	EXPECT_EQ(voxel::VoxelType::Grass, rotated->voxel(0, 1, 0).getMaterial());
 	EXPECT_EQ(voxel::VoxelType::Dirt, rotated->voxel(0, 0, 1).getMaterial());
-	ASSERT_EQ(nullptr, rotated) << "Rotated: " << *rotated << "Original: " << smallVolume;
 	delete rotated;
-}
-
-TEST_F(VolumeRotatorTest, testRotateY90Steps) {
-	rotateY90Steps(0);
-	rotateY90Steps(90);
-	rotateY90Steps(180);
-	rotateY90Steps(270);
-	rotateY90Steps(360);
-	rotateY90Steps(720);
 }
 
 TEST_F(VolumeRotatorTest, testRotate45Y) {
