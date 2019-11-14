e8e41365526c837342c3e5e229681a33bc99bbc6
Martin Gerhardy <martin.gerhardy@gmail.com>
2019-11-14 07:32:59 +0100

    CORE: changed FileTest to fix the new path() behaviour expectation

--- src/modules/core/tests/FileTest.cpp
+++ src/modules/core/tests/FileTest.cpp
@@ -10,7 +10,7 @@ TEST_F(FileTest, testGetPath) {
 	io::Filesystem fs;
 	fs.init("test", "test");
 	const io::FilePtr& file = fs.open("foobar/1.txt", io::FileMode::Read);
-	ASSERT_TRUE(core::string::endsWith(file->path(), "foobar"));
+	ASSERT_TRUE(core::string::endsWith(file->path(), "foobar/"));
 	ASSERT_EQ("txt", file->extension());
 	ASSERT_EQ("1", file->fileName());
 	ASSERT_TRUE(core::string::endsWith(file->name(), "foobar/1.txt"));
