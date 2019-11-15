abb6a3eb734ee675b31ffc160c135beddd7c5fc5
Martin Gerhardy <martin.gerhardy@gmail.com>
2019-11-15 13:32:34 +0100

    BACKEND: doxygen about the disconnect/logout cooldown

--- src/modules/backend/network/UserDisconnectHandler.h
+++ src/modules/backend/network/UserDisconnectHandler.h
@@ -9,6 +9,12 @@
 
 namespace backend {
 
+/**
+ * A client is not directly disconnected if the game is closed. This would otherwise always lead to
+ * a situation where you could escape a fight. Thus we trigger a cooldown and abort it once the player
+ * was hit or got interacted with. If nothing happens for that player for quite some time, the cooldown
+ * is triggered again...
+ */
 USERPROTOHANDLERIMPL(UserDisconnect) {
 	user->logoutMgr().triggerLogout();
 }
