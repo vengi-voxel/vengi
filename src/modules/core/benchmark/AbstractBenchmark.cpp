2bba12cfcfba172c5299dee44d2829a50cac8cd6
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-01-06 21:47:13 +0100

    CORE: change log level to warn for benchmarks

--- src/modules/core/benchmark/AbstractBenchmark.cpp
+++ src/modules/core/benchmark/AbstractBenchmark.cpp
@@ -33,6 +33,7 @@ void AbstractBenchmark::TearDown(benchmark::State& st) {
 AbstractBenchmark::BenchmarkApp::BenchmarkApp(const metric::MetricPtr& metric, const io::FilesystemPtr& filesystem, const core::EventBusPtr& eventBus, const core::TimeProviderPtr& timeProvider, AbstractBenchmark* benchmark) :
 		Super(metric, filesystem, eventBus, timeProvider), _benchmark(benchmark) {
 	init(ORGANISATION, "benchmark");
+	_initialLogLevel = SDL_LOG_PRIORITY_WARN;
 	while (_curState < AppState::Running) {
 		core_trace_scoped(AppMainLoop);
 		onFrame();
