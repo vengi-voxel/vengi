37b460d2a30457d71324c75f4fbb7e9a7a7202e6
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-01-16 20:26:44 +0100

    CORE: added CharPointerMap type

--- src/modules/core/collection/Map.h
+++ src/modules/core/collection/Map.h
@@ -8,6 +8,7 @@
 #include "core/PoolAllocator.h"
 #include <stddef.h>
 #include <initializer_list>
+#include <SDL_stdinc.h>
 
 namespace core {
 
@@ -245,5 +246,24 @@ public:
 		return true;
 	}
 };
+
+struct hashCharPtr {
+	size_t operator()(const char *p) const {
+		size_t result = 0;
+		const size_t prime = 31;
+		const size_t s = SDL_strlen(p);
+		for (size_t i = 0; i < s; ++i) {
+			result = SDL_tolower(p[i]) + (result * prime);
+		}
+		return result;
+	}
+};
+struct hashCharCompare {
+	inline bool operator()(const char *lhs, const char *rhs) const {
+		return SDL_strcasecmp(lhs, rhs) == 0;
+	}
+};
+
+using CharPointerMap = core::Map<const char*, const char*, 8, hashCharPtr, hashCharCompare>;
 
 }
