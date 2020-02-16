fca59cd6b72fff83ab94ac6532d5eee128b4786e
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-02-16 21:56:36 +0100

    VOXELFORMAT: fixed whitespaces

--- src/modules/voxelformat/VoxelVolumes.cpp
+++ src/modules/voxelformat/VoxelVolumes.cpp
@@ -4,46 +4,46 @@
 
 #include "VoxelVolumes.h"
 #include "voxelutil/VolumeMerger.h"
-#include <vector>
 #include <utility>
+#include <vector>
 
 namespace voxel {
 
-VoxelVolume::VoxelVolume(voxel::RawVolume* _volume, const core::String& _name, bool _visible) :
-        volume(_volume), name(_name), visible(_visible) {
-    if (volume != nullptr) {
-        pivot = volume->region().getCentre();
-    } else {
-        pivot = glm::ivec3(0.0f);
-    }
+VoxelVolume::VoxelVolume(voxel::RawVolume *_volume, const core::String &_name, bool _visible)
+	: volume(_volume), name(_name), visible(_visible) {
+	if (volume != nullptr) {
+		pivot = volume->region().getCentre();
+	} else {
+		pivot = glm::ivec3(0.0f);
+	}
 }
-VoxelVolume::VoxelVolume(voxel::RawVolume* _volume, const core::String& _name, bool _visible, const glm::ivec3& _pivot) :
-        volume(_volume), name(_name), visible(_visible), pivot(_pivot) {
+VoxelVolume::VoxelVolume(voxel::RawVolume *_volume, const core::String &_name, bool _visible, const glm::ivec3 &_pivot)
+	: volume(_volume), name(_name), visible(_visible), pivot(_pivot) {
 }
 
 VoxelVolumes::~VoxelVolumes() {
-    volumes.clear();
+	volumes.clear();
 }
 
-voxel::RawVolume* VoxelVolumes::merge() const {
-    if (volumes.empty()) {
-        return nullptr;
-    }
-    if (volumes.size() == 1) {
-        return new RawVolume(volumes[0].volume);
-    }
-    std::vector<const RawVolume*> rawVolumes;
-    rawVolumes.reserve(volumes.size());
-    for (const auto& v : volumes) {
-        if (v.volume == nullptr) {
-            continue;
-        }
-        rawVolumes.push_back(v.volume);
-    }
-    if (rawVolumes.empty()) {
-        return nullptr;
-    }
-    return ::voxel::merge(rawVolumes);
+voxel::RawVolume *VoxelVolumes::merge() const {
+	if (volumes.empty()) {
+		return nullptr;
+	}
+	if (volumes.size() == 1) {
+		return new RawVolume(volumes[0].volume);
+	}
+	std::vector<const RawVolume *> rawVolumes;
+	rawVolumes.reserve(volumes.size());
+	for (const auto &v : volumes) {
+		if (v.volume == nullptr) {
+			continue;
+		}
+		rawVolumes.push_back(v.volume);
+	}
+	if (rawVolumes.empty()) {
+		return nullptr;
+	}
+	return ::voxel::merge(rawVolumes);
 }
 
 }
