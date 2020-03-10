0a685c55b70542bfce6114c6dde9a768a0a44cf5
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-03-10 22:35:26 +0100

    CLIENT: debug logging

--- src/client/network/AttribUpdateHandler.h
+++ src/client/network/AttribUpdateHandler.h
@@ -11,13 +11,17 @@ CLIENTPROTOHANDLERIMPL(AttribUpdate) {
 	const flatbuffers::Vector<flatbuffers::Offset<AttribEntry>> *attribs = message->attribs();
 	const frontend::ClientEntityPtr& entity = client->getEntity(id);
 	if (!entity) {
+		Log::error("Got attribute update for unknown entity");
 		return;
 	}
 	attrib::ShadowAttributes& attrib = entity->attrib();
 	for (const AttribEntry* e : *attribs) {
+		const char *typeStr = network::toString(e->type(), ::network::EnumNamesAttribType());
 		if (e->current()) {
+			Log::debug("Set current for %s to %f", typeStr, e->value());
 			attrib.setCurrent(e->type(), e->value());
 		} else {
+			Log::debug("Set max for %s to %f", typeStr, e->value());
 			attrib.setMax(e->type(), e->value());
 		}
 	}
