bf52daca904c0dee0eb6aff7cdbf471a6d6e31e4
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-03-26 00:31:03 +0100

    VOXEL: removed unused QuadNeeded functor

--- src/modules/voxel/IsQuadNeeded.h
+++ src/modules/voxel/IsQuadNeeded.h
@@ -21,18 +21,4 @@ struct IsQuadNeeded {
 	}
 };
 
-/**
- * @brief The criteria used here are that the voxel in front of the potential
- * quad should have a Air voxel while the voxel behind the potential quad
- * would have a voxel that is not Air.
- */
-struct IsWaterQuadNeeded {
-	inline bool operator()(const VoxelType& back, const VoxelType& front, FaceNames face) const {
-		if (!isBlocked(front) && isWater(back)) {
-			return true;
-		}
-		return false;
-	}
-};
-
 }
