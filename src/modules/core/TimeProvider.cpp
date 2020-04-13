f7239fb2b859c0f91420cafc7731cdf1820d6db1
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-04-14 01:07:30 +0200

    CORE: only use SDL_GetPerformanceCounter for milli and nanoseconds

--- src/modules/core/TimeProvider.cpp
+++ src/modules/core/TimeProvider.cpp
@@ -3,50 +3,11 @@
  */
 
 #include "TimeProvider.h"
-#include "core/Assert.h"
-#include <chrono>
 #include <sstream>
 #include <iostream>
 #include <iomanip>
-#include <time.h>
 #include <inttypes.h>
 #include <SDL.h>
-
-#ifdef __WINDOWS__
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#include <stdint.h> // portable: uint64_t   MSVC: __int64
-
-#ifndef _WINSOCK2API_
-// MSVC defines this in winsock2.h!?
-typedef struct timeval {
-	long tv_sec;
-	long tv_usec;
-} timeval;
-#endif
-
-int gettimeofday(struct timeval * tp, struct timezone * tzp) {
-	// Note: some broken versions only have 8 trailing zero's, the correct epoch has 9 trailing zero's
-	// This magic number is the number of 100 nanosecond intervals since January 1, 1601 (UTC)
-	// until 00:00:00 January 1, 1970
-	static const uint64_t EPOCH = ((uint64_t) 116444736000000000ULL);
-
-	SYSTEMTIME system_time;
-	FILETIME file_time;
-	uint64_t time;
-
-	GetSystemTime(&system_time);
-	SystemTimeToFileTime(&system_time, &file_time);
-	time = ((uint64_t)file_time.dwLowDateTime);
-	time += ((uint64_t)file_time.dwHighDateTime) << 32;
-
-	tp->tv_sec = (long) ((time - EPOCH) / 10000000L);
-	tp->tv_usec = (long) (system_time.wMilliseconds * 1000);
-	return 0;
-}
-#else
-#include <sys/time.h>
-#endif
 
 namespace core {
 
@@ -55,23 +16,7 @@ TimeProvider::TimeProvider() :
 }
 
 uint64_t TimeProvider::systemMillis() const {
-#if 1
-	struct timeval tp;
-	gettimeofday(&tp, nullptr);
-	uint64_t timestamp = tp.tv_sec * 1000 + tp.tv_usec / 1000;
-#else
-	const uint64_t timestamp = SDL_GetPerformanceCounter() / 1000000UL;
-#endif
-#ifdef DEBUG
-	static uint64_t last = 0;
-#endif
-#ifdef DEBUG
-	core_assert_msg(timestamp >= last,
-			"timestamp:%" PRIu64 ", last: %" PRIu64,
-			timestamp, last);
-	last = timestamp;
-#endif
-	return timestamp;
+	return systemNanos() / 1000000;
 }
 
 uint64_t TimeProvider::systemNanos() {
