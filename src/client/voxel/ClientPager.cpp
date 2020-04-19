37ca2f0a7c4eb75df9f94ebefe5d6fd61223723a
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-04-19 18:51:56 +0200

    CLIENT: added logging to ClientPager

--- src/client/voxel/ClientPager.cpp
+++ src/client/voxel/ClientPager.cpp
@@ -44,7 +44,8 @@ bool ClientPager::pageIn(voxel::PagedVolume::PagerContext& pctx) {
 		const int z = pctx.region.getLowerZ();
 		const http::ResponseParser& response = _httpClient.get("?x=%i&y=%i&z=%i&mapid=%i", x, y, z, _mapId);
 		if (response.status != http::HttpStatus::Ok) {
-			Log::error("Failed to download the chunk for position %i:%i:%i and seed %u on map %i", x, y, z, _seed, _mapId);
+			Log::error("Failed to download the chunk for position %i:%i:%i and seed %u on map %i",
+					x, y, z, _seed, _mapId);
 			if (response.isHeaderValue(http::header::CONTENT_TYPE, http::mimetype::TEXT_PLAIN)) {
 				const core::String s(response.content, response.contentLength);
 				Log::error("%s", s.c_str());
@@ -55,24 +56,32 @@ bool ClientPager::pageIn(voxel::PagedVolume::PagerContext& pctx) {
 		const char* data = response.content;
 		const char *contentType;
 		if (!response.headers.get(http::header::CONTENT_TYPE, contentType)) {
-			Log::error("No content type set in response");
+			Log::error("No content type set in chunk response for position %i:%i:%i and seed %u on map %i",
+					x, y, z, _seed, _mapId);
 			return false;
 		}
 		if (SDL_strcmp(contentType, "application/chunk")) {
-			Log::error("Unexpected content type: %s", contentType);
+			Log::error("Unexpected content type: %s for chunk at position %i:%i:%i and seed %u on map %i",
+					contentType, x, y, z, _seed, _mapId);
 			return false;
 		}
 		if (!_chunkPersister.loadCompressed(pctx.chunk.get(), (const uint8_t*)data, length)) {
-			Log::error("Failed to uncompress the chunk for position %i:%i:%i and seed %u", x, y, z, _seed);
+			Log::error("Failed to uncompress the chunk for position %i:%i:%i and seed %u on map %i",
+					x, y, z, _seed, _mapId);
 			return false;
 		}
 		if (!_chunkPersister.save(pctx.chunk.get(), _seed)) {
-			Log::error("Failed to save the downloaded chunk");
+			Log::error("Failed to save the downloaded chunk for position %i:%i:%i and seed %u on map %i",
+					x, y, z, _seed, _mapId);
 			return false;
 		}
 	}
 	if (!_chunkPersister.load(pctx.chunk.get(), _seed)) {
-		Log::error("Failed to load the world");
+		const int x = pctx.region.getLowerX();
+		const int y = pctx.region.getLowerY();
+		const int z = pctx.region.getLowerZ();
+		Log::error("Failed to load the world for position %i:%i:%i and seed %u on map %i",
+				x, y, z, _seed, _mapId);
 	}
 	return false;
 }
