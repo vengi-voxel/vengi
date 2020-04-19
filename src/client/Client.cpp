22bb7bd3a860315f9215f77258816a78ac7fcd11
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-04-19 21:32:05 +0200

    CLIENT: extracted lua code into own header

--- src/client/Client.cpp
+++ src/client/Client.cpp
@@ -29,7 +29,7 @@
 #include "core/metric/Metric.h"
 #include "core/TimeProvider.h"
 #include "core/SharedPtr.h"
-#include "commonlua/LUAFunctions.h"
+#include "ClientLUA.h"
 #include <SDL.h>
 #include <engine-config.h>
 
@@ -235,32 +235,6 @@ core::AppState Client::onInit() {
 	rootWindow("main");
 
 	return state;
-}
-
-static inline Client* clientlua_ctx(lua_State* s) {
-	Client* client = lua::LUA::globalData<Client>(s, "clientpointer");
-	core_assert(client != nullptr);
-	return client;
-}
-
-static int clientlua_disconnect(lua_State* s) {
-	Client* client = clientlua_ctx(s);
-	client->disconnect();
-	return 0;
-}
-
-static int clientlua_connect(lua_State* s) {
-	Client* client = clientlua_ctx(s);
-
-	clua_assert_argc(s, lua_gettop(s) == 2);
-	const int port = luaL_checkinteger(s, 1);
-	const char* host = luaL_checkstring(s, 2);
-	if (client->connect(port, host)) {
-		lua_pushboolean(s, 1);
-	} else {
-		lua_pushboolean(s, 0);
-	}
-	return 1;
 }
 
 void Client::configureLUA(lua::LUA& lua) {
