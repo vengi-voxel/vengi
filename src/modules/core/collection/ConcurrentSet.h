ba8c0daebef69d515a045cc0928c71c07b11dea3
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-04-19 22:41:38 +0200

    CORE: use own mutex and conditionvar structures

--- src/modules/core/collection/ConcurrentSet.h
+++ src/modules/core/collection/ConcurrentSet.h
@@ -4,13 +4,12 @@
 
 #pragma once
 
+#include "core/concurrent/Lock.h"
+#include "core/concurrent/ConditionVariable.h"
+#include "core/Common.h"
 #include <stdint.h>
 #include <unordered_set>
-#include <condition_variable>
-#include <mutex>
-#include <atomic>
 #include <algorithm>
-#include "core/Trace.h"
 
 namespace collection {
 
@@ -20,57 +19,57 @@ public:
 	using underlying_type = std::unordered_set<Data>;
 private:
 	underlying_type _data;
-	mutable core_trace_mutex(std::mutex, _mutex);
-	std::condition_variable_any _conditionVariable;
+	mutable core::Lock _mutex;
+	core::ConditionVariable _conditionVariable;
 public:
 	void swap(underlying_type& target) {
-		std::unique_lock lock(_mutex);
+		core::ScopedLock lock(_mutex);
 		std::swap(_data, target);
 	}
 
 	void clear() {
-		std::unique_lock lock(_mutex);
+		core::ScopedLock lock(_mutex);
 		_data.clear();
 	}
 
 	bool insert(Data const& data) {
 		bool result;
 		{
-			std::unique_lock lock(_mutex);
+			core::ScopedLock lock(_mutex);
 			result = _data.insert(data).second;
 		}
-		_conditionVariable.notify_one();
+		_conditionVariable.signalOne();
 		return result;
 	}
 
 	bool insert(Data&& data) {
 		bool result;
 		{
-			std::unique_lock lock(_mutex);
+			core::ScopedLock lock(_mutex);
 			result = _data.insert(data).second;
 		}
-		_conditionVariable.notify_one();
+		_conditionVariable.signalOne();
 		return result;
 	}
 
 	bool contains(Data const& data) const {
-		std::unique_lock lock(_mutex);
+		core::ScopedLock lock(_mutex);
 		return _data.find(data) != _data.end();
 	}
 
 	inline bool empty() const {
-		std::unique_lock lock(_mutex);
+		core::ScopedLock lock(_mutex);
 		return _data.empty();
 	}
 
 	inline uint32_t size() const {
-		std::unique_lock lock(_mutex);
+		core::ScopedLock lock(_mutex);
 		return _data.size();
 	}
 
 	template<class VISITOR>
 	void visit(VISITOR&& visitor) const {
-		std::unique_lock lock(_mutex);
+		core::ScopedLock lock(_mutex);
 		for (const Data& d : _data) {
 			visitor(d);
 		}
