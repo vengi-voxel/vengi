vengi-voxel/vengi

commit c38a89aab96c2ed7170598f2f29fb9f49dfa0716

MATH: reduced header size

src/modules/render/Skybox.h
@@ -9,6 +9,10 @@
 #include "video/Camera.h"
 #include "SkyboxShader.h"
 
+namespace video {
+class Video;
+}
+
 namespace render {
 
 class Skybox {

src/modules/math/AABB.h
@@ -5,7 +5,6 @@
 #pragma once
 
 #include <glm/vec3.hpp>
-#include <glm/common.hpp>
 #include <limits>
 
 namespace math {
@@ -367,12 +366,12 @@ inline void AABB<TYPE>::setUpperCorner(const glm::tvec3<TYPE>& v3dUpperCorner) {
  */
 template<typename TYPE>
 inline void AABB<TYPE>::accumulate(TYPE iX, TYPE iY, TYPE iZ) {
-	_mins.x = (glm::min)(_mins.x, iX);
-	_mins.y = (glm::min)(_mins.y, iY);
-	_mins.z = (glm::min)(_mins.z, iZ);
-	_maxs.x = (glm::max)(_maxs.x, iX);
-	_maxs.y = (glm::max)(_maxs.y, iY);
-	_maxs.z = (glm::max)(_maxs.z, iZ);
+	_mins.x = core_min(_mins.x, iX);
+	_mins.y = core_min(_mins.y, iY);
+	_mins.z = core_min(_mins.z, iZ);
+	_maxs.x = core_max(_maxs.x, iX);
+	_maxs.y = core_max(_maxs.y, iY);
+	_maxs.z = core_max(_maxs.z, iZ);
 }
 
 /**

src/modules/render/Axis.cpp
@@ -3,6 +3,7 @@
  */
 
 #include "Axis.h"
+#include "video/Camera.h"
 #include "video/ScopedLineWidth.h"
 #include "video/ScopedState.h"
 #define GLM_ENABLE_EXPERIMENTAL

src/modules/render/Axis.h
@@ -4,11 +4,14 @@
 
 #pragma once
 
-#include "video/Camera.h"
 #include "video/ShapeBuilder.h"
 #include "render/ShapeRenderer.h"
 #include "core/IComponent.h"
 #include "math/Axis.h"
+
+namespace video {
+class Camera;
+}
 
 namespace render {
 
@@ -24,8 +27,8 @@ private:
 	render::ShapeRenderer _shapeRenderer;
 	int32_t _meshIndex = -1;
 	float _lineWidth = 4.0f;
-	glm::vec3 _pos = glm::zero<glm::vec3>();
-	glm::vec3 _size = glm::one<glm::vec3>();
+	glm::vec3 _pos { 0.0f };
+	glm::vec3 _size { 1.0f };
 	const bool _flipZ;
 
 public:

src/modules/render/CameraFrustum.cpp
@@ -5,6 +5,7 @@
 #include "CameraFrustum.h"
 #include "video/Renderer.h"
 #include "math/AABB.h"
+#include "video/Camera.h"
 
 namespace render {
 

src/modules/render/CameraFrustum.h
@@ -7,8 +7,11 @@
 #include "render/ShapeRenderer.h"
 #include "core/Common.h"
 #include "core/Color.h"
-#include "video/Camera.h"
 #include "video/ShapeBuilder.h"
+
+namespace video {
+class Video;
+}
 
 namespace render {
 

src/modules/render/Gizmo.cpp
@@ -5,6 +5,7 @@
 #include "Gizmo.h"
 #include "core/GLM.h"
 #include "video/Ray.h"
+#include "video/Camera.h"
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/intersect.hpp>
 #include <glm/gtx/norm.hpp>

src/modules/render/Gizmo.h
@@ -8,6 +8,10 @@
 #include "core/command/ActionButton.h"
 #include "Axis.h"
 
+namespace video {
+class Video;
+}
+
 namespace render {
 
 enum class GizmoMode {
@@ -35,7 +39,7 @@ public:
 private:
 	render::Axis _axis;
 	GizmoMode _mode = GizmoMode::None;
-	glm::vec3 _pos = glm::zero<glm::vec3>();
+	glm::vec3 _pos { 0.0f };
 	bool _modelSpace = true;
 
 public:

commit a164a039a9f4402620ff4e9b84e58f2d881bef91

RENDER: fixed forward decl error
introduced in c38a89aab96c2ed7170598f2f29fb9f49dfa0716

src/modules/render/Skybox.h
@@ -6,11 +6,10 @@
 
 #include "video/Cubemap.h"
 #include "video/Buffer.h"
-#include "video/Camera.h"
 #include "SkyboxShader.h"
 
 namespace video {
-class Video;
+class Camera;
 }
 
 namespace render {