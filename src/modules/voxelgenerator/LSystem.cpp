7e31868615da54e51111496ea9f889045c6704ed
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-05-05 22:58:25 +0200

    VOXELGENERATOR: fixed tokenizer for lsystem rules

--- src/modules/voxelgenerator/LSystem.cpp
+++ src/modules/voxelgenerator/LSystem.cpp
@@ -8,7 +8,7 @@ namespace voxelgenerator {
 namespace lsystem {
 
 bool parseRules(const core::String& rulesStr, std::vector<Rule>& rules) {
-	core::Tokenizer tokenizer(rulesStr);
+	core::Tokenizer tokenizer(rulesStr, " \n");
 	while (tokenizer.hasNext()) {
 		const core::String block = tokenizer.next();
 		if (block != "{") {
