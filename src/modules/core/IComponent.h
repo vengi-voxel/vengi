722b1a35e6b144c552f03e84778bff48de0908f6
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-05-12 20:04:50 +0200

    CORE: doxygen

--- src/modules/core/IComponent.h
+++ src/modules/core/IComponent.h
@@ -14,8 +14,21 @@ namespace core {
 struct IComponent : public core::NonCopyable {
 	virtual ~IComponent() {}
 
+	/**
+	 * @brief Register commands and cvars before @c init() is called
+	 * @note This is useful to be able to print all commands and cvars with
+	 * e.g. @c --help command line parameter
+	 */
 	virtual void construct() {}
+	/**
+	 * @brief Called after @c construct()
+	 * @return Returns @c true if the initialization was successful. On @c false
+	 * @c shutdown() is called, too
+	 */
 	virtual bool init() = 0;
+	/**
+	 * @brief Called on restarting or shutting down
+	 */
 	virtual void shutdown() = 0;
 };
 
