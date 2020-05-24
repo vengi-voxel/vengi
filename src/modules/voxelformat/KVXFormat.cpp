6e4720dde4e53fa9505db8e2834b6d4152b54a46
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-05-24 21:28:44 +0200

    VOXELFORMAT: fixed kvx loading and reduced memory allocations

--- src/modules/voxelformat/KVXFormat.cpp
+++ src/modules/voxelformat/KVXFormat.cpp
@@ -14,8 +14,6 @@ namespace voxel {
 #define wrap(read) \
 	if (read != 0) { \
 		Log::error("Could not load kvx file: Not enough data in stream " CORE_STRINGIFY(read) " - still %i bytes left", (int)stream.remaining()); \
-		delete[] xyoffset; \
-		delete[] xoffset; \
 		return false; \
 	}
 
@@ -26,28 +24,19 @@ bool KVXFormat::loadGroups(const io::FilePtr& file, VoxelVolumes& volumes) {
 	}
 	io::FileStream stream(file.get());
 
-	/**
-	 * For compression purposes, I store the column pointers
-	 * in a way that offers quick access to the data, but with slightly more
-	 * overhead in calculating the positions.  See example of usage in voxdata.
-	 * NOTE: xoffset[0] = (xsiz+1)*4 + xsiz*(ysiz+1)*2 (ALWAYS)
-	 */
-	uint32_t* xoffset = nullptr;
-	uint16_t* xyoffset = nullptr;
-
 	// Total # of bytes (not including numbytes) in each mip-map level
 	// but there is only 1 mip-map level
 	uint32_t numbytes;
 	wrap(stream.readInt(numbytes))
 
 	// Dimensions of voxel. (our depth is kvx height)
-	uint32_t width, height, depth;
-	wrap(stream.readInt(width))
-	wrap(stream.readInt(height))
-	wrap(stream.readInt(depth))
+	uint32_t xsiz, ysiz, zsiz;
+	wrap(stream.readInt(xsiz))
+	wrap(stream.readInt(ysiz))
+	wrap(stream.readInt(zsiz))
 
-	if (width > 256 || height > 255 || depth > 256) {
-		Log::error("Dimensions exceeded: w: %i, h: %i, d: %i", width, height, depth);
+	if (xsiz > 256 || ysiz > 256 || zsiz > 255) {
+		Log::error("Dimensions exceeded: w: %i, h: %i, d: %i", xsiz, zsiz, ysiz);
 		return false;
 	}
 
@@ -60,32 +49,37 @@ bool KVXFormat::loadGroups(const io::FilePtr& file, VoxelVolumes& volumes) {
 	wrap(stream.readFloat(pivot.z))
 	pivot /= 256.0f;
 
-	if (width > MaxRegionSize || height > MaxRegionSize || depth > MaxRegionSize) {
-		Log::error("Volume exceeds the max allowed size: %i:%i:%i", width, height, depth);
+	if (xsiz > MaxRegionSize || ysiz > MaxRegionSize || zsiz > MaxRegionSize) {
+		Log::error("Volume exceeds the max allowed size: %i:%i:%i", xsiz, zsiz, ysiz);
 		return false;
 	}
 
-	const voxel::Region region(0, 0, 0, width - 1, depth - 1, height - 1);
+	const voxel::Region region(0, 0, 0, xsiz - 1, zsiz - 1, ysiz - 1);
 	if (!region.isValid()) {
-		Log::error("Invalid region: %i:%i:%i", width, depth, height);
+		Log::error("Invalid region: %i:%i:%i", xsiz, zsiz, ysiz);
 		return false;
 	}
 
-	xoffset = new uint32_t[width + 1];
-	xyoffset = new uint16_t[width * (height + 1)];
+	/**
+	 * For compression purposes, I store the column pointers
+	 * in a way that offers quick access to the data, but with slightly more
+	 * overhead in calculating the positions.  See example of usage in voxdata.
+	 * NOTE: xoffset[0] = (xsiz+1)*4 + xsiz*(ysiz+1)*2 (ALWAYS)
+	 */
+	uint16_t xyoffset[256][257];
+	uint32_t xoffset;
+	wrap(stream.readInt(xoffset))
 
-	for (uint32_t i = 0u; i < width + 1; ++i) {
-		wrap(stream.readInt(xoffset[i]))
-	}
-	for (uint32_t x = 0u; x < width; ++x) {
-		for (uint32_t y = 0u; y <= height; ++y) {
-			wrap(stream.readShort(xyoffset[x * width + y]))
+	core_assert(((xsiz + 1) << 2) == sizeof(uint32_t) * (xsiz + 1));
+	stream.skip(sizeof(uint32_t) * xsiz);
+	for (uint32_t x = 0u; x < xsiz; ++x) {
+		for (uint32_t y = 0u; y <= ysiz; ++y) {
+			wrap(stream.readShort(xyoffset[x][y]))
 		}
 	}
 
-	if (xoffset[0] != (width + 1) * 4 + width * (height + 1) * 2) {
-		delete[] xyoffset;
-		delete[] xoffset;
+	if (xoffset != (xsiz + 1) * 4 + xsiz * (ysiz + 1) * 2) {
+		Log::error("Invalid offset values found");
 		return false;
 	}
 	// Read the color palette from the end of the file and convert to our palette
@@ -106,12 +100,9 @@ bool KVXFormat::loadGroups(const io::FilePtr& file, VoxelVolumes& volumes) {
 		wrap(stream.readByte(g))
 		wrap(stream.readByte(b))
 
-		const int sr = glm::round((r * 255) / 63.0f);
-		const int sg = glm::round((g * 255) / 63.0f);
-		const int sb = glm::round((b * 255) / 63.0f);
-		const uint8_t nr = glm::clamp(sr, 0, 255);
-		const uint8_t ng = glm::clamp(sg, 0, 255);
-		const uint8_t nb = glm::clamp(sb, 0, 255);
+		const uint8_t nr = glm::clamp((uint32_t)glm::round((r * 255) / 63.0f), 0u, 255u);
+		const uint8_t ng = glm::clamp((uint32_t)glm::round((g * 255) / 63.0f), 0u, 255u);
+		const uint8_t nb = glm::clamp((uint32_t)glm::round((b * 255) / 63.0f), 0u, 255u);
 
 		const glm::vec4& color = core::Color::fromRGBA(nr, ng, nb, 255);
 		const int index = core::Color::getClosestMatch(color, materialColors);
@@ -147,46 +138,50 @@ bool KVXFormat::loadGroups(const io::FilePtr& file, VoxelVolumes& volumes) {
 	 * char col[slabzleng];       //The array of colors from top to bottom
 	 * @endcode
 	 */
+
+	struct slab {
+		uint8_t slabztop;
+		uint8_t slabzleng;
+		uint8_t slabbackfacecullinfo;
+	};
+
 	uint32_t lastZ = 0;
 	voxel::Voxel lastCol;
-	for (uint32_t x = 0; x < width; x++) {
-		for (uint32_t y = 0; y < height; y++) {
-			uint16_t start = xyoffset[x * width + y];
-			const uint16_t end = xyoffset[x * width + y + 1];
 
-			while (start < end) {
-				uint8_t zpos;
-				uint8_t zlen;
-				uint8_t visfaces;
-				wrap(stream.readByte(zpos))
-				wrap(stream.readByte(zlen))
-				wrap(stream.readByte(visfaces))
-				for (uint8_t i = 0u; i < zlen; ++i) {
-					uint8_t index;
-					wrap(stream.readByte(index))
-					lastCol = voxel::createVoxel(voxel::VoxelType::Generic, convertPaletteIndex(index));
-					volume->setVoxel(x - pivot.x, region.getHeightInCells() - (zpos + i - pivot.z), y - pivot.y, lastCol);
+	for (uint32_t x = 0; x < xsiz; ++x) {
+		for (uint32_t y = 0; y < ysiz; ++y) {
+			const uint16_t end = xyoffset[x][y + 1];
+			const uint16_t start = xyoffset[x][y];
+			int32_t n = end - start;
+
+			while (n > 0) {
+				slab header;
+				wrap(stream.readByte(header.slabztop))
+				wrap(stream.readByte(header.slabzleng))
+				wrap(stream.readByte(header.slabbackfacecullinfo))
+				for (uint8_t i = 0u; i < header.slabzleng; ++i) {
+					uint8_t col;
+					wrap(stream.readByte(col))
+					lastCol = voxel::createVoxel(voxel::VoxelType::Generic, convertPaletteIndex(col));
+					volume->setVoxel(x, (zsiz - 1) - (header.slabztop + i), y, lastCol);
 				}
 
 				/**
 				 * the format only saves the visible voxels - we have to take the face info to
 				 * fill the inner voxels
 				 */
-				if (!(visfaces & (1 << 4))) {
-					for (int i = lastZ + 1; i < zpos; i++) {
-						volume->setVoxel(x - pivot.x, region.getHeightInCells() - (i - pivot.z), y - pivot.y, lastCol);
+				if (!(header.slabbackfacecullinfo & (1 << 4))) {
+					for (int i = lastZ + 1; i < header.slabztop; ++i) {
+						volume->setVoxel(x, (zsiz - 1) - i, y, lastCol);
 					}
 				}
-				if (!(visfaces & (1 << 5))) {
-					lastZ = zpos + zlen - 1;
+				if (!(header.slabbackfacecullinfo & (1 << 5))) {
+					lastZ = header.slabztop + header.slabzleng;
 				}
-				start += zlen + 3;
+				n -= (header.slabzleng + sizeof(header));
 			}
 		}
 	}
-
-	delete[] xyoffset;
-	delete[] xoffset;
 
 	return true;
 }
