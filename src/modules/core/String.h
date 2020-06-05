vengi-voxel/vengi

commit 754eb6c04ee316b86f182f0062dba804ab6e646b

CORE: allow String() as constexpr

src/modules/core/String.h
@@ -26,7 +26,7 @@ private:
 	// buffer to prevent memory allocation. If string is longer than the available buffer size,
 	// perform dynamic memory allocation
 	// ensure proper alignment
-	char _buf[64 - sizeof(data)];
+	char _buf[64 - sizeof(data)] = "";
 
 	static const constexpr size_t _stackBufCapacity = sizeof(_buf);
 
@@ -35,7 +35,7 @@ private:
 	void checkBufferSize(size_t len);
 public:
 	static constexpr const size_t npos = ~0u;
-	String();
+	constexpr String() {}
 	String(size_t len, char chr);
 	String(const char *str);
 	String(const char *str, size_t len);

src/modules/core/String.cpp
@@ -69,10 +69,6 @@ void String::copyBuf(const char *str, size_t len) {
 	_data._str[len] = '\0';
 }
 
-String::String() :
-	String("", 0u) {
-}
-
 String::String(size_t len, char chr) {
 	_buf[0] = '\0';
 	_data._size = len;

commit 5d5b2159f3bee2a300a865beee6e44af4b8e59b9

CORE: fixed segfault introduced in 754eb6c04ee3

src/modules/core/String.h
@@ -35,7 +35,9 @@ private:
 	void checkBufferSize(size_t len);
 public:
 	static constexpr const size_t npos = ~0u;
-	constexpr String() {}
+	constexpr String() {
+		_data._str = _buf;
+	}
 	String(size_t len, char chr);
 	String(const char *str);
 	String(const char *str, size_t len);