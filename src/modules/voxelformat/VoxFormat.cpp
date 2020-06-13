4159047307b1c303e7526ccc46af957a879b7ce4
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-06-13 23:39:27 +0200

    VOXELFORMAT: added todo comment about a bug for saving magicavoxel files

--- src/modules/voxelformat/VoxFormat.cpp
+++ src/modules/voxelformat/VoxFormat.cpp
@@ -266,6 +266,7 @@ bool VoxFormat::saveSceneGraph(io::FileStream& stream, const VoxelVolumes& volum
 		const voxel::Region& region = v.volume->region();
 		const glm::ivec3& size = region.getDimensionsInVoxels();
 		const glm::vec3 pivot = glm::vec3(size.x & ~1u, size.z & ~1u, size.y & ~1u) - glm::vec3(1.0f);
+		// TODO: the translation is broken for saves.
 		wrapBool(saveChunk_nTRN(stream, nodeId, nodeId + 1, pivot))
 		wrapBool(saveChunk_nSHP(stream, nodeId + 1, modelId))
 
