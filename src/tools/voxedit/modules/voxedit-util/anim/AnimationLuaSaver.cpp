171aeb3acc23d4be53d4da58574dcf870b609b9f
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-07-04 07:38:58 +0200

    VOXEDIT: check the return values of Filestream methods while writing aniamtion lua files

--- src/tools/voxedit/modules/voxedit-util/anim/AnimationLuaSaver.cpp
+++ src/tools/voxedit/modules/voxedit-util/anim/AnimationLuaSaver.cpp
@@ -8,50 +8,60 @@
 #include "core/GLM.h"
 #include <glm/gtc/constants.hpp>
 #include <glm/common.hpp>
+#include "core/Assert.h"
+#include "core/Log.h"
 
 namespace voxedit {
+
+#define wrapBool(expression) \
+	if (!(expression)) { \
+		Log::error("Error: writing animation entity lua at " SDL_FILE ":%i", SDL_LINE); \
+		return false; \
+	}
 
 bool saveAnimationEntityLua(const animation::AnimationSettings& settings, const animation::SkeletonAttribute& sa, const char *name, const io::FilePtr& file) {
 	if (!file || !file->validHandle()) {
 		return false;
 	}
 	io::FileStream stream(file);
-	stream.addString("require 'chr.bones'\n", false);
-	stream.addString("require 'chr.shared'\n\n", false);
-	stream.addString("function init()\n", false);
+	wrapBool(stream.addString("require 'chr.bones'\n", false))
+	wrapBool(stream.addString("require 'chr.shared'\n\n", false))
+	wrapBool(stream.addString("function init()\n", false))
 	// TODO race and gender
-	stream.addString("  settings.setBasePath(\"human\", \"male\")\n", false);
-	stream.addString("  settings.setMeshTypes(", false);
+	wrapBool(stream.addString("  settings.setBasePath(\"human\", \"male\")\n", false))
+	wrapBool(stream.addString("  settings.setMeshTypes(", false))
 	for (size_t i = 0; i < settings.types().size(); ++i) {
 		const core::String& type = settings.types()[i];
-		stream.addStringFormat(false, "\"%s\"", type.c_str());
+		wrapBool(stream.addStringFormat(false, "\"%s\"", type.c_str()))
 		if (i != settings.types().size() - 1) {
-			stream.addString(", ", false);
+			wrapBool(stream.addString(", ", false))
 		}
 	}
-	stream.addString("  )\n", false);
+	wrapBool(stream.addString("  )\n", false))
 	for (const core::String& t : settings.types()) {
 		const int meshTypeIdx = settings.getMeshTypeIdxForName(t.c_str());
 		const core::String& path = settings.path(meshTypeIdx, name);
 		if (path.empty()) {
 			continue;
 		}
-		stream.addStringFormat(false, "  settings.setPath(\"%s\", \"%s\")\n", t.c_str(), path.c_str());
+		wrapBool(stream.addStringFormat(false, "  settings.setPath(\"%s\", \"%s\")\n", t.c_str(), path.c_str()))
 	}
 
-	stream.addString("  local attributes = defaultSkeletonAttributes()\n", false);
+	wrapBool(stream.addString("  local attributes = defaultSkeletonAttributes()\n", false))
 	const animation::CharacterSkeletonAttribute dv {};
 	for (const animation::SkeletonAttributeMeta* metaIter = sa.metaArray(); metaIter->name; ++metaIter) {
 		const animation::SkeletonAttributeMeta& meta = *metaIter;
 		const float *saVal = (const float*)(((const uint8_t*)&sa) + meta.offset);
 		const float *dvVal = (const float*)(((const uint8_t*)&dv) + meta.offset);
 		if (glm::abs(*saVal - *dvVal) > glm::epsilon<float>()) {
-			stream.addStringFormat(false, "  attributes[\"%s\"] = %f\n", meta.name, *saVal);
+			wrapBool(stream.addStringFormat(false, "  attributes[\"%s\"] = %f\n", meta.name, *saVal))
 		}
 	}
-	stream.addString("  return attributes\n", false);
-	stream.addString("end\n", false);
+	wrapBool(stream.addString("  return attributes\n", false))
+	wrapBool(stream.addString("end\n", false))
 	return true;
 }
+
+#undef wrapBool
 
 }
