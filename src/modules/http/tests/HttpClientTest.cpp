44838f60acfa49de3653a12a9b1d7c6f443216f2
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-07-05 23:01:17 +0200

    HTTP: fixed HttpClientTest

--- src/modules/http/tests/HttpClientTest.cpp
+++ src/modules/http/tests/HttpClientTest.cpp
@@ -6,6 +6,8 @@
 #include "http/HttpClient.h"
 #include "http/HttpServer.h"
 #include "core/concurrent/ThreadPool.h"
+#include "core/concurrent/Lock.h"
+#include "core/concurrent/ConditionVariable.h"
 
 namespace http {
 
@@ -13,16 +15,25 @@ class HttpClientTest : public core::AbstractTest {
 };
 
 TEST_F(HttpClientTest, testSimple) {
-	_testApp->threadPool().enqueue([this] () {
+	core_trace_mutex(core::Lock, serverStartMutex, "Server start mutex");
+	core::ConditionVariable startCondition;
+
+	core::ScopedLock lock(serverStartMutex);
+	_testApp->threadPool().enqueue([this, &serverStartMutex, &startCondition] () {
 		http::HttpServer _httpServer(_testApp->metric());
 		_httpServer.init(8095);
 		_httpServer.registerRoute(http::HttpMethod::GET, "/", [] (const http::RequestParser& request, HttpResponse* response) {
 			response->setText("Success");
 		});
+		startCondition.notify_one();
+
 		while (_testApp->state() == core::AppState::Running) {
 			_httpServer.update();
 		}
 		_httpServer.shutdown();
+	});
+	startCondition.wait(serverStartMutex, [] {
+		return true;
 	});
 	HttpClient client("http://localhost:8095");
 	ResponseParser response = client.get("/");
@@ -32,7 +43,7 @@ TEST_F(HttpClientTest, testSimple) {
 	EXPECT_STREQ("7", length);
 	const char *type;
 	EXPECT_TRUE(response.headers.get(http::header::CONTENT_TYPE, type));
-	EXPECT_STREQ("text/plain", length);
+	EXPECT_STREQ("text/plain", type);
 }
 
 }
