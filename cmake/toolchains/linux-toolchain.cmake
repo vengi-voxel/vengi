59d66fd6e03e6739550c773b8deab13f380f7827
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-08-04 16:06:45 +0200

    CMAKE: disabled ub sanitizer
    glslang tools are throwing a lot of errors with having this activated

--- cmake/toolchains/linux-toolchain.cmake
+++ cmake/toolchains/linux-toolchain.cmake
@@ -87,6 +87,7 @@ set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -O3 -DNDEBUG")
 
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_C_FLAGS}")
 set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} ${CMAKE_C_FLAGS_RELEASE}")
-set(SANITIZE_FLAGS "-fsanitize=undefined -fsanitize=address -fsanitize-address-use-after-scope")
+#-fsanitize=undefined
+set(SANITIZE_FLAGS "-fsanitize=address -fsanitize-address-use-after-scope")
 set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} ${CMAKE_C_FLAGS_DEBUG} -fstack-protector-strong -fno-omit-frame-pointer ${SANITIZE_FLAGS}")
 set(CMAKE_LINKER_FLAGS_DEBUG "${CMAKE_LINKER_FLAGS_DEBUG} -fno-omit-frame-pointer ${SANITIZE_FLAGS}")
