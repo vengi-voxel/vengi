d0fee48b27e008204807d4f3350e142d8b7791c3
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-08-18 16:37:50 +0200

    VOXEDIT: added cover script

---
+++ src/tools/voxedit/lua/cover.lua
@@ -0,0 +1,22 @@
+function arguments()
+	return {
+		{ name = 'n', desc = 'height level', type = 'int' }
+	}
+end
+
+function main(volume, region, color, n)
+	local mins = region:mins()
+	local maxs = region:maxs()
+	for x = mins.x, maxs.x do
+		for z = mins.z, maxs.z do
+			for y = maxs.y, mins.y, -1 do
+				if volume:voxel(x, y, z) ~= -1 then
+					for h = 1, n do
+						volume:setVoxel(x, y + h, z, color)
+					end
+					break
+				end
+			end
+		end
+	end
+end
