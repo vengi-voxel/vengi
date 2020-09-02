3bd63aa937af1fd422985d8803e9a3e8840e2358
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-09-02 21:23:29 +0200

    VOXEDIT: finished grass script

--- src/tools/voxedit/lua/scripts/grass.lua
+++ src/tools/voxedit/lua/scripts/grass.lua
@@ -10,14 +10,22 @@ end
 
 function main(volume, region, color, height, density, grasscolor)
 	print("create grass with color " .. grasscolor .. " and in the height range 1.." .. height)
+	local newindices = palette.similar(grasscolor, 4)
+
 	local visitor = function (volume, x, y, z)
 		local rndHeight = math.random(1, height)
+		local indidx = math.random(1, #newindices)
+		local c = newindices[indidx]
 		for h = 1, rndHeight do
-			volume:setVoxel(x, y + h, z, grasscolor)
+			volume:setVoxel(x, y + h, z, c)
 		end
 	end
 
 	local condition = function (volume, x, y, z)
+		local d = math.random(1, density)
+		if d ~= 1 then
+			return false
+		end
 		local voxel = volume:voxel(x, y, z)
 		if voxel == color then
 			return true
