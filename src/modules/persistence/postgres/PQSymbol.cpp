17b5c8db55f0187c0d27b4d99719bf7cb5a18209
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-09-04 15:47:48 +0200

    SERVER: fixed docker image

--- src/modules/persistence/postgres/PQSymbol.cpp
+++ src/modules/persistence/postgres/PQSymbol.cpp
@@ -72,7 +72,7 @@ bool postgresInit() {
 #ifdef POSTGRESQL_LIBS
 				POSTGRESQL_LIBS,
 #endif
-				"libpq.dll", "libpq.so", nullptr};
+				"libpq.dll", "libpq.so", "libpq.so.5", nullptr};
 		for (const char **searchPath = searchPaths; *searchPath; ++searchPath) {
 			obj = SDL_LoadObject(*searchPath);
 			if (obj != nullptr) {
