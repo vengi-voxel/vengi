7daf8e09e856fdb8c27933cc41506342459e8c38
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-09-06 18:01:56 +0200

    VOXEDIT: refactored modifier tests

--- src/tools/voxedit/modules/voxedit-util/tests/ModifierTest.cpp
+++ src/tools/voxedit/modules/voxedit-util/tests/ModifierTest.cpp
@@ -3,63 +3,59 @@
  */
 
 #include "app/tests/AbstractTest.h"
+#include "voxel/tests/TestHelper.h"
 #include "../modifier/Modifier.h"
+#include "core/ArrayLength.h"
 #include "voxel/Voxel.h"
 
 namespace voxedit {
 
 class ModifierTest: public app::AbstractTest {
-private:
-	using Super = app::AbstractTest;
 protected:
-	Modifier _modifier;
-
-	void prepare(const glm::ivec3& mins, const glm::ivec3& maxs, ModifierType modifierType) {
-		_modifier.setModifierType(modifierType);
-		_modifier.setCursorVoxel(voxel::createVoxel(voxel::VoxelType::Generic, 1));
-		_modifier.setGridResolution(1);
-		_modifier.setCursorPosition(mins, voxel::FaceNames::PositiveX);
-		EXPECT_TRUE(_modifier.aabbStart());
-		EXPECT_TRUE(_modifier.aabbMode());
-		_modifier.setCursorPosition(maxs, voxel::FaceNames::PositiveX);
-		_modifier.aabbStep();
-	}
-
-public:
-	void SetUp() override {
-		Super::SetUp();
-		ASSERT_TRUE(_modifier.init()) << "Initialization failed";
-	}
-
-	void TearDown() override {
-		_modifier.shutdown();
-		Super::TearDown();
+	void prepare(Modifier& modifier, const glm::ivec3 &mins, const glm::ivec3 &maxs, ModifierType modifierType) {
+		modifier.setModifierType(modifierType);
+		modifier.setCursorVoxel(voxel::createVoxel(voxel::VoxelType::Generic, 1));
+		modifier.setGridResolution(1);
+		modifier.setCursorPosition(mins, voxel::FaceNames::PositiveX);
+		EXPECT_TRUE(modifier.aabbStart());
+		EXPECT_TRUE(modifier.aabbMode());
+		modifier.setCursorPosition(maxs, voxel::FaceNames::PositiveX);
+		modifier.aabbStep();
 	}
 };
 
 TEST_F(ModifierTest, testModifierStartStop) {
-	EXPECT_TRUE(_modifier.aabbStart());
-	EXPECT_TRUE(_modifier.aabbMode());
-	_modifier.aabbStop();
-	EXPECT_FALSE(_modifier.aabbMode());
+	Modifier modifier;
+	ASSERT_TRUE(modifier.init());
+	EXPECT_TRUE(modifier.aabbStart());
+	EXPECT_TRUE(modifier.aabbMode());
+	modifier.aabbStop();
+	EXPECT_FALSE(modifier.aabbMode());
+	modifier.shutdown();
 }
 
 TEST_F(ModifierTest, testModifierDim) {
-	prepare(glm::ivec3(-1), glm::ivec3(1), ModifierType::Place);
-	const glm::ivec3& dim = _modifier.aabbDim();
+	Modifier modifier;
+	ASSERT_TRUE(modifier.init());
+	prepare(modifier, glm::ivec3(-1), glm::ivec3(1), ModifierType::Place);
+	const glm::ivec3& dim = modifier.aabbDim();
 	EXPECT_EQ(glm::ivec3(3), dim);
+	modifier.shutdown();
 }
 
 TEST_F(ModifierTest, testModifierAction) {
-	prepare(glm::ivec3(-1), glm::ivec3(1), ModifierType::Place);
+	Modifier modifier;
+	ASSERT_TRUE(modifier.init());
+	prepare(modifier, glm::ivec3(-1), glm::ivec3(1), ModifierType::Place);
 	const voxel::Region region(-10, 10);
 	voxel::RawVolume volume(region);
 	bool modifierExecuted = false;
-	_modifier.aabbAction(&volume, [&] (const voxel::Region &region, ModifierType modifierType) {
+	modifier.aabbAction(&volume, [&] (const voxel::Region &region, ModifierType modifierType) {
 		modifierExecuted = true;
 		EXPECT_EQ(voxel::Region(glm::ivec3(-1), glm::ivec3(1)), region);
 	});
 	EXPECT_TRUE(modifierExecuted);
+	modifier.shutdown();
 }
 
 }
