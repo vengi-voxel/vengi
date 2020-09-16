d0c41e0bdcdeaa26d4696529f579514a45c3aad1
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-09-16 19:50:59 +0200

    VOXELFORMAT: support writing mtl files for obj export

--- src/modules/voxelformat/OBJFormat.cpp
+++ src/modules/voxelformat/OBJFormat.cpp
@@ -3,16 +3,31 @@
  */
 
 #include "OBJFormat.h"
+#include "app/App.h"
 #include "core/Log.h"
+#include "core/StringUtil.h"
 #include "core/Var.h"
 #include "io/File.h"
 #include "io/FileStream.h"
+#include "io/Filesystem.h"
 #include "voxel/MaterialColor.h"
 #include "voxel/VoxelVertex.h"
 #include "voxel/Mesh.h"
 #include "voxelformat/VoxelVolumes.h"
 
 namespace voxel {
+
+void OBJFormat::writeMtlFile(const core::String& mtlName) const {
+	const io::FilePtr& file = io::filesystem()->open(mtlName, io::FileMode::SysWrite);
+	if (!file->validHandle()) {
+		Log::error("Failed to create mtl file at %s", file->name().c_str());
+		return;
+	}
+	io::FileStream stream(file);
+	stream.addStringFormat(false, "newmtl palette\n");
+	stream.addStringFormat(false, "map_Ka palette-nippon.png\n");
+	stream.addStringFormat(false, "map_Kd palette-nippon.png\n");
+}
 
 bool OBJFormat::saveMesh(const voxel::Mesh& mesh, const io::FilePtr &file) {
 	bool withColor = true;
@@ -27,12 +42,20 @@ bool OBJFormat::saveMesh(const voxel::Mesh& mesh, const io::FilePtr &file) {
 	}
 	const voxel::VoxelVertex* vertices = mesh.getRawVertexData();
 	const voxel::IndexType* indices = mesh.getRawIndexData();
+	const MaterialColorArray& colors = getMaterialColors();
+
+	// 1 x 256 is the texture format that we are using for our palette
+	const float texcoord = 1.0f / (float)colors.size();
+	// it is only 1 pixel high
+	const float v1 = 0.0f;
+	const float v2 = 1.0f;
 
 	stream.addStringFormat(false, "# github.com/mgerhardy/engine\n");
-
-	const MaterialColorArray& colors = getMaterialColors();
+	stream.addStringFormat(false, "mtllib palette.mtl\n");
 	for (int i = 0; i < nv; ++i) {
 		const voxel::VoxelVertex& v = vertices[i];
+		const float u1 = (float)v.colorIndex * texcoord;
+		const float u2 = (float)(v.colorIndex + 1) * texcoord;
 		if (withColor) {
 			const glm::vec4& color = colors[v.colorIndex];
 			stream.addStringFormat(false, "v %i %i %i %f %f %f\n",
@@ -41,14 +64,28 @@ bool OBJFormat::saveMesh(const voxel::Mesh& mesh, const io::FilePtr &file) {
 			stream.addStringFormat(false, "v %i %i %i\n",
 				v.position.x, v.position.y, v.position.z);
 		}
+		if (i % 3 == 0) {
+			stream.addStringFormat(false, "vt %f %f\n", u1, v1);
+		} else if (i % 3 == 1) {
+			stream.addStringFormat(false, "vt %f %f\n", u2, v1);
+		} else {
+			stream.addStringFormat(false, "vt %f %f\n", u2, v2);
+		}
 	}
 
+	stream.addStringFormat(false, "usemtl palette\n");
 	for (int i = 0; i < ni; i += 3) {
 		const voxel::IndexType one   = indices[i + 0] + 1;
 		const voxel::IndexType two   = indices[i + 1] + 1;
 		const voxel::IndexType three = indices[i + 2] + 1;
 		stream.addStringFormat(false, "f %i %i %i\n", (int)one, (int)two, (int)three);
 	}
+
+	core::String name = file->name();
+	core::String mtlname = core::string::stripExtension(name);
+	mtlname.append(".mtl");
+	writeMtlFile(mtlname);
+
 	return true;
 }
 
--- src/modules/voxelformat/OBJFormat.h
+++ src/modules/voxelformat/OBJFormat.h
@@ -6,12 +6,15 @@
 
 #include "VoxFileFormat.h"
 #include "io/File.h"
+#include "core/String.h"
 
 namespace voxel {
 /**
  * @brief Wavefront Object
  */
 class OBJFormat : public MeshExporter {
+private:
+	void writeMtlFile(const core::String& mtlName) const;
 public:
 	bool saveMesh(const voxel::Mesh& mesh, const io::FilePtr& file) override;
 };
