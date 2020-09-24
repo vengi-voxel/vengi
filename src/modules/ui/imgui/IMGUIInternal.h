6174b9d43c886348b726cc85d6e12b674de97b42
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-09-24 19:27:44 +0200

    UI: extended vec2 and vec4

--- src/modules/ui/imgui/IMGUIInternal.h
+++ src/modules/ui/imgui/IMGUIInternal.h
@@ -39,12 +39,15 @@ struct ImDrawVert { \
 };
 
 #define IM_VEC2_CLASS_EXTRA                                                   \
+        ImVec2(float v) { x = v; y = v; }                                     \
         ImVec2(const glm::ivec2& f) { x = f.x; y = f.y; }                     \
         operator glm::ivec2() const { return glm::ivec2(x,y); }
 
 #define IM_VEC4_CLASS_EXTRA                                                   \
         ImVec4(const glm::ivec4& f) { x = f.x; y = f.y; z = f.z; w = f.w; }   \
-        operator glm::ivec4() const { return glm::ivec4(x,y,z,w); }
+        ImVec4(const glm::vec4& f) { x = f.x; y = f.y; z = f.z; w = f.w; }    \
+        operator glm::ivec4() const { return glm::ivec4(x,y,z,w); }           \
+        operator glm::vec4() const { return glm::vec4(x,y,z,w); }
 
 #define IM_ASSERT(_EXPR) core_assert(_EXPR)
 #include <dearimgui/imgui.h>
