2288ab7be46567138fb70f7b13c6c1e5b7227aa5
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-09-24 21:05:45 +0200

    BACKEND: extended AITest

--- src/modules/backend/tests/AITest.cpp
+++ src/modules/backend/tests/AITest.cpp
@@ -3,6 +3,12 @@
  */
 
 #include "NpcTest.h"
+#include "backend/entity/ai/filter/Complement.h"
+#include "backend/entity/ai/filter/Difference.h"
+#include "backend/entity/ai/filter/FilteredEntities.h"
+#include "backend/entity/ai/filter/First.h"
+#include "backend/entity/ai/filter/Intersection.h"
+#include "backend/entity/ai/filter/Last.h"
 #include "backend/entity/ai/filter/SelectIncreasePartner.h"
 #include "backend/entity/ai/filter/SelectVisible.h"
 #include "backend/entity/ai/filter/SelectEntitiesOfTypes.h"
@@ -17,12 +23,27 @@
 #include "backend/entity/ai/action/Spawn.h"
 #include "backend/entity/ai/action/TriggerCooldown.h"
 #include "backend/entity/ai/action/TriggerCooldownOnSelection.h"
+#include "backend/entity/ai/filter/Union.h"
+#include "core/Tokenizer.h"
 
 namespace backend {
 
 class AITest: public NpcTest {
 private:
 	using Super = NpcTest;
+};
+
+class FakeFilter: public IFilter {
+public:
+	FILTER_ACTION_CLASS(FakeFilter)
+	FILTER_ACTION_FACTORY(FakeFilter)
+
+	void filter (const AIPtr& entity) override {
+		core::Tokenizer tok(_parameters, ",");
+		while (tok.hasNext()) {
+			entity->addFilteredEntity(core::string::toInt(tok.next()));
+		}
+	}
 };
 
 TEST_F(AITest, testFilterSelectIncreasePartner) {
@@ -45,7 +66,7 @@ TEST_F(AITest, testFilterSelectVisible) {
 	const backend::FilteredEntities& fe = npc->ai()->getFilteredEntities();
 	EXPECT_EQ(2u, fe.size())
 		<< "Expected to have two of the npcs visible, but " << fe.size() << " are";
-	EXPECT_TRUE(std::find(fe.begin(), fe.end(), npcNotVisible->id()) == fe.end())
+	EXPECT_TRUE(core::find(fe.begin(), fe.end(), npcNotVisible->id()) == fe.end())
 		<< "This npc should not be part of the visible set";
 }
 
@@ -148,6 +169,101 @@ TEST_F(AITest, testActionAttackOnSelection) {
 	EXPECT_EQ(ai::TreeNodeStatus::FAILED, action->execute(npc->ai(), 0L));
 	setVisible(npc);
 	EXPECT_EQ(ai::TreeNodeStatus::FINISHED, action->execute(npc->ai(), 0L));
+}
+
+TEST_F(AITest, testLast) {
+	const NpcPtr& npc = create();
+	Filters filters;
+	filters.push_back(std::make_shared<FakeFilter>("3,9,10,2,1", Filters()));
+	filters.push_back(std::make_shared<FakeFilter>("3,10,2,4", Filters()));
+	backend::Last filter("", filters);
+	filter.filter(npc->ai());
+	const FilteredEntities& filteredEntities = npc->ai()->getFilteredEntities();
+	ASSERT_GE(filteredEntities.size(), 1u);
+	EXPECT_EQ(1u, filteredEntities.size());
+	EXPECT_EQ(4, filteredEntities[0]);
+}
+
+TEST_F(AITest, testFirst) {
+	const NpcPtr& npc = create();
+	Filters filters;
+	filters.push_back(std::make_shared<FakeFilter>("11,2,3", Filters()));
+	filters.push_back(std::make_shared<FakeFilter>("3,10,4", Filters()));
+	backend::First filter("", filters);
+	filter.filter(npc->ai());
+	const FilteredEntities& filteredEntities = npc->ai()->getFilteredEntities();
+	ASSERT_GE(filteredEntities.size(), 1u);
+	EXPECT_EQ(1u, filteredEntities.size());
+	EXPECT_EQ(11, filteredEntities[0]);
+}
+
+TEST_F(AITest, testIntersection) {
+	const NpcPtr& npc = create();
+	Filters filters;
+	filters.push_back(std::make_shared<FakeFilter>("1,2,3,5,6,10,4", Filters()));
+	filters.push_back(std::make_shared<FakeFilter>("3,9,10,2,4", Filters()));
+	filters.push_back(std::make_shared<FakeFilter>("3,10,2,4", Filters()));
+	backend::Intersection filter("", filters);
+	filter.filter(npc->ai());
+	const FilteredEntities& filteredEntities = npc->ai()->getFilteredEntities();
+	ASSERT_GE(filteredEntities.size(), 3u);
+	EXPECT_EQ(4u, filteredEntities.size());
+	EXPECT_EQ(2, filteredEntities[0]);
+	EXPECT_EQ(3, filteredEntities[1]);
+	EXPECT_EQ(4, filteredEntities[2]);
+	EXPECT_EQ(10, filteredEntities[3]);
+}
+
+TEST_F(AITest, testDifference) {
+	const NpcPtr& npc = create();
+	Filters filters;
+	filters.push_back(std::make_shared<FakeFilter>("1,2,3,5,6,10,4", Filters()));
+	filters.push_back(std::make_shared<FakeFilter>("3,9,10,2,4", Filters()));
+	backend::Difference filter("", filters);
+	filter.filter(npc->ai());
+	const FilteredEntities& filteredEntities = npc->ai()->getFilteredEntities();
+	ASSERT_GE(filteredEntities.size(), 3u);
+	EXPECT_EQ(3u, filteredEntities.size());
+	EXPECT_EQ(1, filteredEntities[0]);
+	EXPECT_EQ(5, filteredEntities[1]);
+	EXPECT_EQ(6, filteredEntities[2]);
+}
+
+TEST_F(AITest, testComplement) {
+	const NpcPtr& npc = create();
+	Filters filters;
+	npc->ai()->addFilteredEntity(1);
+	npc->ai()->addFilteredEntity(2);
+	npc->ai()->addFilteredEntity(4);
+	npc->ai()->addFilteredEntity(19);
+	filters.push_back(std::make_shared<FakeFilter>("1,2,3,5,6,10,4", Filters()));
+	filters.push_back(std::make_shared<FakeFilter>("3,9,10,2,4", Filters()));
+	backend::Complement filter("", filters);
+	filter.filter(npc->ai());
+	const FilteredEntities& filteredEntities = npc->ai()->getFilteredEntities();
+	ASSERT_GE(filteredEntities.size(), 1u);
+	EXPECT_EQ(1u, filteredEntities.size());
+	EXPECT_EQ(19, filteredEntities[0]);
+}
+
+TEST_F(AITest, testUnion) {
+	const NpcPtr& npc = create();
+	Filters filters;
+	filters.push_back(std::make_shared<FakeFilter>("1,2,3,5,6,10,4", Filters()));
+	filters.push_back(std::make_shared<FakeFilter>("3,9,10,2,4", Filters()));
+	backend::Union filter("", filters);
+	filter.filter(npc->ai());
+	const FilteredEntities& filteredEntities = npc->ai()->getFilteredEntities();
+	ASSERT_GE(filteredEntities.size(), 8u);
+	EXPECT_EQ(8u, filteredEntities.size());
+	EXPECT_EQ(1, filteredEntities[0]);
+	EXPECT_EQ(2, filteredEntities[1]);
+	EXPECT_EQ(3, filteredEntities[2]);
+	EXPECT_EQ(4, filteredEntities[3]);
+	EXPECT_EQ(5, filteredEntities[4]);
+	EXPECT_EQ(6, filteredEntities[5]);
+	EXPECT_EQ(9, filteredEntities[6]);
+	EXPECT_EQ(10, filteredEntities[7]);
 }
 
 }
