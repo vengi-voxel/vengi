eff53340ddda5f507caec301f0b29da138496a51
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-09-30 22:26:51 +0200

    VOXFORMAT: support exporting quads to obj

--- src/modules/voxelformat/OBJFormat.h
+++ src/modules/voxelformat/OBJFormat.h
@@ -16,6 +16,6 @@ class OBJFormat : public MeshExporter {
 private:
 	void writeMtlFile(const core::String& mtlName) const;
 public:
-	bool saveMesh(const voxel::Mesh& mesh, const io::FilePtr& file, float scale) override;
+	bool saveMesh(const voxel::Mesh& mesh, const io::FilePtr& file, float scale, bool quad) override;
 };
 }
