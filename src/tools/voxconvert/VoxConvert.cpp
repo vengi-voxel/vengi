3f2a692d5c35fec7ed47db9f414e17eee85314f5
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-10-01 18:16:42 +0200

    VOXCONVERT: list convert/export options

--- src/tools/voxconvert/VoxConvert.cpp
+++ src/tools/voxconvert/VoxConvert.cpp
@@ -73,8 +73,24 @@ app::AppState VoxConvert::onInit() {
 	const core::String infile = _argv[_argc - 2];
 	const core::String outfile = _argv[_argc - 1];
 
-	Log::debug("infile: %s", infile.c_str());
-	Log::debug("outfile: %s", outfile.c_str());
+	const bool mergeVolumes = hasArg("--merge") || hasArg("-m");
+	const bool scaleVolumes = hasArg("--scale") || hasArg("-s");
+
+	Log::info("Options");
+	if (voxelformat::isMeshFormat(outfile)) {
+		Log::info("* palette:          - %s", _palette->strVal().c_str());
+		Log::info("* mergeQuads:       - %s", _mergeQuads->strVal().c_str());
+		Log::info("* reuseVertices:    - %s", _reuseVertices->strVal().c_str());
+		Log::info("* ambientOcclusion: - %s", _ambientOcclusion->strVal().c_str());
+		Log::info("* scale:            - %s", _scale->strVal().c_str());
+		Log::info("* quads:            - %s", _quads->strVal().c_str());
+		Log::info("* withColor:        - %s", _withColor->strVal().c_str());
+		Log::info("* withTexCoords:    - %s", _withTexCoords->strVal().c_str());
+	}
+	Log::info("* infile:           - %s", infile.c_str());
+	Log::info("* outfile:          - %s", outfile.c_str());
+	Log::info("* mergeVolumes:     - %s", (mergeVolumes ? "true" : "false"));
+	Log::info("* scaleVolumes:     - %s", (scaleVolumes ? "true" : "false"));
 
 	const io::FilePtr inputFile = filesystem()->open(infile, io::FileMode::SysRead);
 	if (!inputFile->exists()) {
@@ -101,7 +117,7 @@ app::AppState VoxConvert::onInit() {
 		return app::AppState::InitFailure;
 	}
 
-	if (hasArg("--merge") || hasArg("-m")) {
+	if (mergeVolumes) {
 		voxel::RawVolume* merged = volumes.merge();
 		if (merged == nullptr) {
 			Log::error("Failed to merge volumes");
@@ -111,7 +127,7 @@ app::AppState VoxConvert::onInit() {
 		volumes.push_back(voxel::VoxelVolume(merged));
 	}
 
-	if (hasArg("--scale") || hasArg("-s")) {
+	if (scaleVolumes) {
 		for (auto& v : volumes) {
 			const voxel::Region srcRegion = v.volume->region();
 			const glm::ivec3& targetDimensionsHalf = (srcRegion.getDimensionsInVoxels() / 2) - 1;
