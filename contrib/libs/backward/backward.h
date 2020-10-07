c60051b823d92fa5b77f0bf27475dd2a1e205ad0
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-10-07 22:35:12 +0200

    LIBS: hopefully fixed msvc build

--- contrib/libs/backward/backward.h
+++ contrib/libs/backward/backward.h
@@ -1116,7 +1116,7 @@ public:
   NOINLINE
   size_t load_here(size_t depth = 32, void *context = nullptr,
                    void *error_addr = nullptr) {
-    set_context(context);
+    set_context((CONTEXT*)context);
     set_error_addr(error_addr);
     CONTEXT localCtx; // used when no context is provided
 
