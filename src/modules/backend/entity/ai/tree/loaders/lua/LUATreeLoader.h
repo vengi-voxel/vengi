0295ac8d3849abe99b00be15e4a60c6fb74236da
Martin Gerhardy <martin.gerhardy@gmail.com>
2020-10-12 18:16:36 +0200

    BACKEND: whitespace fixes

--- src/modules/backend/entity/ai/tree/loaders/lua/LUATreeLoader.h
+++ src/modules/backend/entity/ai/tree/loaders/lua/LUATreeLoader.h
@@ -4,32 +4,32 @@
  * @{
  * @code
  * function idle (parentnode)
- * 	local prio = parentnode:addNode("PrioritySelector", "walkuncrowded")
- * 		prio:addNode("Steer(Wander)", "wanderfreely")
+ *  local prio = parentnode:addNode("PrioritySelector", "walkuncrowded")
+ *    prio:addNode("Steer(Wander)", "wanderfreely")
  * end
  *
  * function wolf ()
- * 	local name = "ANIMAL_WOLF"
- * 	local rootNode = AI.createTree(name):createRoot("PrioritySelector", name)
- * 	local parallel = rootnode:addNode("Parallel", "hunt")
- * 	parallel:setCondition("Not(IsOnCooldown{HUNT})")
- * 		parallel:addNode("Steer(SelectionSeek)", "follow"):setCondition("Filter(SelectEntitiesOfType{ANIMAL_RABBIT})")
- * 		parallel:addNode("AttackOnSelection", "attack"):setCondition("IsCloseToSelection{1}")
- * 		parallel:addNode("SetPointOfInterest", "setpoi"):setCondition("IsCloseToSelection{1}")
- * 		parallel:addNode("TriggerCooldown{HUNT}", "increasecooldown"):setCondition("Not(IsSelectionAlive)")
- * 	idle(rootNode)
+ *  local name = "ANIMAL_WOLF"
+ *  local rootNode = AI.createTree(name):createRoot("PrioritySelector", name)
+ *  local parallel = rootnode:addNode("Parallel", "hunt")
+ *  parallel:setCondition("Not(IsOnCooldown{HUNT})")
+ *    parallel:addNode("Steer(SelectionSeek)", "follow"):setCondition("Filter(SelectEntitiesOfType{ANIMAL_RABBIT})")
+ *    parallel:addNode("AttackOnSelection", "attack"):setCondition("IsCloseToSelection{1}")
+ *    parallel:addNode("SetPointOfInterest", "setpoi"):setCondition("IsCloseToSelection{1}")
+ *    parallel:addNode("TriggerCooldown{HUNT}", "increasecooldown"):setCondition("Not(IsSelectionAlive)")
+ *  idle(rootNode)
  * end
  *
  * function rabbit ()
- * 	local name = "ANIMAL_RABBIT"
- * 	local rootNode = AI.createTree(name):createRoot("PrioritySelector", name)
- * 	rootnode:addNode("Steer(SelectionFlee)", "fleefromhunter"):setCondition("And(Filter(SelectEntitiesOfTypes{ANIMAL_WOLF}),IsCloseToSelection{10})")
- * 	idle(rootNode)
+ *  local name = "ANIMAL_RABBIT"
+ *  local rootNode = AI.createTree(name):createRoot("PrioritySelector", name)
+ *  rootnode:addNode("Steer(SelectionFlee)", "fleefromhunter"):setCondition("And(Filter(SelectEntitiesOfTypes{ANIMAL_WOLF}),IsCloseToSelection{10})")
+ *  idle(rootNode)
  * end
  *
  * function init ()
- * 	wolf()
- * 	rabbit()
+ *  wolf()
+ *  rabbit()
  * end
  * @endcode
  */
