ba220512cb26aaa7c2c75994b660fa5ad09d92b1
Martin Gerhardy <martin.gerhardy@gmail.com>
2021-01-04 17:23:18 +0100

    VIDEO: minor cleanup

--- src/modules/video/WindowedApp.cpp
+++ src/modules/video/WindowedApp.cpp
@@ -381,18 +381,18 @@ app::AppState WindowedApp::onConstruct() {
 	core::Var::get(cfg::ClientMultiSampleSamples, "0");
 	core::Var::get(cfg::ClientFullscreen, "true");
 	core::Var::get(cfg::ClientWindowHighDPI, "false", core::CV_READONLY);
-	core::Var::get(cfg::ClientFog, "true", core::CV_SHADER)->setHelp("Render the world with fog");
+	core::Var::get(cfg::ClientFog, "true", core::CV_SHADER, "Render the world with fog");
 	core::Var::get(cfg::ClientShadowMap, "true", core::CV_SHADER);
 	core::Var::get(cfg::ClientWater, "true", core::CV_SHADER);
 	core::Var::get(cfg::ClientDebugShadow, "false", core::CV_SHADER);
 	core::Var::get(cfg::ClientShadowMapSize, "1024");
 	core::Var::get(cfg::ClientDebugShadowMapCascade, "false", core::CV_SHADER);
-	core::Var::get(cfg::ClientGamma, "2.2", core::CV_SHADER)->setHelp("Gamma correction");
+	core::Var::get(cfg::ClientGamma, "2.2", core::CV_SHADER, "Gamma correction");
 	core::Var::get(cfg::ClientWindowDisplay, 0);
 	core::Var::get(cfg::ClientOpenGLVersion, "3.3", core::CV_READONLY);
 	core::Var::get(cfg::ClientMouseRotationSpeed, "0.01");
 	core::Var::get(cfg::RenderOutline, "false", core::CV_SHADER);
-	core::Var::get(cfg::ClientVSync, "true")->setHelp("Limit the framerate to the monitor refresh rate");
+	core::Var::get(cfg::ClientVSync, "true", "Limit the framerate to the monitor refresh rate");
 
 	_keybindingHandler.construct();
 
