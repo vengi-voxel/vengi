182b7908d261f4b07015333e2aa4803aad162601
Martin Gerhardy <martin.gerhardy@gmail.com>
2021-01-07 17:01:29 +0100

    VOXEDIT: disabled some code and added todo

--- src/tools/voxedit/modules/voxedit-imgui/Viewport.cpp
+++ src/tools/voxedit/modules/voxedit-imgui/Viewport.cpp
@@ -33,10 +33,31 @@ void Viewport::update() {
 	Super::update();
 	ImGui::SetNextWindowSize(ImGui::GetWindowSize());
 	_hovered = false;
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
 	if (ImGui::Begin(_cameraMode.c_str(), nullptr, ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoInputs)) {
+		const ImVec2& windowPos = ImGui::GetWindowPos();
 		const double deltaFrameSeconds = app::App::getInstance()->deltaFrameSeconds();
 		_controller.update(deltaFrameSeconds);
+		const ImVec2& windowSize = ImGui::GetWindowSize();
+		resize(glm::ivec2(windowSize.x, windowSize.y));
 
+		const bool relative = video::WindowedApp::getInstance()->isRelativeMouseMode();
+		const bool alt = ImGui::GetIO().KeyAlt;
+		const bool middle = ImGui::IsMouseDown(ImGuiMouseButton_Middle);
+		const int mouseX = (int)ImGui::GetIO().MousePos.x - windowPos.x;
+		const int mouseY = (int)ImGui::GetIO().MousePos.y - windowPos.y;
+		cursorMove(relative || middle || alt, mouseX, mouseY);
+		renderToFrameBuffer();
+
+		// use the uv coords here to take a potential fb flip into account
+		const glm::vec4 &uv = _frameBuffer.uv();
+		const glm::vec2 uva(uv.x, uv.y);
+		const glm::vec2 uvc(uv.z, uv.w);
+		const video::TexturePtr &texture = _frameBuffer.texture(video::FrameBufferAttachment::Color0);
+
+#if 0 // TODO: this doesn't work, as we don't render directly in dearimgui - but only collect render commands
 		video::Shader *shader = nullptr;
 		video::Id prevShader = video::InvalidId;
 		switch (_controller.shaderType()) {
@@ -55,38 +76,22 @@ void Viewport::update() {
 				shader->setUniformMatrix(loc, projectionMatrix);
 			}
 		}
-		const ImVec2& windowSize = ImGui::GetWindowSize();
-		resize(glm::ivec2(windowSize.x, windowSize.y));
-		renderToFrameBuffer();
-
-		// use the uv coords here to take a potential fb flip into account
-		const glm::vec4 &uv = _frameBuffer.uv();
-		const glm::vec2 uva(uv.x, uv.y);
-		const glm::vec2 uvc(uv.z, uv.w);
-		const video::TexturePtr &texture = _frameBuffer.texture(video::FrameBufferAttachment::Color0);
-		ImGui::Image(texture->handle(), ImGui::GetWindowSize(), uva, uvc);
-
-		const bool relative = video::WindowedApp::getInstance()->isRelativeMouseMode();
-		const bool alt = ImGui::GetIO().KeyAlt;
-		const bool middle = ImGui::IsMouseDown(ImGuiMouseButton_Middle);
-		const int mouseX = (int)ImGui::GetIO().MousePos.x;
-		const int mouseY = (int)ImGui::GetIO().MousePos.y;
-		cursorMove(relative || middle || alt, mouseX, mouseY);
-
+#endif
+		ImGui::Image(texture->handle(), windowSize, uva, uvc);
+#if 0
 		if (shader != nullptr) {
 			shader->deactivate();
 			video::useProgram(prevShader);
 		}
-
-		if (ImGui::IsItemActive()) {
-			voxedit::sceneMgr().setActiveCamera(&_controller.camera());
-		}
+#endif
 		if (ImGui::IsItemHovered()) {
 			_hovered = true;
+			voxedit::sceneMgr().setActiveCamera(&_controller.camera());
 			voxedit::sceneMgr().trace();
 		}
 	}
 	ImGui::End();
+	ImGui::PopStyleVar(3);
 }
 
 }
