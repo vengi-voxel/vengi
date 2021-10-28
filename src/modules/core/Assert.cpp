d2aee5a167352c8f7b1d96636427aa395f843268
Martin Gerhardy <martin.gerhardy@gmail.com>
2021-10-28 18:45:10 +0200

    CORE: no need to add an extra newline here

--- src/modules/core/Assert.cpp
+++ src/modules/core/Assert.cpp
@@ -18,7 +18,7 @@ void core_stacktrace() {
 	tr.load_stacktrace(st);
 	for (size_t __stacktrace_i = 0; __stacktrace_i < st.size(); ++__stacktrace_i) {
 		const backward::ResolvedTrace& trace = tr.resolve(st[__stacktrace_i]);
-		Log::error("#%i %s %s [%p]\n", int(__stacktrace_i), trace.object_filename.c_str(), trace.object_function.c_str(), trace.addr);
+		Log::error("#%i %s %s [%p]", int(__stacktrace_i), trace.object_filename.c_str(), trace.object_function.c_str(), trace.addr);
 	}
 #endif
 }
