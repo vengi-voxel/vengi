667fa6f55026b4f7aa98c2be1fccf41693b64d17
Martin Gerhardy <martin.gerhardy@gmail.com>
2021-11-20 13:16:31 +0100

    IO: ensure that a home path is always set

--- src/modules/io/Filesystem.cpp
+++ src/modules/io/Filesystem.cpp
@@ -41,11 +41,12 @@ bool Filesystem::init(const core::String& organisation, const core::String& appn
 	}
 
 	char *prefPath = SDL_GetPrefPath(_organisation.c_str(), _appname.c_str());
-	if (prefPath == nullptr) {
-		_homePath = "./";
-	} else {
+	if (prefPath != nullptr) {
 		_homePath = prefPath;
 		SDL_free(prefPath);
+	}
+	if (_homePath.empty()) {
+		_homePath = "./";
 	}
 	const core::VarPtr& homePathVar = core::Var::get(cfg::AppHomePath, _homePath.c_str(), core::CV_READONLY | core::CV_NOPERSIST);
 
