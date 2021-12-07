50df4f6d4d47546cae659f4901035b7602a1e547
Martin Gerhardy <martin.gerhardy@gmail.com>
2021-12-07 20:33:49 +0100

    VOXELUTIL: new visitVolume helpers

--- src/modules/voxelutil/VolumeVisitor.h
+++ src/modules/voxelutil/VolumeVisitor.h
@@ -20,13 +20,12 @@ struct SkipEmpty {
 };
 
 template<class Volume, class Visitor, typename Condition = SkipEmpty>
-int visitVolume(const Volume& volume, Visitor&& visitor, Condition condition = Condition()) {
+int visitVolume(const Volume& volume, const voxel::Region& region, int xOff, int yOff, int zOff, Visitor&& visitor, Condition condition = Condition()) {
 	core_trace_scoped(VisitVolume);
-	const voxel::Region& region = volume.region();
 	int cnt = 0;
-	for (int32_t z = region.getLowerZ(); z <= region.getUpperZ(); ++z) {
-		for (int32_t y = region.getLowerY(); y <= region.getUpperY(); ++y) {
-			for (int32_t x = region.getLowerX(); x <= region.getUpperX(); ++x) {
+	for (int32_t z = region.getLowerZ(); z <= region.getUpperZ(); z += zOff) {
+		for (int32_t y = region.getLowerY(); y <= region.getUpperY(); y += yOff) {
+			for (int32_t x = region.getLowerX(); x <= region.getUpperX(); x += xOff) {
 				const voxel::Voxel& voxel = volume.voxel(x, y, z);
 				if (!condition(voxel)) {
 					continue;
@@ -37,6 +36,22 @@ int visitVolume(const Volume& volume, Visitor&& visitor, Condition condition = C
 		}
 	}
 	return cnt;
+}
+
+template<class Volume, class Visitor, typename Condition = SkipEmpty>
+int visitVolume(const Volume& volume, int xOff, int yOff, int zOff, Visitor&& visitor, Condition condition = Condition()) {
+	const voxel::Region& region = volume.region();
+	return visitVolume(volume, region, xOff, yOff, zOff, visitor, condition);
+}
+
+template<class Volume, class Visitor, typename Condition = SkipEmpty>
+int visitVolume(const Volume& volume, Visitor&& visitor, Condition condition = Condition()) {
+	return visitVolume(volume, 1, 1, 1, visitor, condition);
+}
+
+template<class Volume, class Visitor, typename Condition = SkipEmpty>
+int visitVolume(const Volume& volume, const voxel::Region& region, Visitor&& visitor, Condition condition = Condition()) {
+	return visitVolume(volume, region, 1, 1, 1, visitor, condition);
 }
 
 }
