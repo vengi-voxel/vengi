4166558ab20dc4921068d86cac7cbcfe9e6bdaf6
Martin Gerhardy <martin.gerhardy@gmail.com>
2021-12-10 06:59:15 +0100

    IO: fixed missing nullptr terminator

--- src/modules/io/FormatDescription.h
+++ src/modules/io/FormatDescription.h
@@ -25,14 +25,21 @@ extern core::String convertToFilePattern(const FormatDescription &desc);
 extern core::String getWildcardsFromPattern(const core::String &pattern);
 
 namespace format {
+
 inline const FormatDescription* png() {
-	static FormatDescription desc{"Image", "png", nullptr, 0u};
-	return &desc;
+	static FormatDescription desc[] = {
+		{"Image", "png", nullptr, 0u},
+		{nullptr, nullptr, nullptr, 0u}
+	};
+	return desc;
 }
 
 inline const FormatDescription* lua() {
-	static FormatDescription desc{"LUA script", "lua", nullptr, 0u};
-	return &desc;
+	static FormatDescription desc[] = {
+		{"LUA script", "lua", nullptr, 0},
+		{nullptr, nullptr, nullptr, 0u}
+	};
+	return desc;
 }
 
 }
