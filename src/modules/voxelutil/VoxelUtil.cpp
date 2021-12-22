a6e79e0a55c808be82251f4c2855c429d04f5fad
Martin Gerhardy <martin.gerhardy@gmail.com>
2021-12-22 22:10:14 +0100

    VOXELUTIL: small helper function to copy parts of a region

--- src/modules/voxelutil/VoxelUtil.cpp
+++ src/modules/voxelutil/VoxelUtil.cpp
@@ -7,14 +7,14 @@
 
 namespace voxelutil {
 
-void copyIntoRegion(const voxel::RawVolume &in, voxel::RawVolume &out, const voxel::Region &targetRegion) {
+bool copy(const voxel::RawVolume &in, const voxel::Region& inRegion, voxel::RawVolume &out, const voxel::Region &outRegion) {
 	int32_t xIn, yIn, zIn;
 	int32_t xOut, yOut, zOut;
 	voxel::RawVolumeWrapper wrapper(&out);
-	const glm::ivec3 &inmins = in.region().getLowerCorner();
-	const glm::ivec3 &inmaxs = in.region().getUpperCorner();
-	const glm::ivec3 &outmins = targetRegion.getLowerCorner();
-	const glm::ivec3 &outmaxs = targetRegion.getUpperCorner();
+	const glm::ivec3 &inmins = inRegion.getLowerCorner();
+	const glm::ivec3 &inmaxs = inRegion.getUpperCorner();
+	const glm::ivec3 &outmins = outRegion.getLowerCorner();
+	const glm::ivec3 &outmaxs = outRegion.getUpperCorner();
 	for (zIn = inmins.z, zOut = outmins.z; zIn <= inmaxs.z && zOut <= outmaxs.z; ++zIn, ++zOut) {
 		for (yIn = inmins.y, yOut = outmins.y; yIn <= inmaxs.y && yOut <= outmaxs.y; ++yIn, ++yOut) {
 			for (xIn = inmins.x, xOut = outmins.x; xIn <= inmaxs.x && xOut <= outmaxs.x; ++xIn, ++xOut) {
@@ -23,6 +23,11 @@ void copyIntoRegion(const voxel::RawVolume &in, voxel::RawVolume &out, const vox
 			}
 		}
 	}
+	return wrapper.dirtyRegion().isValid();
+}
+
+bool copyIntoRegion(const voxel::RawVolume &in, voxel::RawVolume &out, const voxel::Region &targetRegion) {
+	return copy(in, in.region(), out, targetRegion);
 }
 
 } // namespace voxelutil
--- src/modules/voxelutil/VoxelUtil.h
+++ src/modules/voxelutil/VoxelUtil.h
@@ -8,6 +8,7 @@
 
 namespace voxelutil {
 
-extern void copyIntoRegion(const voxel::RawVolume &in, voxel::RawVolume &out, const voxel::Region &targetRegion);
+extern bool copyIntoRegion(const voxel::RawVolume &in, voxel::RawVolume &out, const voxel::Region &targetRegion);
+extern bool copy(const voxel::RawVolume &in, const voxel::Region& inRegion, voxel::RawVolume &out, const voxel::Region &outRegion);
 
 }
