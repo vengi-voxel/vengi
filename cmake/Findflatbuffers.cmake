14febac20b346507f888673041296cd3fe1a39f3
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-01-25 19:48:43 +0100

    CMAKE: allow to use system wide installation of flatbuffers

--- cmake/Findflatbuffers.cmake
+++ cmake/Findflatbuffers.cmake
@@ -1,2 +1,2 @@
 include("${ROOT_DIR}/cmake/macros.cmake")
-engine_find_header_only(flatbuffers flatbuffers.h "" "" "")
+engine_find_header_only(flatbuffers flatbuffers.h flatbuffers "")
--- cmake/Findglm.cmake
+++ cmake/Findglm.cmake
@@ -1,3 +1,3 @@
 #We need 0.9.8
 include("${ROOT_DIR}/cmake/macros.cmake")
-engine_find_header_only(glm detail/type_mat.hpp glm "" "")
+engine_find_header_only(glm detail/type_mat.hpp glm "")
