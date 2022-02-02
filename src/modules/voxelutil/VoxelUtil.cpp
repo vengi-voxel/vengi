ea36b771757912a67bc483af23a808561924953e
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-02-02 23:25:01 +0100

    VOXELUTIL: implement the fill algorithm

--- src/modules/voxelutil/VoxelUtil.cpp
+++ src/modules/voxelutil/VoxelUtil.cpp
@@ -3,7 +3,12 @@
  */
 
 #include "VoxelUtil.h"
+#include "core/collection/Array3DView.h"
+#include "core/collection/Buffer.h"
+#include "core/collection/DynamicArray.h"
 #include "voxel/RawVolumeWrapper.h"
+#include "voxel/Voxel.h"
+#include "voxelutil/VolumeVisitor.h"
 
 namespace voxelutil {
 
@@ -47,6 +52,99 @@ bool copyIntoRegion(const voxel::RawVolume &in, voxel::RawVolume &out, const vox
 }
 
 void fillHollow(voxel::RawVolume &in, const voxel::Voxel &voxel) {
+	const int width = in.width();
+	const int height = in.height();
+	const int depth = in.depth();
+	const int size = width * height * depth;
+	core::DynamicArray<glm::ivec3> positions;
+	positions.reserve(size);
+	core::Buffer<bool> visitedData(size);
+	core::Array3DView<bool> visited(visitedData.data(), width, height, depth);
+
+	visitVolume(in, [&](int x, int y, int z, const voxel::Voxel &) { visited.set(x, y, z, true); }, SkipEmpty());
+
+	for (int x = 0; x < width; ++x) {
+		for (int z = 1; z < depth - 1; ++z) {
+			const glm::ivec3 v1(x, 0, z);
+			if (voxel::isAir(in.voxel(v1).getMaterial())) {
+				positions.push_back(v1);
+				visited.set(v1, true);
+			}
+			const glm::ivec3 v2(x, height - 1, z);
+			if (voxel::isAir(in.voxel(v2).getMaterial())) {
+				positions.push_back(v2);
+				visited.set(v2, true);
+			}
+		}
+		for (int y = 0; y < height; ++y) {
+			const glm::ivec3 v1(x, y, 0);
+			if (voxel::isAir(in.voxel(v1).getMaterial())) {
+				positions.push_back(v1);
+				visited.set(v1, true);
+			}
+			const glm::ivec3 v2(x, y, depth - 1);
+			if (voxel::isAir(in.voxel(v2).getMaterial())) {
+				positions.push_back(v2);
+				visited.set(v2, true);
+			}
+		}
+	}
+	for (int y = 1; y < height - 1; ++y) {
+		for (int z = 1; z < depth - 1; ++z) {
+			const glm::ivec3 v1(0, y, z);
+			if (voxel::isAir(in.voxel(v1).getMaterial())) {
+				positions.push_back(v1);
+				visited.set(v1, true);
+			}
+			const glm::ivec3 v2(width - 1, y, z);
+			if (voxel::isAir(in.voxel(v2).getMaterial())) {
+				positions.push_back(v2);
+				visited.set(v2, true);
+			}
+		}
+	}
+
+	while (!positions.empty()) {
+		const glm::ivec3 v = positions.back();
+		positions.erase(positions.size() - 1);
+
+		if (v.z > 0 && !visited.get(v.x, v.y, v.z - 1)) {
+			const glm::ivec3 v1(v.x, v.y, v.z - 1);
+			visited.set(v1, true);
+			positions.push_back(v1);
+		}
+		if (v.y > 0 && !visited.get(v.x, v.y - 1, v.z)) {
+			const glm::ivec3 v1(v.x, v.y - 1, v.z);
+			visited.set(v1, true);
+			positions.push_back(v1);
+		}
+		if (v.x > 0 && !visited.get(v.x - 1, v.y, v.z)) {
+			const glm::ivec3 v1(v.x - 1, v.y, v.z);
+			visited.set(v1, true);
+			positions.push_back(v1);
+		}
+		if (v.z < depth - 1 && !visited.get(v.x, v.y, v.z + 1)) {
+			const glm::ivec3 v1(v.x, v.y, v.z + 1);
+			visited.set(v1, true);
+			positions.push_back(v1);
+		}
+		if (v.y < height - 1 && !visited.get(v.x, v.y + 1, v.z)) {
+			const glm::ivec3 v1(v.x, v.y + 1, v.z);
+			visited.set(v1, true);
+			positions.push_back(v1);
+		}
+		if (v.x < width - 1 && !visited.get(v.x + 1, v.y, v.z)) {
+			const glm::ivec3 v1(v.x + 1, v.y, v.z);
+			visited.set(v1, true);
+			positions.push_back(v1);
+		}
+	}
+
+	visitVolume(in, [&](int x, int y, int z, const voxel::Voxel&) {
+		if (!visited.get(x, y, z)) {
+			in.setVoxel(x, y, z, voxel);
+		}
+	}, VisitAll());
 }
 
 } // namespace voxelutil
