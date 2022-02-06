2b8615fd6c4d0ae06e26f19fc93b85ab8735bdda
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-02-06 14:43:12 +0100

    CMAKE: enable parallel builds for msvc

--- cmake/toolchains/msvc-toolchain.cmake
+++ cmake/toolchains/msvc-toolchain.cmake
@@ -2,3 +2,5 @@ set(CMAKE_SYSTEM_NAME Windows)
 
 set(CMAKE_C_STANDARD_LIBRARIES "kernel32.lib user32.lib gdi32.lib winspool.lib winmm.lib imm32.lib comctl32.lib version.lib shell32.lib ole32.lib oleaut32.lib uuid.lib comdlg32.lib advapi32.lib dbghelp.lib wsock32.lib ws2_32.lib iphlpapi.lib rpcrt4.lib wininet.lib")
 set(CMAKE_CXX_STANDARD_LIBRARIES "${CMAKE_C_STANDARD_LIBRARIES}")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /MP")
