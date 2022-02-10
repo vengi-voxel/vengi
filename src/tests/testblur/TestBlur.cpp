69c1d3eb20f6e6b8b98f39f542bd319e65b046c8
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-02-10 17:26:50 +0100

    TESTS: added blur test application

--- src/tests/testblur/TestBlur.cpp
+++ src/tests/testblur/TestBlur.cpp
@@ -2,21 +2,21 @@
  * @file
  */
 
-#include "TestBloom.h"
+#include "TestBlur.h"
 #include "image/Image.h"
 #include "imgui.h"
 #include "testcore/TestAppMain.h"
 #include "video/Texture.h"
 
-TestBloom::TestBloom(const metric::MetricPtr& metric, const io::FilesystemPtr& filesystem,
+TestBlur::TestBlur(const metric::MetricPtr& metric, const io::FilesystemPtr& filesystem,
 		const core::EventBusPtr& eventBus, const core::TimeProviderPtr& timeProvider) :
 		Super(metric, filesystem, eventBus, timeProvider) {
-	init(ORGANISATION, "testbloom");
+	init(ORGANISATION, "TestBlur");
 	setCameraMotion(false);
 	_allowRelativeMouseMode = false;
 }
 
-app::AppState TestBloom::onInit() {
+app::AppState TestBlur::onInit() {
 	app::AppState state = Super::onInit();
 	if (state != app::AppState::Running) {
 		return state;
@@ -26,11 +26,6 @@ app::AppState TestBloom::onInit() {
 
 	if (!_blurRenderer.init(false)) {
 		Log::error("Failed to initialize the blur renderer");
-		return app::AppState::InitFailure;
-	}
-
-	if (!_bloomRenderer.init(false)) {
-		Log::error("Failed to initialize the bloom renderer");
 		return app::AppState::InitFailure;
 	}
 
@@ -45,26 +40,11 @@ app::AppState TestBloom::onInit() {
 		return app::AppState::InitFailure;
 	}
 
-	const image::ImagePtr& bloomImg = image::loadImage("bloom_extracted", false);
-	if (!bloomImg->isLoaded()) {
-		Log::error("Failed to load the image for the bloom");
-		return app::AppState::InitFailure;
-	}
-	_bloomTexture = video::createTextureFromImage(bloomImg);
-	if (!_bloomTexture) {
-		Log::error("Failed to create texture for the bloom");
-		return app::AppState::InitFailure;
-	}
-
 	return state;
 }
 
-app::AppState TestBloom::onCleanup() {
+app::AppState TestBlur::onCleanup() {
 	_blurRenderer.shutdown();
-	_bloomRenderer.shutdown();
-	if (_bloomTexture) {
-		_bloomTexture->shutdown();
-	}
 	if (_sceneTexture) {
 		_sceneTexture->shutdown();
 	}
@@ -72,31 +52,25 @@ app::AppState TestBloom::onCleanup() {
 	return state;
 }
 
-void TestBloom::onRenderUI() {
+void TestBlur::onRenderUI() {
 	Super::onRenderUI();
 
-	static glm::ivec2 size(256, 256);
+	static glm::ivec2 size(512, 512);
 
 	if (ImGui::InputInt("blur passes: ", &_passes)) {
 		_passes = glm::clamp(_passes, 0, 20);
 	}
 
-	ImGui::Image(_bloomTexture->handle(), size);
-
 	ImGui::Text("scene");
 	ImGui::Image(_sceneTexture->handle(), size);
 
-	ImGui::Text("bloom raw");
-	ImGui::Image(_bloomTexture->handle(), size);
-
 	const video::TexturePtr& blurred = _blurRenderer.texture();
-	ImGui::Text("blurred bloom: %i:%i", blurred->width(), blurred->height());
+	ImGui::Text("blurred: %i:%i", blurred->width(), blurred->height());
 	ImGui::Image(blurred->handle(), size);
 }
 
-void TestBloom::doRender() {
-	_blurRenderer.render(_bloomTexture->handle(), _passes);
-	_bloomRenderer.render(_sceneTexture->handle(), _blurRenderer.texture()->handle());
+void TestBlur::doRender() {
+	_blurRenderer.render(_sceneTexture->handle(), _passes);
 }
 
-TEST_APP(TestBloom)
+TEST_APP(TestBlur)
