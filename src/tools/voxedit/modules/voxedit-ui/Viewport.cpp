vengi-voxel/vengi

commit e158e0f5c1c083d8dd8cde06c5235c995fc4e728

ALL: removed setHelp calls for cvars

src/tools/voxedit/modules/voxedit-ui/Viewport.cpp
@@ -40,8 +40,7 @@ bool Viewport::init(ViewportController::RenderMode renderMode) {
 	_controller.setMode(ViewportController::SceneCameraMode::Free);
 	resetCamera();
 
-	_debug = core::Var::get("ve_viewportdebugflag", 0);
-	_debug->setHelp("Debug bit mask. 1 means rendering the traces for the active camera");
+	_debug = core::Var::get("ve_viewportdebugflag", 0, "Debug bit mask. 1 means rendering the traces for the active camera");
 	return true;
 }
 

src/modules/ui/imgui/IMGUIApp.cpp
@@ -150,8 +150,8 @@ app::AppState IMGUIApp::onConstruct() {
 	_console.construct();
 	_lastDirectory = core::Var::get(cfg::UILastDirectory, io::filesystem()->homePath().c_str());
 	core::Var::get(cfg::UILastFilter, "0");
-	core::Var::get(cfg::UIStyle, "0");
-	core::Var::get(cfg::UIShowHidden, "false")->setHelp("Show hidden file system entities");
+	core::Var::get(cfg::UIStyle, "0", "Change the ui colors - [0-3]");
+	core::Var::get(cfg::UIShowHidden, "false", "Show hidden file system entities");
 	_renderUI = core::Var::get(cfg::ClientRenderUI, "true");
 	_showMetrics = core::Var::get(cfg::UIShowMetrics, "false", core::CV_NOPERSIST);
 	_uiFontSize = core::Var::get(cfg::UIFontSize, "14", -1, "Allow to change the ui font size",

src/tools/voxconvert/VoxConvert.cpp
@@ -61,22 +61,14 @@ app::AppState VoxConvert::onConstruct() {
 	registerArg("--src-palette").setShort("-p").setDescription("Keep the source palette and don't perform quantization");
 	registerArg("--translate").setShort("-t").setDescription("Translate the volumes by x (right), y (up), z (back)");
 
-	_mergeQuads = core::Var::get(cfg::VoxformatMergequads, "true", core::CV_NOPERSIST);
-	_mergeQuads->setHelp("Merge similar quads to optimize the mesh");
-	_reuseVertices = core::Var::get(cfg::VoxformatReusevertices, "true", core::CV_NOPERSIST);
-	_reuseVertices->setHelp("Reuse vertices or always create new ones");
-	_ambientOcclusion = core::Var::get(cfg::VoxformatAmbientocclusion, "false", core::CV_NOPERSIST);
-	_ambientOcclusion->setHelp("Extra vertices for ambient occlusion");
-	_scale = core::Var::get(cfg::VoxformatScale, "1.0", core::CV_NOPERSIST);
-	_scale->setHelp("Scale the vertices by the given factor");
-	_quads = core::Var::get(cfg::VoxformatQuads, "true", core::CV_NOPERSIST);
-	_quads->setHelp("Export as quads. If this false, triangles will be used.");
-	_withColor = core::Var::get(cfg::VoxformatWithcolor, "true", core::CV_NOPERSIST);
-	_withColor->setHelp("Export with vertex colors");
-	_withTexCoords = core::Var::get(cfg::VoxformatWithtexcoords, "true", core::CV_NOPERSIST);
-	_withTexCoords->setHelp("Export with uv coordinates of the palette image");
-	_palette = core::Var::get("palette", voxel::getDefaultPaletteName());
-	_palette->setHelp("This is the NAME part of palette-<NAME>.png or absolute png file to use (1x256)");
+	_mergeQuads = core::Var::get(cfg::VoxformatMergequads, "true", core::CV_NOPERSIST, "Merge similar quads to optimize the mesh");
+	_reuseVertices = core::Var::get(cfg::VoxformatReusevertices, "true", core::CV_NOPERSIST, "Reuse vertices or always create new ones");
+	_ambientOcclusion = core::Var::get(cfg::VoxformatAmbientocclusion, "false", core::CV_NOPERSIST, "Extra vertices for ambient occlusion");
+	_scale = core::Var::get(cfg::VoxformatScale, "1.0", core::CV_NOPERSIST, "Scale the vertices by the given factor");
+	_quads = core::Var::get(cfg::VoxformatQuads, "true", core::CV_NOPERSIST, "Export as quads. If this false, triangles will be used.");
+	_withColor = core::Var::get(cfg::VoxformatWithcolor, "true", core::CV_NOPERSIST, "Export with vertex colors");
+	_withTexCoords = core::Var::get(cfg::VoxformatWithtexcoords, "true", core::CV_NOPERSIST, "Export with uv coordinates of the palette image");
+	_palette = core::Var::get("palette", voxel::getDefaultPaletteName(), "This is the NAME part of palette-<NAME>.png or absolute png file to use (1x256)");
 
 	if (!filesystem()->registerPath("scripts/")) {
 		Log::warn("Failed to register lua generator script path");

src/tools/voxedit/modules/voxedit-util/SceneManager.cpp
@@ -1599,18 +1599,16 @@ void SceneManager::construct() {
 		}
 	}).setHelp("Duplicates the current node or the given node id");
 
-	_grayInactive = core::Var::get(cfg::VoxEditGrayInactive, "false");
-	_grayInactive->setHelp("Render the inactive layers in gray scale mode");
-	_hideInactive = core::Var::get(cfg::VoxEditHideInactive, "false");
-	_hideInactive->setHelp("Hide the inactive layers");
-	core::Var::get(cfg::VoxformatMergequads, "true", core::CV_NOPERSIST)->setHelp("Merge similar quads to optimize the mesh");
-	core::Var::get(cfg::VoxformatReusevertices, "true", core::CV_NOPERSIST)->setHelp("Reuse vertices or always create new ones");
-	core::Var::get(cfg::VoxformatAmbientocclusion, "false", core::CV_NOPERSIST)->setHelp("Extra vertices for ambient occlusion");
-	core::Var::get(cfg::VoxformatScale, "1.0", core::CV_NOPERSIST)->setHelp("Scale the vertices by the given factor");
-	core::Var::get(cfg::VoxformatQuads, "true", core::CV_NOPERSIST)->setHelp("Export as quads. If this false, triangles will be used.");
-	core::Var::get(cfg::VoxformatWithcolor, "true", core::CV_NOPERSIST)->setHelp("Export with vertex colors");
-	core::Var::get(cfg::VoxformatWithtexcoords, "true", core::CV_NOPERSIST)->setHelp("Export with uv coordinates of the palette image");
-	core::Var::get("palette", voxel::getDefaultPaletteName())->setHelp("This is the NAME part of palette-<NAME>.png or absolute png file to use (1x256)");
+	_grayInactive = core::Var::get(cfg::VoxEditGrayInactive, "false", "Render the inactive layers in gray scale mode");
+	_hideInactive = core::Var::get(cfg::VoxEditHideInactive, "false", "Hide the inactive layers");
+	core::Var::get(cfg::VoxformatMergequads, "true", core::CV_NOPERSIST, "Merge similar quads to optimize the mesh");
+	core::Var::get(cfg::VoxformatReusevertices, "true", core::CV_NOPERSIST, "Reuse vertices or always create new ones");
+	core::Var::get(cfg::VoxformatAmbientocclusion, "false", core::CV_NOPERSIST, "Extra vertices for ambient occlusion");
+	core::Var::get(cfg::VoxformatScale, "1.0", core::CV_NOPERSIST, "Scale the vertices by the given factor");
+	core::Var::get(cfg::VoxformatQuads, "true", core::CV_NOPERSIST, "Export as quads. If this false, triangles will be used.");
+	core::Var::get(cfg::VoxformatWithcolor, "true", core::CV_NOPERSIST, "Export with vertex colors");
+	core::Var::get(cfg::VoxformatWithtexcoords, "true", core::CV_NOPERSIST, "Export with uv coordinates of the palette image");
+	core::Var::get("palette", voxel::getDefaultPaletteName(), "This is the NAME part of palette-<NAME>.png or absolute png file to use (1x256)");
 }
 
 void SceneManager::flip(math::Axis axis) {

commit 1f425ccb68090ed804432a141f197071f7d55d1e

VOXEDIT: fixed error introduced in e158e0f5

src/tools/voxedit/modules/voxedit-ui/Viewport.cpp
@@ -34,13 +34,14 @@ Viewport::~Viewport() {
 
 bool Viewport::init(ViewportController::RenderMode renderMode) {
 	if (!_controller.init()) {
+		Log::error("Failed to initialize the viewport controller");
 		return false;
 	}
 	_controller.setRenderMode(renderMode);
 	_controller.setMode(ViewportController::SceneCameraMode::Free);
 	resetCamera();
 
-	_debug = core::Var::get("ve_viewportdebugflag", 0, "Debug bit mask. 1 means rendering the traces for the active camera");
+	_debug = core::Var::get("ve_viewportdebugflag", "0", "Debug bit mask. 1 means rendering the traces for the active camera");
 	return true;
 }
 