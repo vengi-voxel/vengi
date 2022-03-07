978b331857a22c8d5c8c1812d1041df15786cf17
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-03-07 20:43:43 +0100

    VOXELFORMAT: fixed used format

--- src/modules/voxelformat/tests/MCRFormatTest.cpp
+++ src/modules/voxelformat/tests/MCRFormatTest.cpp
@@ -18,8 +18,8 @@ TEST_F(MCRFormatTest, DISABLED_testLoad) {
 	ASSERT_NE(nullptr, volume) << "Could not load mca volume";
 
 	QBFormat f2;
-	std::unique_ptr<RawVolume> volumeqb(load("r.0.-2.qb", f));
-	ASSERT_NE(nullptr, volume) << "Could not load qb volume";
+	std::unique_ptr<RawVolume> volumeqb(load("r.0.-2.qb", f2));
+	ASSERT_NE(nullptr, volumeqb) << "Could not load qb volume";
 
 	ASSERT_TRUE(volumeComparator(*volume.get(), *volumeqb.get(), true, true));
 }
