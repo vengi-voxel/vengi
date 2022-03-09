b4019dc29f8db80c78eee61eb19b1110e27b9f5c
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-03-09 18:46:17 +0100

    VOXELFORMAT: renamed variables for VXL AoS format

--- src/modules/voxelformat/AoSVXLFormat.cpp
+++ src/modules/voxelformat/AoSVXLFormat.cpp
@@ -14,9 +14,9 @@
 namespace voxel {
 
 bool AoSVXLFormat::loadGroups(const core::String& filename, io::SeekableReadStream &stream, SceneGraph &sceneGraph) {
-	const int width = 512, height = 512, depth = 64;
-	const voxel::Region region(0, 0, 0, width - 1, depth - 1, height - 1);
-	const int flipHeight = depth - 1;
+	const int width = 512, depths = 512, height = 64;
+	const voxel::Region region(0, 0, 0, width - 1, height - 1, depths - 1);
+	const int flipHeight = height - 1;
 	core_assert(region.isValid());
 	RawVolume *volume = new RawVolume(region);
 	SceneGraphNode node;
@@ -34,24 +34,25 @@ bool AoSVXLFormat::loadGroups(const core::String& filename, io::SeekableReadStre
 	const uint8_t *base = v;
 	// TODO: allow to export the palette/colors
 	core::Map<uint32_t, int, 521> paletteMap(32768);
-	for (int y = 0; y < height; ++y) {
+	for (int z = 0; z < depths; ++z) {
 		for (int x = 0; x < width; ++x) {
-			int z = 0;
+			int y = 0;
 			for (;;) {
 				const int number4byteChunks = v[0];
 				const int topColorStart = v[1];
 				const int topColorEnd = v[2]; // inclusive
 				int paletteIndex = 1;
 				const uint32_t *rgba = (const uint32_t *)(v + sizeof(uint32_t));
-				if (topColorStart < 0 || topColorStart >= depth) {
-					Log::error("depth (top start) exceeds the max allowed value of %i", depth);
+				if (topColorStart < 0 || topColorStart >= height) {
+					Log::error("depth (top start) exceeds the max allowed value of %i", height);
 					return false;
 				}
-				if (topColorEnd < 0 || topColorEnd >= depth) {
-					Log::error("depth (top end) exceeds the max allowed value of %i", depth);
+				if (topColorEnd < 0 || topColorEnd >= height) {
+					Log::error("depth (top end) exceeds the max allowed value of %i", height);
 					return false;
 				}
-				for (z = topColorStart; z <= topColorEnd; ++z) {
+				for (y = topColorStart; y <= topColorEnd; ++y) {
+					// TODO: BGRA with A not being alpha - but some shading stuff?
 					if (!paletteMap.get(*rgba, paletteIndex)) {
 						const glm::vec4& color = core::Color::fromRGBA(*rgba);
 						paletteIndex = findClosestIndex(color);
@@ -59,11 +60,11 @@ bool AoSVXLFormat::loadGroups(const core::String& filename, io::SeekableReadStre
 							paletteMap.put(*rgba, paletteIndex);
 						}
 					}
-					volume->setVoxel(x, flipHeight - z, y, voxel::createVoxel(voxel::VoxelType::Generic, paletteIndex));
+					volume->setVoxel(x, flipHeight - y, z, voxel::createVoxel(voxel::VoxelType::Generic, paletteIndex));
 					++rgba;
 				}
-				for (int i = z; i < depth; ++i) {
-					volume->setVoxel(x, flipHeight - i, y, voxel::createVoxel(voxel::VoxelType::Generic, paletteIndex));
+				for (int i = y; i < height; ++i) {
+					volume->setVoxel(x, flipHeight - i, z, voxel::createVoxel(voxel::VoxelType::Generic, paletteIndex));
 				}
 
 				const int lenBottom = topColorEnd - topColorStart + 1;
@@ -83,16 +84,16 @@ bool AoSVXLFormat::loadGroups(const core::String& filename, io::SeekableReadStre
 
 				const int bottomColorEnd = v[3]; // aka air start - exclusive
 				const int bottomColorStart = bottomColorEnd - len_top;
-				if (bottomColorStart < 0 || bottomColorStart >= depth) {
-					Log::error("depth (bottom start) exceeds the max allowed value of %i", depth);
+				if (bottomColorStart < 0 || bottomColorStart >= height) {
+					Log::error("depth (bottom start) exceeds the max allowed value of %i", height);
 					return false;
 				}
-				if (bottomColorEnd < 0 || bottomColorEnd >= depth) {
-					Log::error("depth (bottom end) exceeds the max allowed value of %i", depth);
+				if (bottomColorEnd < 0 || bottomColorEnd >= height) {
+					Log::error("depth (bottom end) exceeds the max allowed value of %i", height);
 					return false;
 				}
 
-				for (z = bottomColorStart; z < bottomColorEnd; ++z) {
+				for (y = bottomColorStart; y < bottomColorEnd; ++y) {
 					if (!paletteMap.get(*rgba, paletteIndex)) {
 						const glm::vec4& color = core::Color::fromRGBA(*rgba);
 						paletteIndex = findClosestIndex(color);
@@ -100,7 +101,7 @@ bool AoSVXLFormat::loadGroups(const core::String& filename, io::SeekableReadStre
 							paletteMap.put(*rgba, paletteIndex);
 						}
 					}
-					volume->setVoxel(x, flipHeight - z, y, voxel::createVoxel(voxel::VoxelType::Generic, paletteIndex));
+					volume->setVoxel(x, flipHeight - y, z, voxel::createVoxel(voxel::VoxelType::Generic, paletteIndex));
 					++rgba;
 				}
 			}
