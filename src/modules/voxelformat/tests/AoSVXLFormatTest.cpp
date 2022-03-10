332a0dc015b8ffa511ee2af74e5b9e176d5ee7cf
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-03-10 19:54:18 +0100

    VOXELFORMAT: added disabled aos vxl save test

--- src/modules/voxelformat/tests/AoSVXLFormatTest.cpp
+++ src/modules/voxelformat/tests/AoSVXLFormatTest.cpp
@@ -16,4 +16,29 @@ TEST_F(AoSVXLFormatTest, testLoad) {
 	ASSERT_NE(nullptr, volume) << "Could not load ace of spades file";
 }
 
+TEST_F(AoSVXLFormatTest, DISABLED_testSave) {
+	AoSVXLFormat f;
+	Region region(glm::ivec3(0), glm::ivec3(255, 64, 255));
+	RawVolume layer1(region);
+	const char *filename = "tests-aos.vxl";
+	for (int x = 0; x < region.getWidthInVoxels(); ++x) {
+		for (int z = 0; z < region.getDepthInVoxels(); ++z) {
+			EXPECT_TRUE(layer1.setVoxel(x, 0, z, createVoxel(VoxelType::Generic, 1)));
+			EXPECT_TRUE(layer1.setVoxel(x, 1, z, createVoxel(VoxelType::Generic, 1)));
+		}
+	}
+	SceneGraph sceneGraph;
+	voxel::SceneGraphNode node1;
+	node1.setVolume(&layer1, false);
+	sceneGraph.emplace(core::move(node1));
+	const io::FilePtr &sfile = open(filename, io::FileMode::SysWrite);
+	io::FileStream sstream(sfile);
+	ASSERT_TRUE(f.saveGroups(sceneGraph, sfile->name(), sstream));
+	SceneGraph sceneGraphLoad;
+	const io::FilePtr &file = open(filename);
+	io::FileStream stream(file);
+	EXPECT_TRUE(f.loadGroups(file->name(), stream, sceneGraphLoad));
+	EXPECT_EQ(sceneGraphLoad.size(), sceneGraph.size());
+}
+
 }
