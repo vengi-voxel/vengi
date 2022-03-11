3632ff73571a18c908a24eeacde1d2b7167695ee
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-03-11 12:58:48 +0100

    VOXEL: updated comment

--- src/modules/voxel/IsQuadNeeded.h
+++ src/modules/voxel/IsQuadNeeded.h
@@ -10,9 +10,13 @@
 namespace voxel {
 
 /**
- * @brief The criteria used here are that the voxel in front of the potential
- * quad should have a Air voxel while the voxel behind the potential quad
- * would have a voxel that is not Air.
+ * @brief implementation of a function object for deciding when
+ * the cubic surface extractor should insert a face between two voxels.
+ *
+ * The criteria used here are that the voxel in front of the potential
+ * quad should have a value of zero (which would typically indicate empty
+ * space) while the voxel behind the potential quad would have a value
+ * greater than zero (typically indicating it is solid).
  */
 struct IsQuadNeeded {
 	inline bool operator()(const VoxelType& back, const VoxelType& front, FaceNames face) const {
