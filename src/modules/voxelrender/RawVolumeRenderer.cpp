vengi-voxel/vengi

commit d4e58c03e2fa1620389aa26ed73e4fdd00568c0d

VOXELRENDER: removed polygon offset - produced render errors with multiple volumes

src/modules/voxelrender/RawVolumeRenderer.cpp
@@ -568,8 +568,6 @@ void RawVolumeRenderer::renderVolumes(const video::Camera& camera, bool shadow) 
 		if (indices[idx] <= 0u) {
 			continue;
 		}
-		const glm::vec2 offset(-0.25f * (float)idx, -0.5f * (float)idx);
-		video::ScopedPolygonMode polygonMode(mode, offset);
 		video::ScopedBuffer scopedBuf(_vertexBuffer[idx]);
 		_voxelShader.setGray(_state[idx]._gray);
 		_voxelShader.setModel(_models[idx]);

commit ec7096c3fe7ce61d62347f0f0a65de6387cb98bb

VOXELRENDER: fixed missing polygon mode
bug introduced in d4e58c03e2fa1620389aa26ed73e4fdd00568c0d

src/modules/voxelrender/RawVolumeRenderer.cpp
@@ -568,6 +568,7 @@ void RawVolumeRenderer::renderVolumes(const video::Camera& camera, bool shadow) 
 		if (indices[idx] <= 0u) {
 			continue;
 		}
+		video::ScopedPolygonMode polygonMode(mode);
 		video::ScopedBuffer scopedBuf(_vertexBuffer[idx]);
 		_voxelShader.setGray(_state[idx]._gray);
 		_voxelShader.setModel(_models[idx]);