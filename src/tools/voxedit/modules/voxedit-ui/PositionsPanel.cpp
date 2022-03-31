b746adc184dcdddd5747896bcc5beb13a7f5bd04
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-03-31 22:34:53 +0200

    VOXEDIT: statusbar for smaller windows

--- src/tools/voxedit/modules/voxedit-ui/PositionsPanel.cpp
+++ src/tools/voxedit/modules/voxedit-ui/PositionsPanel.cpp
@@ -14,7 +14,74 @@
 
 namespace voxedit {
 
+static bool xyzValues(const char *title, glm::ivec3 &v) {
+	bool retVal = false;
+	const float width = ImGui::CalcTextSize("10000").x + ImGui::GetStyle().FramePadding.x * 2.0f;
+
+	char buf[64];
+	core::String id = "##";
+	id.append(title);
+	id.append("0");
+
+	id.c_str()[id.size() - 1] = '0';
+	core::string::formatBuf(buf, sizeof(buf), "%i", v.x);
+	{
+		ui::imgui::ScopedStyle style;
+		style.setColor(ImGuiCol_Text, core::Color::Red);
+		ImGui::PushItemWidth(width);
+		if (ImGui::InputText(id.c_str(), buf, sizeof(buf),
+							 ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_AutoSelectAll)) {
+			retVal = true;
+			v.x = core::string::toInt(buf);
+		}
+		ImGui::SameLine(0.0f, 2.0f);
+
+		id.c_str()[id.size() - 1] = '1';
+		core::string::formatBuf(buf, sizeof(buf), "%i", v.y);
+		style.setColor(ImGuiCol_Text, core::Color::Green);
+		ImGui::PushItemWidth(width);
+		if (ImGui::InputText(id.c_str(), buf, sizeof(buf),
+							 ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_AutoSelectAll)) {
+			retVal = true;
+			v.y = core::string::toInt(buf);
+		}
+		ImGui::SameLine(0.0f, 2.0f);
+
+		id.c_str()[id.size() - 1] = '2';
+		core::string::formatBuf(buf, sizeof(buf), "%i", v.z);
+		style.setColor(ImGuiCol_Text, core::Color::Blue);
+		ImGui::PushItemWidth(width);
+		if (ImGui::InputText(id.c_str(), buf, sizeof(buf),
+							 ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_AutoSelectAll)) {
+			retVal = true;
+			v.z = core::string::toInt(buf);
+		}
+	}
+	ImGui::SameLine();
+	ImGui::TextUnformatted(title);
+
+	return retVal;
+}
+
 void PositionsPanel::modelView(command::CommandExecutionListener &listener) {
+	if (ImGui::CollapsingHeader(ICON_FA_ARROWS_ALT " Region", ImGuiTreeNodeFlags_DefaultOpen)) {
+		const int nodeId = sceneMgr().sceneGraph().activeNode();
+		const voxel::RawVolume *v = sceneMgr().volume(nodeId);
+		const voxel::Region &region = v->region();
+		glm::ivec3 mins = region.getLowerCorner();
+		glm::ivec3 maxs = region.getDimensionsInVoxels();
+		if (xyzValues("pos", mins)) {
+			const glm::ivec3 &f = mins - region.getLowerCorner();
+			sceneMgr().shift(nodeId, f);
+		}
+		if (xyzValues("size", maxs)) {
+			const glm::ivec3 &f = maxs - region.getDimensionsInVoxels();
+			sceneMgr().resize(nodeId, f);
+		}
+	}
+
+	ImGui::NewLine();
+
 	if (ImGui::CollapsingHeader(ICON_FA_ARROWS_ALT " Translate", ImGuiTreeNodeFlags_DefaultOpen)) {
 		static glm::ivec3 translate{0};
 		veui::InputAxisInt(math::Axis::X, "X##translate", &translate.x, 1);
@@ -75,7 +142,8 @@ void PositionsPanel::sceneView(command::CommandExecutionListener &listener) {
 			const uint32_t frame = sceneMgr().currentFrame();
 			voxelformat::SceneGraphTransform &transform = node.transform(node.keyFrameForFrame(frame));
 			float matrixTranslation[3], matrixRotation[3], matrixScale[3];
-			ImGuizmo::DecomposeMatrixToComponents(glm::value_ptr(transform.matrix()), matrixTranslation, matrixRotation, matrixScale);
+			ImGuizmo::DecomposeMatrixToComponents(glm::value_ptr(transform.matrix()), matrixTranslation, matrixRotation,
+												  matrixScale);
 			bool change = false;
 			glm::vec3 pivot = transform.pivot();
 			change |= ImGui::InputFloat3("Tr", matrixTranslation);
@@ -84,7 +152,8 @@ void PositionsPanel::sceneView(command::CommandExecutionListener &listener) {
 			change |= ImGui::InputFloat3("Pv", glm::value_ptr(pivot));
 			if (change) {
 				glm::mat4 matrix;
-				ImGuizmo::RecomposeMatrixFromComponents(matrixTranslation, matrixRotation, matrixScale, glm::value_ptr(matrix));
+				ImGuizmo::RecomposeMatrixFromComponents(matrixTranslation, matrixRotation, matrixScale,
+														glm::value_ptr(matrix));
 				transform.setMatrix(matrix);
 				transform.setPivot(glm::clamp(pivot, 0.0f, 1.0f));
 				transform.update();
@@ -94,7 +163,7 @@ void PositionsPanel::sceneView(command::CommandExecutionListener &listener) {
 
 	ImGui::NewLine();
 
-	if (ImGui::CollapsingHeader(ICON_FA_CUBE " Settings", ImGuiTreeNodeFlags_DefaultOpen)) {
+	if (ImGui::CollapsingHeader(ICON_FA_CUBE " Guizmo settings", ImGuiTreeNodeFlags_DefaultOpen)) {
 		ImGui::CheckboxVar("Flip Axis", cfg::VoxEditGuizmoAllowAxisFlip);
 		ImGui::CheckboxVar("Activate rotate", cfg::VoxEditGuizmoRotation);
 		ImGui::CheckboxVar("Snap", cfg::VoxEditGuizmoSnap);
--- src/tools/voxedit/modules/voxedit-ui/StatusBar.cpp
+++ src/tools/voxedit/modules/voxedit-ui/StatusBar.cpp
@@ -14,52 +14,6 @@
 
 namespace voxedit {
 
-static bool xyzValues(const char *title, glm::ivec3 &v) {
-	ImGui::TextUnformatted(title);
-	ImGui::SameLine();
-
-	bool retVal = false;
-	const float width = ImGui::CalcTextSize("10000").x + ImGui::GetStyle().FramePadding.x * 2.0f;
-
-	char buf[64];
-	core::String id = "##";
-	id.append(title);
-	id.append("0");
-
-	id.c_str()[id.size() - 1] = '0';
-	core::string::formatBuf(buf, sizeof(buf), "%i", v.x);
-	ui::imgui::ScopedStyle style;
-	style.setColor(ImGuiCol_Text, core::Color::Red);
-	ImGui::PushItemWidth(width);
-	if (ImGui::InputText(id.c_str(), buf, sizeof(buf), ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_AutoSelectAll)) {
-		retVal = true;
-		v.x = core::string::toInt(buf);
-	}
-	ImGui::SameLine(0.0f, 2.0f);
-
-	id.c_str()[id.size() - 1] = '1';
-	core::string::formatBuf(buf, sizeof(buf), "%i", v.y);
-	style.setColor(ImGuiCol_Text, core::Color::Green);
-	ImGui::PushItemWidth(width);
-	if (ImGui::InputText(id.c_str(), buf, sizeof(buf), ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_AutoSelectAll)) {
-		retVal = true;
-		v.y = core::string::toInt(buf);
-	}
-	ImGui::SameLine(0.0f, 2.0f);
-
-	id.c_str()[id.size() - 1] = '2';
-	core::string::formatBuf(buf, sizeof(buf), "%i", v.z);
-	style.setColor(ImGuiCol_Text, core::Color::Blue);
-	ImGui::PushItemWidth(width);
-	if (ImGui::InputText(id.c_str(), buf, sizeof(buf), ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_AutoSelectAll)) {
-		retVal = true;
-		v.z = core::string::toInt(buf);
-	}
-	ImGui::SameLine(0.0f, 2.0f);
-
-	return retVal;
-}
-
 void StatusBar::update(const char *title, float height, const core::String &lastExecutedCommand) {
 	ImGuiViewport *viewport = ImGui::GetMainViewport();
 	ImGui::SetNextWindowViewport(viewport->ID);
@@ -71,53 +25,17 @@ void StatusBar::update(const char *title, float height, const core::String &last
 	const uint32_t statusBarFlags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoBringToFrontOnFocus;
 	if (ImGui::Begin(title, nullptr, statusBarFlags)) {
 		core_trace_scoped(StatusBar);
+		ui::imgui::ScopedStyle scopedStyle;
+		scopedStyle.setItemSpacing(ImVec2(20, 0));
 		voxedit::SceneManager& sceneMgr = voxedit::sceneMgr();
-		const voxelformat::SceneGraph& sceneGraph = sceneMgr.sceneGraph();
 		const voxedit::ModifierFacade& modifier = sceneMgr.modifier();
-		const float fields = 4.0f;
-		const int nodeId = sceneGraph.activeNode();
-		const voxel::RawVolume* v = sceneMgr.volume(nodeId);
-		const voxel::Region& region = v->region();
-		glm::ivec3 mins = region.getLowerCorner();
-		glm::ivec3 maxs = region.getDimensionsInVoxels();
-		if (xyzValues("pos", mins)) {
-			const glm::ivec3 &f = mins - region.getLowerCorner();
-			sceneMgr.shift(nodeId, f);
-		}
-		ImGui::SameLine();
-		if (xyzValues("size", maxs)) {
-			const glm::ivec3 &f = maxs - region.getDimensionsInVoxels();
-			sceneMgr.resize(nodeId, f);
-		}
-		ImGui::SameLine();
-		ImGui::SetCursorPosX(size.x / fields * 1.0f);
-		if (modifier.aabbMode()) {
-			const glm::ivec3& dim = modifier.aabbDim();
-			ImGui::Text("w: %i, h: %i, d: %i", dim.x, dim.y, dim.z);
-		} else {
-			ImGui::TextUnformatted("w: 0, h: 0, d: 0");
-		}
-		ImGui::SameLine();
-		ImGui::SetCursorPosX(size.x / fields * 2.0f);
-		if (lastExecutedCommand.empty()) {
-			ImGui::TextUnformatted("Command: -");
-		} else {
-			const video::WindowedApp* app = video::WindowedApp::getInstance();
-			const core::String& keybindingStr = app->getKeyBindingsString(lastExecutedCommand.c_str());
-			if (keybindingStr.empty()) {
-				ImGui::Text("Command: %s", lastExecutedCommand.c_str());
-			} else {
-				ImGui::Text("Command: %s (%s)", lastExecutedCommand.c_str(), keybindingStr.c_str());
-			}
-		}
-		ImGui::SameLine();
-		ImGui::SetCursorPosX(size.x / fields * 3.0f);
-		ImGui::SetNextItemWidth(140.0f);
-		ImGui::InputVarInt("Grid size", cfg::VoxEditGridsize);
-		ImGui::SameLine();
 		ImGui::CheckboxVar("Grayscale", cfg::VoxEditGrayInactive);
 		ImGui::SameLine();
 		ImGui::CheckboxVar("Only active", cfg::VoxEditHideInactive);
+		ImGui::SameLine();
+
+		ImGui::SetNextItemWidth(120.0f);
+		ImGui::InputVarInt("Grid size", cfg::VoxEditGridsize);
 		ImGui::SameLine();
 		const EditMode editMode = sceneMgr.editMode();
 		if (editMode == EditMode::Animation) {
@@ -133,6 +51,27 @@ void StatusBar::update(const char *title, float height, const core::String &last
 		}
 		if (editMode == EditMode::Animation) {
 			ImGui::EndDisabled();
+		}
+		ImGui::SameLine();
+		const float afterSizeX = ImGui::GetCursorPosX();
+		if (modifier.aabbMode()) {
+			const glm::ivec3& dim = modifier.aabbDim();
+			ImGui::Text("w: %i, h: %i, d: %i", dim.x, dim.y, dim.z);
+		} else {
+			ImGui::TextUnformatted("w: 0, h: 0, d: 0");
+		}
+		ImGui::SameLine();
+		ImGui::SetCursorPosX(afterSizeX + 200.0f);
+		if (lastExecutedCommand.empty()) {
+			ImGui::TextUnformatted("Command: -");
+		} else {
+			const video::WindowedApp* app = video::WindowedApp::getInstance();
+			const core::String& keybindingStr = app->getKeyBindingsString(lastExecutedCommand.c_str());
+			if (keybindingStr.empty()) {
+				ImGui::Text("Command: %s", lastExecutedCommand.c_str());
+			} else {
+				ImGui::Text("Command: %s (%s)", lastExecutedCommand.c_str(), keybindingStr.c_str());
+			}
 		}
 	}
 	ImGui::End();
