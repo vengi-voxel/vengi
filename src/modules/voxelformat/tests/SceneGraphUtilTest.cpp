1238ec6b9841a28a4379ab42df1020ff98ef7943
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-05-31 19:18:42 +0200

    VOXELFORMAT: extended unittest

--- src/modules/voxelformat/tests/SceneGraphUtilTest.cpp
+++ src/modules/voxelformat/tests/SceneGraphUtilTest.cpp
@@ -28,6 +28,11 @@ TEST_F(SceneGraphUtilTest, testAddSceneGraphNodes) {
 	}
 	SceneGraph target;
 	EXPECT_EQ(1, addSceneGraphNodes(target, source, target.root().id()));
+	ASSERT_TRUE(target.hasNode(1));
+	EXPECT_EQ(SceneGraphNodeType::Group, target.node(1).type());
+	ASSERT_TRUE(target.hasNode(2));
+	EXPECT_EQ(SceneGraphNodeType::Model, target.node(2).type());
+	ASSERT_EQ(1, target.node(2).parent());
 }
 
 } // namespace voxelformat
