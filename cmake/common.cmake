8eca8027bdebd6221e46e530b988a3fc5c7c9b55
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-06-15 19:24:22 +0200

    CMAKE: search for luac5.4, too

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -3,8 +3,6 @@ include(CheckCXXCompilerFlag)
 include(CheckCXXSourceCompiles)
 include(CheckCCompilerFlag)
 
-set(DEFAULT_LUA_EXECUTABLE lua lua5.2 lua5.3)
-set(DEFAULT_LUAC_EXECUTABLE luac luac5.2 luac5.3)
 set(DATA_DIR ${ROOT_DIR}/data CACHE STRING "" FORCE)
 
 set(CMAKE_CXX_STANDARD 17)
--- cmake/lua.cmake
+++ cmake/lua.cmake
@@ -1,21 +1,13 @@
 function(check_lua_files TARGET)
 	set(files ${ARGN})
-	find_program(LUAC_EXECUTABLE NAMES ${DEFAULT_LUAC_EXECUTABLE})
-	set(ADDITIONAL_DEPENDENCIES)
-	if (LUAC_EXECUTABLE)
-		message(STATUS "${LUAC_EXECUTABLE} found")
-	else()
-		set(LUAC_EXECUTABLE luac)
-		list(APPEND ADDITIONAL_DEPENDENCIES luac)
-	endif()
 	foreach(_file ${files})
 		string(REGEX REPLACE "[/]" "_" targetname ${_file})
 		get_filename_component(filename ${_file} NAME)
 		set(_outfile ${targetname}.out)
 		add_custom_command(
 			OUTPUT ${_outfile}
-			COMMAND ${LUAC_EXECUTABLE} -o ${_outfile} ${CMAKE_CURRENT_SOURCE_DIR}/lua/${_file}
-			DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/lua/${_file} ${ADDITIONAL_DEPENDENCIES}
+			COMMAND $<TARGET_FILE:luac> -o ${_outfile} ${CMAKE_CURRENT_SOURCE_DIR}/lua/${_file}
+			DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/lua/${_file}
 			COMMENT "Validate ${_file}"
 		)
 		add_custom_target(${TARGET}-${_outfile} DEPENDS ${_outfile})
