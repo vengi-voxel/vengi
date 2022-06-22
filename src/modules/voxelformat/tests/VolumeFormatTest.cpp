090d94e9f18e7ca5e6177cf53a909c7df9181dc3
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-06-22 20:00:09 +0200

    VOXELFORMAT: added missing file

---
+++ src/modules/voxelformat/tests/VolumeFormatTest.cpp
@@ -0,0 +1,30 @@
+/**
+ * @file
+ */
+
+#include "voxelformat/VolumeFormat.h"
+#include "AbstractVoxFormatTest.h"
+#include "app/App.h"
+#include "io/File.h"
+
+namespace voxelformat {
+
+class VolumeFormatTest : public AbstractVoxFormatTest {};
+
+TEST_F(VolumeFormatTest, testImportPalette) {
+	voxel::Palette palette;
+	EXPECT_EQ(0, palette.colorCount);
+	EXPECT_TRUE(importPalette("vox_character.vox", palette));
+	EXPECT_EQ(256, palette.colorCount);
+}
+
+TEST_F(VolumeFormatTest, testLoadFormat) {
+	io::FilePtr file = io::filesystem()->open("vox_character.vox");
+	ASSERT_TRUE(file->validHandle());
+	io::FileStream stream(file);
+	SceneGraph newSceneGraph;
+	EXPECT_TRUE(loadFormat(file->name(), stream, newSceneGraph));
+	EXPECT_EQ(16u, newSceneGraph.size());
+}
+
+} // namespace voxelformat
