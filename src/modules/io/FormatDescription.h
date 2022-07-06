1a1e771ba0c8c47b5b755205b9c3b872424052c6
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-07-06 20:59:45 +0200

    IO: doxygen

--- src/modules/io/FormatDescription.h
+++ src/modules/io/FormatDescription.h
@@ -12,25 +12,34 @@ namespace io {
 
 struct FormatDescription {
 	core::String name;						/**< the name of the format */
-	core::Vector<core::String, 8> exts;					/**< the file extension - nullptr terminated list */
+	core::Vector<core::String, 8> exts;		/**< the file extension - nullptr terminated list */
 	bool (*isA)(uint32_t magic) = nullptr;	/**< function to check whether a magic byte matches for the format description */
 	uint32_t flags = 0u;					/**< flags for user defines properties */
 
-	inline bool valid() const { return !name.empty(); }
+	inline bool valid() const {
+		return !name.empty();
+	}
 	bool operator<(const FormatDescription &rhs) const;
 	core::String wildCard() const;
 	bool matchesExtension(const core::String &fileExt) const;
 };
 
+/**
+ * @param desc a terminated list of @c FormatDescription objects
+ * @return a comma separated list of the extension wildcards (e.g. @code *.ext,*.ext2 @endcode)
+ */
 extern core::String convertToAllFilePattern(const FormatDescription *desc);
+/**
+ * @return @code Name (*.ext1,*.ext2) @endcode
+ */
 extern core::String convertToFilePattern(const FormatDescription &desc);
-extern bool isImage(const core::String& file);
+extern bool isImage(const core::String &file);
 
 namespace format {
 
-const FormatDescription* images();
-const FormatDescription* lua();
+const FormatDescription *images();
+const FormatDescription *lua();
 
-}
+} // namespace format
 
-}
+} // namespace io
