8083af6d37a74548fe316377f12839df82aa73c8
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-07-17 00:49:06 +0200

    VOXELUTIL: allow to resize into target region

--- src/modules/voxelutil/VolumeResizer.cpp
+++ src/modules/voxelutil/VolumeResizer.cpp
@@ -8,6 +8,14 @@
 
 namespace voxelutil {
 
+voxel::RawVolume* resize(const voxel::RawVolume* source, const voxel::Region &region) {
+	voxel::RawVolume* newVolume = new voxel::RawVolume(region);
+	const voxel::Region& srcRegion = source->region();
+	voxel::RawVolumeWrapper wrapper(newVolume);
+	voxelutil::mergeVolumes(&wrapper, source, srcRegion, srcRegion);
+	return newVolume;
+}
+
 voxel::RawVolume* resize(const voxel::RawVolume* source, const glm::ivec3& size, bool extendMins) {
 	voxel::Region region = source->region();
 	region.shiftUpperCorner(size);
@@ -17,11 +25,7 @@ voxel::RawVolume* resize(const voxel::RawVolume* source, const glm::ivec3& size,
 	if (!region.isValid()) {
 		return nullptr;
 	}
-	voxel::RawVolume* newVolume = new voxel::RawVolume(region);
-	const voxel::Region& srcRegion = source->region();
-	voxel::RawVolumeWrapper wrapper(newVolume);
-	voxelutil::mergeVolumes(&wrapper, source, srcRegion, srcRegion);
-	return newVolume;
+	return resize(source, region);
 }
 
 }
--- src/modules/voxelutil/VolumeResizer.h
+++ src/modules/voxelutil/VolumeResizer.h
@@ -8,6 +8,7 @@
 
 namespace voxelutil {
 
+extern voxel::RawVolume *resize(const voxel::RawVolume *source, const voxel::Region &region);
 extern voxel::RawVolume *resize(const voxel::RawVolume *source, const glm::ivec3 &size, bool extendMins = false);
 
-}
+} // namespace voxelutil
