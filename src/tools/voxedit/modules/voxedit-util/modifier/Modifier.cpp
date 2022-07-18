20471c5346c4a8d9687b9dbcaceacafe4aacbc3b
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-07-19 01:40:47 +0200

    VOXEDIT: yet another attempt to please msvc

--- src/tools/voxedit/modules/voxedit-util/modifier/Modifier.cpp
+++ src/tools/voxedit/modules/voxedit-util/modifier/Modifier.cpp
@@ -424,21 +424,22 @@ bool Modifier::aabbAction(voxel::RawVolume *volume,
 	}
 	const bool pathFlag = (_modifierType & ModifierType::Path) == ModifierType::Path;
 	if (pathFlag) {
-		auto isVoxelValidForPath = [](const voxel::RawVolume *vol, const glm::ivec3 &pos) {
-			if (voxel::isBlocked(vol->voxel(pos).getMaterial())) {
-				return false;
-			}
-			return voxelutil::isTouching(vol, pos);
-		};
-		auto isVoxelValidForLine = [](const voxel::RawVolume *vol, const glm::ivec3 &pos) {
-			return true;
-		};
-		auto func = lineFlag ? isVoxelValidForLine : isVoxelValidForPath;
 		core::List<glm::ivec3> listResult(4096);
 		const glm::ivec3 &start = referencePosition();
 		const glm::ivec3 &end = cursorPosition();
-		voxelutil::AStarPathfinderParams<voxel::RawVolume> params(volume, start, end, &listResult, func,
-																  4.0f, 10000, voxelutil::Connectivity::EighteenConnected);
+		voxelutil::AStarPathfinderParams<voxel::RawVolume> params(
+			volume, start, end, &listResult,
+			[=](const voxel::RawVolume *vol, const glm::ivec3 &pos) {
+				if (lineFlag) {
+					return true;
+				}
+
+				if (voxel::isBlocked(vol->voxel(pos).getMaterial())) {
+					return false;
+				}
+				return voxelutil::isTouching(vol, pos);
+			},
+			4.0f, 10000, voxelutil::Connectivity::EighteenConnected);
 		voxelutil::AStarPathfinder pathfinder(params);
 		if (!pathfinder.execute()) {
 			return false;
