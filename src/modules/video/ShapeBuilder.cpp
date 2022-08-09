ef4254f340c93ea2992f8aeb1e3bd6f4766b2145
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-08-09 18:01:03 +0200

    VIDEO: do the translation after we rotated

--- src/modules/video/ShapeBuilder.cpp
+++ src/modules/video/ShapeBuilder.cpp
@@ -166,7 +166,7 @@ void ShapeBuilder::obb(const math::OBB<float>& obb) {
 	reserve(lengthof(vecs), 24);
 
 	for (size_t i = 0; i < lengthof(vecs); ++i) {
-		addVertex(obb.rotation() * glm::vec4(vecs[i] * halfWidth + center, 1.0f));
+		addVertex(glm::vec3(obb.rotation() * glm::vec4(vecs[i] * halfWidth, 1.0f)) + center);
 	}
 
 	// front
