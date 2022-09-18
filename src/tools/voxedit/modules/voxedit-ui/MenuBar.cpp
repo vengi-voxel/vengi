a6f9482413e5e26340080caac0877b848c399783
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-09-18 22:39:06 +0200

    VOXEDIT: allow to modify the ui font size in the options menu

--- src/tools/voxedit/modules/voxedit-ui/MenuBar.cpp
+++ src/tools/voxedit/modules/voxedit-ui/MenuBar.cpp
@@ -81,6 +81,7 @@ bool MenuBar::update(ui::imgui::IMGUIApp* app, command::CommandExecutionListener
 				ImGui::CheckboxVar("Shadow", cfg::VoxEditRendershadow);
 				ImGui::CheckboxVar("Bloom", cfg::ClientBloom);
 				ImGui::SliderVarInt("Zoom speed", cfg::VoxEditCameraZoomSpeed, 10, 200);
+				ImGui::InputVarInt("Font size", cfg::UIFontSize, 1, 5);
 				ImGui::CheckboxVar("Outlines", cfg::RenderOutline);
 				ImGui::InputVarFloat("Notifications", cfg::UINotifyDismissMillis);
 				if (ImGui::Button("Reset layout")) {
