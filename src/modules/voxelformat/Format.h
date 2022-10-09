23cff862654cf2386a7c831eb4cc3f0cdd1d22c6
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-10-09 19:39:32 +0200

    VOXELFORMAT: missing final for method

--- src/modules/voxelformat/Format.h
+++ src/modules/voxelformat/Format.h
@@ -109,7 +109,7 @@ protected:
 
 public:
 	size_t loadPalette(const core::String &filename, io::SeekableReadStream& stream, voxel::Palette &palette) override;
-	bool loadGroups(const core::String &filename, io::SeekableReadStream& stream, SceneGraph& sceneGraph) override {
+	bool loadGroups(const core::String &filename, io::SeekableReadStream& stream, SceneGraph& sceneGraph) override final {
 		voxel::Palette palette;
 		if (!loadGroupsPalette(filename, stream, sceneGraph, palette)) {
 			return false;
