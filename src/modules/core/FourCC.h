a2823dd3eec767a768d6e488c051efa114cacbac
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-11-02 00:04:48 +0100

    CORE: reverted: "added big endian support to FourCC"
    This reverts commit 454a1bbc178282f80ba198596b8158d252199a52.

--- src/modules/core/FourCC.h
+++ src/modules/core/FourCC.h
@@ -5,26 +5,14 @@
 #pragma once
 
 #include <stdint.h>
-#include <SDL_endian.h>
 
 inline constexpr uint32_t FourCC(uint8_t a, uint8_t b, uint8_t c, uint8_t d) {
-#if SDL_BYTEORDER == SDL_LIL_ENDIAN
-	return (uint32_t) ((uint32_t(d) << 24) | (uint32_t(c) << 16) | (uint32_t(b) << 8) | uint32_t(a));
-#else
-	return (uint32_t) ((uint32_t(a) << 24) | (uint32_t(b) << 16) | (uint32_t(c) << 8) | uint32_t(d));
-#endif
+	return ((uint32_t) ((uint32_t(d) << 24) | (uint32_t(c) << 16) | (uint32_t(b) << 8) | uint32_t(a)));
 }
 
 inline void FourCCRev(uint8_t out[4], uint32_t in) {
-#if SDL_BYTEORDER == SDL_LIL_ENDIAN
 	out[3] = (uint8_t)((in >> 24) & 0xff);
 	out[2] = (uint8_t)((in >> 16) & 0xff);
 	out[1] = (uint8_t)((in >> 8) & 0xff);
 	out[0] = (uint8_t)((in >> 0) & 0xff);
-#else
-	out[0] = (uint8_t)((in >> 24) & 0xff);
-	out[1] = (uint8_t)((in >> 16) & 0xff);
-	out[2] = (uint8_t)((in >> 8) & 0xff);
-	out[3] = (uint8_t)((in >> 0) & 0xff);
-#endif
 }
