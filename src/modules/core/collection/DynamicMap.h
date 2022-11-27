61b557c5072bc1dd404e2ba351d73841fe952c68
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-11-27 14:55:48 +0100

    CORE: added dynamic growing map without size limitation

---
+++ src/modules/core/collection/DynamicMap.h
@@ -0,0 +1,308 @@
+/**
+ * @file
+ */
+
+#pragma once
+
+#include "core/Common.h"
+#include "core/collection/Array.h"
+#include <stdint.h>
+#include <stddef.h>
+#include <initializer_list>
+
+namespace core {
+
+namespace privdynamicmap {
+
+struct EqualCompare {
+	template<typename T>
+	inline bool operator() (const T& lhs, const T& rhs) const {
+		return lhs == rhs;
+	}
+};
+
+struct DefaultHasher {
+	template<typename T>
+	inline size_t operator() (const T& o) const {
+		return (size_t)o;
+	}
+};
+
+struct SharedPtrHasher {
+	template<typename T>
+	inline size_t operator() (const T& o) const {
+		return (size_t)(intptr_t)o.get();
+	}
+};
+
+}
+
+/**
+ * @brief Dynamic growing hash map.
+ * @sa Map
+ * @ingroup Collections
+ */
+template<typename KEYTYPE, typename VALUETYPE, size_t BUCKETSIZE = 11, typename HASHER = privdynamicmap::DefaultHasher, typename COMPARE = privdynamicmap::EqualCompare>
+class DynamicMap {
+public:
+	using value_type = VALUETYPE;
+	using key_type = KEYTYPE;
+
+	struct KeyValue {
+		inline KeyValue(const KEYTYPE& _key, const VALUETYPE& _value) :
+				key(_key), value(_value), next(nullptr), first(key), second(value) {
+		}
+
+		inline KeyValue(const KEYTYPE& _key, VALUETYPE&& _value) :
+				key(_key), value(core::forward<VALUETYPE>(_value)), next(nullptr), first(key), second(value) {
+		}
+
+		inline KeyValue(KeyValue &&other) noexcept :
+				key(core::move(other.key)), value(core::move(other.value)), next(
+						nullptr), first(key), second(value) {
+		}
+
+		KEYTYPE key;
+		VALUETYPE value;
+		KeyValue *next;
+		const KEYTYPE &first;
+		const VALUETYPE &second;
+	};
+private:
+	core::Array<KeyValue *, BUCKETSIZE> _buckets;
+	HASHER _hasher;
+	size_t _size;
+public:
+	DynamicMap(std::initializer_list<KeyValue> other) {
+		_buckets.fill(nullptr);
+		for (auto i = other.begin(); i != other.end(); ++i) {
+			put(i->key, i->value);
+		}
+	}
+	DynamicMap() {
+		_buckets.fill(nullptr);
+	}
+	DynamicMap(const DynamicMap& other) {
+		_buckets.fill(nullptr);
+		for (auto i = other.begin(); i != other.end(); ++i) {
+			put(i->key, i->value);
+		}
+	}
+	DynamicMap(DynamicMap&& other) noexcept {
+		_buckets = other._buckets;
+		_hasher = other._hasher;
+		_size = other._size;
+		other._buckets.fill(nullptr);
+	}
+	~DynamicMap() {
+		clear();
+	}
+	DynamicMap &operator=(DynamicMap &&other) noexcept {
+		if (this != &other) {
+			clear();
+			_buckets = other._buckets;
+			_hasher = other._hasher;
+			_size = other._size;
+			other._buckets.fill(nullptr);
+		}
+		return *this;
+	}
+
+	DynamicMap& operator=(const DynamicMap& other) {
+		clear();
+		_buckets.fill(nullptr);
+		for (auto i = other.begin(); i != other.end(); ++i) {
+			put(i->key, i->value);
+		}
+		return *this;
+	}
+
+	class iterator {
+	private:
+		const DynamicMap* _map;
+		size_t _bucket;
+		KeyValue* _ptr;
+	public:
+		constexpr iterator() :
+			_map(nullptr), _bucket(0), _ptr(nullptr) {
+		}
+
+		iterator(const DynamicMap* map, size_t bucket, KeyValue *ptr) :
+				_map(map), _bucket(bucket), _ptr(ptr) {
+		}
+
+		inline KeyValue* operator*() const {
+			return _ptr;
+		}
+
+		iterator& operator++() {
+			if (_ptr->next != nullptr) {
+				_ptr = _ptr->next;
+				return *this;
+			}
+			size_t bucket = _bucket;
+			for (++bucket; bucket < BUCKETSIZE; ++bucket) {
+				KeyValue* ptr = _map->_buckets[bucket];
+				if (ptr != nullptr) {
+					_ptr = ptr;
+					_bucket = bucket;
+					return *this;
+				}
+			}
+			_ptr = nullptr;
+			_bucket = 0;
+			return *this;
+		}
+
+		inline KeyValue* operator->() const {
+			return _ptr;
+		}
+
+		inline bool operator!=(const iterator& rhs) const {
+			return _ptr != rhs._ptr;
+		}
+
+		inline bool operator==(const iterator& rhs) const {
+			return _ptr == rhs._ptr;
+		}
+	};
+
+	inline size_t size() const {
+		return _size;
+	}
+
+	bool get(const KEYTYPE& key, VALUETYPE& value) const {
+		const size_t hashValue = (size_t)_hasher(key);
+		KeyValue *entry = _buckets[hashValue % BUCKETSIZE];
+		while (entry != nullptr) {
+			if (COMPARE()(entry->key, key)) {
+				value = entry->value;
+				return true;
+			}
+			entry = entry->next;
+		}
+		return false;
+	}
+
+	bool hasKey(const KEYTYPE& key) const {
+		return find(key) != end();
+	}
+
+	iterator find(const KEYTYPE& key) const {
+		const size_t hashValue = (size_t)_hasher(key);
+		KeyValue *entry = _buckets[hashValue % BUCKETSIZE];
+		while (entry != nullptr) {
+			if (COMPARE()(entry->key, key)) {
+				return iterator(this, hashValue % BUCKETSIZE, entry);
+			}
+			entry = entry->next;
+		}
+		return end();
+	}
+
+	void emplace(const KEYTYPE& key, VALUETYPE&& value) {
+		const size_t hashValue = (size_t)_hasher(key);
+		KeyValue *prev = nullptr;
+		KeyValue *entry = _buckets[hashValue % BUCKETSIZE];
+
+		while (entry != nullptr && !COMPARE()(entry->key, key)) {
+			prev = entry;
+			entry = entry->next;
+		}
+
+		if (entry == nullptr) {
+			entry = new KeyValue(key, core::forward<VALUETYPE>(value));
+			if (prev == nullptr) {
+				_buckets[hashValue % BUCKETSIZE] = entry;
+			} else {
+				prev->next = entry;
+			}
+		} else {
+			entry->value = core::forward<VALUETYPE>(value);
+		}
+		++_size;
+	}
+
+	void put(const KEYTYPE& key, const VALUETYPE& value) {
+		const size_t hashValue = (size_t)_hasher(key);
+		KeyValue *prev = nullptr;
+		KeyValue *entry = _buckets[hashValue % BUCKETSIZE];
+
+		while (entry != nullptr && !COMPARE()(entry->key, key)) {
+			prev = entry;
+			entry = entry->next;
+		}
+
+		if (entry == nullptr) {
+			entry = new KeyValue(key, value);
+			if (prev == nullptr) {
+				_buckets[hashValue % BUCKETSIZE] = entry;
+			} else {
+				prev->next = entry;
+			}
+		} else {
+			entry->value = value;
+		}
+		++_size;
+	}
+
+	iterator begin() const {
+		for (size_t i = 0u; i < BUCKETSIZE; ++i) {
+			KeyValue *entry = _buckets[i];
+			if (entry != nullptr) {
+				return iterator(this, i, entry);
+			}
+		}
+		return end();
+	}
+
+	constexpr iterator end() const {
+		return iterator();
+	}
+
+	void clear() {
+		for (size_t i = 0u; i < BUCKETSIZE; ++i) {
+			KeyValue *entry = _buckets[i];
+			while (entry != nullptr) {
+				KeyValue *prev = entry;
+				entry = entry->next;
+				delete prev;
+			}
+			_buckets[i] = nullptr;
+		}
+		_size = 0;
+	}
+
+	inline void erase(const iterator& iter) {
+		remove(iter->key);
+	}
+
+	bool remove(const KEYTYPE& key) {
+		const size_t hashValue = (size_t)_hasher(key);
+		KeyValue *entry = _buckets[hashValue % BUCKETSIZE];
+		KeyValue *prev = nullptr;
+
+		while (entry != nullptr) {
+			if (COMPARE()(entry->key, key)) {
+				break;
+			}
+			prev = entry;
+			entry = entry->next;
+		}
+		if (entry == nullptr) {
+			return false;
+		}
+
+		if (prev == nullptr) {
+			_buckets[hashValue % BUCKETSIZE] = entry->next;
+		} else {
+			prev->next = entry->next;
+		}
+
+		delete entry;
+		--_size;
+		return true;
+	}
+};
+
+}
---
+++ src/modules/core/tests/DynamicMapTest.cpp
@@ -0,0 +1,160 @@
+/**
+ * @file
+ */
+
+#include <gtest/gtest.h>
+#include "core/collection/StringMap.h"
+#include "core/SharedPtr.h"
+
+namespace core {
+
+TEST(DynamicMapTest, testPutGet) {
+	core::Map<int64_t, int64_t, 11, std::hash<int64_t>> map;
+	map.put(1, 1);
+	map.put(1, 2);
+	map.put(2, 1);
+	map.put(3, 1337);
+	map.put(4, 42);
+	map.put(5, 111);
+	map.put(6, 1111);
+	int64_t value;
+	EXPECT_TRUE(map.get(1, value));
+	EXPECT_EQ(2, value);
+	EXPECT_TRUE(map.get(2, value));
+	EXPECT_EQ(1, value);
+	EXPECT_TRUE(map.get(3, value));
+	EXPECT_EQ(1337, value);
+	EXPECT_TRUE(map.get(4, value));
+	EXPECT_EQ(42, value);
+	EXPECT_TRUE(map.get(5, value));
+	EXPECT_EQ(111, value);
+	EXPECT_TRUE(map.get(6, value));
+	EXPECT_EQ(1111, value);
+}
+
+TEST(DynamicMapTest, testCollision) {
+	core::Map<int64_t, int64_t, 11, std::hash<int64_t>> map;
+	for (int64_t i = 0; i < 128; ++i) {
+		map.put(i, i);
+	}
+	int64_t value;
+	for (int64_t i = 0; i < 128; ++i) {
+		EXPECT_TRUE(map.get(i, value));
+		EXPECT_EQ(i, value);
+	}
+}
+
+TEST(DynamicMapTest, testClear) {
+	core::Map<int64_t, int64_t, 11, std::hash<int64_t>> map;
+	for (int64_t i = 0; i < 16; ++i) {
+		map.put(i, i);
+	}
+	EXPECT_EQ(16u, map.size());
+	EXPECT_FALSE(map.empty());
+	map.clear();
+	EXPECT_EQ(0u, map.size());
+	EXPECT_TRUE(map.empty());
+}
+
+TEST(DynamicMapTest, testFind) {
+	core::Map<int64_t, int64_t, 11, std::hash<int64_t>> map;
+	for (int64_t i = 0; i < 1024; i += 2) {
+		map.put(i, i);
+	}
+	auto iter = map.find(0);
+	EXPECT_NE(map.end(), iter);
+	EXPECT_EQ(0u, iter->value);
+
+	iter = map.find(1);
+	EXPECT_EQ(map.end(), iter);
+}
+
+TEST(DynamicMapTest, testIterator) {
+	core::Map<int64_t, int64_t, 11, std::hash<int64_t>> map;
+	EXPECT_EQ(map.begin(), map.end());
+	EXPECT_EQ(map.end(), map.find(42));
+	map.put(1, 1);
+	EXPECT_NE(map.begin(), map.end());
+	EXPECT_EQ(++map.begin(), map.end());
+}
+
+TEST(DynamicMapTest, testIterate) {
+	// leave empty buckets
+	core::Map<int64_t, int64_t, 11, std::hash<int64_t>> map;
+	for (int64_t i = 0; i < 32; i += 2) {
+		map.put(i, i);
+	}
+	EXPECT_EQ(16u, map.size());
+	int cnt = 0;
+	for (auto iter = map.begin(); iter != map.end(); ++iter) {
+		++cnt;
+	}
+	EXPECT_EQ(16, cnt);
+
+	for (int64_t i = 0; i < 1024; ++i) {
+		map.put(i, i);
+	}
+	EXPECT_EQ(1024u, map.size());
+	cnt = 0;
+	for (auto iter = map.begin(); iter != map.end(); ++iter) {
+		++cnt;
+	}
+	EXPECT_EQ(1024, cnt);
+}
+
+TEST(DynamicMapTest, testIterateRangeBased) {
+	core::Map<int64_t, int64_t, 11, std::hash<int64_t>> map;
+	for (int64_t i = 0; i < 32; i += 2) {
+		map.put(i, i);
+	}
+	EXPECT_EQ(16u, map.size());
+	int cnt = 0;
+	for (auto iter : map) {
+		EXPECT_EQ(iter->key, iter->value);
+		++cnt;
+	}
+	EXPECT_EQ(16, cnt);
+}
+
+TEST(DynamicMapTest, testStringSharedPtr) {
+	core::StringMap<core::SharedPtr<core::String>, 4> map;
+	auto foobar = core::SharedPtr<core::String>::create("foobar");
+	map.put("foobar", foobar);
+	map.put("barfoo", core::SharedPtr<core::String>::create("barfoo"));
+	map.put("foobar", core::SharedPtr<core::String>::create("barfoo"));
+	for (auto iter = map.begin(); iter != map.end(); ++iter) {
+	}
+	map.clear();
+	foobar = core::SharedPtr<core::String>();
+}
+
+TEST(DynamicMapTest, testCopy) {
+	core::StringMap<core::SharedPtr<core::String>> map;
+	map.put("foobar", core::SharedPtr<core::String>::create("barfoo"));
+	auto map2 = map;
+	map2.clear();
+}
+
+TEST(DynamicMapTest, testErase) {
+	core::StringMap<core::SharedPtr<core::String>> map;
+	map.put("foobar", core::SharedPtr<core::String>::create("barfoo"));
+	EXPECT_EQ(1u, map.size());
+	auto iter = map.find("foobar");
+	EXPECT_NE(iter, map.end());
+	map.erase(iter);
+	EXPECT_EQ(0u, map.size());
+}
+
+TEST(DynamicMapTest, testAssign) {
+	core::StringMap<core::SharedPtr<core::String>> map;
+	map.put("foobar", core::SharedPtr<core::String>::create("barfoo"));
+	core::StringMap<core::SharedPtr<core::String>> map2;
+	map2 = map;
+	EXPECT_EQ(1u, map.size());
+	EXPECT_EQ(1u, map2.size());
+	map2.clear();
+	EXPECT_EQ(1u, map.size());
+	EXPECT_EQ(0u, map2.size());
+}
+
+}
