fc011e7b812fee2654e26d26cf83f9a7b5af2e97
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-12-03 19:31:04 +0100

    CORE: extended Pair functionality

--- src/modules/core/Pair.h
+++ src/modules/core/Pair.h
@@ -6,10 +6,18 @@
 
 namespace core {
 
-template <typename FIRST, typename SECOND>
-struct Pair {
-	FIRST first {};
-	SECOND second {};
+template <typename FIRST, typename SECOND> struct Pair {
+	FIRST first{};
+	SECOND second{};
+
+	constexpr Pair() {
+	}
+
+	constexpr Pair(FIRST &&_first, SECOND &&_second) : first(_first), second(_second) {
+	}
+
+	Pair(const FIRST &_first, const SECOND &_second) : first(_first), second(_second) {
+	}
 };
 
 } // namespace core
