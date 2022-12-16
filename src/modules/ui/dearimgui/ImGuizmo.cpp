8fcde826691cf446586133cf336ae339b651a8a7
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-12-16 16:44:40 +0100

    LIBS: updated dearimgui

--- src/modules/ui/dearimgui/ImGuizmo.cpp
+++ src/modules/ui/dearimgui/ImGuizmo.cpp
@@ -1067,7 +1067,6 @@ namespace IMGUIZMO_NAMESPACE
       // compute scale from the size of camera right vector projected on screen at the matrix position
       vec_t pointRight = viewInverse.v.right;
       pointRight.TransformPoint(gContext.mViewProjection);
-      gContext.mScreenFactor = gContext.mGizmoSizeClipSpace / (pointRight.x / pointRight.w - gContext.mMVP.v.position.x / gContext.mMVP.v.position.w);
 
       vec_t rightViewInverse = viewInverse.v.right;
       rightViewInverse.TransformVector(gContext.mModelInverse);
@@ -2934,7 +2933,7 @@ namespace IMGUIZMO_NAMESPACE
                interpolationUp = referenceUp;
             }
             interpolationFrames = 40;
-            
+
          }
          isClicking = false;
          isDraging = false;
