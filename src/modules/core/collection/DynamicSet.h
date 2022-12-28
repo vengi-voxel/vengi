cbdfdad6df635417fe259349796815e38a465ec8
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-12-28 18:43:13 +0100

    CORE: added DynamicSet class
    and extended color tests

---
+++ src/modules/core/collection/DynamicSet.h
@@ -0,0 +1,44 @@
+/**
+ * @file
+ */
+
+#pragma once
+
+#include "core/collection/DynamicMap.h"
+
+namespace core {
+
+template<class T, size_t BUCKETSIZE = 11, typename HASHER = privdynamicmap::DefaultHasher, typename COMPARE = privdynamicmap::EqualCompare>
+class DynamicSet : public DynamicMap<T, bool, BUCKETSIZE, HASHER, COMPARE> {
+private:
+	using Super = DynamicMap<T, bool, BUCKETSIZE, HASHER, COMPARE>;
+public:
+	bool insert(const T& key) {
+		if (has(key)) {
+			return false;
+		}
+		this->put(key, true);
+		return true;
+	}
+
+	template<class ITER>
+	void insert(ITER first, ITER last) {
+		while (first != last) {
+			this->put(*first, true);
+			++first;
+		}
+	}
+
+	template<class FUNC>
+	void insert(size_t n, FUNC&& func) {
+		for (size_t i = 0u; i < n; ++i) {
+			insert(TYPE(func(i)));
+		}
+	}
+
+	inline bool has(const T& key) const {
+		return this->hasKey(key);
+	}
+};
+
+}
