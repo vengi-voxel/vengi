39b289cce04d988c693906f1d9c8240870344ead
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-12-30 19:37:59 +0100

    VOXELFORMAT: removed final from loadGroups

--- src/modules/voxelformat/Format.h
+++ src/modules/voxelformat/Format.h
@@ -162,7 +162,7 @@ class RGBAFormat : public Format {
 protected:
 	uint8_t _flattenFactor;
 	virtual bool loadGroupsRGBA(const core::String &filename, io::SeekableReadStream& stream, SceneGraph& sceneGraph, const voxel::Palette &palette) = 0;
-	bool loadGroups(const core::String &filename, io::SeekableReadStream& stream, SceneGraph& sceneGraph) override final;
+	bool loadGroups(const core::String &filename, io::SeekableReadStream& stream, SceneGraph& sceneGraph) override;
 	core::RGBA flattenRGB(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 255) const;
 	core::RGBA flattenRGB(core::RGBA rgba) const;
 public:
