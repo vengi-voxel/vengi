vengi-voxel/vengi

commit 27a92dbd3289bcd6cc0df0b5b746332cb88daf49

VOXEDIT: implemented feature request #214
you can now toggle the scene mode per viewport

src/tools/voxedit/modules/voxedit-util/modifier/ModifierButton.cpp
@@ -14,7 +14,8 @@ ModifierButton::ModifierButton(ModifierType newType) :
 
 bool ModifierButton::handleDown(int32_t key, double pressedMillis) {
 	const bool initialDown = Super::handleDown(key, pressedMillis);
-	if (sceneMgr().editMode() == EditMode::Scene) {
+	// scene mode
+	if (core::bindingContext() == core::BindingContext::Context1) {
 		return initialDown;
 	}
 	if (_secondAction) {

src/modules/voxelrender/RawVolumeRenderer.h
@@ -44,6 +44,7 @@ namespace voxelrender {
 struct RenderContext {
 	video::FrameBuffer frameBuffer;
 	render::BloomRenderer bloomRenderer;
+	bool sceneMode = false;
 
 	bool init(const glm::ivec2 &size);
 	void shutdown();

src/tools/voxedit/VoxEdit.cpp
@@ -11,6 +11,7 @@
 #include "core/concurrent/Concurrency.h"
 #include "io/FormatDescription.h"
 #include "video/WindowedApp.h"
+#include "voxedit-ui/Viewport.h"
 #include "voxedit-util/Config.h"
 #include "core/TimeProvider.h"
 #include "command/Command.h"
@@ -86,6 +87,13 @@ static bool ivec3ListValidator(const core::String& value) {
 	return true;
 }
 
+void VoxEdit::toggleScene() {
+	if (_mainWindow == nullptr) {
+		return;
+	}
+	_mainWindow->toggleScene();
+}
+
 app::AppState VoxEdit::onConstruct() {
 	core::Var::get(cfg::ClientCameraMaxZoom, "1000.0");
 	core::Var::get(cfg::ClientCameraMinZoom, "0.1");
@@ -141,6 +149,10 @@ app::AppState VoxEdit::onConstruct() {
 		}
 		_mainWindow->saveScreenshot(args[0]);
 	}).setArgumentCompleter(command::fileCompleter(io::filesystem(), _lastDirectory)).setHelp("Save the current viewport as screenshot");
+
+	command::Command::registerCommand("togglescene", [this](const command::CmdArgs &args) {
+		toggleScene();
+	}).setHelp("Toggle scene mode on/off");
 
 	command::Command::registerCommand("save", [this](const command::CmdArgs &args) {
 		if (_mainWindow == nullptr) {
@@ -317,9 +329,9 @@ app::AppState VoxEdit::onRunning() {
 	if (state != app::AppState::Running) {
 		return state;
 	}
-	const bool isSceneHovered = _mainWindow->isSceneHovered();
-	if (isSceneHovered) {
-		if (voxedit::sceneMgr().editMode() == voxedit::EditMode::Scene) {
+	const voxedit::Viewport *scene = _mainWindow->hoveredScene();
+	if (scene) {
+		if (scene->isSceneMode()) {
 			core::setBindingContext(core::BindingContext::Context1);
 		} else {
 			core::setBindingContext(core::BindingContext::Context2);

src/tools/voxedit/VoxEdit.h
@@ -41,4 +41,6 @@ public:
 	app::AppState onInit() override;
 	app::AppState onCleanup() override;
 	app::AppState onRunning() override;
+
+	void toggleScene();
 };

src/tools/voxedit/modules/voxedit-ui/AnimationTimeline.cpp
@@ -16,100 +16,96 @@
 namespace voxedit {
 
 bool AnimationTimeline::update(const char *sequencerTitle) {
-	const EditMode editMode = sceneMgr().editMode();
 	voxelformat::FrameIndex currentFrame = sceneMgr().currentFrame();
-	if (editMode == EditMode::Scene) {
-		const voxelformat::SceneGraph &sceneGraph = sceneMgr().sceneGraph();
-		uint32_t startFrame = 0;
-		uint32_t endFrame = 0;
-		for (voxelformat::SceneGraphNode &modelNode : sceneGraph) {
-			endFrame = core_max(modelNode.maxFrame(), endFrame);
+	const voxelformat::SceneGraph &sceneGraph = sceneMgr().sceneGraph();
+	uint32_t startFrame = 0;
+	uint32_t endFrame = 0;
+	for (voxelformat::SceneGraphNode &modelNode : sceneGraph) {
+		endFrame = core_max(modelNode.maxFrame(), endFrame);
+	}
+
+	if (_play) {
+		if (endFrame <= 0) {
+			_play = false;
+		} else {
+			// TODO: anim fps
+			currentFrame = (currentFrame + 1) % endFrame;
+			sceneMgr().setCurrentFrame(currentFrame);
 		}
+	}
+	if (ImGui::Begin(sequencerTitle)) {
+		if (ImGui::DisabledButton(ICON_FA_SQUARE_PLUS " Add", _play)) {
+			sceneMgr().nodeForeachGroup([&](int nodeId) {
+				voxelformat::SceneGraphNode &node = sceneGraph.node(nodeId);
+				if (node.addKeyFrame(currentFrame) == InvalidKeyFrame) {
+					Log::error("Failed to add keyframe for frame %i", (int)currentFrame);
+					return;
+				}
+				const uint32_t newKeyFrameIdx = node.keyFrameForFrame(currentFrame);
+				if (newKeyFrameIdx > 0) {
+					node.keyFrame(newKeyFrameIdx).setTransform(node.keyFrame(newKeyFrameIdx - 1).transform());
+				}
+			});
+		}
+		ImGui::SameLine();
+		if (ImGui::DisabledButton(ICON_FA_SQUARE_MINUS " Remove", _play)) {
+			sceneMgr().nodeForeachGroup([&](int nodeId) {
+				voxelformat::SceneGraphNode &node = sceneGraph.node(nodeId);
+				if (!node.removeKeyFrame(currentFrame)) {
+					Log::error("Failed to remove frame %i", (int)currentFrame);
+				}
+			});
+		}
+		ImGui::SameLine();
 
 		if (_play) {
-			if (endFrame <= 0) {
+			if (ImGui::Button(ICON_FA_STOP)) {
 				_play = false;
-			} else {
-				// TODO: anim fps
-				currentFrame = (currentFrame + 1) % endFrame;
-				sceneMgr().setCurrentFrame(currentFrame);
+			}
+		} else {
+			if (ImGui::DisabledButton(ICON_FA_PLAY, endFrame <= 0)) {
+				_play = true;
 			}
 		}
-		if (ImGui::Begin(sequencerTitle)) {
-			if (ImGui::DisabledButton(ICON_FA_SQUARE_PLUS " Add", _play)) {
-				sceneMgr().nodeForeachGroup([&](int nodeId) {
-					voxelformat::SceneGraphNode &node = sceneGraph.node(nodeId);
-					if (node.addKeyFrame(currentFrame) == InvalidKeyFrame) {
-						Log::error("Failed to add keyframe for frame %i", (int)currentFrame);
-						return;
-					}
-					const uint32_t newKeyFrameIdx = node.keyFrameForFrame(currentFrame);
-					if (newKeyFrameIdx > 0) {
-						node.keyFrame(newKeyFrameIdx).setTransform(node.keyFrame(newKeyFrameIdx - 1).transform());
-					}
-				});
-			}
-			ImGui::SameLine();
-			if (ImGui::DisabledButton(ICON_FA_SQUARE_MINUS " Remove", _play)) {
-				sceneMgr().nodeForeachGroup([&](int nodeId) {
-					voxelformat::SceneGraphNode &node = sceneGraph.node(nodeId);
-					if (!node.removeKeyFrame(currentFrame)) {
-						Log::error("Failed to remove frame %i", (int)currentFrame);
-					}
-				});
-			}
-			ImGui::SameLine();
 
-			if (_play) {
-				if (ImGui::Button(ICON_FA_STOP)) {
-					_play = false;
-				}
-			} else {
-				if (ImGui::DisabledButton(ICON_FA_PLAY, endFrame <= 0)) {
-					_play = true;
-				}
-			}
-
-			if (endFrame < 10) {
-				endFrame = 64;
-			} else {
-				endFrame += endFrame / 10;
-			}
-
-			ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, {0.0f,0.0f});
-			if (ImGui::BeginChild("##neo-sequencer-child")) {
-				if (ImGui::BeginNeoSequencer("##neo-sequencer", &currentFrame, &startFrame, &endFrame, {0, 0}, ImGuiNeoSequencerFlags_AlwaysShowHeader)) {
-					for (voxelformat::SceneGraphNode &modelNode : sceneGraph) {
-						core::DynamicArray<voxelformat::FrameIndex *> keys;
-						keys.reserve(modelNode.keyFrames().size());
-						for (voxelformat::SceneGraphKeyFrame &kf : modelNode.keyFrames()) {
-							keys.push_back(&kf.frameIdx);
-						}
-						const core::String &label =
-							core::String::format("%s###node-%i", modelNode.name().c_str(), modelNode.id());
-						uint32_t **keyframes = keys.data();
-						const uint32_t keyframeCount = keys.size();
-						if (ImGui::BeginNeoTimeline(label.c_str(), keyframes, keyframeCount, nullptr,
-													ImGuiNeoTimelineFlags_None)) {
-							sceneMgr().setCurrentFrame(currentFrame);
-							if (ImGui::IsNeoTimelineSelected(ImGuiNeoTimelineIsSelectedFlags_NewlySelected)) {
-								sceneMgr().nodeActivate(modelNode.id());
-							} else if (sceneMgr().sceneGraph().activeNode() == modelNode.id()) {
-								ImGui::SetSelectedTimeline(label.c_str());
-							}
-							ImGui::EndNeoTimeLine();
-						}
-					}
-					ImGui::EndNeoSequencer();
-				}
-			}
-			ImGui::EndChild();
-			ImGui::PopStyleVar();
+		if (endFrame < 10) {
+			endFrame = 64;
+		} else {
+			endFrame += endFrame / 10;
 		}
-		ImGui::End();
-		return true;
+
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, {0.0f,0.0f});
+		if (ImGui::BeginChild("##neo-sequencer-child")) {
+			if (ImGui::BeginNeoSequencer("##neo-sequencer", &currentFrame, &startFrame, &endFrame, {0, 0}, ImGuiNeoSequencerFlags_AlwaysShowHeader)) {
+				for (voxelformat::SceneGraphNode &modelNode : sceneGraph) {
+					core::DynamicArray<voxelformat::FrameIndex *> keys;
+					keys.reserve(modelNode.keyFrames().size());
+					for (voxelformat::SceneGraphKeyFrame &kf : modelNode.keyFrames()) {
+						keys.push_back(&kf.frameIdx);
+					}
+					const core::String &label =
+						core::String::format("%s###node-%i", modelNode.name().c_str(), modelNode.id());
+					uint32_t **keyframes = keys.data();
+					const uint32_t keyframeCount = keys.size();
+					if (ImGui::BeginNeoTimeline(label.c_str(), keyframes, keyframeCount, nullptr,
+												ImGuiNeoTimelineFlags_None)) {
+						sceneMgr().setCurrentFrame(currentFrame);
+						if (ImGui::IsNeoTimelineSelected(ImGuiNeoTimelineIsSelectedFlags_NewlySelected)) {
+							sceneMgr().nodeActivate(modelNode.id());
+						} else if (sceneMgr().sceneGraph().activeNode() == modelNode.id()) {
+							ImGui::SetSelectedTimeline(label.c_str());
+						}
+						ImGui::EndNeoTimeLine();
+					}
+				}
+				ImGui::EndNeoSequencer();
+			}
+		}
+		ImGui::EndChild();
+		ImGui::PopStyleVar();
 	}
-	return false;
+	ImGui::End();
+	return true;
 }
 
 } // namespace voxedit

src/tools/voxedit/modules/voxedit-ui/MainWindow.cpp
@@ -228,7 +228,13 @@ void MainWindow::mainWidget() {
 
 	// bottom
 	_scriptPanel.updateEditor(TITLE_SCRIPT_EDITOR, _app);
-	_animationTimeline.update(TITLE_ANIMATION_TIMELINE);
+	if (isSceneMode()) {
+		_animationTimeline.update(TITLE_ANIMATION_TIMELINE);
+	}
+}
+
+bool MainWindow::isSceneMode() const {
+	return _scene->isSceneMode() || _sceneTop->isSceneMode() || _sceneLeft->isSceneMode() || _sceneFront->isSceneMode();
 }
 
 // end of main space
@@ -252,7 +258,11 @@ void MainWindow::configureRightBottomWidgetDock(ImGuiID dockId) {
 
 void MainWindow::rightWidget() {
 	// top
-	_positionsPanel.update(TITLE_POSITIONS, _lastExecutedCommand);
+	bool sceneMode = false;
+	if (const Viewport *viewport = hoveredScene()) {
+		sceneMode = viewport->isSceneMode();
+	}
+	_positionsPanel.update(TITLE_POSITIONS, sceneMode, _lastExecutedCommand);
 	_toolsPanel.update(TITLE_TOOLS, _lastExecutedCommand);
 	_assetPanel.update(TITLE_ASSET, _lastExecutedCommand);
 	_animationPanel.update(TITLE_ANIMATION_SETTINGS, _lastExecutedCommand);
@@ -554,9 +564,20 @@ void MainWindow::update() {
 	updateSettings();
 }
 
-bool MainWindow::isSceneHovered() const {
-	return _scene->isHovered() || _sceneTop->isHovered() ||
-		   _sceneLeft->isHovered() || _sceneFront->isHovered();
+Viewport* MainWindow::hoveredScene() {
+	if (_scene->isHovered()) {
+		return _scene;
+	}
+	if (_sceneTop->isHovered()) {
+		return _sceneTop;
+	}
+	if (_sceneLeft->isHovered()) {
+		return _sceneLeft;
+	}
+	if (_sceneFront->isHovered()) {
+		return _sceneFront;
+	}
+	return nullptr;
 }
 
 bool MainWindow::saveScreenshot(const core::String& file) {
@@ -566,6 +587,17 @@ bool MainWindow::saveScreenshot(const core::String& file) {
 	}
 	Log::info("Screenshot created at '%s'", file.c_str());
 	return true;
+}
+
+void MainWindow::toggleScene() {
+	if (Viewport *scene = hoveredScene()) {
+		scene->toggleScene();
+	} else {
+		_scene->toggleScene();
+		_sceneTop->toggleScene();
+		_sceneLeft->toggleScene();
+		_sceneFront->toggleScene();
+	}
 }
 
 } // namespace voxedit

src/tools/voxedit/modules/voxedit-ui/MainWindow.h
@@ -66,6 +66,7 @@ private:
 	*/
 	void loadLastOpenedFiles(const core::String &string);
 	void addLastOpenedFile(const core::String &file);
+	bool isSceneMode() const;
 
 	ModelNodeSettings _modelNodeSettings;
 
@@ -119,10 +120,11 @@ public:
 	bool isSceneGraphDropTarget() const;
 	bool isPaletteWidgetDropTarget() const;
 
+	void toggleScene();
 	void resetCamera();
 	void update();
 	bool allowToQuit();
-	bool isSceneHovered() const;
+	Viewport* hoveredScene();
 
 	bool saveScreenshot(const core::String& file);
 };

src/tools/voxedit/modules/voxedit-ui/MenuBar.cpp
@@ -3,6 +3,7 @@
  */
 
 #include "MenuBar.h"
+#include "command/CommandHandler.h"
 #include "core/Color.h"
 #include "core/GameConfig.h"
 #include "ui/IMGUIEx.h"
@@ -135,12 +136,7 @@ bool MenuBar::update(ui::IMGUIApp* app, command::CommandExecutionListener &liste
 			}
 			if (ImGui::BeginMenu(ICON_FA_EYE " View")) {
 				actionMenuItem(ICON_FK_VIDEO_CAMERA " Reset camera", "resetcamera", listener);
-				if (ImGui::MenuItem(ICON_FK_TH " View toggle")) {
-					// don't use togglescene action menu item - as tab is used for the menu navigation
-					// that's the reason why this is bound to editor mode only
-					sceneMgr().toggleEditMode();
-					listener("togglescene", {});
-				}
+				actionMenuItem(ICON_FK_TH " View toggle", "togglescene", listener);
 				actionMenuItem(ICON_FK_TERMINAL " Console", "toggleconsole", listener);
 				ImGui::EndMenu();
 			}

commit 6c7aa086b529e642520d60040eac5d78d7215daa

VOXEDIT: single click to delete a voxel isn't working anymore #231
regression introduced in 27a92dbd3289bcd6cc0df0b5b746332cb88daf49

src/tools/voxedit/modules/voxedit-util/modifier/ModifierButton.cpp
@@ -28,7 +28,7 @@ bool ModifierButton::handleDown(int32_t key, double pressedMillis) {
 		if (_newType != ModifierType::None) {
 			_oldType = modifier.modifierType();
 			modifier.setModifierType(_newType);
-			sceneMgr().trace(false);
+			sceneMgr().trace(false, true);
 		}
 		modifier.aabbStart();
 	}
@@ -76,7 +76,7 @@ void ModifierButton::execute(bool single) {
 	sceneMgr().nodeForeachGroup(func);
 	if (_oldType != ModifierType::None) {
 		modifier.setModifierType(_oldType);
-		sceneMgr().trace(true);
+		sceneMgr().trace(false, true);
 		_oldType = ModifierType::None;
 	}
 	if (!single) {