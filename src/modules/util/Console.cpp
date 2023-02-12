vengi-voxel/vengi

commit 271fc0fea8cc572ae88002af1e05df4b10ce74d1

UTIL: better for for issue #222

src/modules/util/Console.cpp
@@ -16,6 +16,7 @@
 #include "core/Tokenizer.h"
 #include "command/CommandHandler.h"
 #include "VarUtil.h"
+#include "util/CustomButtonNames.h"
 #include <SDL.h>
 
 namespace util {
@@ -133,7 +134,7 @@ bool Console::onKeyPress(int32_t key, int16_t modifier) {
 		return true;
 	}
 
-	if (modifier & KMOD_CTRL) {
+	if (modifier & KMOD_CONTROL) {
 		if (key == SDLK_TAB || key == SDLK_d) {
 			toggle();
 		} else if (key == SDLK_a || key == SDLK_b) {
@@ -282,7 +283,7 @@ void Console::insertText(const core::String& text) {
 		return;
 	}
 	const SDL_Keymod state = SDL_GetModState();
-	if (state & (KMOD_CTRL | KMOD_ALT)) {
+	if (state & (KMOD_LCONTROL | KMOD_ALT)) {
 		return;
 	}
 	if (_overwrite && _cursorPos < int(_commandLine.size())) {

src/modules/util/CustomButtonNames.h
@@ -12,6 +12,16 @@
 namespace util {
 namespace button {
 
+#ifdef __APPLE__
+#define KMOD_CONTROL KMOD_GUI
+#define KMOD_LCONTROL KMOD_LGUI
+#define KMOD_RCONTROL KMOD_RGUI
+#else
+#define KMOD_CONTROL KMOD_CTRL
+#define KMOD_LCONTROL KMOD_LCTRL
+#define KMOD_RCONTROL KMOD_RCTRL
+#endif
+
 #define CUSTOM_SDL_BUTTON_OFFSET (SDL_BUTTON_X2 + 10)
 static const int32_t CUSTOM_SCANCODES               = SDL_NUM_SCANCODES + 1;
 static const int32_t CUSTOM_SDLK_MOUSE_LEFT         = CUSTOM_SDL_KEYCODE(SDL_BUTTON_LEFT);
@@ -47,17 +57,17 @@ static constexpr struct ModifierMapping {
 } MODIFIERMAPPING[] = {
 	{KMOD_LSHIFT, "left_shift"},
 	{KMOD_RSHIFT, "right_shift"},
-	{KMOD_LCTRL, "left_ctrl"},
-	{KMOD_RCTRL, "right_ctrl"},
+	{KMOD_LCONTROL, "left_ctrl"},
+	{KMOD_RCONTROL, "right_ctrl"},
 	{KMOD_LALT, "left_alt"},
 	{KMOD_RALT, "right_alt"},
 	{KMOD_ALT, "alt"},
 	{KMOD_SHIFT, "shift"},
-	{KMOD_CTRL, "ctrl"},
+	{KMOD_CONTROL, "ctrl"},
 	{KMOD_ALT | KMOD_SHIFT, "alt+shift"},
-	{KMOD_CTRL | KMOD_SHIFT, "ctrl+shift"},
-	{KMOD_ALT | KMOD_CTRL, "alt+ctrl"},
-	{KMOD_ALT | KMOD_SHIFT | KMOD_SHIFT, "ctrl+alt+shift"},
+	{KMOD_CONTROL | KMOD_SHIFT, "ctrl+shift"},
+	{KMOD_ALT | KMOD_CONTROL, "alt+ctrl"},
+	{KMOD_CONTROL | KMOD_ALT | KMOD_SHIFT, "ctrl+alt+shift"},
 	{0, nullptr}
 };
 

src/modules/util/KeybindingHandler.cpp
@@ -54,7 +54,7 @@ bool isValidForBinding(int16_t pressedModMask, int16_t commandModMask) {
 		if (!checkModifierBitMask(KMOD_ALT, pressedModMask, commandModMask)) {
 			return false;
 		}
-		if (!checkModifierBitMask(KMOD_CTRL, pressedModMask, commandModMask)) {
+		if (!checkModifierBitMask(KMOD_CONTROL, pressedModMask, commandModMask)) {
 			return false;
 		}
 	}
@@ -70,7 +70,7 @@ bool isValidForBinding(int16_t pressedModMask, int16_t commandModMask) {
  */
 static bool executeCommandsForBinding(const BindMap& bindings, int32_t key, int16_t modMask, double nowSeconds, uint16_t count) {
 	auto range = bindings.equal_range(key);
-	const int16_t modifier = modMask & (KMOD_SHIFT | KMOD_CTRL | KMOD_ALT);
+	const int16_t modifier = modMask & (KMOD_SHIFT | KMOD_CONTROL | KMOD_ALT);
 	bool handled = false;
 	for (auto i = range.first; i != range.second; ++i) {
 		if (count > 0 && i->second.count != count) {

src/modules/util/KeybindingParser.cpp
@@ -38,23 +38,11 @@ void KeybindingParser::parseKeyAndCommand(core::String key, const core::String& 
 			} else if (lower == "right_alt") {
 				modifier |= KMOD_RALT;
 			} else if (lower == "ctrl") {
-#ifdef __APPLE__
-				modifier |= KMOD_GUI;
-#else
-				modifier |= KMOD_CTRL;
-#endif
+				modifier |= KMOD_CONTROL;
 			} else if (lower == "left_ctrl") {
-#ifdef __APPLE__
-				modifier |= KMOD_LGUI;
-#else
-				modifier |= KMOD_LCTRL;
-#endif
+				modifier |= KMOD_LCONTROL;
 			} else if (lower == "right_ctrl") {
-#ifdef __APPLE__
-				modifier |= KMOD_RGUI;
-#else
-				modifier |= KMOD_RCTRL;
-#endif
+				modifier |= KMOD_RCONTROL;
 			} else {
 				key = token;
 				if (key.empty()) {

src/modules/util/tests/KeybindingHandlerTest.cpp
@@ -140,22 +140,22 @@ TEST_F(KeybindingHandlerTest, testLeftShiftModifier) {
 }
 
 TEST_F(KeybindingHandlerTest, testAllValidModifier) {
-	execute(SDLK_w, KMOD_LSHIFT | KMOD_LCTRL | KMOD_LALT);
+	execute(SDLK_w, KMOD_LSHIFT | KMOD_LCONTROL | KMOD_LALT);
 	EXPECT_TRUE(_allmodscommand) << "expected command wasn't executed";
 }
 
 TEST_F(KeybindingHandlerTest, testAllValidModifier2) {
-	execute(SDLK_w, KMOD_RSHIFT | KMOD_LCTRL | KMOD_RALT);
+	execute(SDLK_w, KMOD_RSHIFT | KMOD_LCONTROL | KMOD_RALT);
 	EXPECT_TRUE(_allmodscommand) << "expected command wasn't executed";
 }
 
 TEST_F(KeybindingHandlerTest, testCtrlShiftModifier) {
-	execute(SDLK_w, KMOD_LSHIFT | KMOD_LCTRL);
+	execute(SDLK_w, KMOD_LSHIFT | KMOD_LCONTROL);
 	EXPECT_TRUE(_ctrlshiftmodcommand) << "expected command wasn't executed";
 }
 
 TEST_F(KeybindingHandlerTest, testLShiftRCtrlModifier) {
-	execute(SDLK_w, KMOD_LSHIFT | KMOD_RCTRL);
+	execute(SDLK_w, KMOD_LSHIFT | KMOD_RCONTROL);
 	EXPECT_TRUE(_ctrlshiftmodcommand) << "expected command wasn't executed";
 }
 
@@ -185,15 +185,15 @@ TEST_F(KeybindingHandlerTest, testDoubleLeftClick) {
 }
 
 TEST_F(KeybindingHandlerTest, testCtrlModifierA) {
-	executeActionButtonCommand(SDLK_a, KMOD_RCTRL);
+	executeActionButtonCommand(SDLK_a, KMOD_RCONTROL);
 }
 
 TEST_F(KeybindingHandlerTest, testCtrlModifierAWrongModifierPressed) {
-	notExecute(SDLK_a, KMOD_LCTRL);
+	notExecute(SDLK_a, KMOD_LCONTROL);
 }
 
 TEST_F(KeybindingHandlerTest, testCtrlModifier) {
-	executeActionButtonCommand(SDLK_w, KMOD_LCTRL);
+	executeActionButtonCommand(SDLK_w, KMOD_LCONTROL);
 }
 
 TEST_F(KeybindingHandlerTest, testModifierMasksAltSimple) {
@@ -213,11 +213,11 @@ TEST_F(KeybindingHandlerTest, testModifierMasksShiftSimple) {
 }
 
 TEST_F(KeybindingHandlerTest, testModifierMasksCtrlSimple) {
-	EXPECT_TRUE(isValidForBinding(KMOD_LCTRL, KMOD_CTRL)) << "lctrl pressed - ctrl bound";
-	EXPECT_TRUE(isValidForBinding(KMOD_RCTRL, KMOD_CTRL)) << "rctrl pressed - ctrl bound";
+	EXPECT_TRUE(isValidForBinding(KMOD_LCONTROL, KMOD_CONTROL)) << "lctrl pressed - ctrl bound";
+	EXPECT_TRUE(isValidForBinding(KMOD_RCONTROL, KMOD_CONTROL)) << "rctrl pressed - ctrl bound";
 
-	EXPECT_TRUE(isValidForBinding(KMOD_LCTRL, KMOD_LCTRL)) << "lctrl pressed - lctrl bound";
-	EXPECT_FALSE(isValidForBinding(KMOD_RCTRL, KMOD_LCTRL)) << "rctrl pressed - lctrl bound";
+	EXPECT_TRUE(isValidForBinding(KMOD_LCONTROL, KMOD_LCONTROL)) << "lctrl pressed - lctrl bound";
+	EXPECT_FALSE(isValidForBinding(KMOD_RCONTROL, KMOD_LCONTROL)) << "rctrl pressed - lctrl bound";
 }
 
 TEST_F(KeybindingHandlerTest, testModifierMasksInvalidModifiers) {

src/modules/util/tests/KeybindingParserTest.cpp
@@ -3,6 +3,7 @@
  */
 
 #include "app/tests/AbstractTest.h"
+#include "util/CustomButtonNames.h"
 #include "util/KeybindingParser.h"
 #include <SDL.h>
 
@@ -49,11 +50,11 @@ TEST(KeybindingParserTest, testParsing) {
 		const CommandModifierPair& pair = i->second;
 		const core::String& command = pair.command;
 		const int16_t mod = pair.modifier;
-		if ((mod & KMOD_SHIFT) && (mod & KMOD_ALT) && (mod & KMOD_CTRL)) {
+		if ((mod & KMOD_SHIFT) && (mod & KMOD_ALT) && (mod & KMOD_CONTROL)) {
 			EXPECT_EQ("allmodscommand", command);
 			EXPECT_FALSE(allmodscommand);
 			allmodscommand = true;
-		} else if ((mod & KMOD_SHIFT) && (mod & KMOD_CTRL)) {
+		} else if ((mod & KMOD_SHIFT) && (mod & KMOD_CONTROL)) {
 			EXPECT_EQ("ctrlshiftmodcommand", command);
 			EXPECT_FALSE(ctrlshiftmodcommand);
 			ctrlshiftmodcommand = true;
@@ -61,7 +62,7 @@ TEST(KeybindingParserTest, testParsing) {
 			EXPECT_EQ("+xyz", command);
 			EXPECT_FALSE(xyz);
 			xyz = true;
-		} else if (mod & KMOD_CTRL) {
+		} else if (mod & KMOD_CONTROL) {
 			EXPECT_EQ("+bar", command);
 			EXPECT_FALSE(bar);
 			bar = true;
@@ -109,7 +110,7 @@ TEST(KeybindingParserTest, testParsing) {
 		const core::String& command = pair.command;
 		const int16_t mod = pair.modifier;
 		EXPECT_EQ("echo +", command);
-		EXPECT_TRUE(mod & KMOD_CTRL) << "command " << command << " modifier wasn't parsed properly";
+		EXPECT_TRUE(mod & KMOD_CONTROL) << "command " << command << " modifier wasn't parsed properly";
 		++count;
 	}
 	EXPECT_EQ(1, count);

src/tests/testcamera/TestCamera.cpp
@@ -5,6 +5,7 @@
 #include "TestCamera.h"
 #include "testcore/TestAppMain.h"
 #include "core/Log.h"
+#include "util/CustomButtonNames.h"
 #include <SDL.h>
 
 TestCamera::TestCamera(const io::FilesystemPtr& filesystem, const core::TimeProviderPtr& timeProvider) :
@@ -150,7 +151,7 @@ bool TestCamera::onMouseWheel(int32_t x, int32_t y) {
 	const SDL_Keymod mods = SDL_GetModState();
 	if (mods & KMOD_SHIFT) {
 		video::Camera& c = _renderCamera[_targetCamera];
-		if (mods & KMOD_CTRL) {
+		if (mods & KMOD_CONTROL) {
 			c.setNearPlane(c.nearPlane() + (float)y);
 		} else {
 			c.setFarPlane(c.farPlane() + (float)y);
@@ -186,7 +187,7 @@ bool TestCamera::onKeyPress(int32_t key, int16_t modifier) {
 			delta = 1;
 		}
 
-		if (modifier & KMOD_CTRL) {
+		if (modifier & KMOD_CONTROL) {
 			c.setNearPlane(c.nearPlane() + (float)delta);
 		} else {
 			c.setFarPlane(c.farPlane() + (float)delta);

commit 179cd71c49da7f18987d655ed9c808b7556a8a33

UTIL: fixed copy&paste error
introduced in 271fc0fea8cc572ae88002af1e05df4b10ce74d1

src/modules/util/Console.cpp
@@ -283,7 +283,7 @@ void Console::insertText(const core::String& text) {
 		return;
 	}
 	const SDL_Keymod state = SDL_GetModState();
-	if (state & (KMOD_LCONTROL | KMOD_ALT)) {
+	if (state & (KMOD_CONTROL | KMOD_ALT)) {
 		return;
 	}
 	if (_overwrite && _cursorPos < int(_commandLine.size())) {