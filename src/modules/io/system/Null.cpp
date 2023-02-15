85579f92eda7ac0872454c2e83604a327d79949b
Martin Gerhardy <martin.gerhardy@gmail.com>
2023-02-15 21:13:59 +0100

    IO: fixed Null io system functions

--- src/modules/io/system/Null.cpp
+++ src/modules/io/system/Null.cpp
@@ -13,6 +13,47 @@ bool initState(io::FilesystemState &state) {
 	return false;
 }
 
+bool fs_mkdir(const char *path) {
+	return false;
+}
+
+bool fs_rmdir(const char *path) {
+	return false;
+}
+
+bool fs_unlink(const char *path) {
+	return false;
+}
+
+bool fs_exists(const char *path) {
+	return false;
+}
+
+bool fs_chdir(const char *path) {
+	return false;
+}
+
+core::String fs_realpath(const char *path) {
+	return path;
+}
+
+bool fs_stat(const char *path, FilesystemEntry &entry) {
+	return false;
+}
+
+core::DynamicArray<FilesystemEntry> fs_scandir(const char *path) {
+	core::DynamicArray<FilesystemEntry> foo;
+	return foo;
+}
+
+core::String fs_readlink(const char *path) {
+	return path;
+}
+
+core::String fs_cwd() {
+	return "/";
+}
+
 } // namespace io
 
 #endif
