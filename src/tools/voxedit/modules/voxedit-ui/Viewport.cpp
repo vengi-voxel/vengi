5b3103854cb2a981cbc3c0805b56451ac5b059a4
Martin Gerhardy <martin.gerhardy@gmail.com>
2023-02-15 20:09:44 +0100

    VOXEDIT: code cleanup

--- src/tools/voxedit/modules/voxedit-ui/Viewport.cpp
+++ src/tools/voxedit/modules/voxedit-ui/Viewport.cpp
@@ -334,25 +334,21 @@ void Viewport::renderMenuBar(command::CommandExecutionListener *listener) {
 		}
 		if (ImGui::BeginMenu(ICON_FA_EYE " View")) {
 			ImGui::CommandMenuItem(ICON_FK_VIDEO_CAMERA " Reset camera", "resetcamera", true, listener);
+
 			glm::vec3 omega = _camera.omega();
 			if (ImGui::InputFloat("Camera rotation", &omega.y)) {
 				_camera.setOmega(omega);
 			}
+
 			const core::String command = core::string::format("screenshot %i", _id);
 			ImGui::CommandMenuItem(ICON_FA_CAMERA " Screenshot", command.c_str(), listener);
-			if (_renderContext.sceneMode) {
-				const core::String commandVideo = core::string::format("video %i", _id);
-				const char *title = ICON_FA_CAMERA " Video";
-				if (_avi.isRecording()) {
-					title = ICON_FA_STOP " Video";
-				}
-				if (ImGui::MenuItem(title)) {
-					toggleVideoRecording();
-				}
-				const uint32_t pendingFrames = _avi.pendingFrames();
-				if (pendingFrames > 0u) {
-					ImGui::TooltipText("Pending frames: %u", _avi.pendingFrames());
-				}
+
+			if (ImGui::MenuItem(_avi.isRecording() ? ICON_FA_STOP " Video" : ICON_FA_CAMERA " Video")) {
+				toggleVideoRecording();
+			}
+			const uint32_t pendingFrames = _avi.pendingFrames();
+			if (pendingFrames > 0u) {
+				ImGui::TooltipText("Pending frames: %u", pendingFrames);
 			}
 			ImGui::EndMenu();
 		}
