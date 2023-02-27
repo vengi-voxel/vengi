96c2184e7a1a982592f187cc293dc037a0dc6c77
Martin Gerhardy <martin.gerhardy@gmail.com>
2023-02-27 18:00:05 +0100

    VIDEO: unified warning message

--- src/modules/video/WindowedApp.cpp
+++ src/modules/video/WindowedApp.cpp
@@ -255,7 +255,7 @@ app::AppState WindowedApp::onInit() {
 		return app::AppState::InitFailure;
 	}
 	if (!_keybindingHandler.load(_keybindingsVersion)) {
-		Log::warn("failed to load the keybindings");
+		Log::warn("Failed to load the keybindings");
 	}
 
 	core::Singleton<io::EventHandler>::getInstance().registerObserver(this);
