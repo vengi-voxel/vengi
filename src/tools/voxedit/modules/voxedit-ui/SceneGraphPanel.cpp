vengi-voxel/vengi

commit af0f1ba87629c63d31056f76ab3bd19b6a5d9cd5

VOXELFORMAT: fixed assert for moving a node
see issue #238

src/tools/voxedit/modules/voxedit-ui/SceneGraphPanel.cpp
@@ -204,17 +204,22 @@ void SceneGraphPanel::recursiveAddNodes(video::Camera &camera, const voxelformat
 			}
 		}
 		if (ImGui::BeginDragDropTarget()) {
-			if (const ImGuiPayload *payload = ImGui::AcceptDragDropPayload(dragdrop::SceneNodePayload)) {
+			const ImGuiPayload *payload = ImGui::GetDragDropPayload();
+			if (payload->IsDataType(dragdrop::SceneNodePayload)) {
 				const int sourceNodeId = *(int *)payload->Data;
-				const int targetNode = node.id();
-				if (!sceneMgr().nodeMove(sourceNodeId, targetNode)) {
-					Log::error("Failed to move node");
+				if (sceneGraph.canChangeParent(node, sourceNodeId)) {
+					if (ImGui::AcceptDragDropPayload(dragdrop::SceneNodePayload) != nullptr) {
+						const int targetNode = node.id();
+						if (!sceneMgr().nodeMove(sourceNodeId, targetNode)) {
+							Log::error("Failed to move node");
+						}
+						ImGui::EndDragDropTarget();
+						if (open) {
+							ImGui::TreePop();
+						}
+						return;
+					}
 				}
-				ImGui::EndDragDropTarget();
-				if (open) {
-					ImGui::TreePop();
-				}
-				return;
 			}
 			ImGui::EndDragDropTarget();
 		}

src/modules/voxelformat/SceneGraph.cpp
@@ -262,17 +262,39 @@ int SceneGraph::emplace(SceneGraphNode &&node, int parent) {
 	return nodeId;
 }
 
-bool SceneGraph::changeParent(int nodeId, int newParentId) {
-	if (nodeId == root().id()) {
-		return false;
+bool SceneGraph::nodeHasChildren(const SceneGraphNode &n, int childId) const {
+	for (int c : n.children()) {
+		if (c == childId) {
+			return true;
+		}
 	}
-	if (!hasNode(nodeId)) {
+	for (int c : n.children()) {
+		if (nodeHasChildren(node(c), childId)) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool SceneGraph::canChangeParent(const SceneGraphNode &node, int newParentId) const {
+	if (node.id() == root().id()) {
 		return false;
 	}
 	if (!hasNode(newParentId)) {
 		return false;
 	}
+	return !nodeHasChildren(node, newParentId);
+}
+
+bool SceneGraph::changeParent(int nodeId, int newParentId) {
+	if (!hasNode(nodeId)) {
+		return false;
+	}
 	SceneGraphNode& n = node(nodeId);
+	if (!canChangeParent(n, newParentId)) {
+		return false;
+	}
+
 	const int oldParentId = n.parent();
 	if (!node(oldParentId).removeChild(nodeId)) {
 		return false;

src/modules/voxelformat/SceneGraph.h
@@ -107,6 +107,8 @@ public:
 	bool hasNode(int nodeId) const;
 	bool removeNode(int nodeId, bool recursive);
 	bool changeParent(int nodeId, int newParentId);
+	bool nodeHasChildren(const SceneGraphNode &node, int childId) const;
+	bool canChangeParent(const SceneGraphNode &node, int newParentId) const;
 
 	/**
 	 * @brief Pre-allocated memory in the graph without adding the nodes

src/modules/voxelformat/tests/SceneGraphTest.cpp
@@ -273,4 +273,50 @@ TEST_F(SceneGraphTest, testKeyframes) {
 	EXPECT_EQ(1u, node.keyFrames().size());
 }
 
+TEST_F(SceneGraphTest, testMoveParentAsNewChild) {
+	SceneGraph sceneGraph;
+	int originalParentNodeId = 1;
+	int originalChildNodeId = 2;
+	{
+		SceneGraphNode node(SceneGraphNodeType::Model);
+		node.setVolume(new voxel::RawVolume(voxel::Region(0, 1)), true);
+		node.setName("originalparent");
+		EXPECT_EQ(originalParentNodeId, sceneGraph.emplace(core::move(node), 0)) << "Unexpected node id returned - root node is 0 - next should be 1";
+	}
+	{
+		SceneGraphNode node(SceneGraphNodeType::Model);
+		node.setVolume(new voxel::RawVolume(voxel::Region(0, 1)), true);
+		node.setName("originalchild");
+		EXPECT_EQ(originalChildNodeId, sceneGraph.emplace(core::move(node), originalParentNodeId));
+	}
+	ASSERT_FALSE(sceneGraph.changeParent(originalParentNodeId, originalChildNodeId));
+}
+
+TEST_F(SceneGraphTest, testMove) {
+	SceneGraph sceneGraph;
+	int originalParentNodeId = 1;
+	int originalChildNodeId = 2;
+	{
+		SceneGraphNode node(SceneGraphNodeType::Model);
+		node.setVolume(new voxel::RawVolume(voxel::Region(0, 1)), true);
+		node.setName("model1");
+		EXPECT_EQ(originalParentNodeId, sceneGraph.emplace(core::move(node), 0)) << "Unexpected node id returned - root node is 0 - next should be 1";
+	}
+	{
+		SceneGraphNode node(SceneGraphNodeType::Model);
+		node.setVolume(new voxel::RawVolume(voxel::Region(0, 1)), true);
+		node.setName("model2");
+		EXPECT_EQ(originalChildNodeId, sceneGraph.emplace(core::move(node), 0));
+	}
+	ASSERT_TRUE(sceneGraph.changeParent(originalParentNodeId, originalChildNodeId));
+	ASSERT_EQ(1u, sceneGraph.root().children().size()) << "Expected to have one child after the move";
+	ASSERT_EQ(originalChildNodeId, sceneGraph.root().children().front());
+	const SceneGraphNode &newParentNode = sceneGraph.node(originalChildNodeId);
+	ASSERT_EQ(1u, newParentNode.children().size());
+	ASSERT_EQ(originalParentNodeId, newParentNode.children().front());
+	for (const auto &n : sceneGraph) {
+		EXPECT_FALSE(n.transform(0).dirty()) << "node " << n.name().c_str() << " still has a dirty transform after the move";
+	}
+}
+
 }

commit ea5387ecd431f3bf06aa9cf0771f2a6829b13724

VOXEDIT: fixed issue introduced in af0f1ba87629c63d31056f76ab3bd19b6a5d9cd5
also see issue #238

src/tools/voxedit/modules/voxedit-ui/SceneGraphPanel.cpp
@@ -207,9 +207,9 @@ void SceneGraphPanel::recursiveAddNodes(video::Camera &camera, const voxelformat
 			const ImGuiPayload *payload = ImGui::GetDragDropPayload();
 			if (payload->IsDataType(dragdrop::SceneNodePayload)) {
 				const int sourceNodeId = *(int *)payload->Data;
-				if (sceneGraph.canChangeParent(node, sourceNodeId)) {
+				const int targetNode = node.id();
+				if (sceneGraph.canChangeParent(sceneGraph.node(sourceNodeId), targetNode)) {
 					if (ImGui::AcceptDragDropPayload(dragdrop::SceneNodePayload) != nullptr) {
-						const int targetNode = node.id();
 						if (!sceneMgr().nodeMove(sourceNodeId, targetNode)) {
 							Log::error("Failed to move node");
 						}