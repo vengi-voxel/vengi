38f01cac5284d5e97a77f85120075d6b150d45c9
Martin Gerhardy <martin.gerhardy@gmail.com>
2023-03-04 16:24:42 +0100

    CMAKE: don't validate lua files in cross-compilation mode

--- cmake/lua.cmake
+++ cmake/lua.cmake
@@ -1,16 +1,18 @@
 function(check_lua_files TARGET)
-	set(files ${ARGN})
-	foreach(_file ${files})
-		string(REGEX REPLACE "[/]" "_" targetname ${_file})
-		get_filename_component(filename ${_file} NAME)
-		set(_outfile ${targetname}.out)
-		add_custom_command(
-			OUTPUT ${_outfile}
-			COMMAND $<TARGET_FILE:luac> -o ${_outfile} ${CMAKE_CURRENT_SOURCE_DIR}/lua/${_file}
-			DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/lua/${_file}
-			COMMENT "Validate ${_file}"
-		)
-		add_custom_target(${TARGET}-${_outfile} DEPENDS ${_outfile})
-		add_dependencies(${TARGET} ${TARGET}-${_outfile})
-	endforeach()
+	if (NOT CMAKE_CROSSCOMPILING)
+		set(files ${ARGN})
+		foreach(_file ${files})
+			string(REGEX REPLACE "[/]" "_" targetname ${_file})
+			get_filename_component(filename ${_file} NAME)
+			set(_outfile ${targetname}.out)
+			add_custom_command(
+				OUTPUT ${_outfile}
+				COMMAND $<TARGET_FILE:luac> -o ${_outfile} ${CMAKE_CURRENT_SOURCE_DIR}/lua/${_file}
+				DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/lua/${_file}
+				COMMENT "Validate ${_file}"
+			)
+			add_custom_target(${TARGET}-${_outfile} DEPENDS ${_outfile})
+			add_dependencies(${TARGET} ${TARGET}-${_outfile})
+		endforeach()
+	endif()
 endfunction()
