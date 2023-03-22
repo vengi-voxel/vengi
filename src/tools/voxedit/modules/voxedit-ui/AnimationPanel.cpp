01aca9348823796972e7547fdcb8c6cf5fabe179
Martin Gerhardy <martin.gerhardy@gmail.com>
2023-03-22 20:25:32 +0100

    VOXEDIT: use SceneGraphAnimationIds type

--- src/tools/voxedit/modules/voxedit-ui/AnimationPanel.cpp
+++ src/tools/voxedit/modules/voxedit-ui/AnimationPanel.cpp
@@ -11,7 +11,7 @@
 namespace voxedit {
 
 void AnimationPanel::update(const char *title, command::CommandExecutionListener &listener) {
-	const core::DynamicArray<core::String> &animations = sceneMgr().sceneGraph().animations();
+	const scenegraph::SceneGraphAnimationIds &animations = sceneMgr().sceneGraph().animations();
 	if (animations.size() <= 1) {
 		return;
 	}
