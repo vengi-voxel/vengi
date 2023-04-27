d0d01a69b64c0c64a0e954f404edb46541471ef4
Martin Gerhardy <martin.gerhardy@gmail.com>
2023-04-27 19:33:21 +0200

    CMAKE: added pch for sdl and glm

---
+++ contrib/libs/glm/pch.h
@@ -0,0 +1,9 @@
+#include <glm/fwd.hpp>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/matrix.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/gtx/quaternion.hpp>
+#include <glm/common.hpp>
+#include <glm/geometric.hpp>
