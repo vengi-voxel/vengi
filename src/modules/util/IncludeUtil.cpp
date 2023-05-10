84c658c88e76c74fab917e096974f766b5de8053
Martin Gerhardy <martin.gerhardy@gmail.com>
2023-05-10 20:28:52 +0200

    UTIL: fixed warning and format

--- src/modules/util/IncludeUtil.cpp
+++ src/modules/util/IncludeUtil.cpp
@@ -3,14 +3,16 @@
  */
 
 #include "IncludeUtil.h"
-#include "core/StringUtil.h"
-#include "io/Filesystem.h"
 #include "app/App.h"
 #include "core/Log.h"
+#include "core/StringUtil.h"
+#include "io/Filesystem.h"
 
 namespace util {
 
-core::Pair<core::String, bool> handleIncludes(const core::String& filename, const core::String& buffer, const core::List<core::String>& includeDirs, core::List<core::String>* includedFiles) {
+core::Pair<core::String, bool> handleIncludes(const core::String &filename, const core::String &buffer,
+											  const core::List<core::String> &includeDirs,
+											  core::List<core::String> *includedFiles) {
 	core::String src;
 	const core::String include = "#include";
 	int index = 0;
@@ -39,7 +41,7 @@ core::Pair<core::String, bool> handleIncludes(const core::String& filename, cons
 
 			++index;
 			++i;
-			const io::FilesystemPtr& fs = io::filesystem();
+			const io::FilesystemPtr &fs = io::filesystem();
 			for (; i != buffer.end(); ++i, ++index) {
 				const char *cEnd = &buffer[index];
 				if (*cEnd != '"' && *cEnd != '>') {
@@ -48,30 +50,32 @@ core::Pair<core::String, bool> handleIncludes(const core::String& filename, cons
 
 				bool found = false;
 				const core::String includeFile(cStart + 1, (size_t)(cEnd - (cStart + 1)));
-				for (const core::String& dir : includeDirs) {
-					const core::String& fullPath = core::string::path(dir, includeFile);
+				for (const core::String &dir : includeDirs) {
+					const core::String &fullPath = core::string::path(dir, includeFile);
 					if (!fs->exists(fullPath)) {
 						continue;
 					}
-					const core::String& includeBuffer = fs->load(fullPath);
+					const core::String &includeBuffer = fs->load(fullPath);
 					if (includeBuffer.empty()) {
-						Log::error("could not load shader include %s from dir %s", includeFile.c_str(), dir.c_str());
+						Log::error("could not load include file '%s' from dir '%s' at line %i from file '%s'",
+								   includeFile.c_str(), dir.c_str(), line, filename.c_str());
 						success = false;
 					} else {
 						if (includedFiles != nullptr) {
 							includedFiles->insert(fullPath);
 						}
-						//src.append("#line 1 ").append(fullPath).append("\n");
+						// src.append("#line 1 ").append(fullPath).append("\n");
 						src.append(includeBuffer);
 						found = true;
 					}
 					break;
 				}
-				//src.append("#line ").append(line).append(" ").append(filename).append("\n");
+				// src.append("#line ").append(line).append(" ").append(filename).append("\n");
 				if (!found) {
 					success = false;
-					Log::error("Failed to resolve include '%s'", includeFile.c_str());
-					for (const core::String& dir : includeDirs) {
+					Log::error("Failed to resolve include '%s' at line %i from file '%s'", includeFile.c_str(), line,
+							   filename.c_str());
+					for (const core::String &dir : includeDirs) {
 						Log::error("Include paths: %s", dir.c_str());
 					}
 				}
@@ -86,5 +90,4 @@ core::Pair<core::String, bool> handleIncludes(const core::String& filename, cons
 	return {src, success};
 }
 
-}
-
+} // namespace util
