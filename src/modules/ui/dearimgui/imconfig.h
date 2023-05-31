2ef5b2f08e450bc56dfac93b15db6f8376204129
Martin Gerhardy <martin.gerhardy@gmail.com>
2023-05-31 17:31:51 +0200

    LIBS: updated dearimgui

--- src/modules/ui/dearimgui/imconfig.h
+++ src/modules/ui/dearimgui/imconfig.h
@@ -96,6 +96,17 @@
         ImVec4(const glm::vec4& f) { x = f.x; y = f.y; z = f.z; w = f.w; }    \
         operator glm::ivec4() const { return glm::ivec4(x,y,z,w); }           \
         operator glm::vec4() const { return glm::vec4(x,y,z,w); }
+/*
+#define IM_VEC2_CLASS_EXTRA                                                     \
+        constexpr ImVec2(const MyVec2& f) : x(f.x), y(f.y) {}                   \
+        operator MyVec2() const { return MyVec2(x,y); }
+
+#define IM_VEC4_CLASS_EXTRA                                                     \
+        constexpr ImVec4(const MyVec4& f) : x(f.x), y(f.y), z(f.z), w(f.w) {}   \
+        operator MyVec4() const { return MyVec4(x,y,z,w); }
+*/
+//---- ...Or use Dear ImGui's own very basic math operators.
+//#define IMGUI_DEFINE_MATH_OPERATORS
 
 //---- Use 32-bit vertex indices (default is 16-bit) is one way to allow large meshes with more than 64K vertices.
 // Your renderer backend will need to support it (most example renderer backends support both 16/32-bit indices).
