e7e2179bda41f5b6c765c97e39e7c879c9a78d31
Martin Gerhardy <martin.gerhardy@gmail.com>
2023-07-08 19:07:11 +0200

    VIEWPORT: set imguizmo id

--- src/tools/voxedit/modules/voxedit-ui/Viewport.cpp
+++ src/tools/voxedit/modules/voxedit-ui/Viewport.cpp
@@ -669,6 +669,7 @@ bool Viewport::renderGizmo(video::Camera &camera, float headerSize, const ImVec2
 
 	const bool orthographic = camera.mode() == video::CameraMode::Orthogonal;
 
+	ImGuizmo::SetID(_id);
 	ImGuizmo::BeginFrame();
 	const ImVec2 &windowPos = ImGui::GetWindowPos();
 	ImGuizmo::Enable(_renderContext.sceneMode || _modelGizmo->boolVal());
