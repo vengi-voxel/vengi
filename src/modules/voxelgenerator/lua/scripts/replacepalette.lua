4ab005417492c59fa28e4e067f4398a3d69d4520
Martin Gerhardy <martin.gerhardy@gmail.com>
2023-07-16 13:59:55 +0200

    VOXELGENERATOR: set a default value to the remap parameter

--- src/modules/voxelgenerator/lua/scripts/replacepalette.lua
+++ src/modules/voxelgenerator/lua/scripts/replacepalette.lua
@@ -5,7 +5,7 @@
 function arguments()
 	return {
 		{ name = 'palettename', desc = 'the palette name', type = 'string' },
-		{ name = 'remap', desc = 'false to only replace, true to remap the colors', type = 'bool' }
+		{ name = 'remap', desc = 'false to only replace, true to remap the colors', type = 'bool', default = 'false' }
 	}
 end
 
