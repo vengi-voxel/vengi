9d49240d3d8c958c35726cc0e82e59fd11efaf94
Martin Gerhardy <martin.gerhardy@gmail.com>
2023-08-03 19:19:12 +0200

    VOXEL: added Sampler support to SparseVolume

--- src/modules/voxel/SparseVolume.cpp
+++ src/modules/voxel/SparseVolume.cpp
@@ -43,4 +43,194 @@ void SparseVolume::copyFrom(const voxel::RawVolume &source) {
 	voxelutil::visitVolume(source, visitor);
 }
 
+SparseVolume::Sampler::Sampler(const SparseVolume* volume) :
+		_volume(const_cast<SparseVolume*>(volume)) {
+}
+
+SparseVolume::Sampler::Sampler(const SparseVolume& volume) :
+		_volume(const_cast<SparseVolume*>(&volume)) {
+}
+
+SparseVolume::Sampler::~Sampler() {
+}
+
+bool SparseVolume::Sampler::setVoxel(const Voxel& voxel) {
+	if (_currentPositionInvalid) {
+		return false;
+	}
+	_currentVoxel = voxel;
+	return true;
+}
+
+bool SparseVolume::Sampler::setPosition(int32_t xPos, int32_t yPos, int32_t zPos) {
+	_posInVolume.x = xPos;
+	_posInVolume.y = yPos;
+	_posInVolume.z = zPos;
+
+	const voxel::Region& region = this->region();
+	_currentPositionInvalid = 0u;
+	if (!region.containsPointInX(xPos)) {
+		_currentPositionInvalid |= SAMPLER_INVALIDX;
+	}
+	if (!region.containsPointInY(yPos)) {
+		_currentPositionInvalid |= SAMPLER_INVALIDY;
+	}
+	if (!region.containsPointInZ(zPos)) {
+		_currentPositionInvalid |= SAMPLER_INVALIDZ;
+	}
+
+	// Then we update the voxel pointer
+	if (currentPositionValid()) {
+		_currentVoxel = _volume->voxel(_posInVolume);
+		return true;
+	}
+	return false;
+}
+
+void SparseVolume::Sampler::movePositive(math::Axis axis, uint32_t offset) {
+	switch (axis) {
+	case math::Axis::X:
+		movePositiveX(offset);
+		break;
+	case math::Axis::Y:
+		movePositiveY(offset);
+		break;
+	case math::Axis::Z:
+		movePositiveZ(offset);
+		break;
+	default:
+		break;
+	}
+}
+
+void SparseVolume::Sampler::movePositiveX(uint32_t offset) {
+	const bool bIsOldPositionValid = currentPositionValid();
+
+	_posInVolume.x += (int)offset;
+
+	if (!region().containsPointInX(_posInVolume.x)) {
+		_currentPositionInvalid |= SAMPLER_INVALIDX;
+	} else {
+		_currentPositionInvalid &= ~SAMPLER_INVALIDX;
+	}
+
+	// Then we update the voxel pointer
+	if (!bIsOldPositionValid) {
+		setPosition(_posInVolume);
+	} else if (currentPositionValid()) {
+		_currentVoxel = _volume->voxel(_posInVolume);
+	}
+}
+
+void SparseVolume::Sampler::movePositiveY(uint32_t offset) {
+	const bool bIsOldPositionValid = currentPositionValid();
+
+	_posInVolume.y += (int)offset;
+
+	if (!region().containsPointInY(_posInVolume.y)) {
+		_currentPositionInvalid |= SAMPLER_INVALIDY;
+	} else {
+		_currentPositionInvalid &= ~SAMPLER_INVALIDY;
+	}
+
+	// Then we update the voxel pointer
+	if (!bIsOldPositionValid) {
+		setPosition(_posInVolume);
+	} else if (currentPositionValid()) {
+		_currentVoxel = _volume->voxel(_posInVolume);
+	}
+}
+
+void SparseVolume::Sampler::movePositiveZ(uint32_t offset) {
+	const bool bIsOldPositionValid = currentPositionValid();
+
+	_posInVolume.z += (int)offset;
+
+	if (!region().containsPointInZ(_posInVolume.z)) {
+		_currentPositionInvalid |= SAMPLER_INVALIDZ;
+	} else {
+		_currentPositionInvalid &= ~SAMPLER_INVALIDZ;
+	}
+
+	// Then we update the voxel pointer
+	if (!bIsOldPositionValid) {
+		setPosition(_posInVolume);
+	} else if (currentPositionValid()) {
+		_currentVoxel = _volume->voxel(_posInVolume);
+	}
+}
+
+void SparseVolume::Sampler::moveNegative(math::Axis axis, uint32_t offset) {
+	switch (axis) {
+	case math::Axis::X:
+		moveNegativeX(offset);
+		break;
+	case math::Axis::Y:
+		moveNegativeY(offset);
+		break;
+	case math::Axis::Z:
+		moveNegativeZ(offset);
+		break;
+	default:
+		break;
+	}
+}
+
+void SparseVolume::Sampler::moveNegativeX(uint32_t offset) {
+	const bool bIsOldPositionValid = currentPositionValid();
+
+	_posInVolume.x -= (int)offset;
+
+	if (!region().containsPointInX(_posInVolume.x)) {
+		_currentPositionInvalid |= SAMPLER_INVALIDX;
+	} else {
+		_currentPositionInvalid &= ~SAMPLER_INVALIDX;
+	}
+
+	// Then we update the voxel pointer
+	if (!bIsOldPositionValid) {
+		setPosition(_posInVolume);
+	} else if (currentPositionValid()) {
+		_currentVoxel = _volume->voxel(_posInVolume);
+	}
+}
+
+void SparseVolume::Sampler::moveNegativeY(uint32_t offset) {
+	const bool bIsOldPositionValid = currentPositionValid();
+
+	_posInVolume.y -= (int)offset;
+
+	if (!region().containsPointInY(_posInVolume.y)) {
+		_currentPositionInvalid |= SAMPLER_INVALIDY;
+	} else {
+		_currentPositionInvalid &= ~SAMPLER_INVALIDY;
+	}
+
+	// Then we update the voxel pointer
+	if (!bIsOldPositionValid) {
+		setPosition(_posInVolume);
+	} else if (currentPositionValid()) {
+		_currentVoxel = _volume->voxel(_posInVolume);
+	}
+}
+
+void SparseVolume::Sampler::moveNegativeZ(uint32_t offset) {
+	const bool bIsOldPositionValid = currentPositionValid();
+
+	_posInVolume.z -= (int)offset;
+
+	if (!region().containsPointInZ(_posInVolume.z)) {
+		_currentPositionInvalid |= SAMPLER_INVALIDZ;
+	} else {
+		_currentPositionInvalid &= ~SAMPLER_INVALIDZ;
+	}
+
+	// Then we update the voxel pointer
+	if (!bIsOldPositionValid) {
+		setPosition(_posInVolume);
+	} else if (currentPositionValid()) {
+		_currentVoxel = _volume->voxel(_posInVolume);
+	}
+}
+
 } // namespace voxel
--- src/modules/voxel/SparseVolume.h
+++ src/modules/voxel/SparseVolume.h
@@ -6,10 +6,7 @@
 
 #include "core/GLM.h"
 #include "core/collection/DynamicMap.h"
-#include "voxel/Mesh.h"
 #include "voxel/RawVolumeWrapper.h"
-#include "voxel/Region.h"
-#include "voxel/Voxel.h"
 
 namespace voxel {
 
@@ -21,6 +18,80 @@ private:
 	const bool _isRegionValid;
 
 public:
+	class Sampler {
+	private:
+		static const uint8_t SAMPLER_INVALIDX = 1 << 0;
+		static const uint8_t SAMPLER_INVALIDY = 1 << 1;
+		static const uint8_t SAMPLER_INVALIDZ = 1 << 2;
+
+	public:
+		Sampler(const SparseVolume &volume);
+		Sampler(const SparseVolume *volume);
+		virtual ~Sampler();
+
+		const Voxel &voxel() const;
+		virtual const Region region() const;
+
+		bool currentPositionValid() const;
+
+		bool setPosition(const glm::ivec3 &pos);
+		bool setPosition(int32_t x, int32_t y, int32_t z);
+		virtual bool setVoxel(const Voxel &voxel);
+		const glm::ivec3 &position() const;
+
+		void movePositiveX(uint32_t offset = 1);
+		void movePositiveY(uint32_t offset = 1);
+		void movePositiveZ(uint32_t offset = 1);
+		void movePositive(math::Axis axis, uint32_t offset = 1);
+
+		void moveNegativeX(uint32_t offset = 1);
+		void moveNegativeY(uint32_t offset = 1);
+		void moveNegativeZ(uint32_t offset = 1);
+		void moveNegative(math::Axis axis, uint32_t offset = 1);
+
+		const Voxel &peekVoxel1nx1ny1nz() const;
+		const Voxel &peekVoxel1nx1ny0pz() const;
+		const Voxel &peekVoxel1nx1ny1pz() const;
+		const Voxel &peekVoxel1nx0py1nz() const;
+		const Voxel &peekVoxel1nx0py0pz() const;
+		const Voxel &peekVoxel1nx0py1pz() const;
+		const Voxel &peekVoxel1nx1py1nz() const;
+		const Voxel &peekVoxel1nx1py0pz() const;
+		const Voxel &peekVoxel1nx1py1pz() const;
+
+		const Voxel &peekVoxel0px1ny1nz() const;
+		const Voxel &peekVoxel0px1ny0pz() const;
+		const Voxel &peekVoxel0px1ny1pz() const;
+		const Voxel &peekVoxel0px0py1nz() const;
+		const Voxel &peekVoxel0px0py0pz() const;
+		const Voxel &peekVoxel0px0py1pz() const;
+		const Voxel &peekVoxel0px1py1nz() const;
+		const Voxel &peekVoxel0px1py0pz() const;
+		const Voxel &peekVoxel0px1py1pz() const;
+
+		const Voxel &peekVoxel1px1ny1nz() const;
+		const Voxel &peekVoxel1px1ny0pz() const;
+		const Voxel &peekVoxel1px1ny1pz() const;
+		const Voxel &peekVoxel1px0py1nz() const;
+		const Voxel &peekVoxel1px0py0pz() const;
+		const Voxel &peekVoxel1px0py1pz() const;
+		const Voxel &peekVoxel1px1py1nz() const;
+		const Voxel &peekVoxel1px1py0pz() const;
+		const Voxel &peekVoxel1px1py1pz() const;
+
+	protected:
+		SparseVolume *_volume;
+
+		// The current position in the volume
+		glm::ivec3 _posInVolume{0, 0, 0};
+
+		/** Other current position information */
+		Voxel _currentVoxel;
+
+		/** Whether the current position is inside the volume */
+		uint8_t _currentPositionInvalid = 0u;
+	};
+
 	SparseVolume(const voxel::Region &region = voxel::Region::InvalidRegion);
 
 	[[nodiscard]] inline const voxel::Region &region() const {
@@ -54,9 +125,176 @@ public:
 		return _map.size();
 	}
 
+	/**
+	 * @return The width of the volume in voxels. Note that this value is inclusive, so that if the valid range is e.g.
+	 * 0 to 63 then the width is 64.
+	 * @sa height(), getDepth()
+	 */
+	int32_t width() const;
+
+	/**
+	 * @return The height of the volume in voxels. Note that this value is inclusive, so that if the valid range is e.g.
+	 * 0 to 63 then the height is 64.
+	 * @sa width(), getDepth()
+	 */
+	int32_t height() const;
+
+	/**
+	 * @return The depth of the volume in voxels. Note that this value is inclusive, so that if the valid range is e.g.
+	 * 0 to 63 then the depth is 64.
+	 * @sa width(), height()
+	 */
+	int32_t depth() const;
+
 	void copyTo(voxel::RawVolumeWrapper &target) const;
 
 	void copyFrom(const voxel::RawVolume &source);
 };
+
+inline int32_t SparseVolume::width() const {
+	return _region.getWidthInVoxels();
+}
+
+inline int32_t SparseVolume::height() const {
+	return _region.getHeightInVoxels();
+}
+
+inline int32_t SparseVolume::depth() const {
+	return _region.getDepthInVoxels();
+}
+
+inline const Region SparseVolume::Sampler::region() const {
+	return _volume->region();
+}
+
+inline const glm::ivec3 &SparseVolume::Sampler::position() const {
+	return _posInVolume;
+}
+
+inline const Voxel &SparseVolume::Sampler::voxel() const {
+	if (this->currentPositionValid()) {
+		return _currentVoxel;
+	}
+	return this->_volume->voxel(this->_posInVolume.x, this->_posInVolume.y, this->_posInVolume.z);
+}
+
+inline bool SparseVolume::Sampler::currentPositionValid() const {
+	return !_currentPositionInvalid;
+}
+
+inline bool SparseVolume::Sampler::setPosition(const glm::ivec3 &v3dNewPos) {
+	return setPosition(v3dNewPos.x, v3dNewPos.y, v3dNewPos.z);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel1nx1ny1nz() const {
+	return this->_volume->voxel(this->_posInVolume.x - 1, this->_posInVolume.y - 1, this->_posInVolume.z - 1);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel1nx1ny0pz() const {
+	return this->_volume->voxel(this->_posInVolume.x - 1, this->_posInVolume.y - 1, this->_posInVolume.z);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel1nx1ny1pz() const {
+	return this->_volume->voxel(this->_posInVolume.x - 1, this->_posInVolume.y - 1, this->_posInVolume.z + 1);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel1nx0py1nz() const {
+	return this->_volume->voxel(this->_posInVolume.x - 1, this->_posInVolume.y, this->_posInVolume.z - 1);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel1nx0py0pz() const {
+	return this->_volume->voxel(this->_posInVolume.x - 1, this->_posInVolume.y, this->_posInVolume.z);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel1nx0py1pz() const {
+	return this->_volume->voxel(this->_posInVolume.x - 1, this->_posInVolume.y, this->_posInVolume.z + 1);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel1nx1py1nz() const {
+	return this->_volume->voxel(this->_posInVolume.x - 1, this->_posInVolume.y + 1, this->_posInVolume.z - 1);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel1nx1py0pz() const {
+	return this->_volume->voxel(this->_posInVolume.x - 1, this->_posInVolume.y + 1, this->_posInVolume.z);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel1nx1py1pz() const {
+	return this->_volume->voxel(this->_posInVolume.x - 1, this->_posInVolume.y + 1, this->_posInVolume.z + 1);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel0px1ny1nz() const {
+	return this->_volume->voxel(this->_posInVolume.x, this->_posInVolume.y - 1, this->_posInVolume.z - 1);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel0px1ny0pz() const {
+	return this->_volume->voxel(this->_posInVolume.x, this->_posInVolume.y - 1, this->_posInVolume.z);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel0px1ny1pz() const {
+	return this->_volume->voxel(this->_posInVolume.x, this->_posInVolume.y - 1, this->_posInVolume.z + 1);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel0px0py1nz() const {
+	return this->_volume->voxel(this->_posInVolume.x, this->_posInVolume.y, this->_posInVolume.z - 1);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel0px0py0pz() const {
+	if (this->currentPositionValid()) {
+		return _currentVoxel;
+	}
+	return this->_volume->voxel(this->_posInVolume.x, this->_posInVolume.y, this->_posInVolume.z);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel0px0py1pz() const {
+	return this->_volume->voxel(this->_posInVolume.x, this->_posInVolume.y, this->_posInVolume.z + 1);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel0px1py1nz() const {
+	return this->_volume->voxel(this->_posInVolume.x, this->_posInVolume.y + 1, this->_posInVolume.z - 1);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel0px1py0pz() const {
+	return this->_volume->voxel(this->_posInVolume.x, this->_posInVolume.y + 1, this->_posInVolume.z);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel0px1py1pz() const {
+	return this->_volume->voxel(this->_posInVolume.x, this->_posInVolume.y + 1, this->_posInVolume.z + 1);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel1px1ny1nz() const {
+	return this->_volume->voxel(this->_posInVolume.x + 1, this->_posInVolume.y - 1, this->_posInVolume.z - 1);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel1px1ny0pz() const {
+	return this->_volume->voxel(this->_posInVolume.x + 1, this->_posInVolume.y - 1, this->_posInVolume.z);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel1px1ny1pz() const {
+	return this->_volume->voxel(this->_posInVolume.x + 1, this->_posInVolume.y - 1, this->_posInVolume.z + 1);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel1px0py1nz() const {
+	return this->_volume->voxel(this->_posInVolume.x + 1, this->_posInVolume.y, this->_posInVolume.z - 1);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel1px0py0pz() const {
+	return this->_volume->voxel(this->_posInVolume.x + 1, this->_posInVolume.y, this->_posInVolume.z);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel1px0py1pz() const {
+	return this->_volume->voxel(this->_posInVolume.x + 1, this->_posInVolume.y, this->_posInVolume.z + 1);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel1px1py1nz() const {
+	return this->_volume->voxel(this->_posInVolume.x + 1, this->_posInVolume.y + 1, this->_posInVolume.z - 1);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel1px1py0pz() const {
+	return this->_volume->voxel(this->_posInVolume.x + 1, this->_posInVolume.y + 1, this->_posInVolume.z);
+}
+
+inline const Voxel &SparseVolume::Sampler::peekVoxel1px1py1pz() const {
+	return this->_volume->voxel(this->_posInVolume.x + 1, this->_posInVolume.y + 1, this->_posInVolume.z + 1);
+}
 
 } // namespace voxel
