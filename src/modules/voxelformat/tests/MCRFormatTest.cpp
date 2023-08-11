vengi-voxel/vengi

commit c39046e3605670b71ff0b31655db42b55be73149

VOXELFORMAT: NBT: DynamicArray push_back replaced with functor based append call

src/modules/voxelformat/private/NamedBinaryTag.cpp
@@ -383,12 +383,16 @@ NamedBinaryTag NamedBinaryTag::parseType(TagType type, NamedBinaryTagContext &ct
 		}
 		core::DynamicArray<int8_t> array;
 		array.reserve(length);
-		for (size_t i = 0; i < length; i++) {
+		bool error = false;
+		array.append(length, [&ctx, &error] (int i) {
 			int8_t val;
 			if (ctx.stream->readInt8(val) != 0) {
-				return NamedBinaryTag{};
+				error = true;
 			}
-			array.push_back(val);
+			return val;
+		});
+		if (error) {
+			return NamedBinaryTag{};
 		}
 		return NamedBinaryTag{core::move(array)};
 	}
@@ -399,13 +403,14 @@ NamedBinaryTag NamedBinaryTag::parseType(TagType type, NamedBinaryTagContext &ct
 		}
 		core::DynamicArray<int32_t> array;
 		array.reserve(length);
-		for (size_t i = 0; i < length; i++) {
+		bool error = false;
+		array.append(length, [&ctx, &error] (int i) {
 			int32_t val;
-			if (ctx.stream->readInt32BE(val) != 0) {
-				return NamedBinaryTag{};
+			if (ctx.stream->readInt32(val) != 0) {
+				error = true;
 			}
-			array.push_back(val);
-		}
+			return val;
+		});
 		return NamedBinaryTag{core::move(array)};
 	}
 	case TagType::LONG_ARRAY: {
@@ -415,13 +420,14 @@ NamedBinaryTag NamedBinaryTag::parseType(TagType type, NamedBinaryTagContext &ct
 		}
 		core::DynamicArray<int64_t> array;
 		array.reserve(length);
-		for (size_t i = 0; i < length; i++) {
+		bool error = false;
+		array.append(length, [&ctx, &error] (int i) {
 			int64_t val;
-			if (ctx.stream->readInt64BE(val) != 0) {
-				return NamedBinaryTag{};
+			if (ctx.stream->readInt64(val) != 0) {
+				error = true;
 			}
-			array.push_back(val);
-		}
+			return val;
+		});
 		return NamedBinaryTag{core::move(array)};
 	}
 	case TagType::LIST: {

commit 5caa2cfc32f578e9481e8b9778fdb5680b3a8fef

VOXELFORMAT: MINECRAFT: fixed reading nbt
error was introduced in c39046e3605670b71ff0b31655db42b55be73149

src/modules/voxelformat/private/minecraft/NamedBinaryTag.cpp
@@ -405,7 +405,7 @@ NamedBinaryTag NamedBinaryTag::parseType(TagType type, NamedBinaryTagContext &ct
 		bool error = false;
 		array.append(length, [&ctx, &error] (int i) {
 			int32_t val;
-			if (ctx.stream->readInt32(val) != 0) {
+			if (ctx.stream->readInt32BE(val) != 0) {
 				error = true;
 			}
 			return val;
@@ -422,7 +422,7 @@ NamedBinaryTag NamedBinaryTag::parseType(TagType type, NamedBinaryTagContext &ct
 		bool error = false;
 		array.append(length, [&ctx, &error] (int i) {
 			int64_t val;
-			if (ctx.stream->readInt64(val) != 0) {
+			if (ctx.stream->readInt64BE(val) != 0) {
 				error = true;
 			}
 			return val;

src/modules/voxelformat/tests/MCRFormatTest.cpp
@@ -22,6 +22,11 @@ TEST_F(MCRFormatTest, testLoad117) {
 	ASSERT_EQ(node.type(), scenegraph::SceneGraphNodeType::Model);
 	const voxel::RawVolume *v = node.volume();
 	const int cnt = voxelutil::visitVolume(*v, [&](int, int, int, const voxel::Voxel &v) {}, voxelutil::VisitAll());
+	EXPECT_EQ(v->voxel(0, 62, -576), voxel::Voxel(voxel::VoxelType::Generic, 8));
+	EXPECT_EQ(v->voxel(0, -45, -576), voxel::Voxel(voxel::VoxelType::Generic, 8));
+	EXPECT_EQ(v->voxel(0, -45, -566), voxel::Voxel(voxel::VoxelType::Generic, 2));
+	EXPECT_EQ(v->voxel(0, -62, -576), voxel::Voxel(voxel::VoxelType::Generic, 118));
+	EXPECT_EQ(v->voxel(0, -64, -576), voxel::Voxel(voxel::VoxelType::Generic, 7));
 	EXPECT_EQ(32512, cnt);
 }
 