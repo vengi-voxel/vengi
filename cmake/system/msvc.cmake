3593e8b00c0303de961e1b00f5dbc494239f8304
Martin Gerhardy <martin.gerhardy@gmail.com>
2023-09-17 19:46:53 +0200

    HTTP: use winhttp api to perform downloads

--- cmake/system/msvc.cmake
+++ cmake/system/msvc.cmake
@@ -1,6 +1,6 @@
 set(CMAKE_SYSTEM_NAME Windows)
 
-set(CMAKE_C_STANDARD_LIBRARIES "kernel32.lib user32.lib gdi32.lib winspool.lib winmm.lib imm32.lib comctl32.lib version.lib shell32.lib ole32.lib oleaut32.lib uuid.lib comdlg32.lib advapi32.lib dbghelp.lib wsock32.lib ws2_32.lib iphlpapi.lib rpcrt4.lib wininet.lib")
+set(CMAKE_C_STANDARD_LIBRARIES "kernel32.lib user32.lib gdi32.lib winspool.lib winmm.lib imm32.lib comctl32.lib version.lib shell32.lib ole32.lib oleaut32.lib uuid.lib comdlg32.lib advapi32.lib winhttp.lib dbghelp.lib wsock32.lib ws2_32.lib iphlpapi.lib rpcrt4.lib wininet.lib")
 set(CMAKE_CXX_STANDARD_LIBRARIES "${CMAKE_C_STANDARD_LIBRARIES}")
 # /W4: set warning level to 4
 # /MP: build with multiple processes
--- src/modules/http/Request.cpp
+++ src/modules/http/Request.cpp
@@ -8,7 +8,11 @@
 #include "core/Log.h"
 #include "core/Var.h"
 #include "engine-config.h"
-#if USE_CURL
+#if __WINDOWS__
+#define WIN32_LEAN_AND_MEAN (1)
+#include <windows.h>
+#include <winhttp.h>
+#elif USE_CURL
 #include <curl/curl.h>
 #endif
 
@@ -21,7 +25,9 @@ static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *use
 #endif
 
 bool Request::supported() {
-#if USE_CURL
+#if __WINDOWS__
+	return true;
+#elif USE_CURL
 	return true;
 #else
 	return false;
@@ -29,7 +35,78 @@ bool Request::supported() {
 }
 
 bool Request::request(const core::String &url, io::WriteStream &stream) {
-#if USE_CURL
+#if __WINDOWS__
+	// Initialize WinHTTP and create a session
+	HINTERNET hSession =
+		WinHttpOpen(nullptr, WINHTTP_ACCESS_TYPE_DEFAULT_PROXY, WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS, 0);
+	if (hSession == nullptr) {
+		Log::error("Failed to create session for http download");
+		return false;
+	}
+
+	// Set the connection timeout
+	const DWORD httpTimeoutMillis = core::Var::get(cfg::HttpTimeout, "5")->intVal() * 1000;
+	DWORD dwResolveTimeout = httpTimeoutMillis;
+	DWORD dwConnectTimeout = core::Var::get(cfg::HttpConnectTimeout, "5")->intVal() * 1000;
+	DWORD dwSendTimeout = httpTimeoutMillis;
+	DWORD dwReceiveTimeout = httpTimeoutMillis;
+	if (!WinHttpSetTimeouts(hSession, dwResolveTimeout, dwConnectTimeout, dwSendTimeout, dwReceiveTimeout)) {
+		Log::error("Failed to set http timeouts");
+		WinHttpCloseHandle(hSession);
+		return false;
+	}
+
+	// Open a connection to the remote server
+	HINTERNET hConnect = WinHttpOpenRequest(hSession, L"GET", L"/path/to/resource", nullptr, WINHTTP_NO_REFERER,
+											WINHTTP_DEFAULT_ACCEPT_TYPES, WINHTTP_FLAG_REFRESH);
+	if (hConnect == nullptr) {
+		Log::error("Failed to connect to url: %s", url.c_str());
+		WinHttpCloseHandle(hSession);
+		return false;
+	}
+
+	// add request headers
+	const wchar_t *reqHeaders = L"User-Agent: Mozilla/5.0\r\n"
+								L"Connection: keep-alive\r\n"
+								L"Accept-Encoding: gzip, deflate";
+	if (!WinHttpAddRequestHeaders(hConnect, reqHeaders, -1L, WINHTTP_ADDREQ_FLAG_ADD | WINHTTP_ADDREQ_FLAG_REPLACE)) {
+		Log::warn("Failed to add request headers to url: %s", url.c_str());
+	}
+
+	// Send the request
+	if (!WinHttpSendRequest(hConnect, WINHTTP_NO_ADDITIONAL_HEADERS, 0, WINHTTP_NO_REQUEST_DATA, 0, 0, 0)) {
+		Log::error("Failed to send request to url: %s", url.c_str());
+		WinHttpCloseHandle(hConnect);
+		WinHttpCloseHandle(hSession);
+		return false;
+	}
+
+	if (!WinHttpReceiveResponse(hConnect, nullptr)) {
+		Log::error("Failed to receive response from url: %s", url.c_str());
+		WinHttpCloseHandle(hConnect);
+		WinHttpCloseHandle(hSession);
+		return false;
+	}
+
+	DWORD dwStatusCode = 0;
+	DWORD dwSize = sizeof(dwStatusCode);
+	WinHttpQueryHeaders(hConnect, WINHTTP_QUERY_STATUS_CODE | WINHTTP_QUERY_FLAG_NUMBER, nullptr, &dwStatusCode,
+						&dwSize, nullptr);
+	if (dwStatusCode != 200) {
+		Log::warn("Failed to download url: %s with status code: %d", url.c_str(), dwStatusCode);
+	}
+
+	// Read and save the response data
+	DWORD bytesRead;
+	BYTE buffer[4096];
+	while (WinHttpReadData(hConnect, buffer, sizeof(buffer), &bytesRead)) {
+		// Write the 'bytesRead' bytes from the buffer
+		stream.write(buffer, bytesRead);
+	}
+	WinHttpCloseHandle(hConnect);
+	WinHttpCloseHandle(hSession);
+	return true;
+#elif USE_CURL
 	CURL *curl = curl_easy_init();
 	if (curl == nullptr) {
 		return false;
