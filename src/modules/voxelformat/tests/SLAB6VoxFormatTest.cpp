cdca3c9b2736c3145d5c30719226528daaf84ce8
Martin Gerhardy <martin.gerhardy@gmail.com>
2023-09-28 19:38:04 +0200

    VOXELFORMAT: SLAB6/VOX: added failing test case

--- src/modules/voxelformat/tests/SLAB6VoxFormatTest.cpp
+++ src/modules/voxelformat/tests/SLAB6VoxFormatTest.cpp
@@ -4,6 +4,8 @@
 
 #include "voxelformat/private/slab6/SLAB6VoxFormat.h"
 #include "AbstractVoxFormatTest.h"
+#include "voxelformat/private/slab6/KV6Format.h"
+#include "voxelformat/tests/TestHelper.h"
 
 namespace voxelformat {
 
@@ -12,6 +14,13 @@ class SLAB6VoxFormatTest : public AbstractVoxFormatTest {};
 TEST_F(SLAB6VoxFormatTest, testSaveSmallVoxel) {
 	SLAB6VoxFormat f;
 	testSaveLoadVoxel("loadvoxel.vox", &f);
+}
+
+TEST_F(SLAB6VoxFormatTest, DISABLED_testChrKnight) {
+	SLAB6VoxFormat f1;
+	KV6Format f2;
+	voxel::ValidateFlags flags = voxel::ValidateFlags::All & ~voxel::ValidateFlags::Pivot;
+	testLoadSceneGraph("slab6_vox_test.vox", f1, "slab6_vox_test.kv6", f2, flags);
 }
 
 } // namespace voxelformat
