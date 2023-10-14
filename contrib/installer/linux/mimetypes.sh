0a549b5df4274fdba38554b6416205e684fc05ba
Martin Gerhardy <martin.gerhardy@gmail.com>
2023-10-14 12:12:26 +0200

    CMAKE: automatically update the copyright year and month
    also add some more version details to the osx plist file

--- contrib/installer/linux/mimetypes.sh
+++ contrib/installer/linux/mimetypes.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-mimetypes=$(xmllint --xpath "//*[local-name()='mime-type']/@type" x-voxel.xml | awk -F\" '{ print $2 }' | sort)
+SELF=$(readlink -f "${0}")
+DIRECTORY=$(dirname "$SELF")
+cd $DIRECTORY
+
+mimetypes=$(xmlstarlet sel -N x="http://www.freedesktop.org/standards/shared-mime-info" -t -m "//x:mime-type/@type" -v . -n x-voxel.xml | sort)
+#mimetypes=$(xmllint --xpath "//*[local-name()='mime-type']/@type" x-voxel.xml | awk -F\" '{ print $2 }' | sort)
 mimetypes=$(echo $mimetypes | sed 's/ /;/g')
 
 sed -i "s#MimeType=.*#MimeType=$mimetypes#g" voxedit.desktop.in
