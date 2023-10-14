5f44437bb6f8f6a93fbd746ed5b0997338c9895d
Martin Gerhardy <martin.gerhardy@gmail.com>
2023-10-14 11:33:27 +0200

    HTTP: extended testcase

--- src/modules/http/tests/RequestTest.cpp
+++ src/modules/http/tests/RequestTest.cpp
@@ -4,24 +4,57 @@
 
 #include "http/Request.h"
 #include "app/tests/AbstractTest.h"
+#include "engine-config.h"
 #include "io/BufferedReadWriteStream.h"
 #include <gtest/gtest.h>
+#include <json.hpp>
 
 namespace http {
 
 class RequestTest : public app::AbstractTest {};
 
-TEST_F(RequestTest, testRequest) {
+TEST_F(RequestTest, testGetRequest) {
 	if (!Request::supported()) {
 		GTEST_SKIP() << "No http support available";
 	}
 	io::BufferedReadWriteStream stream;
-	Request request("https://www.google.com", http::RequestType::GET);
+	Request request("https://httpbin.org/get", http::RequestType::GET);
 	ASSERT_TRUE(request.execute(stream));
-	// stream.seek(0);
-	// core::String response;
-	// stream.readString((int)stream.size(), response);
 	ASSERT_NE(0, stream.size());
+	stream.seek(0);
+	core::String response;
+	stream.readString((int)stream.size(), response);
+	nlohmann::json jsonResponse = nlohmann::json::parse(response);
+	ASSERT_TRUE(jsonResponse.contains("headers")) << response;
+	ASSERT_FALSE(jsonResponse["headers"].contains("Content-Length")) << response;
+	ASSERT_FALSE(jsonResponse["headers"].contains("Content-Type")) << response;
+	ASSERT_TRUE(jsonResponse["headers"].contains("User-Agent")) << response;
+	const core::String userAgent = app::App::getInstance()->fullAppname() + "/" PROJECT_VERSION;
+	EXPECT_STREQ(userAgent.c_str(), jsonResponse["headers"]["User-Agent"].get<std::string>().c_str()) << response;
+}
+
+TEST_F(RequestTest, testPostRequest) {
+	if (!Request::supported()) {
+		GTEST_SKIP() << "No http support available";
+	}
+	io::BufferedReadWriteStream stream;
+	Request request("https://httpbin.org/post", http::RequestType::POST);
+	request.addHeader("Content-Type", "application/json");
+	request.setBody("{}");
+	ASSERT_TRUE(request.execute(stream));
+	ASSERT_NE(0, stream.size());
+	stream.seek(0);
+	core::String response;
+	stream.readString((int)stream.size(), response);
+	nlohmann::json jsonResponse = nlohmann::json::parse(response);
+	ASSERT_TRUE(jsonResponse.contains("headers")) << response;
+	ASSERT_TRUE(jsonResponse["headers"].contains("User-Agent")) << response;
+	const core::String userAgent = app::App::getInstance()->fullAppname() + "/" PROJECT_VERSION;
+	EXPECT_STREQ(userAgent.c_str(), jsonResponse["headers"]["User-Agent"].get<std::string>().c_str()) << response;
+	ASSERT_TRUE(jsonResponse["headers"].contains("Content-Type")) << response;
+	EXPECT_EQ("application/json", jsonResponse["headers"]["Content-Type"].get<std::string>()) << response;
+	ASSERT_TRUE(jsonResponse["headers"].contains("Content-Length")) << response;
+	EXPECT_EQ("2", jsonResponse["headers"]["Content-Length"].get<std::string>()) << response;
 }
 
 } // namespace http
