12074ffbd7037501ee154f92a46e3c24be2eb199
Martin Gerhardy <martin.gerhardy@gmail.com>
2023-10-31 09:55:40 +0100

    METRIC: fixed tests

--- src/modules/metric/tests/MetricTest.cpp
+++ src/modules/metric/tests/MetricTest.cpp
@@ -87,12 +87,12 @@ TEST_F(MetricTest, testCounterIncreaseTwo) {
 }
 
 TEST_F(MetricTest, testCounterJSON) {
-	EXPECT_EQ(count("test1", 1, Flavor::JSON), R"({"name": "test.test1","value": "1","type": "c","tags": [{"uuid": "fake"}]})");
+	EXPECT_EQ(count("test1", 1, Flavor::JSON), R"({"name": "test.test1","value": 1,"type": "c","tags": {"uuid": "fake"}})");
 }
 
 TEST_F(MetricTest, testCounterTagsJSON) {
 	EXPECT_EQ(count("test1", 1, Flavor::JSON, {{"foo", "bar"}}),
-			  R"({"name": "test.test1","value": "1","type": "c","tags": [{"uuid": "fake"},{"foo": "bar"}]})");
+			  R"({"name": "test.test1","value": 1,"type": "c","tags": {"uuid": "fake","foo": "bar"}})");
 }
 
 TEST_F(MetricTest, testGaugeValueOne) {
