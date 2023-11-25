35fcf8f8a5ce551285c38ce2c2c93d07d333b238
Martin Gerhardy <martin.gerhardy@gmail.com>
2023-11-25 22:15:54 +0100

    VOXEDIT: fixed issue #350

--- src/tools/voxedit/modules/voxedit-ui/Viewport.cpp
+++ src/tools/voxedit/modules/voxedit-ui/Viewport.cpp
@@ -746,7 +746,8 @@ bool Viewport::renderGizmo(video::Camera &camera, float headerSize, const ImVec2
 	const bool orthographic = camera.mode() == video::CameraMode::Orthogonal;
 
 	ImGuizmo::SetID(_id);
-	ImGuizmo::SetDrawlist(ImGui::GetWindowDrawList());
+	ImGuizmo::SetDrawlist();
+	ImGuizmo::SetWindow();
 	const ImVec2 &windowPos = ImGui::GetWindowPos();
 	ImGuizmo::Enable(_renderContext.sceneMode || _modelGizmo->boolVal());
 	ImGuizmo::AllowAxisFlip(_gizmoAllowAxisFlip->boolVal());
