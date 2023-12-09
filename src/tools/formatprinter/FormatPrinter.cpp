5409ee2b28a6cd00498f2f63b3d41b6b9df027d8
Martin Gerhardy <martin.gerhardy@gmail.com>
2023-12-09 20:15:09 +0100

    FORMATPRINTER: use Log::printf

--- src/tools/formatprinter/FormatPrinter.cpp
+++ src/tools/formatprinter/FormatPrinter.cpp
@@ -32,9 +32,9 @@ static void printJsonStringArray(const T &array) {
 	int i = 0;
 	for (const core::String &e : array) {
 		if (i != 0) {
-			printf(",");
+			Log::printf(",");
 		}
-		printf("\"%s\"", e.c_str());
+		Log::printf("\"%s\"", e.c_str());
 		++i;
 	}
 }
@@ -44,20 +44,20 @@ static void printJsonMagicArray(const T &array) {
 	int i = 0;
 	for (const core::String &e : array) {
 		if (i != 0) {
-			printf(",");
+			Log::printf(",");
 		}
-		printf("{");
+		Log::printf("{");
 		if (SDL_isprint(e.first())) {
-			printf("\"type\": \"string\", \"value\": \"%s\"", e.c_str());
+			Log::printf("\"type\": \"string\", \"value\": \"%s\"", e.c_str());
 		} else {
-			printf("\"type\": \"bytes\", \"value\": \"");
-			printf("0x");
+			Log::printf("\"type\": \"bytes\", \"value\": \"");
+			Log::printf("0x");
 			for (size_t i = 0; i < e.size(); ++i) {
-				printf("%02X", (uint8_t)e[i]);
+				Log::printf("%02X", (uint8_t)e[i]);
 			}
-			printf("\"");
+			Log::printf("\"");
 		}
-		printf("}");
+		Log::printf("}");
 		++i;
 	}
 }
@@ -134,53 +134,53 @@ void FormatPrinter::printManPageLoadSaveFormats() {
 	}
 	formatDescriptions.sort(core::Greater<io::FormatDescription>());
 
-	printf(".SH LOAD\n");
+	Log::printf(".SH LOAD\n");
 	for (const io::FormatDescription &desc : formatDescriptions) {
-		printf(".TP\n");
-		printf("%s (", desc.name.c_str());
+		Log::printf(".TP\n");
+		Log::printf("%s (", desc.name.c_str());
 		int ext = 0;
 		for (const core::String &e : desc.exts) {
 			if (ext > 0) {
-				printf(", ");
+				Log::printf(", ");
 			}
-			printf("*.%s", e.c_str());
+			Log::printf("*.%s", e.c_str());
 			++ext;
 		}
-		printf(")\n");
+		Log::printf(")\n");
 	}
-	printf("\n");
+	Log::printf("\n");
 
-	printf(".SH SAVE\n");
+	Log::printf(".SH SAVE\n");
 	for (const io::FormatDescription &desc : formatDescriptions) {
 		if (!voxelSaveSupported(desc)) {
 			continue;
 		}
-		printf(".TP\n");
-		printf("%s (", desc.name.c_str());
+		Log::printf(".TP\n");
+		Log::printf("%s (", desc.name.c_str());
 		int ext = 0;
 		for (const core::String &e : desc.exts) {
 			if (ext > 0) {
-				printf(", ");
+				Log::printf(", ");
 			}
-			printf("*.%s", e.c_str());
+			Log::printf("*.%s", e.c_str());
 			++ext;
 		}
-		printf(")\n");
+		Log::printf(")\n");
 	}
-	printf("\n");
+	Log::printf("\n");
 }
 
 void FormatPrinter::printMarkdownTables() {
-	printf("# Formats\n");
-	printf("\n");
-	printf("## Voxel formats\n");
-	printf("\n");
-	printf("> The `vengi` format is the best supported format. Saving into any other format might lose several details "
+	Log::printf("# Formats\n");
+	Log::printf("\n");
+	Log::printf("## Voxel formats\n");
+	Log::printf("\n");
+	Log::printf("> The `vengi` format is the best supported format. Saving into any other format might lose several details "
 		   "from your scene. This depends on the capabilities of the format and the completeness of the implementation "
 		   "for supporting that particular format.\n");
-	printf("\n");
-	printf("| Name                       | Extension   | Loading | Saving | Thumbnails | Palette | Animations |\n");
-	printf("| :------------------------- | ----------- | ------- | ------ | ---------- | ------- | ---------- |\n");
+	Log::printf("\n");
+	Log::printf("| Name                       | Extension   | Loading | Saving | Thumbnails | Palette | Animations |\n");
+	Log::printf("| :------------------------- | ----------- | ------- | ------ | ---------- | ------- | ---------- |\n");
 	core::DynamicArray<io::FormatDescription> formatDescriptions;
 	for (const io::FormatDescription *desc = voxelformat::voxelLoad(); desc->valid(); ++desc) {
 		formatDescriptions.push_back(*desc);
@@ -194,15 +194,15 @@ void FormatPrinter::printMarkdownTables() {
 		const bool palette = desc.flags & VOX_FORMAT_FLAG_PALETTE_EMBEDDED;
 		const bool animation = desc.flags & VOX_FORMAT_FLAG_ANIMATION;
 		const bool save = voxelSaveSupported(desc);
-		printf("| %-26s | %-11s | %-7s | %-6s | %-10s | %-7s | %-10s |\n", desc.name.c_str(),
+		Log::printf("| %-26s | %-11s | %-7s | %-6s | %-10s | %-7s | %-10s |\n", desc.name.c_str(),
 			   desc.mainExtension().c_str(), "X", save ? "X" : " ", screenshot ? "X" : " ", palette ? "X" : " ",
 			   animation ? "X" : " ");
 	}
-	printf("\n");
-	printf("## Mesh formats\n");
-	printf("\n");
-	printf("| Name                       | Extension | Loading | Saving    | Animations |\n");
-	printf("| :------------------------- | --------- | ------- | --------- | ---------- |\n");
+	Log::printf("\n");
+	Log::printf("## Mesh formats\n");
+	Log::printf("\n");
+	Log::printf("| Name                       | Extension | Loading | Saving    | Animations |\n");
+	Log::printf("| :------------------------- | --------- | ------- | --------- | ---------- |\n");
 	for (const io::FormatDescription &desc : formatDescriptions) {
 		if (!voxelformat::isMeshFormat(desc)) {
 			continue;
@@ -210,14 +210,14 @@ void FormatPrinter::printMarkdownTables() {
 		core::String spec;
 		const bool animation = desc.flags & VOX_FORMAT_FLAG_ANIMATION;
 		const bool save = voxelSaveSupported(desc);
-		printf("| %-26s | %-9s | %-7s | %-9s | %-10s |\n", desc.name.c_str(), desc.mainExtension().c_str(), "X",
+		Log::printf("| %-26s | %-9s | %-7s | %-9s | %-10s |\n", desc.name.c_str(), desc.mainExtension().c_str(), "X",
 			   save ? "X" : " ", animation ? "X" : " ");
 	}
-	printf("\n");
-	printf("## Palettes\n");
-	printf("\n");
-	printf("| Name                            | Extension | Loading | Saving |\n");
-	printf("| :------------------------------ | --------- | ------- | ------ |\n");
+	Log::printf("\n");
+	Log::printf("## Palettes\n");
+	Log::printf("\n");
+	Log::printf("| Name                            | Extension | Loading | Saving |\n");
+	Log::printf("| :------------------------------ | --------- | ------- | ------ |\n");
 
 	formatDescriptions.clear();
 	for (const io::FormatDescription *desc = io::format::palettes(); desc->valid(); ++desc) {
@@ -226,192 +226,192 @@ void FormatPrinter::printMarkdownTables() {
 	formatDescriptions.sort(core::Greater<io::FormatDescription>());
 	for (const io::FormatDescription &desc : formatDescriptions) {
 		const bool save = desc.flags & FORMAT_FLAG_SAVE;
-		printf("| %-31s | %-9s | X       | %c      |\n", desc.name.c_str(), desc.mainExtension().c_str(),
+		Log::printf("| %-31s | %-9s | X       | %c      |\n", desc.name.c_str(), desc.mainExtension().c_str(),
 			   save ? 'X' : ' ');
 	}
-	printf("\n");
-	printf("## Images/textures\n");
-	printf("\n");
-	printf("| Name                        | Extension |\n");
-	printf("| :-------------------------- | --------- |\n");
+	Log::printf("\n");
+	Log::printf("## Images/textures\n");
+	Log::printf("\n");
+	Log::printf("| Name                        | Extension |\n");
+	Log::printf("| :-------------------------- | --------- |\n");
 	formatDescriptions.clear();
 	for (const io::FormatDescription *desc = io::format::images(); desc->valid(); ++desc) {
 		formatDescriptions.push_back(*desc);
 	}
 	formatDescriptions.sort(core::Greater<io::FormatDescription>());
 	for (const io::FormatDescription &desc : formatDescriptions) {
-		printf("| %-27s | %-9s |\n", desc.name.c_str(), desc.exts[0].c_str());
+		Log::printf("| %-27s | %-9s |\n", desc.name.c_str(), desc.exts[0].c_str());
 	}
 }
 
 void FormatPrinter::printApplicationPlist() {
-	printf("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
-	printf("<!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" "
+	Log::printf("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+	Log::printf("<!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" "
 		   "\"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n");
-	printf("<plist version=\"1.0\">\n");
-	printf("  <dict>\n");
-	printf("    <key>CFBundleDevelopmentRegion</key>\n");
-	printf("    <string>en-US</string>\n");
-	printf("    <key>CFBundleExecutable</key>\n");
-	printf("    <string>${MACOSX_BUNDLE_EXECUTABLE_NAME}</string>\n");
-	printf("    <key>CFBundleShortVersionString</key>\n");
-	printf("    <string>${MACOSX_BUNDLE_SHORT_VERSION_STRING}</string>\n");
-	printf("    <key>CFBundleLongVersionString</key>\n");
-	printf("    <string>${MACOSX_BUNDLE_LONG_VERSION_STRING}</string>\n");
-	printf("    <key>CFBundleIdentifier</key>\n");
-	printf("    <string>${MACOSX_BUNDLE_GUI_IDENTIFIER}</string>\n");
-	printf("    <key>CFBundleInfoDictionaryVersion</key>\n");
-	printf("    <string>6.0</string>\n");
-	printf("    <key>CFBundleIconFile</key>\n");
-	printf("    <string>${MACOSX_BUNDLE_ICON_FILE}</string>\n");
-	printf("    <key>NSHumanReadableCopyright</key>\n");
-	printf("    <string>${MACOSX_BUNDLE_COPYRIGHT}</string>\n");
-	printf("    <key>CFBundleName</key>\n");
-	printf("    <string>${MACOSX_BUNDLE_BUNDLE_NAME}</string>\n");
-	printf("    <key>CFBundlePackageType</key>\n");
-	printf("    <string>APPL</string>\n");
-	printf("    <key>NSHighResolutionCapable</key>\n");
-	printf("    <true/>\n");
-	printf("    <key>NSRequiresAquaSystemAppearance</key>\n");
-	printf("    <false/>\n");
-	printf("    <key>CFBundleDocumentTypes</key>\n");
-	printf("    <array>\n");
+	Log::printf("<plist version=\"1.0\">\n");
+	Log::printf("  <dict>\n");
+	Log::printf("    <key>CFBundleDevelopmentRegion</key>\n");
+	Log::printf("    <string>en-US</string>\n");
+	Log::printf("    <key>CFBundleExecutable</key>\n");
+	Log::printf("    <string>${MACOSX_BUNDLE_EXECUTABLE_NAME}</string>\n");
+	Log::printf("    <key>CFBundleShortVersionString</key>\n");
+	Log::printf("    <string>${MACOSX_BUNDLE_SHORT_VERSION_STRING}</string>\n");
+	Log::printf("    <key>CFBundleLongVersionString</key>\n");
+	Log::printf("    <string>${MACOSX_BUNDLE_LONG_VERSION_STRING}</string>\n");
+	Log::printf("    <key>CFBundleIdentifier</key>\n");
+	Log::printf("    <string>${MACOSX_BUNDLE_GUI_IDENTIFIER}</string>\n");
+	Log::printf("    <key>CFBundleInfoDictionaryVersion</key>\n");
+	Log::printf("    <string>6.0</string>\n");
+	Log::printf("    <key>CFBundleIconFile</key>\n");
+	Log::printf("    <string>${MACOSX_BUNDLE_ICON_FILE}</string>\n");
+	Log::printf("    <key>NSHumanReadableCopyright</key>\n");
+	Log::printf("    <string>${MACOSX_BUNDLE_COPYRIGHT}</string>\n");
+	Log::printf("    <key>CFBundleName</key>\n");
+	Log::printf("    <string>${MACOSX_BUNDLE_BUNDLE_NAME}</string>\n");
+	Log::printf("    <key>CFBundlePackageType</key>\n");
+	Log::printf("    <string>APPL</string>\n");
+	Log::printf("    <key>NSHighResolutionCapable</key>\n");
+	Log::printf("    <true/>\n");
+	Log::printf("    <key>NSRequiresAquaSystemAppearance</key>\n");
+	Log::printf("    <false/>\n");
+	Log::printf("    <key>CFBundleDocumentTypes</key>\n");
+	Log::printf("    <array>\n");
 	for (const io::FormatDescription *desc = voxelformat::voxelLoad(); desc->valid(); ++desc) {
 		const core::String &m = uniqueMimetype(*desc);
-		printf("      <dict>\n");
-		printf("        <key>CFBundleTypeName</key>\n");
-		printf("        <string>%s</string>\n", desc->name.c_str());
-		printf("        <key>CFBundleTypeIconFile</key>\n");
-		printf("        <string>icon.icns</string>\n");
-		printf("        <key>CFBundleTypeMIMETypes</key>\n");
-		printf("        <array>\n");
-		printf("          <string>%s</string>\n", m.c_str());
-		printf("        </array>\n");
-		printf("        <key>CFBundleTypeExtensions</key>\n");
-		printf("        <array>\n");
+		Log::printf("      <dict>\n");
+		Log::printf("        <key>CFBundleTypeName</key>\n");
+		Log::printf("        <string>%s</string>\n", desc->name.c_str());
+		Log::printf("        <key>CFBundleTypeIconFile</key>\n");
+		Log::printf("        <string>icon.icns</string>\n");
+		Log::printf("        <key>CFBundleTypeMIMETypes</key>\n");
+		Log::printf("        <array>\n");
+		Log::printf("          <string>%s</string>\n", m.c_str());
+		Log::printf("        </array>\n");
+		Log::printf("        <key>CFBundleTypeExtensions</key>\n");
+		Log::printf("        <array>\n");
 		for (const core::String &e : desc->exts) {
-			printf("          <string>%s</string>\n", e.c_str());
+			Log::printf("          <string>%s</string>\n", e.c_str());
 		}
-		printf("        </array>\n");
-		printf("        <key>CFBundleTypeRole</key>\n");
-		printf("        <string>Editor</string>\n");
-		printf("        <key>NSDocumentClass</key>\n");
-		printf("        <string>AppDocument</string>\n");
-		printf("      </dict>\n");
+		Log::printf("        </array>\n");
+		Log::printf("        <key>CFBundleTypeRole</key>\n");
+		Log::printf("        <string>Editor</string>\n");
+		Log::printf("        <key>NSDocumentClass</key>\n");
+		Log::printf("        <string>AppDocument</string>\n");
+		Log::printf("      </dict>\n");
 	}
-	printf("    </array>\n");
-	printf("  </dict>\n");
-	printf("</plist>\n");
+	Log::printf("    </array>\n");
+	Log::printf("  </dict>\n");
+	Log::printf("</plist>\n");
 }
 
 void FormatPrinter::printMimeInfo() {
 	_uniqueMimetypes.clear();
-	printf("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
-	printf("<mime-info xmlns=\"http://www.freedesktop.org/standards/shared-mime-info\">\n");
+	Log::printf("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
+	Log::printf("<mime-info xmlns=\"http://www.freedesktop.org/standards/shared-mime-info\">\n");
 	for (const io::FormatDescription *desc = voxelformat::voxelLoad(); desc->valid(); ++desc) {
 		const core::String &m = uniqueMimetype(*desc);
-		printf("\t<mime-type type=\"%s\">\n", m.c_str());
-		printf("\t\t<comment>%s</comment>\n", desc->name.c_str());
+		Log::printf("\t<mime-type type=\"%s\">\n", m.c_str());
+		Log::printf("\t\t<comment>%s</comment>\n", desc->name.c_str());
 		for (const core::String &e : desc->exts) {
-			printf("\t\t<glob pattern=\"*.%s\"/>\n", e.c_str());
+			Log::printf("\t\t<glob pattern=\"*.%s\"/>\n", e.c_str());
 		}
 		for (const core::String &e : desc->magics) {
-			printf("\t\t<magic priority=\"50\">\n");
+			Log::printf("\t\t<magic priority=\"50\">\n");
 			if (SDL_isprint(e.first())) {
-				printf("\t\t\t<match type=\"string\" offset=\"0\" value=\"%s\"/>\n", e.c_str());
+				Log::printf("\t\t\t<match type=\"string\" offset=\"0\" value=\"%s\"/>\n", e.c_str());
 			} else {
 				for (size_t i = 0; i < e.size(); ++i) {
-					printf("\t\t\t<match type=\"byte\" offset=\"%i\" value=\"%i\"/>\n", (int)i, (int)e[i]);
+					Log::printf("\t\t\t<match type=\"byte\" offset=\"%i\" value=\"%i\"/>\n", (int)i, (int)e[i]);
 				}
 			}
-			printf("\t\t</magic>\n");
+			Log::printf("\t\t</magic>\n");
 		}
-		printf("\t</mime-type>\n");
+		Log::printf("\t</mime-type>\n");
 	}
-	printf("</mime-info>\n");
+	Log::printf("</mime-info>\n");
 }
 
 void FormatPrinter::printJson(bool palette, bool image, bool voxel) {
-	printf("{");
+	Log::printf("{");
 	if (palette) {
-		printf("\"palettes\": [");
+		Log::printf("\"palettes\": [");
 		int i = 0;
 		for (const io::FormatDescription *desc = io::format::palettes(); desc->valid(); ++desc) {
 			if (i != 0) {
-				printf(",");
+				Log::printf(",");
 			}
-			printf("{");
-			printf("\"name\": \"%s\",", desc->name.c_str());
-			printf("\"extensions\": [");
+			Log::printf("{");
+			Log::printf("\"name\": \"%s\",", desc->name.c_str());
+			Log::printf("\"extensions\": [");
 			printJsonStringArray(desc->exts);
-			printf("],\"magics\": [");
+			Log::printf("],\"magics\": [");
 			printJsonMagicArray(desc->magics);
-			printf("]");
-			printf("}");
+			Log::printf("]");
+			Log::printf("}");
 			++i;
 		}
-		printf("]");
+		Log::printf("]");
 	}
 	if (image) {
 		if (palette) {
-			printf(",");
+			Log::printf(",");
 		}
-		printf("\"images\": [");
+		Log::printf("\"images\": [");
 		int i = 0;
 		for (const io::FormatDescription *desc = io::format::images(); desc->valid(); ++desc) {
 			if (i != 0) {
-				printf(",");
+				Log::printf(",");
 			}
-			printf("{");
-			printf("\"name\": \"%s\",", desc->name.c_str());
-			printf("\"extensions\": [");
+			Log::printf("{");
+			Log::printf("\"name\": \"%s\",", desc->name.c_str());
+			Log::printf("\"extensions\": [");
 			printJsonStringArray(desc->exts);
-			printf("],\"magics\": [");
+			Log::printf("],\"magics\": [");
 			printJsonMagicArray(desc->magics);
-			printf("]");
-			printf("}");
+			Log::printf("]");
+			Log::printf("}");
 			++i;
 		}
-		printf("]");
+		Log::printf("]");
 	}
 	if (voxel) {
 		if (palette || image) {
-			printf(",");
+			Log::printf(",");
 		}
-		printf("\"voxels\": [");
+		Log::printf("\"voxels\": [");
 		int i = 0;
 		for (const io::FormatDescription *desc = voxelformat::voxelLoad(); desc->valid(); ++desc) {
 			if (i != 0) {
-				printf(",");
+				Log::printf(",");
 			}
-			printf("{");
-			printf("\"name\": \"%s\",", desc->name.c_str());
-			printf("\"extensions\": [");
+			Log::printf("{");
+			Log::printf("\"name\": \"%s\",", desc->name.c_str());
+			Log::printf("\"extensions\": [");
 			printJsonStringArray(desc->exts);
-			printf("],\"magics\": [");
+			Log::printf("],\"magics\": [");
 			printJsonMagicArray(desc->magics);
-			printf("],");
+			Log::printf("],");
 			const core::String &m = uniqueMimetype(*desc);
-			printf("\"mimetype\": \"%s\",", m.c_str());
+			Log::printf("\"mimetype\": \"%s\",", m.c_str());
 			if (voxelformat::isMeshFormat(*desc)) {
-				printf("\"mesh\": true,");
+				Log::printf("\"mesh\": true,");
 			}
 			if (voxelformat::isAnimationSupported(*desc)) {
-				printf("\"animation\": true,");
+				Log::printf("\"animation\": true,");
 			} else {
-				printf("\"animation\": false,");
+				Log::printf("\"animation\": false,");
 			}
 			if (voxelSaveSupported(*desc)) {
-				printf("\"save\": true");
+				Log::printf("\"save\": true");
 			} else {
-				printf("\"save\": false");
+				Log::printf("\"save\": false");
 			}
-			printf("}");
+			Log::printf("}");
 			++i;
 		}
-		printf("]");
+		Log::printf("]");
 	}
-	printf("}\n");
+	Log::printf("}\n");
 }
 
 int main(int argc, char *argv[]) {
