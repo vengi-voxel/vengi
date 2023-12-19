125c79bd11daec7bc17492b5a4bc77ae919a376c
Martin Gerhardy <martin.gerhardy@gmail.com>
2023-12-19 21:42:02 +0100

    IO: cleanup - found by PVS-Studio V547

--- src/modules/io/system/Unix.cpp
+++ src/modules/io/system/Unix.cpp
@@ -192,9 +192,7 @@ bool fs_mkdir(const char *path) {
 	if (errno == EEXIST) {
 		return true;
 	}
-	if (ret != 0) {
-		Log::error("Failed to mkdir %s: %s", path, strerror(errno));
-	}
+	Log::error("Failed to mkdir %s: %s", path, strerror(errno));
 	return false;
 }
 
