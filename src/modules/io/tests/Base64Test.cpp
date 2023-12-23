ba46a526b91a2f75f4421e461f94c04aa6a98b6d
Martin Gerhardy <martin.gerhardy@gmail.com>
2023-12-23 21:17:42 +0100

    IO: refactored test

--- src/modules/io/tests/Base64Test.cpp
+++ src/modules/io/tests/Base64Test.cpp
@@ -3,21 +3,44 @@
  */
 
 #include "io/Base64.h"
-#include "io/BufferedReadWriteStream.h"
-#include "io/Base64WriteStream.h"
 #include "io/Base64ReadStream.h"
+#include "io/Base64WriteStream.h"
+#include "io/BufferedReadWriteStream.h"
 #include "io/MemoryReadStream.h"
 #include <gtest/gtest.h>
 
 namespace io {
 
-TEST(Base64Test, testBase64Encode) {
+class Base64Test : public testing::Test {
+protected:
+	void decode(const core::String &base64Input, const core::String &expectedOutput) {
+		io::MemoryReadStream stream(base64Input.c_str(), base64Input.size());
+		io::Base64ReadStream base64Stream(stream);
+
+		core::String out;
+		base64Stream.readString(expectedOutput.size(), out);
+		EXPECT_EQ(out, expectedOutput);
+	}
+
+	void encode(const core::String &input, const core::String &expectedBase64Output) {
+		io::BufferedReadWriteStream stream;
+		io::Base64WriteStream base64Stream(stream);
+		base64Stream.writeString(input);
+		stream.seek(0);
+		core::String base64;
+		EXPECT_EQ(expectedBase64Output.size(), stream.size());
+		stream.readString(stream.size(), base64);
+		EXPECT_EQ(expectedBase64Output, base64);
+	}
+};
+
+TEST_F(Base64Test, testBase64Encode) {
 	const char *foobar = "foobar";
 	const core::String &base64 = io::Base64::encode((const uint8_t *)foobar, strlen(foobar));
 	EXPECT_STREQ("Zm9vYmFy", base64.c_str());
 }
 
-TEST(Base64Test, testBase64Decode) {
+TEST_F(Base64Test, testBase64Decode) {
 	const core::String input = "Zm9vYmFy";
 	io::BufferedReadWriteStream stream;
 	EXPECT_TRUE(io::Base64::decode(stream, input));
@@ -30,25 +53,12 @@ TEST(Base64Test, testBase64Decode) {
 	EXPECT_STREQ("foobar", strbuff);
 }
 
-TEST(Base64Test, testBase64WriteStream) {
-	const char *foobar = "foobar";
-	io::BufferedReadWriteStream stream;
-	Base64WriteStream base64Stream(stream);
-	base64Stream.writeString(foobar);
-	stream.seek(0);
-	core::String base64;
-	stream.readString(stream.size(), base64);
-	EXPECT_STREQ("Zm9vYmFy", base64.c_str());
+TEST_F(Base64Test, testBase64WriteStream) {
+	encode("foobar", "Zm9vYmFy");
 }
 
-TEST(Base64Test, testBase64ReadStream) {
-	const core::String input = "Zm9vYmFy";
-	io::MemoryReadStream stream(input.c_str(), input.size());
-	Base64ReadStream base64Stream(stream);
-
-	core::String out;
-	base64Stream.readString(6, out);
-	EXPECT_EQ("foobar", out);
+TEST_F(Base64Test, testBase64ReadStream) {
+	decode("Zm9vYmFy", "foobar");
 }
 
-} // namespace util
+} // namespace io
