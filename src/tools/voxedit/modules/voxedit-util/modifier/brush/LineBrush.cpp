c7a6c9b398688ebc78c457c8797f20d738c3c266
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-01-15 21:09:29 +0100

    VOXEDIT: simplified line brush

--- src/tools/voxedit/modules/voxedit-util/modifier/brush/LineBrush.cpp
+++ src/tools/voxedit/modules/voxedit-util/modifier/brush/LineBrush.cpp
@@ -14,18 +14,9 @@ bool LineBrush::execute(scenegraph::SceneGraph &sceneGraph, ModifierVolumeWrappe
 	const glm::ivec3 &start = context.referencePos;
 	const glm::ivec3 &end = context.cursorPosition;
 	voxel::Voxel voxel = context.cursorVoxel;
-	const ModifierType modifierType = wrapper.modifierType();
-	const bool isErase = (modifierType & ModifierType::Erase) == ModifierType::Erase;
-	const bool isPaint = (modifierType & ModifierType::Paint) == ModifierType::Paint;
-	if (isErase) {
-		voxel = voxel::createVoxel(voxel::VoxelType::Air, 0);
-	}
-	voxelutil::raycastWithEndpoints(&wrapper, start, end, [=](auto &sampler) {
-		const bool air = voxel::isAir(sampler.voxel().getMaterial());
-		if ((!isErase && !isPaint) && !air) {
-			return true;
-		}
-		sampler.setVoxel(voxel);
+	voxelutil::raycastWithEndpoints(&wrapper, start, end, [&](auto &sampler) {
+		const glm::ivec3 &pos = sampler.position();
+		wrapper.setVoxel(pos.x, pos.y, pos.z, voxel);
 		return true;
 	});
 	return true;
