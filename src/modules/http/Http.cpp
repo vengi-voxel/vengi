f4acc07741f8749b6f747fd6cb3296f157672b2f
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-01-27 22:29:59 +0100

    HTTP: check input parameter

--- src/modules/http/Http.cpp
+++ src/modules/http/Http.cpp
@@ -14,6 +14,9 @@ bool isValidStatusCode(int statusCode) {
 }
 
 bool download(const core::String &url, io::WriteStream &stream, int *statusCode) {
+	if (url.empty()) {
+		return false;
+	}
 	Request request(url, RequestType::GET);
 	const core::String userAgent = app::App::getInstance()->fullAppname() + "/" PROJECT_VERSION;
 	request.setUserAgent(userAgent);
