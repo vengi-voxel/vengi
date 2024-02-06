fc81cae65103f541636ebdb2bb3402bdbe085eb3
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-02-06 20:42:47 +0100

    UTIL: added ScopedVarChange helper class

--- src/modules/util/VarUtil.h
+++ src/modules/util/VarUtil.h
@@ -10,20 +10,34 @@
 namespace util {
 
 template<class Functor>
-static void visitVarSorted(Functor&& func, uint32_t flags) {
+static void visitVarSorted(Functor &&func, uint32_t flags) {
 	core::DynamicArray<core::VarPtr> varList;
-	core::Var::visit([&](const core::VarPtr& var) {
+	core::Var::visit([&](const core::VarPtr &var) {
 		if (flags == 0 || (var->getFlags() & flags) != 0) {
 			varList.push_back(var);
 		}
 	});
-	varList.sort([] (const core::VarPtr& v1, const core::VarPtr& v2) {
-		return v1->name() < v2->name();
-	});
-	for (const core::VarPtr& var : varList) {
+	varList.sort([](const core::VarPtr &v1, const core::VarPtr &v2) { return v1->name() < v2->name(); });
+	for (const core::VarPtr &var : varList) {
 		func(var);
 	}
 }
 
+class ScopedVarChange {
+private:
+	core::String _oldValue;
+	core::VarPtr _var;
 
-}
+public:
+	ScopedVarChange(const core::String &name, const core::String &value) {
+		_var = core::Var::getSafe(name);
+		_oldValue = _var->strVal();
+		_var->setVal(value);
+	}
+
+	~ScopedVarChange() {
+		_var->setVal(_oldValue);
+	}
+};
+
+} // namespace util
