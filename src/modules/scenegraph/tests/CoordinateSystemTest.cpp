96ae1fc0f722516b7daa113cbc1026737761b2fc
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-02-21 19:26:50 +0100

    SCENEGRAPH: extended test case

--- src/modules/scenegraph/tests/CoordinateSystemTest.cpp
+++ src/modules/scenegraph/tests/CoordinateSystemTest.cpp
@@ -10,19 +10,29 @@ namespace scenegraph {
 
 class CoordinateSystemTest : public app::AbstractTest {
 protected:
-	void testConvert(CoordinateSystem from, CoordinateSystem to) {
+	void testConvertIdentity(CoordinateSystem from, CoordinateSystem to) {
 		glm::mat4 identity(1.0f);
 		const glm::mat4 &toMatrix = convertCoordinateSystem(from, to, identity);
 		const glm::mat4 &fromMatrix = convertCoordinateSystem(to, from, toMatrix);
 		EXPECT_EQ(identity, fromMatrix);
 	}
+	void testConvert(CoordinateSystem from, CoordinateSystem to) {
+		glm::mat4 src = glm::scale(glm::rotate(glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, 2.0f, 3.0f)),
+											   glm::radians(23.0f), glm::vec3(1.0f, 1.0f, 1.0f)),
+								   glm::vec3(2.0f, 3.0f, 4.0f));
+		const glm::mat4 &toMatrix = convertCoordinateSystem(from, to, src);
+		const glm::mat4 &fromMatrix = convertCoordinateSystem(to, from, toMatrix);
+		EXPECT_EQ(src, fromMatrix);
+	}
 };
 
 TEST_F(CoordinateSystemTest, testVXL) {
+	testConvertIdentity(CoordinateSystem::VXL, CoordinateSystem::Vengi);
 	testConvert(CoordinateSystem::VXL, CoordinateSystem::Vengi);
 }
 
 TEST_F(CoordinateSystemTest, testMagicavoxel) {
+	testConvertIdentity(CoordinateSystem::MagicaVoxel, CoordinateSystem::Vengi);
 	testConvert(CoordinateSystem::MagicaVoxel, CoordinateSystem::Vengi);
 }
 
