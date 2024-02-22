94048a2a5e9250ccca922915bbb1005d410c681b
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-02-22 18:38:33 +0100

    METRIC: ensure that the correct flavor is set for the HTTPMetricTest

--- src/modules/metric/tests/HTTPMetricTest.cpp
+++ src/modules/metric/tests/HTTPMetricTest.cpp
@@ -8,24 +8,20 @@
 #include "http/Request.h"
 #include "metric/HTTPMetricSender.h"
 #include "metric/Metric.h"
+#include "util/VarUtil.h"
 
 namespace metric {
 
 class HTTPMetricTest : public app::AbstractTest {
 private:
 	using Super = app::AbstractTest;
-
-protected:
-	bool onInitApp() override {
-		core::Var::get(cfg::MetricFlavor, "json");
-		return Super::onInitApp();
-	}
 };
 
 TEST_F(HTTPMetricTest, DISABLED_testHTTPMetricSender) {
 	if (!http::Request::supported()) {
 		GTEST_SKIP() << "No http support available";
 	}
+	util::ScopedVarChange change(cfg::MetricFlavor, "json");
 	const core::String &url = core::Var::get(cfg::MetricJsonUrl, "https://vengi-voxel.de/api/metric")->strVal();
 	core::SharedPtr<metric::HTTPMetricSender> sender = core::make_shared<metric::HTTPMetricSender>(url, "test/1.0.0");
 	metric::Metric metric;
