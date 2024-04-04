079fca388961460ed83d6afaa87921f1abeb067a
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-04-04 21:33:49 +0200

    VOXEDIT: optimized memento panel

--- src/tools/voxedit/modules/voxedit-ui/MementoPanel.cpp
+++ src/tools/voxedit/modules/voxedit-ui/MementoPanel.cpp
@@ -4,6 +4,7 @@
 
 #include "MementoPanel.h"
 #include "command/CommandHandler.h"
+#include "core/collection/DynamicArray.h"
 #include "ui/IMGUIEx.h"
 #include "voxedit-util/MementoHandler.h"
 #include "voxedit-util/SceneManager.h"
@@ -18,20 +19,32 @@ void MementoPanel::update(const char *title, command::CommandExecutionListener &
 	if (ImGui::Begin(title, nullptr, ImGuiWindowFlags_NoFocusOnAppearing)) {
 		const MementoHandler &mementoHandler = _sceneMgr->mementoHandler();
 		const int currentStatePos = mementoHandler.statePosition();
-		ImGui::Text(_("pos: %i/%i"), currentStatePos, (int)mementoHandler.stateSize());
+		ImGui::Text(_("Current state: %i / %i"), currentStatePos, (int)mementoHandler.stateSize());
+
 		if (ImGui::BeginListBox("##history-actions", ImVec2(-FLT_MIN, -FLT_MIN))) {
+			core::DynamicArray<const MementoState*> states;
+			states.reserve(mementoHandler.stateSize());
+			for (const MementoState &state : mementoHandler.states()) {
+				states.push_back(&state);
+			}
+
 			int n = 0;
 			int newStatePos = -1;
-			for (const MementoState &state : mementoHandler.states()) {
-				const core::String &info = toString(state, n);
-				const bool selected = n == currentStatePos;
-				if (ImGui::Selectable(info.c_str(), selected)) {
-					newStatePos = n;
+			ImGuiListClipper clipper;
+			clipper.Begin((int)mementoHandler.stateSize());
+			while (clipper.Step()) {
+				for (int row = clipper.DisplayStart; row < clipper.DisplayEnd; row++) {
+					const MementoState &state = *states[row];
+					const core::String &info = toString(state, n);
+					const bool selected = n == currentStatePos;
+					if (ImGui::Selectable(info.c_str(), selected)) {
+						newStatePos = n;
+					}
+					if (selected) {
+						ImGui::SetItemDefaultFocus();
+					}
+					++n;
 				}
-				if (selected) {
-					ImGui::SetItemDefaultFocus();
-				}
-				++n;
 			}
 			ImGui::EndListBox();
 			if (newStatePos != -1) {
