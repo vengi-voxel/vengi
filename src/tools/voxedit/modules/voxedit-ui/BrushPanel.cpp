d0f317f813d0c15a917ef85b21da17bca4ca66eb
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-04-07 22:28:58 +0200

    VOXEDIT: removed code that was moved into a separate file

--- src/tools/voxedit/modules/voxedit-ui/BrushPanel.cpp
+++ src/tools/voxedit/modules/voxedit-ui/BrushPanel.cpp
@@ -353,19 +353,4 @@ void BrushPanel::update(const char *title, command::CommandExecutionListener &li
 	ImGui::End();
 }
 
-#ifdef IMGUI_ENABLE_TEST_ENGINE
-void BrushPanel::registerUITests(ImGuiTestEngine *engine, const char *title) {
-	ImGuiTest *test = IM_REGISTER_TEST(engine, testCategory(), testName());
-	test->TestFunc = [=](ImGuiTestContext *ctx) {
-		voxedit::ModifierFacade &modifier = _sceneMgr->modifier();
-		ctx->SetRef(title);
-		for (int i = 0; i < (int)BrushType::Max; ++i) {
-			ctx->ItemClick(i);
-			const BrushType brushType = modifier.brushType();
-			IM_CHECK_EQ((int)brushType, i);
-		}
-	};
-}
-#endif
-
 } // namespace voxedit
