vengi-voxel/vengi

commit 33804fe36cf9fb84d3b1b9a05cd1b7dbdb9ec8d5

UI: unified button ids

src/modules/ui/Toolbar.cpp
@@ -58,10 +58,9 @@ bool Toolbar::button(const char *icon, const char *command, bool darken) {
 	if (darken) {
 		style.darker(ImGuiCol_Text);
 	}
-	ImGui::PushID(_nextId);
-	bool pressed = ImGui::CommandButton(icon, command, nullptr, _size, _listener);
+	core::String label = core::String::format("%s###button%d", icon, _nextId);
+	bool pressed = ImGui::CommandButton(label.c_str(), command, nullptr, _size, _listener);
 	next();
-	ImGui::PopID();
 	return pressed;
 }
 

commit d51a8f9dd7588d7ac065f1ac3dd72b79b87ed321

UI: fixed toolbar button ids
regression introduced in 33804fe36cf9fb84d3b1b9a05cd1b7dbdb9ec8d5

src/modules/ui/Toolbar.cpp
@@ -8,8 +8,8 @@
 
 namespace ui {
 
-Toolbar::Toolbar(const ImVec2 &size, command::CommandExecutionListener *listener)
-	: _nextId(0), _pos(ImGui::GetCursorScreenPos()), _startingPosX(_pos.x), _size(size), _listener(listener) {
+Toolbar::Toolbar(const core::String &name, const ImVec2 &size, command::CommandExecutionListener *listener)
+	: _nextId(0), _pos(ImGui::GetCursorScreenPos()), _startingPosX(_pos.x), _size(size), _listener(listener), _name(name) {
 }
 
 Toolbar::~Toolbar() {
@@ -58,9 +58,12 @@ bool Toolbar::button(const char *icon, const char *command, bool darken) {
 	if (darken) {
 		style.darker(ImGuiCol_Text);
 	}
+	ImGui::PushID(_name.c_str());
 	core::String label = core::String::format("%s###button%d", icon, _nextId);
 	bool pressed = ImGui::CommandButton(label.c_str(), command, nullptr, _size, _listener);
+	ImGui::PopID();
 	next();
+
 	return pressed;
 }
 

src/modules/ui/Toolbar.h
@@ -20,13 +20,14 @@ protected:
 	const float _startingPosX;
 	const ImVec2 _size;
 	command::CommandExecutionListener *_listener;
+	const core::String _name;
 	float windowWidth() const;
 	void setCursor();
 	void next();
 	void newline();
 	void last();
 public:
-	Toolbar(const ImVec2 &size, command::CommandExecutionListener *listener = nullptr);
+	Toolbar(const core::String &name, const ImVec2 &size, command::CommandExecutionListener *listener = nullptr);
 	~Toolbar();
 
 	bool button(const char *icon, const char *command, bool darken = false);
@@ -43,11 +44,13 @@ public:
 		if (highlight) {
 			style.highlight(ImGuiCol_Text);
 		}
+		ImGui::PushID(_name.c_str());
 		core::String label = core::String::format("%s###button%d", icon, _nextId);
 		bool pressed = ImGui::Button(label.c_str(), _size);
 		if (pressed) {
 			func();
 		}
+		ImGui::PopID();
 		if (tooltip != nullptr && tooltip[0] != '\0') {
 			ui::ScopedStyle tooltipStyle;
 			tooltipStyle.setFont(imguiApp()->defaultFont());

src/tools/voxedit/modules/voxedit-ui/BrushPanel.cpp
@@ -318,7 +318,7 @@ void BrushPanel::addModifiers(command::CommandExecutionListener &listener) {
 	voxedit::ModifierFacade &modifier = _sceneMgr->modifier();
 	const BrushType brushType = modifier.brushType();
 
-	ui::Toolbar toolbarBrush(buttonSize, &listener);
+	ui::Toolbar toolbarBrush("brushes", buttonSize, &listener);
 	for (int i = 0; i < (int)BrushType::Max; ++i) {
 		core::String cmd = core::string::format("brush%s", BrushTypeStr[i]).toLower();
 		auto func = [&listener, cmd]() { command::executeCommands(cmd, &listener); };
@@ -334,7 +334,7 @@ void BrushPanel::addModifiers(command::CommandExecutionListener &listener) {
 		return;
 	}
 
-	ui::Toolbar toolbarModifiers(buttonSize, &listener);
+	ui::Toolbar toolbarModifiers("modifiers", buttonSize, &listener);
 	if (brushType == BrushType::None) {
 		toolbarModifiers.button(ICON_LC_EXPAND, "actionselect", !modifier.isMode(ModifierType::Select));
 		toolbarModifiers.button(ICON_LC_PIPETTE, "actioncolorpicker", !modifier.isMode(ModifierType::ColorPicker));

src/tools/voxedit/modules/voxedit-ui/PositionsPanel.cpp
@@ -93,7 +93,7 @@ void PositionsPanel::modelView(command::CommandExecutionListener &listener) {
 		if (!sizes.empty()) {
 			static const char *max = "888x888x888";
 			const ImVec2 buttonSize(ImGui::CalcTextSize(max).x, ImGui::GetFrameHeight());
-			ui::Toolbar toolbar(buttonSize, &listener);
+			ui::Toolbar toolbar("regions", buttonSize, &listener);
 
 			core::DynamicArray<core::String> regionSizes;
 			core::string::splitString(sizes, regionSizes, ",");

src/tools/voxedit/modules/voxedit-ui/SceneGraphPanel.cpp
@@ -363,7 +363,7 @@ void SceneGraphPanel::update(video::Camera& camera, const char *title, ModelNode
 		if (ImGui::BeginChild("master##scenegraphpanel", size)) {
 			const bool onlyOneModel = sceneGraph.size(scenegraph::SceneGraphNodeType::Model) <= 1;
 			const ImVec2 buttonSize(ImGui::GetFrameHeight(), ImGui::GetFrameHeight());
-			ui::Toolbar toolbar(buttonSize);
+			ui::Toolbar toolbar("scenegraphtools", buttonSize);
 
 			toolbar.button(ICON_LC_PLUS_SQUARE, _("Add a new model node"), [&sceneGraph, this, modelNodeSettings] () {
 				const int nodeId = sceneGraph.activeNode();

src/tools/voxedit/modules/voxedit-ui/ToolsPanel.cpp
@@ -50,7 +50,7 @@ void ToolsPanel::updateSceneMode(command::CommandExecutionListener &listener) {
 			ui::ScopedStyle style;
 			style.setFont(_app->bigIconFont());
 			const ImVec2 buttonSize(ImGui::GetFrameHeight(), ImGui::GetFrameHeight());
-			ui::Toolbar toolbar(buttonSize, &listener);
+			ui::Toolbar toolbar("scenetools", buttonSize, &listener);
 			toolbar.button(ICON_LC_COPY, "nodeduplicate");
 			toolbar.button(ICON_LC_TRASH, "nodedelete");
 			if (nodeType == scenegraph::SceneGraphNodeType::Model) {
@@ -70,7 +70,7 @@ void ToolsPanel::updateEditMode(command::CommandExecutionListener &listener) {
 		ui::ScopedStyle style;
 		style.setFont(_app->bigIconFont());
 		const ImVec2 buttonSize(ImGui::GetFrameHeight(), ImGui::GetFrameHeight());
-		ui::Toolbar toolbar(buttonSize, &listener);
+		ui::Toolbar toolbar("edittools", buttonSize, &listener);
 		toolbar.button(ICON_LC_CROP, "crop");
 		toolbar.button(ICON_LC_SCALING, "resizetoselection", _sceneMgr->modifier().selections().empty());
 		toolbar.button(ICON_LC_SPLIT, "splitobjects");

src/tools/voxedit/modules/voxedit-ui/tests/BrushPanelTest.cpp
@@ -15,8 +15,9 @@ void BrushPanel::registerUITests(ImGuiTestEngine *engine, const char *title) {
 		ctx->SetRef(title);
 		focusWindow(ctx, title);
 		for (int i = 0; i < (int)BrushType::Max; ++i) {
-			const core::String id = core::String::format("###button%d", i);
+			const core::String id = core::String::format("brushes/###button%d", i);
 			ctx->ItemClick(id.c_str());
+			ctx->Yield();
 			const BrushType brushType = modifier.brushType();
 			IM_CHECK_EQ((int)brushType, i);
 		}

src/tools/voxedit/modules/voxedit-ui/tests/MainWindowTest.cpp
@@ -4,7 +4,10 @@
 
 #include "../MainWindow.h"
 #include "../WindowTitles.h"
+#include "core/Log.h"
+#include "imgui_te_context.h"
 #include "voxedit-util/SceneManager.h"
+#include "voxel/RawVolume.h"
 
 namespace voxedit {
 
@@ -22,16 +25,34 @@ void MainWindow::registerUITests(ImGuiTestEngine *engine, const char *title) {
 	_mementoPanel.registerUITests(engine, TITLE_MEMENTO);
 	_positionsPanel.registerUITests(engine, TITLE_POSITIONS);
 	_palettePanel.registerUITests(engine, TITLE_PALETTE);
-	_menuBar.registerUITests(engine, nullptr);
+	_menuBar.registerUITests(engine, "##menubar");
 	_statusBar.registerUITests(engine, TITLE_STATUSBAR);
-	_scriptPanel.registerUITests(engine, TITLE_SCRIPT_EDITOR);
+	_scriptPanel.registerUITests(engine, TITLE_SCRIPT);
 	_animationTimeline.registerUITests(engine, TITLE_ANIMATION_TIMELINE);
 	_cameraPanel.registerUITests(engine, TITLE_CAMERA);
 
-	IM_REGISTER_TEST(engine, testName(), "new scene")->TestFunc = [=](ImGuiTestContext *ctx) {
+	IM_REGISTER_TEST(engine, testName(), "new scene unsaved changes")->TestFunc = [=](ImGuiTestContext *ctx) {
+		_sceneMgr->markDirty();
+		ImGuiContext& g = *ctx->UiContext;
 		ctx->SetRef(title);
 		focusWindow(ctx, title);
 		ctx->MenuClick("###File/###New");
+		ctx->Yield();
+		IM_CHECK_EQ(g.OpenPopupStack.Size, 1);
+		IM_CHECK_EQ(g.OpenPopupStack[0].PopupId, ctx->GetID(POPUP_TITLE_UNSAVED));
+		ctx->SetRef(POPUP_TITLE_UNSAVED);
+		ctx->ItemClick("###Yes");
+		ctx->SetRef(POPUP_TITLE_NEW_SCENE);
+		ctx->ItemInputValue("##newscenename", "Automated ui test");
+		ctx->ItemClick("###OK");
+	};
+
+	IM_REGISTER_TEST(engine, testName(), "new scene")->TestFunc = [=](ImGuiTestContext *ctx) {
+		_sceneMgr->newScene(true, "", new voxel::RawVolume({0, 1}));
+		ctx->SetRef(title);
+		focusWindow(ctx, title);
+		ctx->MenuClick("###File/###New");
+		ctx->Yield();
 		ctx->SetRef(POPUP_TITLE_NEW_SCENE);
 		ctx->ItemInputValue("##newscenename", "Automated ui test");
 		ctx->ItemClick("###OK");