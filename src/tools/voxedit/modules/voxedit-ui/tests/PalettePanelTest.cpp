db95edadf7bd84463656c13dae6a1202e9e5857d
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-04-08 18:42:17 +0200

    VOXEDIT: disable empty tests

--- src/tools/voxedit/modules/voxedit-ui/tests/PalettePanelTest.cpp
+++ src/tools/voxedit/modules/voxedit-ui/tests/PalettePanelTest.cpp
@@ -8,11 +8,13 @@
 namespace voxedit {
 
 void PalettePanel::registerUITests(ImGuiTestEngine *engine, const char *title) {
+#if 0
 	ImGuiTest *test = IM_REGISTER_TEST(engine, testCategory(), testName());
 	test->TestFunc = [=](ImGuiTestContext *ctx) {
 		ctx->SetRef(title);
 		focusWindow(ctx, title);
 	};
+#endif
 }
 
 } // namespace voxedit
