vengi-voxel/vengi

commit 282d73aa8de19ee702163a29f1ba9b2f5b87180d

THUMBNAILER: don't add the camera to the screenshot

src/modules/voxelrender/SceneGraphRenderer.cpp
@@ -148,6 +148,10 @@ void SceneGraphRenderer::prepare(const RenderContext &renderContext) {
 	const int activeNode = sceneGraph.activeNode();
 	for (auto entry : sceneGraph.nodes()) {
 		const scenegraph::SceneGraphNode &node = entry->second;
+		if (renderContext.onlyModels && node.type() != scenegraph::SceneGraphNodeType::Model) {
+			continue;
+		}
+
 		if (node.type() == scenegraph::SceneGraphNodeType::Camera) {
 			const scenegraph::SceneGraphNodeCamera &cameraNode = scenegraph::toCameraNode(node);
 			if (!cameraNode.visible()) {
@@ -157,9 +161,7 @@ void SceneGraphRenderer::prepare(const RenderContext &renderContext) {
 			video::Camera camera = toCamera(size, cameraNode);
 			_cameras.push_back(camera);
 			continue;
-		}
-
-		if (node.type() != scenegraph::SceneGraphNodeType::Model) {
+		} else if (node.type() != scenegraph::SceneGraphNodeType::Model) {
 			continue;
 		}
 

src/modules/voxelrender/ImageGenerator.cpp
@@ -92,6 +92,7 @@ image::ImagePtr volumeThumbnail(const scenegraph::SceneGraph &sceneGraph, const 
 	renderContext.init(ctx.outputSize);
 	renderContext.sceneMode = true;
 	renderContext.sceneGraph = &sceneGraph;
+	renderContext.onlyModels = true;
 
 	if (!volumeRenderer.init()) {
 		Log::error("Failed to initialize the renderer");
@@ -110,6 +111,7 @@ bool volumeTurntable(const scenegraph::SceneGraph &sceneGraph, const core::Strin
 	renderContext.init(ctx.outputSize);
 	renderContext.sceneMode = true;
 	renderContext.sceneGraph = &sceneGraph;
+	renderContext.onlyModels = true;
 
 	volumeRenderer.construct();
 	if (!volumeRenderer.init()) {

src/modules/voxelrender/RawVolumeRenderer.h
@@ -46,6 +46,7 @@ struct RenderContext : public core::NonCopyable {
 	bool hideInactive = false;
 	bool grayInactive = false;
 	bool sceneMode = false;
+	bool onlyModels = false;
 
 	bool init(const glm::ivec2 &size);
 	void shutdown();

commit 61488e3027921f66c599311a3df87dcc6a8e2e86

VOXELRENDER: fixed logic error
introduced in 282d73aa8de19ee702163a29f1ba9b2f5b87180d

src/modules/voxelrender/SceneGraphRenderer.cpp
@@ -187,7 +187,7 @@ void SceneGraphRenderer::prepare(const RenderContext &renderContext) {
 	const int activeNode = sceneGraph.activeNode();
 	for (auto entry : sceneGraph.nodes()) {
 		const scenegraph::SceneGraphNode &node = entry->second;
-		if (renderContext.onlyModels && node.isModelNode()) {
+		if (renderContext.onlyModels && !node.isModelNode()) {
 			continue;
 		}
 