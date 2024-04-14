974443c86e5b87f0224ad79d84d72698397640f5
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-04-14 13:00:01 +0200

    VOXEDIT: extended ui tests

--- src/tools/voxedit/modules/voxedit-ui/tests/ToolsPanelTest.cpp
+++ src/tools/voxedit/modules/voxedit-ui/tests/ToolsPanelTest.cpp
@@ -8,13 +8,28 @@
 namespace voxedit {
 
 void ToolsPanel::registerUITests(ImGuiTestEngine *engine, const char *title) {
-#if 0
-	ImGuiTest *test = IM_REGISTER_TEST(engine, testCategory(), testName());
-	test->TestFunc = [=](ImGuiTestContext *ctx) {
+	IM_REGISTER_TEST(engine, testName(), "toolbar")->TestFunc = [=](ImGuiTestContext *ctx) {
 		ctx->SetRef(title);
 		IM_CHECK(focusWindow(ctx, title));
+		// edittools###button0
+		// scenetools###button0
+		ImGuiTestItemList items;
+		bool editMode = ctx->ItemInfo("edittools/###button0", ImGuiTestOpFlags_NoError)->ID != 0;
+		bool sceneMode = false;
+		if (!editMode)
+			sceneMode = ctx->ItemInfo("scenetools/###button0", ImGuiTestOpFlags_NoError)->ID != 0;
+		IM_CHECK(editMode || sceneMode);
+		int buttonIdx = 0;
+		for (;;) {
+			core::String id = core::string::format("%s/###button%i", editMode ? "edittools" : "scenetools", buttonIdx);
+			if (ctx->ItemInfo(id.c_str(), ImGuiTestOpFlags_NoError)->ID == 0) {
+				break;
+			}
+			ctx->LogInfo("Found button %i", buttonIdx);
+			ctx->ItemClick(id.c_str());
+			++buttonIdx;
+		}
 	};
-#endif
 }
 
 } // namespace voxedit
