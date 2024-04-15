afbde687f337c25a1ec9f774b53dbde9d0a19db6
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-04-15 20:48:19 +0200

    VOXEDIT: ini version bump and title id changes

--- src/tools/voxedit/VoxEdit.cpp
+++ src/tools/voxedit/VoxEdit.cpp
@@ -38,7 +38,7 @@ VoxEdit::VoxEdit(const io::FilesystemPtr &filesystem, const core::TimeProviderPt
 	core::registerBindingContext("model", core::BindingContext::Context2);
 	core::registerBindingContext("editing", core::BindingContext::Context1 + core::BindingContext::Context2);
 	_allowRelativeMouseMode = false;
-	_iniVersion = 5;
+	_iniVersion = 6;
 	_keybindingsVersion = 1;
 
 	// see KeyBindings enum
