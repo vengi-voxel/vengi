bfede2ffb7bf1cc7c9b4400e506c4eb0f100891f
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-04-19 18:16:34 +0200

    VOXEDIT: improved viewport test for edit mode

--- src/tools/voxedit/modules/voxedit-ui/tests/ViewportTest.cpp
+++ src/tools/voxedit/modules/voxedit-ui/tests/ViewportTest.cpp
@@ -5,6 +5,7 @@
 #include "../Viewport.h"
 #include "command/CommandHandler.h"
 #include "voxedit-util/SceneManager.h"
+#include "voxelutil/VolumeVisitor.h"
 
 namespace voxedit {
 
@@ -14,13 +15,27 @@ void Viewport::registerUITests(ImGuiTestEngine *engine, const char *) {
 			return;
 		}
 		IM_CHECK(focusWindow(ctx, _uiId.c_str()));
+		IM_CHECK(_sceneMgr->newScene(true, "set voxel", voxel::Region(0, 31)));
 		ImGuiWindow* window = ImGui::FindWindowByName(_uiId.c_str());
 		IM_CHECK_SILENT(window != nullptr);
 		ctx->MouseMoveToPos(window->Rect().GetCenter());
+		const int activeNode = _sceneMgr->sceneGraph().activeNode();
+		scenegraph::SceneGraphNode *model = _sceneMgr->sceneGraphModelNode(activeNode);
+		IM_CHECK(model != nullptr);
+		const int cnt = voxelutil::visitVolume(*model->volume(), voxelutil::EmptyVisitor(), voxelutil::SkipEmpty());
+		IM_CHECK(cnt == 0);
 		command::executeCommands("+actionexecute 1 1;-actionexecute 1 1");
+		IM_CHECK_EQ(1, voxelutil::visitVolume(*model->volume(), voxelutil::EmptyVisitor(), voxelutil::SkipEmpty()));
 	};
 
-	// TODO: a scene mode test to create another node and select a node
+#if 0
+	IM_REGISTER_TEST(engine, testCategory(), "select node")->TestFunc = [=](ImGuiTestContext *ctx) {
+		if (!isSceneMode()) {
+			return;
+		}
+		// TODO: a scene mode test to create another node and select a node
+	};
+#endif
 }
 
 } // namespace voxedit
