ca12fa873cf8be25cd5ba20a10fb997fc692532f
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-04-24 21:08:43 +0200

    MATH: added first test for AABB class

--- src/modules/math/tests/AABBTest.cpp
+++ src/modules/math/tests/AABBTest.cpp
@@ -10,4 +10,11 @@ namespace math {
 class AABBTest : public app::AbstractTest {
 };
 
+TEST_F(AABBTest, testContains) {
+	AABB aabb(0, 0, 0, 2, 2, 2);
+	ASSERT_FALSE(aabb.isEmpty());
+	ASSERT_TRUE(aabb.containsPoint(1, 1, 1));
+	ASSERT_FALSE(aabb.containsPoint(1, 5, 1));
+}
+
 }
