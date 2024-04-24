0b0c2c7d01373c94bd33c092caf84b97b309fca7
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-04-24 20:49:49 +0200

    VOXELCOLLECTION: download test

--- src/modules/voxelcollection/tests/CubZHAPITest.cpp
+++ src/modules/voxelcollection/tests/CubZHAPITest.cpp
@@ -5,6 +5,8 @@
 #include "voxelcollection/CubZHAPI.h"
 #include "app/tests/AbstractTest.h"
 #include "core/Log.h"
+#include "http/HttpCacheStream.h"
+#include "voxelcollection/Downloader.h"
 
 namespace voxelcollection {
 
@@ -14,6 +16,17 @@ class CubZHAPITest : public app::AbstractTest {};
 TEST_F(CubZHAPITest, DISABLED_testRepoList) {
 	const auto &sources = cubzh::repoList(_testApp->filesystem(), "xxx", "yyy");
 	ASSERT_FALSE(sources.empty());
+	for (const cubzh::TreeEntry &entry : sources) {
+		VoxelFile file;
+		file.source = "cubzh";
+		file.name = entry.repo + "-" + entry.name + ".3zh";
+		file.url = entry.url;
+		file.fullPath = _testApp->filesystem()->writePath(file.targetFile());
+		http::HttpCacheStream stream(_testApp->filesystem(), file.fullPath, file.url);
+		if (stream.isNewInCache()) {
+			Log::debug("Downloaded: %s", file.url.c_str());
+		}
+	}
 }
 
 } // namespace voxelcollection
