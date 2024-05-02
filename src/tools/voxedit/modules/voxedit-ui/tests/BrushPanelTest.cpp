b25c27234c75ba76e630be0a4c29350888fc85ff
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-05-02 19:55:42 +0200

    VOXEDIT: extended brush ui tests

--- src/tools/voxedit/modules/voxedit-ui/tests/BrushPanelTest.cpp
+++ src/tools/voxedit/modules/voxedit-ui/tests/BrushPanelTest.cpp
@@ -3,13 +3,82 @@
  */
 
 #include "../BrushPanel.h"
-#include "ui/IMGUIApp.h"
 #include "command/CommandHandler.h"
 #include "voxedit-ui/Viewport.h"
 #include "voxedit-util/SceneManager.h"
 #include "TestUtil.h"
+#include "voxel/Voxel.h"
 
 namespace voxedit {
+
+static bool activeBrush(BrushPanel *panel, ImGuiTestContext *ctx, const char *title, const SceneManagerPtr &sceneMgr, BrushType type) {
+	IM_CHECK_RETV(sceneMgr->newScene(true, ctx->Test->Name, voxel::Region(0, 31)), false);
+
+	const int viewportId = viewportEditMode(ctx, panel->app());
+	IM_CHECK_SILENT_RETV(viewportId != -1, false);
+
+	// now we can focus the brush panel
+	IM_CHECK_SILENT_RETV(panel->focusWindow(ctx, title), false);
+
+	const core::String buttonId = core::String::format("brushes/###button%d", (int)type);
+	ctx->ItemClick(buttonId.c_str());
+	ctx->Yield();
+
+	IM_CHECK_RETV(sceneMgr->modifier().brushType() == type, false);
+
+	return true;
+}
+
+static bool setModifierType(ImGuiTestContext *ctx, voxedit::ModifierFacade &modifier, ModifierType type) {
+	if (type == ModifierType::Select) {
+		IM_CHECK_RETV(modifier.brushType() == BrushType::None, false);
+		ctx->ItemClick("modifiers/###button0");
+	} else if (type == ModifierType::ColorPicker) {
+		IM_CHECK_RETV(modifier.brushType() == BrushType::None, false);
+		ctx->ItemClick("modifiers/###button1");
+	} else {
+		Brush *brush = modifier.activeBrush();
+		IM_CHECK_RETV(brush != nullptr, false);
+		IM_CHECK_RETV(brush->modifierType(type) == type, false);
+		if (type == ModifierType::Place) {
+			ctx->ItemClick("modifiers/###button0");
+		} else if (type == ModifierType::Erase) {
+			ctx->ItemClick("modifiers/###button1");
+		} else if (type == ModifierType::Override) {
+			ctx->ItemClick("modifiers/###button2");
+		} else {
+			IM_ERRORF("Unsupported modifier type given: %i", (int)type);
+			return false;
+		}
+	}
+	IM_CHECK_RETV(modifier.modifierType() == type, false);
+	ctx->Yield();
+	return true;
+}
+
+static bool runBrushModifiers(BrushPanel *panel, ImGuiTestContext *ctx, const char *title, const SceneManagerPtr &sceneMgr, BrushType type) {
+	IM_CHECK_RETV(activeBrush(panel, ctx, title, sceneMgr, type), false);
+	voxedit::ModifierFacade &modifier = sceneMgr->modifier();
+
+	setModifierType(ctx, modifier, ModifierType::Place);
+	IM_CHECK_RETV(centerOnViewport(ctx, sceneMgr, viewportEditMode(ctx, panel->app()), ImVec2(0, -50)), false);
+	executeViewportClick();
+
+	// change the cursor voxel
+	modifier.setCursorVoxel(voxel::createVoxel(voxel::VoxelType::Generic, 1));
+	setModifierType(ctx, modifier, ModifierType::Override);
+	IM_CHECK_RETV(centerOnViewport(ctx, sceneMgr, viewportEditMode(ctx, panel->app()), ImVec2(0, -50)), false);
+	executeViewportClick();
+
+	setModifierType(ctx, modifier, ModifierType::Erase);
+	IM_CHECK_RETV(centerOnViewport(ctx, sceneMgr, viewportEditMode(ctx, panel->app()), ImVec2(0, -50)), false);
+	executeViewportClick();
+
+	// place is one voxel, override just changed the color of the voxel and erase will wipe it completely again
+	IM_CHECK_RETV(voxelCount(sceneMgr) == 0, false);
+
+	return true;
+}
 
 void BrushPanel::registerUITests(ImGuiTestEngine *engine, const char *title) {
 	IM_REGISTER_TEST(engine, testCategory(), "cycle brush types")->TestFunc = [=](ImGuiTestContext *ctx) {
@@ -29,9 +98,9 @@ void BrushPanel::registerUITests(ImGuiTestEngine *engine, const char *title) {
 	};
 
 	IM_REGISTER_TEST(engine, testCategory(), "select")->TestFunc = [=](ImGuiTestContext *ctx) {
+		IM_CHECK(activeBrush(this, ctx, title, _sceneMgr, BrushType::None));
 		voxedit::ModifierFacade &modifier = _sceneMgr->modifier();
-		modifier.setBrushType(BrushType::None);
-		modifier.setModifierType(ModifierType::Select);
+		IM_CHECK(setModifierType(ctx, modifier, ModifierType::Select));
 
 		const int viewportId = viewportEditMode(ctx, _app);
 		IM_CHECK(centerOnViewport(ctx, _sceneMgr, viewportId));
@@ -46,6 +115,33 @@ void BrushPanel::registerUITests(ImGuiTestEngine *engine, const char *title) {
 
 		modifier.unselect();
 	};
+
+	IM_REGISTER_TEST(engine, testCategory(), "shape brush")->TestFunc = [=](ImGuiTestContext *ctx) {
+		IM_CHECK(runBrushModifiers(this, ctx, title, _sceneMgr, BrushType::Shape));
+	};
+
+#if 0
+	// https://github.com/vengi-voxel/vengi/issues/457
+	IM_REGISTER_TEST(engine, testCategory(), "plane brush")->TestFunc = [=](ImGuiTestContext *ctx) {
+		IM_CHECK(runBrushModifiers(this, ctx, title, _sceneMgr, BrushType::Plane));
+	};
+
+	IM_REGISTER_TEST(engine, testCategory(), "line brush")->TestFunc = [=](ImGuiTestContext *ctx) {
+		// TODO: not all created volumes are overridden and removed on click... check why
+		IM_CHECK(runBrushModifiers(this, ctx, title, _sceneMgr, BrushType::Line));
+	};
+
+	IM_REGISTER_TEST(engine, testCategory(), "path brush")->TestFunc = [=](ImGuiTestContext *ctx) {
+		// TODO: load or create a volume first, path needs existing volumes
+		IM_CHECK(runBrushModifiers(this, ctx, title, _sceneMgr, BrushType::Path));
+	};
+
+	IM_REGISTER_TEST(engine, testCategory(), "paint brush")->TestFunc = [=](ImGuiTestContext *ctx) {
+		// TODO: load or create a volume first, paint needs existing volumes
+		IM_CHECK(runBrushModifiers(this, ctx, title, _sceneMgr, BrushType::Paint));
+	};
+#endif
+
 	// TODO: copy and paste
 }
 
