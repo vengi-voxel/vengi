b0c6cb29ff3eaf04ed15d0b82eb539cbd2700801
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-05-06 01:43:45 +0200

    SCENEGRAPH: allow mergePalettes to use reference nodes, too
    they might have a different palette assigned

--- src/modules/scenegraph/SceneGraph.cpp
+++ src/modules/scenegraph/SceneGraph.cpp
@@ -764,7 +764,7 @@ palette::Palette SceneGraph::mergePalettes(bool removeUnused, int emptyIndex) co
 			core::Array<bool, palette::PaletteMaxColors> used;
 			if (removeUnused) {
 				used.fill(false);
-				voxelutil::visitVolume(*node.volume(), [&used](int, int, int, const voxel::Voxel &voxel) {
+				voxelutil::visitVolume(*resolveVolume(node), [&used](int, int, int, const voxel::Voxel &voxel) {
 					used[voxel.getColor()] = true;
 				});
 			} else {
