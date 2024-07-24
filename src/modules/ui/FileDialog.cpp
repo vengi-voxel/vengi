vengi-voxel/vengi

commit af93f8691100daa49d1ebb6ea68c3fb770a3e15c

UI: make popup title translatable

src/modules/ui/FileDialog.cpp
@@ -27,9 +27,8 @@
 
 namespace ui {
 
-// TODO: I18N
-static const char *FILE_ALREADY_EXISTS_POPUP = "File already exists###fileoverwritepopup";
-static const char *NEW_FOLDER_POPUP = "Create folder###newfolderpopup";
+static const char *FILE_ALREADY_EXISTS_POPUP = "###fileoverwritepopup";
+static const char *NEW_FOLDER_POPUP = "###newfolderpopup";
 static const char *FILEDIALOGBOOKMARKDND = "filedialog-dir";
 
 enum class FileDialogColumnId {
@@ -552,7 +551,8 @@ void FileDialog::popupNewFolder() {
 	const ImVec2 &windowSize = ImGui::GetWindowSize();
 	const ImVec2 center(windowPos.x + windowSize.x * 0.5f, windowPos.y + windowSize.y * 0.5f);
 	ImGui::SetNextWindowPos(center, ImGuiCond_Appearing, ImVec2(0.5f, 0.5f));
-	if (ImGui::BeginPopupModal(NEW_FOLDER_POPUP, nullptr, ImGuiWindowFlags_AlwaysAutoResize)) {
+	const core::String title = makeTitle(NEW_FOLDER_POPUP, _("Create folder"));
+	if (ImGui::BeginPopupModal(title.c_str(), nullptr, ImGuiWindowFlags_AlwaysAutoResize)) {
 		ImGui::TextUnformatted(_("Enter a name for the new folder"));
 		ImGui::InputText("##newfoldername", &_newFolderName.name);
 		if (ImGui::Button(_("Create"))) {
@@ -578,7 +578,8 @@ void FileDialog::popupNewFolder() {
 }
 
 bool FileDialog::popupAlreadyExists() {
-	if (ImGui::BeginPopupModal(FILE_ALREADY_EXISTS_POPUP, nullptr, ImGuiWindowFlags_AlwaysAutoResize)) {
+	const core::String title = makeTitle(FILE_ALREADY_EXISTS_POPUP, _("File already exists"));
+	if (ImGui::BeginPopupModal(title.c_str(), nullptr, ImGuiWindowFlags_AlwaysAutoResize)) {
 		ImGui::AlignTextToFramePadding();
 		ImGui::PushFont(imguiApp()->bigFont());
 		ImGui::TextUnformatted(ICON_LC_TRIANGLE_ALERT);

data/shared/de_DE.po
@@ -169,6 +169,9 @@ msgstr "Versteckte Dateisystem-Entitäten anzeigen"
 msgid "The last selected file type filter in the file dialog"
 msgstr "Der zuletzt ausgewählte Dateitypfilter im Dateidialog"
 
+msgid "Create folder"
+msgstr "Ordner erstellen"
+
 msgid "Enter a name for the new folder"
 msgstr "Geben Sie einen Namen für den neuen Ordner ein"
 
@@ -180,6 +183,9 @@ msgstr "Der Ordnername darf nicht leer sein"
 
 msgid "Cancel"
 msgstr "Abbrechen"
+
+msgid "File already exists"
+msgstr "Datei existiert bereits"
 
 #, c-format
 msgid ""
@@ -417,9 +423,6 @@ msgstr ""
 
 msgid "Minimize the window"
 msgstr "Das Fenster minimieren"
-
-msgid "File already exists"
-msgstr "Datei existiert bereits"
 
 msgid "Overwrite existing target file"
 msgstr "Überschreibe existierende Zieldatei"

data/vengi.pot
@@ -147,6 +147,9 @@ msgstr ""
 msgid "The last selected file type filter in the file dialog"
 msgstr ""
 
+msgid "Create folder"
+msgstr ""
+
 msgid "Enter a name for the new folder"
 msgstr ""
 
@@ -157,6 +160,9 @@ msgid "Folder name can't be empty"
 msgstr ""
 
 msgid "Cancel"
+msgstr ""
+
+msgid "File already exists"
 msgstr ""
 
 #, c-format
@@ -389,9 +395,6 @@ msgid "0 disables it, 1 only highest severity, 2 medium severity, 3 everything"
 msgstr ""
 
 msgid "Minimize the window"
-msgstr ""
-
-msgid "File already exists"
 msgstr ""
 
 msgid "Overwrite existing target file"

commit d51490bc2d29e82ea6d712127ce7524425d83d81

UI: fixed popups not working anymore
regression since af93f8691100daa49d1ebb6ea68c3fb770a3e15c

src/modules/ui/FileDialog.cpp
@@ -584,7 +584,8 @@ void FileDialog::popupNewFolder() {
 	const ImVec2 &windowSize = ImGui::GetWindowSize();
 	const ImVec2 center(windowPos.x + windowSize.x * 0.5f, windowPos.y + windowSize.y * 0.5f);
 	ImGui::SetNextWindowPos(center, ImGuiCond_Appearing, ImVec2(0.5f, 0.5f));
-	const core::String title = makeTitle(NEW_FOLDER_POPUP, _("Create folder"));
+	const core::String title = makeTitle(_("Create folder"), NEW_FOLDER_POPUP);
+
 	if (ImGui::BeginPopupModal(title.c_str(), nullptr, ImGuiWindowFlags_AlwaysAutoResize)) {
 		ImGui::TextUnformatted(_("Enter a name for the new folder"));
 		ImGui::InputText("##newfoldername", &_newFolderName.name);
@@ -611,7 +612,7 @@ void FileDialog::popupNewFolder() {
 }
 
 bool FileDialog::popupAlreadyExists() {
-	const core::String title = makeTitle(FILE_ALREADY_EXISTS_POPUP, _("File already exists"));
+	const core::String title = makeTitle(_("File already exists"), FILE_ALREADY_EXISTS_POPUP);
 	if (ImGui::BeginPopupModal(title.c_str(), nullptr, ImGuiWindowFlags_AlwaysAutoResize)) {
 		ImGui::AlignTextToFramePadding();
 		ImGui::PushFont(imguiApp()->bigFont());