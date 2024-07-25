ce32244004eabe404ac702c982d7ba54308e1a8e
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-07-25 19:56:19 +0200

    VOXEDIT: don't perform any decompose when we hand over the single values for updating the transform

--- src/tools/voxedit/modules/voxedit-ui/NodeInspectorPanel.cpp
+++ src/tools/voxedit/modules/voxedit-ui/NodeInspectorPanel.cpp
@@ -218,44 +218,38 @@ void NodeInspectorPanel::sceneView(command::CommandExecutionListener &listener) 
 
 			const scenegraph::FrameIndex frameIdx = _sceneMgr->currentFrame();
 			scenegraph::KeyFrameIndex keyFrameIdx = node.keyFrameForFrame(frameIdx);
-			const scenegraph::SceneGraphTransform &transform = node.keyFrame(keyFrameIdx).transform();
-			const glm::mat4 &matrix = _localSpace ? transform.localMatrix() : transform.worldMatrix();
-			glm::vec3 matrixTranslation{0.0f};
-			glm::vec3 matrixRotation{0.0f};
-			glm::vec3 matrixScale{1.0f};
-			glm::vec3 skew{0.0f};
-			glm::quat matrixOrientation = glm::quat::wxyz(0, 0, 0, 0);
-			glm::vec4 perspective{0.0f};
+			scenegraph::SceneGraphTransform &transform = node.keyFrame(keyFrameIdx).transform();
+			glm::vec3 matrixTranslation = _localSpace ? transform.localTranslation() : transform.worldTranslation();
+			glm::vec3 matrixScale = _localSpace ? transform.localScale() : transform.worldScale();
+			glm::quat matrixOrientation = _localSpace ? transform.localOrientation() : transform.worldOrientation();
 			bool change = false;
-			if (glm::decompose(matrix, matrixScale, matrixOrientation, matrixTranslation, skew, perspective)) {
-				ImGui::Checkbox(_("Local transforms"), &_localSpace);
-				ImGui::CheckboxVar(_("Update children"), cfg::VoxEditTransformUpdateChildren);
-				change |= ImGui::InputFloat3(_("Tr"), glm::value_ptr(matrixTranslation), "%.3f",
-											ImGuiInputTextFlags_EnterReturnsTrue);
-				ImGui::SameLine();
-				if (ImGui::Button(ICON_LC_X "##resettr")) {
-					matrixTranslation[0] = matrixTranslation[1] = matrixTranslation[2] = 0.0f;
-					change = true;
-				}
-				ImGui::TooltipTextUnformatted(_("Reset"));
-
-				matrixRotation = glm::degrees(glm::eulerAngles(matrixOrientation));
-				change |= ImGui::InputFloat3(_("Rt"), glm::value_ptr(matrixRotation), "%.3f", ImGuiInputTextFlags_EnterReturnsTrue);
-				ImGui::SameLine();
-				if (ImGui::Button(ICON_LC_X "##resetrt")) {
-					matrixRotation[0] = matrixRotation[1] = matrixRotation[2] = 0.0f;
-					change = true;
-				}
-				ImGui::TooltipTextUnformatted(_("Reset"));
-
-				change |= ImGui::InputFloat3(_("Sc"), glm::value_ptr(matrixScale), "%.3f", ImGuiInputTextFlags_EnterReturnsTrue);
-				ImGui::SameLine();
-				if (ImGui::Button(ICON_LC_X "##resetsc")) {
-					matrixScale[0] = matrixScale[1] = matrixScale[2] = 1.0f;
-					change = true;
-				}
-				ImGui::TooltipTextUnformatted(_("Reset"));
+			ImGui::Checkbox(_("Local transforms"), &_localSpace);
+			ImGui::CheckboxVar(_("Update children"), cfg::VoxEditTransformUpdateChildren);
+			change |= ImGui::InputFloat3(_("Tr"), glm::value_ptr(matrixTranslation), "%.3f",
+										ImGuiInputTextFlags_EnterReturnsTrue);
+			ImGui::SameLine();
+			if (ImGui::Button(ICON_LC_X "##resettr")) {
+				matrixTranslation[0] = matrixTranslation[1] = matrixTranslation[2] = 0.0f;
+				change = true;
 			}
+			ImGui::TooltipTextUnformatted(_("Reset"));
+
+			glm::vec3 matrixRotation = glm::degrees(glm::eulerAngles(matrixOrientation));
+			change |= ImGui::InputFloat3(_("Rt"), glm::value_ptr(matrixRotation), "%.3f", ImGuiInputTextFlags_EnterReturnsTrue);
+			ImGui::SameLine();
+			if (ImGui::Button(ICON_LC_X "##resetrt")) {
+				matrixRotation[0] = matrixRotation[1] = matrixRotation[2] = 0.0f;
+				change = true;
+			}
+			ImGui::TooltipTextUnformatted(_("Reset"));
+
+			change |= ImGui::InputFloat3(_("Sc"), glm::value_ptr(matrixScale), "%.3f", ImGuiInputTextFlags_EnterReturnsTrue);
+			ImGui::SameLine();
+			if (ImGui::Button(ICON_LC_X "##resetsc")) {
+				matrixScale[0] = matrixScale[1] = matrixScale[2] = 1.0f;
+				change = true;
+			}
+			ImGui::TooltipTextUnformatted(_("Reset"));
 
 			glm::vec3 pivot = node.pivot();
 			bool pivotChanged =
@@ -287,8 +281,8 @@ void NodeInspectorPanel::sceneView(command::CommandExecutionListener &listener) 
 				if (pivotChanged) {
 					_sceneMgr->nodeUpdatePivot(node.id(), pivot);
 				} else {
-					matrix = glm::recompose(matrixScale, glm::quat(glm::radians(matrixRotation)), matrixTranslation, skew, perspective);
-					_sceneMgr->nodeUpdateTransform(node.id(), matrix, keyFrameIdx, _localSpace);
+					_sceneMgr->nodeUpdateTransform(node.id(), matrixRotation, matrixScale, matrixTranslation,
+												   keyFrameIdx, _localSpace);
 				}
 			}
 		}
