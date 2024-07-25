01d147e6b1e279f48bbae4470b45e43e6f08db95
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-07-25 08:14:14 +0200

    VOXELCOLLECTION: todo comment

--- src/modules/voxelcollection/Downloader.cpp
+++ src/modules/voxelcollection/Downloader.cpp
@@ -46,6 +46,7 @@ core::DynamicArray<VoxelSource> Downloader::sources() {
 	http::Request request("https://vengi-voxel.de/api/browser-data", http::RequestType::GET);
 	const core::String userAgent = app::App::getInstance()->fullAppname() + "/" PROJECT_VERSION;
 	request.setUserAgent(userAgent);
+	// TODO: add new cvars - voxelbrowser does no longer exist
 	request.setConnectTimeoutSecond(core::Var::get("vb_connect_timeout", 10));
 	request.setTimeoutSecond(core::Var::get("vb_timeout", 10));
 	core::String json;
