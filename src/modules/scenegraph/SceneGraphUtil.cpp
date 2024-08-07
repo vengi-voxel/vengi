ca58b95288268e3a3ca5db439612cf1b4c869b9f
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-08-07 17:17:55 +0200

    SCENEGRAPH: extended error logging

--- src/modules/scenegraph/SceneGraphUtil.cpp
+++ src/modules/scenegraph/SceneGraphUtil.cpp
@@ -16,7 +16,7 @@ namespace scenegraph {
 
 static int addToGraph(SceneGraph &sceneGraph, SceneGraphNode &&node, int parent) {
 	if (parent > 0 && !sceneGraph.hasNode(parent)) {
-		Log::error("Can't find parent node %i", parent);
+		Log::error("Can't find parent node %i for %s", parent, node.name().c_str());
 		parent = sceneGraph.root().id();
 	}
 	int newNodeId = sceneGraph.emplace(core::move(node), parent);
