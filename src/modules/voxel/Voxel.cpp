d2992d37bbbf0ae6851553f748445f6fe30328d1
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-08-31 11:14:46 +0200

    VOXEL: also create air voxels if the color is 0

--- src/modules/voxel/Voxel.cpp
+++ src/modules/voxel/Voxel.cpp
@@ -26,8 +26,14 @@ void Voxel::setFlags(uint8_t flags) {
 }
 
 voxel::Voxel createVoxel(const palette::Palette &pal, uint8_t index, uint8_t flags) {
-	if (index < pal.size() && pal.color(index).a != 255) {
-		return createVoxel(VoxelType::Transparent, index, flags);
+	if (index < pal.size()) {
+		const core::RGBA color = pal.color(index);
+		if (color.rgba == 0) {
+			return {};
+		}
+		if (color.a != 255) {
+			return createVoxel(VoxelType::Transparent, index, flags);
+		}
 	}
 	return createVoxel(VoxelType::Generic, index, flags);
 }
