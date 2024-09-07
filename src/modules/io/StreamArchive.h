333bfdea11b6332b90f4b36d54fe4773713662e3
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-09-07 19:49:57 +0200

    IO: StreamArchive is resetting the position to the initial stream value whenever a new stream is requested

--- src/modules/io/StreamArchive.h
+++ src/modules/io/StreamArchive.h
@@ -14,17 +14,23 @@ class StreamArchive : public Archive {
 private:
 	io::SeekableReadStream *_readStream;
 	io::SeekableWriteStream *_writeStream;
+	int64_t _pos;
 
 public:
 	StreamArchive(io::SeekableReadStream *stream) : _readStream(stream) {
+		_pos = stream->pos();
 	}
 	StreamArchive(io::SeekableWriteStream *stream) : _writeStream(stream) {
+		_pos = stream->pos();
 	}
 	~StreamArchive() override = default;
+
 	SeekableReadStream *readStream(const core::String &filePath) override {
+		_readStream->seek(_pos);
 		return new SeekableReadWriteStreamWrapper(_readStream);
 	}
 	SeekableWriteStream *writeStream(const core::String &filePath) override {
+		_writeStream->seek(_pos);
 		return new SeekableReadWriteStreamWrapper(_writeStream);
 	}
 };
