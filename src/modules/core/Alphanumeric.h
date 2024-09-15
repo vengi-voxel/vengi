25ea06cfe997fbd74494c53333e8042f99f40612
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-09-15 14:37:01 +0200

    CORE: added alphanumeric sort implementation
    see issue #516

---
+++ src/modules/core/Alphanumeric.h
@@ -0,0 +1,73 @@
+/**
+ * @file
+ */
+
+#include "String.h"
+#include "core/StringUtil.h"
+
+namespace core {
+
+class Alphanumeric {
+private:
+	const char *_value;
+
+public:
+	Alphanumeric(const char *str) : _value(str) {
+	}
+
+	// Overloading the < operator
+	bool operator<(const Alphanumeric &other) const {
+		size_t i = 0, j = 0;
+		size_t len1 = strlen(_value);
+		size_t len2 = strlen(other._value);
+
+		while (i < len1 && j < len2) {
+			if (isdigit(_value[i]) && isdigit(other._value[j])) {
+				// Compare numeric values
+				core::String num1, num2;
+
+				// Extract full number from the first string
+				while (i < len1 && isdigit(_value[i])) {
+					num1 += _value[i++];
+				}
+
+				// Extract full number from the second string
+				while (j < len2 && isdigit(other._value[j])) {
+					num2 += other._value[j++];
+				}
+
+				// Compare the numerical values as integers
+				int int1 = core::string::toInt(num1);
+				int int2 = core::string::toInt(num2);
+				if (int1 != int2) {
+					return int1 < int2;
+				}
+			} else if (isalpha(_value[i]) && isalpha(other._value[j])) {
+				// Compare alphabetically
+				char v1 = string::toLower(_value[i]);
+				char v2 = string::toLower(other._value[j]);
+				if (v1 != v2) {
+					return v1 < v2;
+				}
+				++i;
+				++j;
+			} else {
+				// Digits come before letters
+				return isdigit(_value[i]) && isalpha(other._value[j]);
+			}
+		}
+
+		// If we reached here, one string is a prefix of the other
+		return len1 < len2;
+	}
+
+	bool operator>(const Alphanumeric &other) const {
+		return other < *this;
+	}
+
+	const char *c_str() const {
+		return _value;
+	}
+};
+
+} // namespace core
---
+++ src/modules/core/tests/AlphanumericTest.cpp
@@ -0,0 +1,44 @@
+/**
+ * @file
+ */
+
+#include "core/Alphanumeric.h"
+#include <gtest/gtest.h>
+
+namespace core {
+
+inline std::ostream &operator<<(::std::ostream &os, const Alphanumeric &dt) {
+	return os << dt.c_str();
+}
+
+TEST(AlphanumericTest, testSort) {
+	Alphanumeric a1("1");
+	Alphanumeric a10("10");
+	Alphanumeric a11("11");
+	Alphanumeric a100("100");
+	Alphanumeric a2("2");
+	Alphanumeric a21("21");
+	Alphanumeric a3("3");
+	Alphanumeric str21("str21");
+	Alphanumeric Str20("Str20");
+
+	EXPECT_LT(a1, a2);
+	EXPECT_LT(a2, a3);
+	EXPECT_LT(a3, a10);
+	EXPECT_LT(a10, a11);
+	EXPECT_LT(a11, a21);
+	EXPECT_LT(a21, a100);
+	EXPECT_LT(a100, str21);
+	EXPECT_LT(Str20, str21);
+
+	EXPECT_GT(a2, a1);
+	EXPECT_GT(a3, a2);
+	EXPECT_GT(a10, a3);
+	EXPECT_GT(a11, a10);
+	EXPECT_GT(a21, a11);
+	EXPECT_GT(a100, a21);
+	EXPECT_GT(str21, a100);
+	EXPECT_GT(str21, Str20);
+}
+
+} // namespace core
