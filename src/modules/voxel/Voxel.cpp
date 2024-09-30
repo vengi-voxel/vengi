bce44f9a0307a8196766b77410bfe8df17b07d50
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-09-30 17:03:22 +0200

    VOXEL: removed unused code

--- src/modules/voxel/Voxel.cpp
+++ src/modules/voxel/Voxel.cpp
@@ -9,16 +9,6 @@
 
 namespace voxel {
 
-VoxelType getVoxelType(const char *str) {
-	for (int j = 0; j < (int)voxel::VoxelType::Max; ++j) {
-		if (SDL_strcmp(VoxelTypeStr[j], str) != 0) {
-			continue;
-		}
-		return (VoxelType)j;
-	}
-	return VoxelType::Max;
-}
-
 void Voxel::setFlags(uint8_t flags) {
 	_flags = flags;
 	// max 3 bits
