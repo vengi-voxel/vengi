5ad1bb180f7141d55a88934f51bfc60c4c8312f1
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-10-31 14:28:04 +0100

    VOXEL: enable binary mesher and disable optimizations for now

--- src/modules/voxelformat/FormatConfig.cpp
+++ src/modules/voxelformat/FormatConfig.cpp
@@ -29,8 +29,8 @@ bool FormatConfig::init() {
 
 	core::Var::get(cfg::VoxformatMergequads, "true", core::CV_NOPERSIST, _("Merge similar quads to optimize the mesh"),
 				   core::Var::boolValidator);
-	core::Var::get(cfg::VoxelMeshMode, core::string::toString((int)voxel::SurfaceExtractionType::Cubic),
-				   core::CV_SHADER, _("0 = cubes, 1 = marching cubes"),
+	core::Var::get(cfg::VoxelMeshMode, core::string::toString((int)voxel::SurfaceExtractionType::Binary),
+				   core::CV_SHADER, _("0 = cubes, 1 = marching cubes, 2 = binary mesher"),
 				   core::Var::minMaxValidator<(int)voxel::SurfaceExtractionType::Cubic,
 											  (int)voxel::SurfaceExtractionType::Max - 1>);
 	core::Var::get(cfg::VoxformatReusevertices, "true", core::CV_NOPERSIST,
