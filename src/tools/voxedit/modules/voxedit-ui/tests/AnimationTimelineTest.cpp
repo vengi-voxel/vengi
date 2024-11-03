f1fd0afe7eec80fac72212547351f3cd0fda7fa8
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-11-03 11:51:54 +0100

    VOXEDIT: use activateViewportSceneMode in timeline test

--- src/tools/voxedit/modules/voxedit-ui/tests/AnimationTimelineTest.cpp
+++ src/tools/voxedit/modules/voxedit-ui/tests/AnimationTimelineTest.cpp
@@ -15,11 +15,7 @@ void AnimationTimeline::registerUITests(ImGuiTestEngine *engine, const char *id)
 	// https://github.com/ocornut/imgui_test_engine/issues/48
 	// https://gitlab.com/GroGy/im-neo-sequencer/-/issues/28
 	IM_REGISTER_TEST(engine, testCategory(), "create keyframe")->TestFunc = [=](ImGuiTestContext *ctx) {
-		const int viewportId = viewportSceneMode(ctx, _app);
-		IM_CHECK_SILENT(viewportId != -1);
-		const core::String id = Viewport::viewportId(viewportId);
-		ctx->ItemClick(id.c_str());
-
+		activateViewportSceneMode(ctx, _app);
 		IM_CHECK(focusWindow(ctx, id));
 		ctx->SetRef(ctx->WindowInfo("##sequencer_child_wrapper").ID);
 		ctx->MouseMove("sequencer/##_top_selector_neo");
