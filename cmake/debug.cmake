d79778dde204236daeb21bcae687a4603dfec14d
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-11-18 18:37:18 +0100

    CMAKE: fixed cmake warnings for 3.31.x
    see https://cmake.org/cmake/help/latest/policy/CMP0175.html

--- cmake/debug.cmake
+++ cmake/debug.cmake
@@ -14,20 +14,18 @@ function(engine_add_debuggger TARGET)
 	if (${DEBUGGER} MATCHES "gdb")
 		add_custom_target(${TARGET}-debug)
 		add_custom_command(TARGET ${TARGET}-debug
+			POST_BUILD
 			COMMAND ${GDB_EXECUTABLE} -ex run --args $<TARGET_FILE:${TARGET}>
 			COMMENT "Starting debugger session for ${TARGET}"
-			USES_TERMINAL
 			WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/${TARGET}
-			DEPENDS ${TARGET}
 		)
 	elseif (${DEBUGGER} MATCHES "lldb")
 		add_custom_target(${TARGET}-debug)
 		add_custom_command(TARGET ${TARGET}-debug
+			POST_BUILD
 			COMMAND CG_CONTEXT_SHOW_BACKTRACE=1 ${LLDB_EXECUTABLE} -b -o run $<TARGET_FILE:${TARGET}>
 			COMMENT "Starting debugger session for ${TARGET}"
-			USES_TERMINAL
 			WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/${TARGET}
-			DEPENDS ${TARGET}
 		)
 	endif()
 endfunction()
