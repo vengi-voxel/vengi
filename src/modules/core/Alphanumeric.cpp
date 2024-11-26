3d818d62cd0f121150728221ef5cc06d27e2f8b9
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-11-27 00:31:39 +0100

    CORE: let the class Alphanumeric also work with utf8 chars
    this fixes an assert on msvc for unicode paths

---
+++ src/modules/core/Alphanumeric.cpp
@@ -0,0 +1,76 @@
+/**
+ * @file
+ */
+
+#include "Alphanumeric.h"
+#include "UTF8.h"
+#include "core/StandardLib.h"
+#include <ctype.h>
+#include <string.h>
+
+namespace core {
+
+int Alphanumeric::ndigits(const char *s) {
+	const int n = utf8::length(s);
+
+	int nd = 0;
+	for (int i = 0; i < n; i++) {
+		int n = utf8::next(&s);
+		if (n < 0) {
+			break;
+		}
+		if (n >= '0' && n <= '9') {
+			++nd;
+		}
+	}
+	return nd;
+}
+
+bool Alphanumeric::operator<(const Alphanumeric &other) const {
+	const char *s = _value;
+	const char *t = other._value;
+
+	const int sd = ndigits(s);
+	const int td = ndigits(t);
+
+	/* presence of digits */
+	if (!sd && !td) {
+		return core_strcasecmp(s, t);
+	}
+	if (!sd) {
+		return 1;
+	}
+	if (!td) {
+		return -1;
+	}
+
+	/* value of digits */
+	int i, j;
+	for (i = 0, j = 0; i < sd && j < td; ++i, ++j) {
+		while (!isdigit(*s)) {
+			++s;
+		}
+		while (!isdigit(*t)) {
+			++t;
+		}
+
+		if (*s != *t) {
+			return *s - *t;
+		}
+		++s;
+		++t;
+	}
+
+	/* number of digits */
+	if (i < sd) {
+		return 1;
+	}
+	if (j < td) {
+		return -1;
+	}
+
+	/* value of string after last digit */
+	return core_strcasecmp(s, t);
+}
+
+} // namespace core
--- src/modules/core/Alphanumeric.h
+++ src/modules/core/Alphanumeric.h
@@ -2,27 +2,13 @@
  * @file
  */
 
-#include "core/StandardLib.h"
-#include <ctype.h>
-#include <string.h>
-
 namespace core {
 
 class Alphanumeric {
 private:
 	const char *_value;
 
-	static int ndigits(const char *s) {
-		const int n = strlen(s);
-
-		int nd = 0;
-		for (int i = 0; i < n; i++) {
-			if (isdigit(s[i])) {
-				++nd;
-			}
-		}
-		return nd;
-	}
+	static int ndigits(const char *s);
 
 public:
 	Alphanumeric(const char *str) : _value(str) {
@@ -30,52 +16,7 @@ public:
 
 	// Overloading the < operator
 	// https://stackoverflow.com/a/10204043/774082
-	bool operator<(const Alphanumeric &other) const {
-		const char *s = _value;
-		const char *t = other._value;
-
-		const int sd = ndigits(s);
-		const int td = ndigits(t);
-
-		/* presence of digits */
-		if (!sd && !td) {
-			return core_strcasecmp(s, t);
-		}
-		if (!sd) {
-			return 1;
-		}
-		if (!td) {
-			return -1;
-		}
-
-		/* value of digits */
-		int i, j;
-		for (i = 0, j = 0; i < sd && j < td; ++i, ++j) {
-			while (!isdigit(*s)) {
-				++s;
-			}
-			while (!isdigit(*t)) {
-				++t;
-			}
-
-			if (*s != *t) {
-				return *s - *t;
-			}
-			++s;
-			++t;
-		}
-
-		/* number of digits */
-		if (i < sd) {
-			return 1;
-		}
-		if (j < td) {
-			return -1;
-		}
-
-		/* value of string after last digit */
-		return core_strcasecmp(s, t);
-	}
+	bool operator<(const Alphanumeric &other) const;
 
 	bool operator>(const Alphanumeric &other) const {
 		return other < *this;
