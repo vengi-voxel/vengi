01a4b0d11118d97e6b59ceb2b108e67263a36abe
Martin Gerhardy <martin.gerhardy@gmail.com>
2024-11-28 17:10:44 +0100

    VOXEDIT: fixed writing palettes on windows
    again about the duplicated file opens...
    
    see issue #495 and #518

--- src/tools/voxedit/modules/voxedit-util/SceneManager.cpp
+++ src/tools/voxedit/modules/voxedit-util/SceneManager.cpp
@@ -144,8 +144,8 @@ bool SceneManager::importPalette(const core::String& file, bool setActive, bool 
 
 	const core::String paletteName(core::string::extractFilename(file));
 	const core::String &paletteFilename = core::string::format("palette-%s.png", paletteName.c_str());
-	const io::FilePtr &pngFile = _filesystem->open(paletteFilename, io::FileMode::Write);
-	if (palette.save(pngFile->name().c_str())) {
+	const core::String &pngFile = _filesystem->open(paletteFilename, io::FileMode::Write)->name();
+	if (palette.save(pngFile.c_str())) {
 		core::Var::getSafe(cfg::VoxEditLastPalette)->setVal(paletteName);
 	}
 
