832fdb45f45aa2f284fbce7e06d34d7c7fe04483
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-02-21 17:56:11 +0100

    VOXEDIT: added ui test for normal palette panel

--- src/tools/voxedit/modules/voxedit-ui/tests/NormalPalettePanelTest.cpp
+++ src/tools/voxedit/modules/voxedit-ui/tests/NormalPalettePanelTest.cpp
@@ -3,11 +3,21 @@
  */
 
 #include "../NormalPalettePanel.h"
+#include "../ViewMode.h"
+#include "util/VarUtil.h"
+#include "voxedit-util/Config.h"
 #include "voxedit-util/SceneManager.h"
 
 namespace voxedit {
 
 void NormalPalettePanel::registerUITests(ImGuiTestEngine *engine, const char *id) {
+	IM_REGISTER_TEST(engine, testCategory(), "none")->TestFunc = [=](ImGuiTestContext *ctx) {
+		// Activate viewmode Command&Conquer to see the normal palette panel
+		util::ScopedVarChange viewMode(cfg::VoxEditViewMode, (int)ViewMode::CommandAndConquer);
+		ctx->Yield(); // allow the cvar change to propagate to the ui
+
+		IM_CHECK(focusWindow(ctx, id));
+	};
 }
 
 } // namespace voxedit
