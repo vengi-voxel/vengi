df54dec6e39eda0c27d2deb647ff5bbff16a07d6
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-02-22 21:15:17 +0100

    VOXEDIT: implemented SelectionManagerTest

--- src/tools/voxedit/modules/voxedit-util/tests/SelectionManagerTest.cpp
+++ src/tools/voxedit/modules/voxedit-util/tests/SelectionManagerTest.cpp
@@ -4,6 +4,7 @@
 
 #include "../modifier/SelectionManager.h"
 #include "app/tests/AbstractTest.h"
+#include "voxel/RawVolume.h"
 
 namespace voxedit {
 
@@ -12,10 +13,15 @@ private:
 	using Super = app::AbstractTest;
 };
 
-TEST_F(SelectionManagerTest, test) {
+TEST_F(SelectionManagerTest, testSelectAndInvert) {
+	voxel::Region region(0, 16);
+	voxel::RawVolume volume(region);
 	SelectionManager mgr;
 	EXPECT_FALSE(mgr.hasSelection());
-	// TODO: SELECTION: implement test
+	EXPECT_TRUE(mgr.select(volume, glm::ivec3(4), glm::ivec3(12)));
+	EXPECT_TRUE(mgr.hasSelection());
+	mgr.invert(volume);
+	EXPECT_EQ(6u, mgr.selections().size());
 }
 
 } // namespace voxedit
