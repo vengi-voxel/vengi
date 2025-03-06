038c57c88c1b4f89278f76ee6c3b51c9310fa85c
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-03-06 18:15:09 +0100

    IMAGE: extended benchmark

--- src/modules/image/benchmarks/ImageWriteBenchmark.cpp
+++ src/modules/image/benchmarks/ImageWriteBenchmark.cpp
@@ -8,24 +8,54 @@
 
 class ImageWriteBenchmark : public app::AbstractBenchmark {
 protected:
-	image::ImagePtr _image;
+	image::ImagePtr _imagePNG;
+	io::FilePtr _filePNG;
+	io::FilePtr _fileJPG;
 
 public:
 	void SetUp(::benchmark::State &state) override {
 		app::AbstractBenchmark::SetUp(state);
-		io::FilePtr file = _benchmarkApp->filesystem()->open("test-palette-in.png");
-		_image = image::loadImage(file);
+
+		_filePNG = {};
+		_fileJPG = {};
+
+		_filePNG = _benchmarkApp->filesystem()->open("test-palette-in.png");
+		_fileJPG = _benchmarkApp->filesystem()->open("benchmark-caveexpress.jpg");
+		_imagePNG = image::loadImage(_filePNG);
+	}
+
+	void TearDown(::benchmark::State &state) override {
+		_imagePNG = {};
+		_filePNG->close();
+		_fileJPG->close();
+		_filePNG = {};
+		_fileJPG = {};
+		app::AbstractBenchmark::TearDown(state);
 	}
 };
 
-BENCHMARK_DEFINE_F(ImageWriteBenchmark, Visit)(benchmark::State &state) {
+BENCHMARK_DEFINE_F(ImageWriteBenchmark, WriteJPG)(benchmark::State &state) {
 	io::BufferedReadWriteStream stream(1024 * 1024 * 4);
 	for (auto _ : state) {
-		_image->writeJPEG(stream);
+		_imagePNG->writeJPEG(stream);
 		stream.seek(0);
 	}
 }
 
-BENCHMARK_REGISTER_F(ImageWriteBenchmark, Visit);
+BENCHMARK_DEFINE_F(ImageWriteBenchmark, LoadJPG)(benchmark::State &state) {
+	for (auto _ : state) {
+		image::loadImage(_fileJPG);
+	}
+}
+
+BENCHMARK_DEFINE_F(ImageWriteBenchmark, LoadPNG)(benchmark::State &state) {
+	for (auto _ : state) {
+		image::loadImage(_filePNG);
+	}
+}
+
+BENCHMARK_REGISTER_F(ImageWriteBenchmark, LoadPNG);
+BENCHMARK_REGISTER_F(ImageWriteBenchmark, LoadJPG);
+BENCHMARK_REGISTER_F(ImageWriteBenchmark, WriteJPG);
 
 BENCHMARK_MAIN();
