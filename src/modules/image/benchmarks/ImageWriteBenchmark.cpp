617ccc6bb3569c679eb9a30d9df48bba6848256b
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-03-06 18:05:51 +0100

    IMAGE: added image writing benchmark

---
+++ src/modules/image/benchmarks/ImageWriteBenchmark.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file
+ */
+
+#include "app/benchmark/AbstractBenchmark.h"
+#include "image/Image.h"
+#include "io/BufferedReadWriteStream.h"
+#include <fcntl.h>
+
+class ImageWriteBenchmark : public app::AbstractBenchmark {
+protected:
+	image::ImagePtr _image;
+
+public:
+	void SetUp(::benchmark::State &state) override {
+		app::AbstractBenchmark::SetUp(state);
+		io::FilePtr file = _benchmarkApp->filesystem()->open("test-palette-in.png");
+		_image = image::loadImage(file);
+	}
+};
+
+BENCHMARK_DEFINE_F(ImageWriteBenchmark, Visit)(benchmark::State &state) {
+	io::BufferedReadWriteStream stream(1024 * 1024 * 4);
+	for (auto _ : state) {
+		_image->writeJPEG(stream);
+		stream.seek(0);
+	}
+}
+
+BENCHMARK_REGISTER_F(ImageWriteBenchmark, Visit);
+
+BENCHMARK_MAIN();
