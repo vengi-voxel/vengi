68fe97ed0d8777a1b448959c8c08471028b50bea
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-03-27 20:56:05 +0100

    CORE: added benchmark for string

---
+++ src/modules/core/benchmarks/StringBenchmark.cpp
@@ -0,0 +1,37 @@
+#include "app/benchmark/AbstractBenchmark.h"
+#include "core/StringUtil.h"
+
+class StringBenchmark: public app::AbstractBenchmark {
+};
+
+BENCHMARK_DEFINE_F(StringBenchmark, ctor1) (benchmark::State& state) {
+	for (auto _ : state) {
+		core::String str("test");
+		benchmark::DoNotOptimize(str);
+	}
+}
+
+BENCHMARK_DEFINE_F(StringBenchmark, ctor2) (benchmark::State& state) {
+	for (auto _ : state) {
+		core::String str("test", 4);
+		benchmark::DoNotOptimize(str);
+	}
+}
+BENCHMARK_DEFINE_F(StringBenchmark, format) (benchmark::State& state) {
+	for (auto _ : state) {
+		core::String str(core::move(core::string::format("%100s", "test")));
+		benchmark::DoNotOptimize(str);
+	}
+}
+
+BENCHMARK_DEFINE_F(StringBenchmark, formatBuf) (benchmark::State& state) {
+	for (auto _ : state) {
+		char buf[101];
+		core::string::formatBuf(buf, sizeof(buf), "%100s", "test");
+	}
+}
+
+BENCHMARK_REGISTER_F(StringBenchmark, format);
+BENCHMARK_REGISTER_F(StringBenchmark, formatBuf);
+BENCHMARK_REGISTER_F(StringBenchmark, ctor1);
+BENCHMARK_REGISTER_F(StringBenchmark, ctor2);
