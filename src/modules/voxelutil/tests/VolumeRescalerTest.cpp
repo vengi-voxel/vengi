c8330df1baf0a5f1786509a2b6404ee7b4854cd5
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-04-15 18:57:39 +0100

    VOXELUTIL: added test for scaleDown

--- src/modules/voxelutil/tests/VolumeRescalerTest.cpp
+++ src/modules/voxelutil/tests/VolumeRescalerTest.cpp
@@ -27,6 +27,10 @@ protected:
 		ASSERT_EQ(dims, volume.region().getDimensionsInVoxels() * 2);
 		ASSERT_EQ(mins, volume.region().getLowerCorner());
 	}
+
+	int countVoxels(const voxel::RawVolume &volume) {
+		return voxelutil::visitVolume(volume, voxelutil::EmptyVisitor(), voxelutil::SkipEmpty());
+	}
 };
 
 TEST_F(VolumeRescalerTest, testScaleUpEmpty) {
@@ -43,6 +47,26 @@ TEST_F(VolumeRescalerTest, testScaleUpEmpty) {
 TEST_F(VolumeRescalerTest, testScaleUpFull) {
 	testScaleUpFull(-8, 8);
 	testScaleUpFull(7, 8);
+}
+
+TEST_F(VolumeRescalerTest, testScaleDown) {
+	voxel::RawVolume volume({-8, 8});
+	for (int y = -8; y <= 8; ++y) {
+		for (int x = -2; x <= 2; ++x) {
+			for (int z = -2; z <= 2; ++z) {
+				volume.setVoxel(x, y, z, voxel::createVoxel(voxel::VoxelType::Generic, y + 8));
+			}
+		}
+	}
+	palette::Palette pal;
+	pal.nippon();
+	ASSERT_EQ(5 * 5 * 17, countVoxels(volume));
+	const voxel::Region &srcRegion = volume.region();
+	const glm::ivec3 &targetDimensionsHalf = (srcRegion.getDimensionsInVoxels() / 2) - 1;
+	const voxel::Region destRegion(srcRegion.getLowerCorner(), srcRegion.getLowerCorner() + targetDimensionsHalf);
+	voxel::RawVolume destVolume(destRegion);
+	voxelutil::scaleDown(volume, pal, destVolume);
+	ASSERT_EQ(32, countVoxels(destVolume));
 }
 
 } // namespace voxelutil
