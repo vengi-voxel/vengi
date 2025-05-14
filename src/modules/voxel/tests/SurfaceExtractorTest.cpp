vengi-voxel/vengi

commit 8a06e249fa390ed85628d180cb3abc691b478cd7

VOXEL: mesh extracation on chunk boundaries is broken #488
also see issue #445

src/modules/voxel/tests/SurfaceExtractorTest.cpp
@@ -126,6 +126,7 @@ TEST_F(SurfaceExtractorTest, testMeshExtractionIssue445) {
 
 	voxel::ChunkMesh mesh;
 
+	region.shiftUpperCorner(1, 1, 1);
 	SurfaceExtractionContext ctx =
 		voxel::buildCubicContext(&v, region, mesh, glm::ivec3(0), mergeQuads, reuseVertices, ambientOcclusion);
 	voxel::extractSurface(ctx);

src/modules/voxel/MeshState.cpp
@@ -254,7 +254,8 @@ bool MeshState::scheduleRegionExtraction(int idx, const voxel::Region &region) {
 	const int s = _meshSize->intVal();
 	const glm::ivec3 meshSize(s);
 	const glm::ivec3 meshSizeMinusOne(s - 1);
-	const voxel::Region &completeRegion = v->region();
+	voxel::Region completeRegion = v->region();
+	completeRegion.shiftUpperCorner(1, 1, 1);
 
 	// convert to step coordinates that are needed to extract
 	// the given region mesh size ranges

src/modules/voxel/SurfaceExtractor.cpp
@@ -26,13 +26,9 @@ SurfaceExtractionContext buildMarchingCubesContext(const RawVolume *volume, cons
 
 void extractSurface(SurfaceExtractionContext &ctx) {
 	if (ctx.type == SurfaceExtractionType::MarchingCubes) {
-		voxel::Region extractRegion = ctx.region;
-		extractRegion.shrink(-1);
-		voxel::extractMarchingCubesMesh(ctx.volume, ctx.palette, extractRegion, &ctx.mesh, ctx.optimize);
+		voxel::extractMarchingCubesMesh(ctx.volume, ctx.palette, ctx.region, &ctx.mesh, ctx.optimize);
 	} else {
-		voxel::Region extractRegion = ctx.region;
-		extractRegion.shiftUpperCorner(1, 1, 1);
-		voxel::extractCubicMesh(ctx.volume, extractRegion, &ctx.mesh, ctx.translate, ctx.mergeQuads, ctx.reuseVertices,
+		voxel::extractCubicMesh(ctx.volume, ctx.region, &ctx.mesh, ctx.translate, ctx.mergeQuads, ctx.reuseVertices,
 								ctx.ambientOcclusion, ctx.optimize);
 	}
 }

src/modules/voxelformat/private/mesh/MeshFormat.cpp
@@ -610,8 +610,11 @@ bool MeshFormat::saveGroups(const scenegraph::SceneGraph &sceneGraph, const core
 		const scenegraph::SceneGraphNode &node = *iter;
 		app::async([&, volume = sceneGraph.resolveVolume(node), region = sceneGraph.resolveRegion(node)]() {
 			voxel::ChunkMesh *mesh = new voxel::ChunkMesh();
+			voxel::Region regionExt = region;
+			// we are increasing the region by one voxel to ensure the inclusion of the boundary voxels in this mesh
+			regionExt.shiftUpperCorner(1, 1, 1);
 			voxel::SurfaceExtractionContext ctx =
-				voxel::createContext(type, volume, region, node.palette(), *mesh, {0, 0, 0}, mergeQuads,
+				voxel::createContext(type, volume, regionExt, node.palette(), *mesh, {0, 0, 0}, mergeQuads,
 									 reuseVertices, ambientOcclusion);
 			voxel::extractSurface(ctx);
 			if (withNormals) {

commit e76ed8b3e04c788331984dad044efb7cad0eb135

VOXEL: fixed marching cubes extraction on volume boundaries
regression since 8a06e24 - see #488

src/modules/voxel/private/MarchingCubesSurfaceExtractor.cpp
@@ -97,9 +97,12 @@ static void generateVertex(math::Axis axis, const palette::Palette &palette, Raw
 	sampler.movePositive(axis);
 }
 
-void extractMarchingCubesMesh(const RawVolume *volume, const palette::Palette &palette, const Region &region, ChunkMesh *result, bool optimize) {
+void extractMarchingCubesMesh(const RawVolume *volume, const palette::Palette &palette, const Region &ctxRegion, ChunkMesh *result, bool optimize) {
 	core_assert_msg(volume != nullptr, "Provided volume cannot be null");
 	core_assert_msg(result != nullptr, "Provided mesh cannot be null");
+
+	voxel::Region region = ctxRegion;
+	region.shrink(-1);
 
 	result->clear();
 

src/modules/voxel/tests/SurfaceExtractorTest.cpp
@@ -4,6 +4,7 @@
 
 #include "voxel/SurfaceExtractor.h"
 #include "app/tests/AbstractTest.h"
+#include "palette/Palette.h"
 #include "voxel/ChunkMesh.h"
 #include "voxel/RawVolume.h"
 
@@ -98,11 +99,12 @@ TEST_F(SurfaceExtractorTest, DISABLED_testMeshExtraction) {
 	const bool mergeQuads = true;
 	const bool reuseVertices = true;
 	const bool ambientOcclusion = false;
+	const bool optimize = true;
 
 	voxel::ChunkMesh mesh;
 
 	SurfaceExtractionContext ctx =
-		voxel::buildCubicContext(&v, region, mesh, glm::ivec3(0), mergeQuads, reuseVertices, ambientOcclusion);
+		voxel::buildCubicContext(&v, region, mesh, glm::ivec3(0), mergeQuads, reuseVertices, ambientOcclusion, optimize);
 	voxel::extractSurface(ctx);
 	EXPECT_EQ(48, (int)mesh.mesh[0].getNoOfVertices());
 }
@@ -131,6 +133,30 @@ TEST_F(SurfaceExtractorTest, testMeshExtractionIssue445) {
 		voxel::buildCubicContext(&v, region, mesh, glm::ivec3(0), mergeQuads, reuseVertices, ambientOcclusion);
 	voxel::extractSurface(ctx);
 	EXPECT_EQ(8, (int)mesh.mesh[0].getNoOfVertices());
+}
+
+TEST_F(SurfaceExtractorTest, testMeshExtractionMarchingCubes) {
+	glm::ivec3 mins(-1, -1, -1);
+	glm::ivec3 maxs(1, -1, 1);
+	voxel::Region region(mins, maxs);
+	voxel::RawVolume v(region);
+	for (int x = mins.x; x <= maxs.x; ++x) {
+		for (int y = mins.y; y <= maxs.y; ++y) {
+			for (int z = mins.z; z <= maxs.z; ++z) {
+				v.setVoxel(x, y, z, voxel::createVoxel(voxel::VoxelType::Generic, 1));
+			}
+		}
+	}
+
+	voxel::ChunkMesh mesh;
+	palette::Palette pal;
+	pal.nippon();
+
+	region.shiftUpperCorner(1, 1, 1);
+	SurfaceExtractionContext ctx =
+		voxel::buildMarchingCubesContext(&v, region, mesh, pal, false);
+	voxel::extractSurface(ctx);
+	EXPECT_EQ(30, (int)mesh.mesh[0].getNoOfVertices());
 }
 
 } // namespace voxel