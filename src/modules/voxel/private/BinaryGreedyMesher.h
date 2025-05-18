353f79bed593a2513d2369365bab3461ef739230
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-05-18 14:27:46 +0200

    VOXEL: doxygen for binary mesher

--- src/modules/voxel/private/BinaryGreedyMesher.h
+++ src/modules/voxel/private/BinaryGreedyMesher.h
@@ -14,6 +14,13 @@ class RawVolume;
 class Region;
 struct ChunkMesh;
 
+/**
+ * The input data includes duplicate edge data from neighboring chunks which is used for visibility culling and AO. For
+ * optimal performance, your world data should already be structured this way so that you can feed the data straight
+ * into this algorithm.
+ *
+ * Input data is ordered in YXZ and is 64^3 which results in a 62^3 mesh.
+ */
 void extractBinaryGreedyMesh(const voxel::RawVolume *volData, const Region &region, ChunkMesh *result,
 							 const glm::ivec3 &translate, bool ambientOcclusion = true);
 
