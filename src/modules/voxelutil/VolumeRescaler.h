b882be561fa1e12f5787c8d0e493f717e7c09c77
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-05-30 20:16:05 +0200

    VOXELUTIL: improved performance for scaleUp

--- src/modules/voxelutil/VolumeRescaler.h
+++ src/modules/voxelutil/VolumeRescaler.h
@@ -222,10 +222,8 @@ void scaleDown(const SourceVolume &sourceVolume, const palette::Palette &palette
 
 [[nodiscard]] voxel::RawVolume *scaleUp(const voxel::RawVolume &sourceVolume) {
 	const voxel::Region srcRegion = sourceVolume.region();
-	const glm::ivec3 &dim = srcRegion.getDimensionsInVoxels();
-	const glm::ivec3 &mins = srcRegion.getLowerCorner();
-	const glm::ivec3 &targetDimensions = dim * 2 - 1;
-	const voxel::Region destRegion(mins, mins + targetDimensions);
+	const glm::ivec3 &targetDimensions = srcRegion.getDimensionsInVoxels() * 2 - 1;
+	const voxel::Region destRegion(srcRegion.getLowerCorner(), srcRegion.getLowerCorner() + targetDimensions);
 	if (!app::App::getInstance()->hasEnoughMemory(voxel::RawVolume::size(destRegion))) {
 		return nullptr;
 	}
@@ -235,24 +233,28 @@ void scaleDown(const SourceVolume &sourceVolume, const palette::Palette &palette
 											 glm::ivec3(0, 1, 1), glm::ivec3(1, 1, 1)};
 
 	voxel::RawVolume *destVolume = new voxel::RawVolume(destRegion);
-	voxel::RawVolume::Sampler sourceSampler(sourceVolume);
-	sourceSampler.setPosition(mins);
-	for (int32_t z = 0; z < dim.z; ++z) {
-		voxel::RawVolume::Sampler sourceSampler2 = sourceSampler;
-		for (int32_t y = 0; y < dim.y; ++y) {
-			voxel::RawVolume::Sampler sourceSampler3 = sourceSampler2;
-			for (int32_t x = 0; x < dim.x; ++x) {
-				const voxel::Voxel &voxel = sourceSampler3.voxel();
-				const glm::ivec3 targetPos(mins.x + x * 2, mins.y + y * 2, mins.z + z * 2);
-				for (int i = 0; i < 8; ++i) {
-					destVolume->setVoxel(targetPos + directions[i], voxel);
+	app::for_parallel(0, srcRegion.getDepthInVoxels(), [&sourceVolume, destVolume](size_t start, size_t end) {
+		voxel::RawVolume::Sampler sourceSampler(sourceVolume);
+		const glm::ivec3 &dim = sourceVolume.region().getDimensionsInVoxels();
+		const glm::ivec3 &mins = sourceVolume.region().getLowerCorner();
+		sourceSampler.setPosition(mins.x, mins.y, mins.z + start);
+		for (size_t z = start; z < end; ++z) {
+			voxel::RawVolume::Sampler sourceSampler2 = sourceSampler;
+			for (int32_t y = 0; y < dim.y; ++y) {
+				voxel::RawVolume::Sampler sourceSampler3 = sourceSampler2;
+				for (int32_t x = 0; x < dim.x; ++x) {
+					const voxel::Voxel &voxel = sourceSampler3.voxel();
+					const glm::ivec3 targetPos(mins.x + x * 2, mins.y + y * 2, mins.z + z * 2);
+					for (int i = 0; i < 8; ++i) {
+						destVolume->setVoxel(targetPos + directions[i], voxel);
+					}
+					sourceSampler3.movePositiveX();
 				}
-				sourceSampler3.movePositiveX();
+				sourceSampler2.movePositiveY();
 			}
-			sourceSampler2.movePositiveY();
+			sourceSampler.movePositiveZ();
 		}
-		sourceSampler.movePositiveZ();
-	}
+	});
 	return destVolume;
 }
 
