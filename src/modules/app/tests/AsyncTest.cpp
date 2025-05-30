f5e1475f53fb5bd9e9d5178434a6f4e18ee3e9da
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-05-30 20:00:06 +0200

    APP: fixed whitespaces

--- src/modules/app/tests/AsyncTest.cpp
+++ src/modules/app/tests/AsyncTest.cpp
@@ -7,23 +7,22 @@
 
 namespace app {
 
-class TestAsync : public app::AbstractTest {
-};
+class TestAsync : public app::AbstractTest {};
 
 TEST_F(TestAsync, testForParallel) {
-    constexpr int size = 512;
-    int buf[size + 1];
-    buf[size] = -1;
-    app::for_parallel(0, size, [&buf](int start, int end) {
-        for (int i = start; i < end; ++i) {
-            buf[i] = i;
-        }
-    });
-    for (int i = 0; i < size; ++i) {
-        ASSERT_EQ(buf[i], i);
-    }
-    // sentinel
-    ASSERT_EQ(buf[size], -1);
+	constexpr int size = 512;
+	int buf[size + 1];
+	buf[size] = -1;
+	app::for_parallel(0, size, [&buf](int start, int end) {
+		for (int i = start; i < end; ++i) {
+			buf[i] = i;
+		}
+	});
+	for (int i = 0; i < size; ++i) {
+		ASSERT_EQ(buf[i], i);
+	}
+	// sentinel
+	ASSERT_EQ(buf[size], -1);
 }
 
 } // namespace app
