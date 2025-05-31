65d7c20021b47bb5da873f4fbe7b5db709cf98e8
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-05-31 23:37:03 +0200

    VOXELFORMAT: MTS: added not yet working save for mts

--- src/modules/voxelformat/private/minecraft/MTSFormat.cpp
+++ src/modules/voxelformat/private/minecraft/MTSFormat.cpp
@@ -7,12 +7,16 @@
 #include "core/FourCC.h"
 #include "core/Log.h"
 #include "core/ScopedPtr.h"
+#include "core/StringUtil.h"
 #include "core/collection/Array3DView.h"
 #include "core/collection/Buffer.h"
 #include "core/collection/DynamicArray.h"
 #include "io/ZipReadStream.h"
+#include "io/ZipWriteStream.h"
 #include "scenegraph/SceneGraph.h"
 #include "scenegraph/SceneGraphNode.h"
+#include "voxel/RawVolume.h"
+#include "voxel/Region.h"
 #include "voxel/Voxel.h"
 #include "MinecraftPaletteMap.h"
 #include "palette/Palette.h"
@@ -120,16 +124,72 @@ bool MTSFormat::loadGroupsPalette(const core::String &filename, const io::Archiv
 	scenegraph::SceneGraphNode node(scenegraph::SceneGraphNodeType::Model);
 	node.setVolume(volume, true);
 	node.setPalette(palette);
-	sceneGraph.emplace(core::move(node));
-
-	return true;
+	node.setName(core::string::extractFilename(filename));
+	return sceneGraph.emplace(core::move(node)) != InvalidNodeId;
 }
 
 #undef wrap
 
+#define wrapBool(read)                                                                                                 \
+	if ((read) == false) {                                                                                                 \
+		Log::error("Could not save mts file: " CORE_STRINGIFY(read));                                                  \
+		return false;                                                                                                  \
+	}
+
 bool MTSFormat::saveGroups(const scenegraph::SceneGraph &sceneGraph, const core::String &filename,
 						   const io::ArchivePtr &archive, const SaveContext &) {
-	return false;
+	scenegraph::SceneGraphNode *node = sceneGraph.firstModelNode();
+	if (node == nullptr) {
+		Log::error("No model node found in scene graph");
+		return false;
+	}
+
+	const voxel::RawVolume *volume = node->volume();
+	const voxel::Region &region = volume->region();
+	const palette::Palette &palette = node->palette();
+
+	core::ScopedPtr<io::SeekableWriteStream> stream(archive->writeStream(filename));
+	if (!stream) {
+		Log::error("Could not open file %s", filename.c_str());
+		return false;
+	}
+	wrapBool(stream->writeUInt32(FourCC('M', 'T', 'S', 'M')))
+	wrapBool(stream->writeUInt16BE(4))						   // version 4
+	wrapBool(stream->writeInt16BE(region.getWidthInVoxels()))  // size x
+	wrapBool(stream->writeInt16BE(region.getHeightInVoxels())) // size y
+	wrapBool(stream->writeInt16BE(region.getDepthInVoxels()))  // size z
+
+	for (int32_t y = 0; y < region.getHeightInVoxels(); ++y) {
+		wrapBool(stream->writeUInt8(0xFF))
+	}
+
+	uint16_t idmapcount = node->palette().colorCount();
+	wrapBool(stream->writeUInt16BE(idmapcount + 1))
+
+	palette::Palette mcpal;
+	mcpal.minecraft();
+
+	stream->writePascalStringUInt16BE("air");
+	for (int i = 0; i < palette.colorCount(); ++i) {
+		const core::RGBA rgba = palette.color(i);
+		const int palIdx = mcpal.getClosestMatch(rgba);
+		const core::String &name = findPaletteName(palIdx);
+		wrapBool(stream->writePascalStringUInt16BE(name))
+	}
+
+	io::ZipWriteStream zipStream(*stream);
+	for (int32_t x = 0; x < region.getWidthInVoxels(); ++x) {
+		for (int32_t y = 0; y < region.getHeightInVoxels(); ++y) {
+			for (int32_t z = 0; z < region.getDepthInVoxels(); ++z) {
+				const voxel::Voxel &v = volume->voxel(x, y, z);
+				wrapBool(zipStream.writeUInt16BE(v.getColor() + 1))
+			}
+		}
+	}
+	zipStream.flush();
+	return true;
 }
+
+#undef wrapBool
 
 } // namespace voxelformat
