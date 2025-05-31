ac240367acda143d2a06f2bab68aceadca8b333c
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-05-31 07:20:46 +0200

    VOXELUTIL: use for_parallel for mergeVolumes

--- src/modules/voxelutil/VolumeMerger.h
+++ src/modules/voxelutil/VolumeMerger.h
@@ -4,7 +4,9 @@
 
 #pragma once
 
+#include "app/Async.h"
 #include "core/collection/Buffer.h"
+#include "core/concurrent/Atomic.h"
 #include "voxel/RawVolume.h"
 #include "palette/Palette.h"
 #include "core/Trace.h"
@@ -31,35 +33,36 @@ template<typename MergeCondition = MergeSkipEmpty, class Volume1, class Volume2>
 int mergeVolumes(Volume1 *destination, const Volume2 *source, const voxel::Region &destReg,
 				 const voxel::Region &sourceReg, MergeCondition mergeCondition = MergeCondition()) {
 	core_trace_scoped(MergeRawVolumes);
-	int cnt = 0;
-	typename Volume2::Sampler sourceSampler(source);
-	typename Volume1::Sampler destSampler(destination);
-	const int relX = destReg.getLowerX();
-	sourceSampler.setPosition(sourceReg.getLowerCorner());
-	// TODO: FOR-PARALLEL
-	for (int32_t z = sourceReg.getLowerZ(); z <= sourceReg.getUpperZ(); ++z) {
-		const int destZ = destReg.getLowerZ() + z - sourceReg.getLowerZ();
-		typename Volume2::Sampler sourceSampler2 = sourceSampler;
-		for (int32_t y = sourceReg.getLowerY(); y <= sourceReg.getUpperY(); ++y) {
-			const int destY = destReg.getLowerY() + y - sourceReg.getLowerY();
-			typename Volume2::Sampler sourceSampler3 = sourceSampler2;
-			destSampler.setPosition(relX, destY, destZ);
-			for (int32_t x = sourceReg.getLowerX(); x <= sourceReg.getUpperX(); ++x) {
-				voxel::Voxel srcVoxel = sourceSampler3.voxel();
-				sourceSampler3.movePositiveX();
-				if (!mergeCondition(srcVoxel)) {
+	core::AtomicInt cnt = 0;
+	app::for_parallel(sourceReg.getLowerZ(), sourceReg.getUpperZ() + 1, [&source, &destination, sourceReg, destReg, &cnt, &mergeCondition] (int start, int end)	{
+		typename Volume2::Sampler sourceSampler(source);
+		typename Volume1::Sampler destSampler(destination);
+		const int relX = destReg.getLowerX();
+		sourceSampler.setPosition(sourceReg.getLowerX(), sourceReg.getLowerY(), start);
+		for (int32_t z = start; z < end; ++z) {
+			const int destZ = destReg.getLowerZ() + z - sourceReg.getLowerZ();
+			typename Volume2::Sampler sourceSampler2 = sourceSampler;
+			for (int32_t y = sourceReg.getLowerY(); y <= sourceReg.getUpperY(); ++y) {
+				const int destY = destReg.getLowerY() + y - sourceReg.getLowerY();
+				typename Volume2::Sampler sourceSampler3 = sourceSampler2;
+				destSampler.setPosition(relX, destY, destZ);
+				for (int32_t x = sourceReg.getLowerX(); x <= sourceReg.getUpperX(); ++x) {
+					voxel::Voxel srcVoxel = sourceSampler3.voxel();
+					sourceSampler3.movePositiveX();
+					if (!mergeCondition(srcVoxel)) {
+						destSampler.movePositiveX();
+						continue;
+					}
+					if (destSampler.setVoxel(srcVoxel)) {
+						++cnt;
+					}
 					destSampler.movePositiveX();
-					continue;
 				}
-				if (destSampler.setVoxel(srcVoxel)) {
-					++cnt;
-				}
-				destSampler.movePositiveX();
+				sourceSampler2.movePositiveY();
 			}
-			sourceSampler2.movePositiveY();
+			sourceSampler.movePositiveZ();
 		}
-		sourceSampler.movePositiveZ();
-	}
+	});
 	return cnt;
 }
 
