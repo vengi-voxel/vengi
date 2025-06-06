852da3636c520f447dd397179e3ae2cf5087d9c3
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-06-06 17:37:50 +0200

    APP: don't schedule in the threadpool if there is only one worker

--- src/modules/app/Async.cpp
+++ src/modules/app/Async.cpp
@@ -13,16 +13,15 @@ void for_parallel(int start, int end, const std::function<void(int, int)> &taskL
 	if (start >= end)
 		return;
 
+	core::ThreadPool &threadPool = app::App::getInstance()->threadPool();
 	// if we are already running in a thread - we might be running in the thread pool - and as the threads in this pool are limited,
 	// we might wait endlessly because these enqueue calls would not get executed.
-	if (end - start == 1 || !app::App::getInstance()->isMainThread(core::getCurrentThreadId())) {
+	if (end - start == 1 || threadPool.size() == 1u || !app::App::getInstance()->isMainThread(core::getCurrentThreadId())) {
 		// if we are not on the main thread, we can just call the taskLambda directly
 		taskLambda(start, end);
-		Log::debug("Do not run async tasks in the thread pool - this can lead to deadlocks");
 		return;
 	}
 
-	core::ThreadPool &threadPool = app::App::getInstance()->threadPool();
 	const int threadCnt = core_max(2u, threadPool.size());
 	const int chunkSize = core_max((end - start + threadCnt - 1) / threadCnt, 1);
 	core::DynamicArray<std::future<void>> futures;
