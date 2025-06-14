44c9d1fb0d7a7b3e5c4cb5a3faf5f9c128afcb3b
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-06-14 14:40:09 +0200

    PALETTE: added benchmark

--- src/modules/palette/benchmarks/PaletteBenchmark.cpp
+++ src/modules/palette/benchmarks/PaletteBenchmark.cpp
@@ -4,16 +4,19 @@
 
 #include "app/benchmark/AbstractBenchmark.h"
 #include "palette/Palette.h"
+#include "palette/PaletteLookup.h"
 
 class PaletteBenchmark : public app::AbstractBenchmark {
 protected:
 	using Super = app::AbstractBenchmark;
 	palette::Palette _palette;
+	palette::PaletteLookup _paletteLookup;
 
 public:
 	void SetUp(::benchmark::State &state) override {
 		Super::SetUp(state);
 		_palette.nippon();
+		_paletteLookup = palette::PaletteLookup(_palette);
 	}
 };
 
@@ -23,5 +26,14 @@ BENCHMARK_DEFINE_F(PaletteBenchmark, findReplacement)(benchmark::State &state) {
 	}
 }
 
+BENCHMARK_DEFINE_F(PaletteBenchmark, paletteLookup)(benchmark::State &state) {
+	int i = 0;
+	for (auto _ : state) {
+		benchmark::DoNotOptimize(_paletteLookup.findClosestIndex(core::RGBA((255 + i) % 255, (124 + 3 * i) % 255, (34 * i) % 255)));
+		++i;
+	}
+}
+
 BENCHMARK_REGISTER_F(PaletteBenchmark, findReplacement);
+BENCHMARK_REGISTER_F(PaletteBenchmark, paletteLookup);
 BENCHMARK_MAIN();
