c551d095039adf58c39cfdb36c8df28561752f3a
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-07-09 22:05:39 +0200

    VOXELUTIL: added test messages

--- src/modules/voxelutil/tests/PickingTest.cpp
+++ src/modules/voxelutil/tests/PickingTest.cpp
@@ -2,23 +2,28 @@
  * @file
  */
 
-#include "app/tests/AbstractTest.h"
-#include "voxel/RawVolume.h"
 #include "voxelutil/Picking.h"
+#include "app/tests/AbstractTest.h"
 #include "core/GLM.h"
+#include "voxel/RawVolume.h"
 
 namespace voxelutil {
 
-class PickingTest: public app::AbstractTest {
-};
+class PickingTest : public app::AbstractTest {};
 
 TEST_F(PickingTest, testPicking) {
 	voxel::RawVolume v(voxel::Region(glm::ivec3(0), glm::ivec3(10)));
-	v.setVoxel(glm::ivec3(0), voxel::createVoxel(voxel::VoxelType::Generic, 0));
-	const PickResult& result = pickVoxel(&v, glm::vec3(0.0f, 3.0f, 0.0f), glm::down() * 100.0f, voxel::Voxel());
-	ASSERT_TRUE(result.didHit);
-	ASSERT_EQ(glm::ivec3(0), result.hitVoxel);
-	ASSERT_EQ(glm::ivec3(0, 1, 0), result.previousPosition);
+	ASSERT_TRUE(v.setVoxel(glm::ivec3(0), voxel::createVoxel(voxel::VoxelType::Generic, 0)));
+	const glm::vec3 start(0.0f, 3.0f, 0.0f);
+	const glm::vec3 direction = glm::down() * 100.0f;
+	const PickResult &result = pickVoxel(&v, start, direction, voxel::Voxel());
+	ASSERT_TRUE(result.didHit) << "Expected to hit the voxel at (0, 0, 0)";
+	ASSERT_EQ(glm::ivec3(0), result.hitVoxel)
+		<< "Expected to hit the voxel at (0, 0, 0) - but got " << result.hitVoxel.x << ", " << result.hitVoxel.y << ", "
+		<< result.hitVoxel.z;
+	ASSERT_EQ(glm::ivec3(0, 1, 0), result.previousPosition)
+		<< "Expected to get a previous position of (0, 1, 0) - but got " << result.previousPosition.x << ", "
+		<< result.previousPosition.y << ", " << result.previousPosition.z;
 }
 
-}
+} // namespace voxelutil
