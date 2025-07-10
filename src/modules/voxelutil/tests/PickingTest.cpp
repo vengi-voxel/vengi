5d3838e8165a4f164d47a5804bab8cdaeb7924a8
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-07-10 07:43:41 +0200

    VOXELUTIL: moved unused code into test code

--- src/modules/voxelutil/tests/PickingTest.cpp
+++ src/modules/voxelutil/tests/PickingTest.cpp
@@ -10,7 +10,52 @@
 
 namespace voxelutil {
 
-class PickingTest : public app::AbstractTest {};
+class PickingTest : public app::AbstractTest {
+protected:
+	/**
+	 * @brief This is just an implementation class for the pickVoxel function
+	 *
+	 * It makes note of the sort of empty voxel you're looking for in the constructor.
+	 *
+	 * Each time the operator() is called:
+	 *  * if it's hit a voxel it sets up the result and returns false
+	 *  * otherwise it preps the result for the next iteration and returns true
+	 */
+	template<typename VolumeType>
+	class RaycastPickingFunctor {
+	public:
+		RaycastPickingFunctor(const voxel::Voxel &emptyVoxelExample)
+			: _emptyVoxelExample(emptyVoxelExample), _result() {
+		}
+
+		bool operator()(typename VolumeType::Sampler &sampler) {
+			if (sampler.voxel() != _emptyVoxelExample) {
+				// If we've hit something
+				_result.didHit = true;
+				_result.hitVoxel = sampler.position();
+				return false;
+			}
+
+			if (sampler.currentPositionValid()) {
+				_result.validPreviousPosition = true;
+				_result.previousPosition = sampler.position();
+			}
+			return true;
+		}
+		const voxel::Voxel &_emptyVoxelExample;
+		PickResult _result;
+	};
+
+	/** Pick the first solid voxel along a vector */
+	template<typename VolumeType>
+	PickResult pickVoxel(const VolumeType *volData, const glm::vec3 &v3dStart, const glm::vec3 &v3dDirectionAndLength,
+						 const voxel::Voxel &emptyVoxelExample) {
+		core_trace_scoped(pickVoxel);
+		RaycastPickingFunctor<VolumeType> functor(emptyVoxelExample);
+		raycastWithDirection(volData, v3dStart, v3dDirectionAndLength, functor);
+		return functor._result;
+	}
+};
 
 TEST_F(PickingTest, testPicking) {
 	voxel::RawVolume v(voxel::Region(glm::ivec3(0), glm::ivec3(10)));
