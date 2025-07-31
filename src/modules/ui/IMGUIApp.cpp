6f9a7d88ac8b0aa2debe1652a1b3858d1b3c285f
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-07-31 19:41:08 +0200

    UI: use the default style settings

--- src/modules/ui/IMGUIApp.cpp
+++ src/modules/ui/IMGUIApp.cpp
@@ -386,28 +386,6 @@ void IMGUIApp::setColorTheme() {
 		break;
 	}
 
-	style.PopupRounding = 3;
-
-	style.WindowPadding = ImVec2(4, 4);
-	style.FramePadding = ImVec2(3, 3);
-	style.ItemSpacing = ImVec2(3, 2);
-	style.ItemInnerSpacing = ImVec2(1, 1);
-	style.ScrollbarSize = 10;
-
-	style.WindowBorderSize = 1;
-	style.ChildBorderSize = 1;
-	style.PopupBorderSize = 1;
-	style.FrameBorderSize = 1;
-
-	style.WindowRounding = 2;
-	style.ChildRounding = 2;
-	style.FrameRounding = 2;
-	style.ScrollbarRounding = 2;
-	style.GrabRounding = 2;
-
-	style.TabBorderSize = 1;
-	style.TabRounding = 0;
-
 	if (ImGui::GetIO().ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
 		style.WindowRounding = 0.0f;
 		style.Colors[ImGuiCol_WindowBg].w = 1.0f;
