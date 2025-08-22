f0cfd925e40b64e77f32a5ab528fdf18bf0d1056
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-08-22 16:15:11 +0200

    VOXELFORMAT: MDL: extended tests

--- src/modules/voxelformat/tests/MDLFormatTest.cpp
+++ src/modules/voxelformat/tests/MDLFormatTest.cpp
@@ -4,6 +4,9 @@
 
 #include "AbstractFormatTest.h"
 #include "scenegraph/SceneGraph.h"
+#include "scenegraph/SceneGraphNode.h"
+#include "voxel/RawVolume.h"
+#include "voxelformat/tests/TestHelper.h"
 
 namespace voxelformat {
 
@@ -14,6 +17,19 @@ TEST_F(MDLFormatTest, testVoxelize) {
 	// or from https://github.com/QW-Group/ezquake-media/blob/master/game/progs/flame0.mdl
 	scenegraph::SceneGraph sceneGraph;
 	testLoad(sceneGraph, "flame0.mdl", 1);
+	if (IsSkipped()) {
+		return;
+	}
+	scenegraph::SceneGraphNode *node = sceneGraph.firstModelNode();
+	ASSERT_NE(node, nullptr);
+	EXPECT_EQ(node->name(), "flame1");
+	const voxel::RawVolume *v = node->volume();
+	ASSERT_NE(v, nullptr);
+	EXPECT_EQ(v->region().getDimensionsInVoxels(), glm::ivec3(8, 18, 8));
+	EXPECT_EQ(178, voxel::countVoxels(*v));
+	EXPECT_GT(node->palette().colorCount(), 50);
+	EXPECT_LT(node->palette().colorCount(), 250);
+	// EXPECT_EQ(core::RGBA(0x25, 0x1b, 0x0d), node->palette().color(0));
 }
 
 } // namespace voxelformat
