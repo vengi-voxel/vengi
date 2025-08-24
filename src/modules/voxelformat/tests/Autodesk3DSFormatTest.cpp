ccb541a406976d1a2b0e052aa17f1b504ea154b3
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-08-24 23:15:31 +0200

    VOXELFORMAT: 3DS: enable and extended unittest
    gun.3ds is released under WTFPL: https://opengameart.org/content/gununtextured

--- src/modules/voxelformat/tests/Autodesk3DSFormatTest.cpp
+++ src/modules/voxelformat/tests/Autodesk3DSFormatTest.cpp
@@ -3,13 +3,23 @@
  */
 
 #include "AbstractFormatTest.h"
+#include "scenegraph/SceneGraphNode.h"
 
 namespace voxelformat {
 
 class Autodesk3DSFormatTest : public AbstractFormatTest {};
 
 TEST_F(Autodesk3DSFormatTest, testLoad) {
-	testLoad("abrams3.3ds", 3);
+	scenegraph::SceneGraph sceneGraph;
+	testLoad(sceneGraph, "gun.3ds", 2);
+	scenegraph::SceneGraphNode *node = sceneGraph.firstModelNode();
+	ASSERT_NE(nullptr, node);
+	EXPECT_EQ("cube5", node->name());
+	EXPECT_NE(nullptr, node->volume());
+	EXPECT_EQ(4, node->volume()->region().getWidthInVoxels());
+	EXPECT_EQ(5, node->volume()->region().getHeightInVoxels());
+	EXPECT_EQ(4, node->volume()->region().getDepthInVoxels());
+	EXPECT_EQ(1, node->palette().colorCount());
 }
 
 } // namespace voxelformat
