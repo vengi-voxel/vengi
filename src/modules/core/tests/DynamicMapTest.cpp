b9d645a27c52265250a30bd84c6b38a293f5cce0
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-08-24 21:10:00 +0200

    CORE: fixed DynamicMapTest to use the DynamicMap, not the Map

--- src/modules/core/tests/DynamicMapTest.cpp
+++ src/modules/core/tests/DynamicMapTest.cpp
@@ -3,13 +3,13 @@
  */
 
 #include <gtest/gtest.h>
-#include "core/collection/StringMap.h"
+#include "core/collection/DynamicMap.h"
 #include "core/SharedPtr.h"
 
 namespace core {
 
 TEST(DynamicMapTest, testPutGet) {
-	core::Map<int64_t, int64_t, 11, std::hash<int64_t>> map;
+	core::DynamicMap<int64_t, int64_t, 11, std::hash<int64_t>> map;
 	map.put(1, 1);
 	map.put(1, 2);
 	map.put(2, 1);
@@ -33,7 +33,7 @@ TEST(DynamicMapTest, testPutGet) {
 }
 
 TEST(DynamicMapTest, testCollision) {
-	core::Map<int64_t, int64_t, 11, std::hash<int64_t>> map;
+	core::DynamicMap<int64_t, int64_t, 11, std::hash<int64_t>> map;
 	for (int64_t i = 0; i < 128; ++i) {
 		map.put(i, i);
 	}
@@ -45,7 +45,7 @@ TEST(DynamicMapTest, testCollision) {
 }
 
 TEST(DynamicMapTest, testClear) {
-	core::Map<int64_t, int64_t, 11, std::hash<int64_t>> map;
+	core::DynamicMap<int64_t, int64_t, 11, std::hash<int64_t>> map;
 	for (int64_t i = 0; i < 16; ++i) {
 		map.put(i, i);
 	}
@@ -57,7 +57,7 @@ TEST(DynamicMapTest, testClear) {
 }
 
 TEST(DynamicMapTest, testFind) {
-	core::Map<int64_t, int64_t, 11, std::hash<int64_t>> map;
+	core::DynamicMap<int64_t, int64_t, 11, std::hash<int64_t>> map;
 	for (int64_t i = 0; i < 1024; i += 2) {
 		map.put(i, i);
 	}
@@ -70,7 +70,7 @@ TEST(DynamicMapTest, testFind) {
 }
 
 TEST(DynamicMapTest, testIterator) {
-	core::Map<int64_t, int64_t, 11, std::hash<int64_t>> map;
+	core::DynamicMap<int64_t, int64_t, 11, std::hash<int64_t>> map;
 	EXPECT_EQ(map.begin(), map.end());
 	EXPECT_EQ(map.end(), map.find(42));
 	map.put(1, 1);
@@ -80,7 +80,7 @@ TEST(DynamicMapTest, testIterator) {
 
 TEST(DynamicMapTest, testIterate) {
 	// leave empty buckets
-	core::Map<int64_t, int64_t, 11, std::hash<int64_t>> map;
+	core::DynamicMap<int64_t, int64_t, 11, std::hash<int64_t>> map;
 	for (int64_t i = 0; i < 32; i += 2) {
 		map.put(i, i);
 	}
@@ -103,7 +103,7 @@ TEST(DynamicMapTest, testIterate) {
 }
 
 TEST(DynamicMapTest, testIterateRangeBased) {
-	core::Map<int64_t, int64_t, 11, std::hash<int64_t>> map;
+	core::DynamicMap<int64_t, int64_t, 11, std::hash<int64_t>> map;
 	for (int64_t i = 0; i < 32; i += 2) {
 		map.put(i, i);
 	}
@@ -114,47 +114,6 @@ TEST(DynamicMapTest, testIterateRangeBased) {
 		++cnt;
 	}
 	EXPECT_EQ(16, cnt);
-}
-
-TEST(DynamicMapTest, testStringSharedPtr) {
-	core::StringMap<core::SharedPtr<core::String>, 4> map;
-	auto foobar = core::SharedPtr<core::String>::create("foobar");
-	map.put("foobar", foobar);
-	map.put("barfoo", core::SharedPtr<core::String>::create("barfoo"));
-	map.put("foobar", core::SharedPtr<core::String>::create("barfoo"));
-	for (auto iter = map.begin(); iter != map.end(); ++iter) {
-	}
-	map.clear();
-	foobar = core::SharedPtr<core::String>();
-}
-
-TEST(DynamicMapTest, testCopy) {
-	core::StringMap<core::SharedPtr<core::String>> map;
-	map.put("foobar", core::SharedPtr<core::String>::create("barfoo"));
-	auto map2 = map;
-	map2.clear();
-}
-
-TEST(DynamicMapTest, testErase) {
-	core::StringMap<core::SharedPtr<core::String>> map;
-	map.put("foobar", core::SharedPtr<core::String>::create("barfoo"));
-	EXPECT_EQ(1u, map.size());
-	auto iter = map.find("foobar");
-	EXPECT_NE(iter, map.end());
-	map.erase(iter);
-	EXPECT_EQ(0u, map.size());
-}
-
-TEST(DynamicMapTest, testAssign) {
-	core::StringMap<core::SharedPtr<core::String>> map;
-	map.put("foobar", core::SharedPtr<core::String>::create("barfoo"));
-	core::StringMap<core::SharedPtr<core::String>> map2;
-	map2 = map;
-	EXPECT_EQ(1u, map.size());
-	EXPECT_EQ(1u, map2.size());
-	map2.clear();
-	EXPECT_EQ(1u, map.size());
-	EXPECT_EQ(0u, map2.size());
 }
 
 }
