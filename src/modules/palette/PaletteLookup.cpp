07bddfc3ee669279e25f48843fad8cb610d2e842
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-08-25 22:48:58 +0200

    PALETTE: write cache via atomics to allow using one instance in for_parallel szenarios

---
+++ src/modules/palette/PaletteLookup.cpp
@@ -0,0 +1,65 @@
+/**
+ * @file
+ */
+
+#include "PaletteLookup.h"
+#include "core/Color.h"
+#include "palette/Palette.h"
+#if defined(_MSC_VER)
+#include <intrin.h>
+#endif
+
+namespace palette {
+
+namespace priv {
+
+static constexpr int Q_BITS = 5;											 // 5 bits per channel
+static constexpr int Q_LEVELS = 1 << Q_BITS;								 // 32
+static constexpr int CACHE_SIZE = Q_LEVELS * Q_LEVELS * Q_LEVELS * Q_LEVELS; // 32^4 = 1048576
+
+static inline uint16_t quantizeChannel(uint8_t value) {
+	return value >> (8 - Q_BITS); // shift to keep top Q_BITS
+}
+
+static inline size_t computeIndex(core::RGBA rgba) {
+	uint16_t r = quantizeChannel(rgba.r);
+	uint16_t g = quantizeChannel(rgba.g);
+	uint16_t b = quantizeChannel(rgba.b);
+	uint16_t a = quantizeChannel(rgba.a);
+	return ((r << (3 * Q_BITS)) | (g << (2 * Q_BITS)) | (b << Q_BITS) | a);
+}
+
+} // namespace priv
+
+PaletteLookup::PaletteLookup(const palette::Palette &palette)
+	: _palette(palette), _cache(priv::CACHE_SIZE, PaletteColorNotFound) {
+}
+
+uint8_t PaletteLookup::findClosestIndex(const glm::vec4 &color) {
+	return findClosestIndex(core::Color::getRGBA(color));
+}
+
+uint8_t PaletteLookup::findClosestIndex(core::RGBA rgba) {
+	size_t idx = priv::computeIndex(rgba);
+
+#if defined(_MSC_VER)
+	uint16_t oldValue = _cache[idx];
+	if (oldValue == (uint16_t)PaletteColorNotFound) {
+		core_assert_always(_palette.colorCount() > 0);
+		uint16_t newValue = _palette.getClosestMatch(rgba);
+		InterlockedExchange16(reinterpret_cast<volatile SHORT *>(&_cache[idx]), newValue);
+	}
+#elif defined(__GNUC__) || defined(__clang__)
+	uint16_t oldValue = __atomic_load_n(&_cache[idx], __ATOMIC_RELAXED);
+	if (oldValue == (uint16_t)PaletteColorNotFound) {
+		core_assert_always(_palette.colorCount() > 0);
+		uint16_t newValue = _palette.getClosestMatch(rgba);
+		__atomic_exchange_n(&_cache[idx], newValue, __ATOMIC_SEQ_CST);
+	}
+#else
+#error "Atomic operations not implemented for this compiler"
+#endif
+	return _cache[idx];
+}
+
+} // namespace palette
--- src/modules/palette/PaletteLookup.h
+++ src/modules/palette/PaletteLookup.h
@@ -4,12 +4,13 @@
 
 #pragma once
 
-#include "core/Assert.h"
-#include "core/Color.h"
+#include "core/RGBA.h"
 #include "core/collection/Buffer.h"
-#include "palette/Palette.h"
+#include <glm/fwd.hpp>
 
 namespace palette {
+
+class Palette;
 
 /**
  * @brief A lookup table for palette colors, allowing fast retrieval of the closest color index
@@ -22,30 +23,11 @@ namespace palette {
  */
 class PaletteLookup {
 private:
-	static constexpr int Q_BITS = 5;											 // 5 bits per channel
-	static constexpr int Q_LEVELS = 1 << Q_BITS;								 // 32
-	static constexpr int CACHE_SIZE = Q_LEVELS * Q_LEVELS * Q_LEVELS * Q_LEVELS; // 32^4 = 1048576
-
 	const palette::Palette &_palette;
 	core::Buffer<uint16_t> _cache; // Fixed-size lookup
 
-	static inline uint16_t quantizeChannel(uint8_t value) {
-		return value >> (8 - Q_BITS); // shift to keep top Q_BITS
-	}
-
-	static inline size_t computeIndex(core::RGBA rgba) {
-		uint16_t r = quantizeChannel(rgba.r);
-		uint16_t g = quantizeChannel(rgba.g);
-		uint16_t b = quantizeChannel(rgba.b);
-		uint16_t a = quantizeChannel(rgba.a);
-		return ((r << (3 * Q_BITS)) | (g << (2 * Q_BITS)) | (b << Q_BITS) | a);
-	}
-
 public:
-	PaletteLookup(const palette::Palette &palette) : _palette(palette) {
-		_cache.resize(CACHE_SIZE);
-		_cache.fill(PaletteColorNotFound);
-	}
+	PaletteLookup(const palette::Palette &palette);
 
 	inline const palette::Palette &palette() const {
 		return _palette;
@@ -56,23 +38,13 @@ public:
 	 * @param color Normalized color value [0.0-1.0]
 	 * @sa core::Color::getClosestMatch()
 	 */
-	inline uint8_t findClosestIndex(const glm::vec4 &color) {
-		return findClosestIndex(core::Color::getRGBA(color));
-	}
+	uint8_t findClosestIndex(const glm::vec4 &color);
 
 	/**
 	 * @brief Find the closed index in the currently in-use palette for the given color
 	 * @sa core::Color::getClosestMatch()
 	 */
-	uint8_t findClosestIndex(core::RGBA rgba) {
-		size_t idx = computeIndex(rgba);
-		uint16_t &cached = _cache[idx];
-		if (cached == (uint16_t)PaletteColorNotFound) {
-			core_assert_always(_palette.colorCount() > 0);
-			cached = _palette.getClosestMatch(rgba);
-		}
-		return cached;
-	}
+	uint8_t findClosestIndex(core::RGBA rgba);
 };
 
 } // namespace palette
