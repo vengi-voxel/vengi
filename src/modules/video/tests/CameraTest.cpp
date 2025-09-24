ae0a957af88cf40c35af08e2e07a23699814871e
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-09-24 20:10:35 +0200

    VIDEO: extended camera tests

--- src/modules/video/tests/CameraTest.cpp
+++ src/modules/video/tests/CameraTest.cpp
@@ -6,9 +6,7 @@
 #include "core/GLM.h"
 #include "core/StringUtil.h"
 #include "math/Frustum.h"
-#include "math/AABB.h"
 #include "video/Camera.h"
-#include "math/Ray.h"
 
 namespace video {
 
@@ -96,6 +94,57 @@ TEST_F(CameraTest, testCameraFrustumCullingPerspective) {
 	EXPECT_EQ(math::FrustumResult::Inside, frustum.test(glm::vec3(0.0, 0.0, 0.0)));
 	EXPECT_EQ(math::FrustumResult::Outside, frustum.test(glm::vec3(0.0, 1.0, 0.0)));
 	EXPECT_EQ(math::FrustumResult::Intersect, frustum.test(glm::vec3(-1.0, -1.0, -1.0), glm::vec3(0.5, 0.5, 0.5)));
+}
+
+TEST_F(CameraTest, testMoveAndPan) {
+	Camera camera = setup(glm::vec2(1024, 768), glm::vec3(0.0, 1.0, 0.0));
+	const glm::vec3 before = camera.worldPosition();
+
+	// moving along +z in camera-space should change world position
+	const bool moved = camera.move(glm::vec3(0.0f, 0.0f, 1.0f));
+	EXPECT_TRUE(moved);
+	const glm::vec3 after = camera.worldPosition();
+	// For the default look-at (looking down) moving +z moves the camera up in world Y
+	EXPECT_NE(before, after);
+
+	// panning should change panOffset
+	const glm::vec3 panBefore = camera.panOffset();
+	camera.pan(10, 20);
+	const glm::vec3 panAfter = camera.panOffset();
+	EXPECT_NE(panBefore, panAfter);
+}
+
+TEST_F(CameraTest, testSlerp) {
+	Camera camera = setup();
+	const glm::quat startQuat = camera.quaternion();
+	// rotate target by 90 degrees around Y
+	const glm::quat targetQuat = glm::angleAxis(glm::half_pi<float>(), glm::up());
+	camera.slerp(targetQuat, 0.5f);
+	const glm::quat midQuat = camera.quaternion();
+	// Expect quaternion to have changed but not equal to target yet
+	EXPECT_FALSE(glm::all(glm::epsilonEqual(startQuat, midQuat, 0.0001f)));
+	EXPECT_FALSE(glm::all(glm::epsilonEqual(targetQuat, midQuat, 0.0001f)));
+}
+
+TEST_F(CameraTest, testLerpBetweenCameras) {
+	Camera a = setup(glm::vec2(200, 200), glm::vec3(0.0f, 10.0f, 0.0));
+	Camera b = setup(glm::vec2(200, 200), glm::vec3(10.0f, 10.0f, 0.0));
+	// ensure target is clean before lerp
+	b.update(0.0);
+	const glm::vec3 aBefore = a.worldPosition();
+	const glm::vec3 bPos = b.worldPosition();
+	a.lerp(b);
+	// progress half-way
+	a.update(0.5);
+	const glm::vec3 aMid = a.worldPosition();
+	EXPECT_FALSE(glm::all(glm::epsilonEqual(aBefore, aMid, 0.0001f)));
+	// finish lerp
+	a.update(1.0);
+	const glm::vec3 aAfter = a.worldPosition();
+	// after finishing lerp the world position should be (approximately) equal to target
+	EXPECT_NEAR(bPos.x, aAfter.x, 0.001f);
+	EXPECT_NEAR(bPos.y, aAfter.y, 0.001f);
+	EXPECT_NEAR(bPos.z, aAfter.z, 0.001f);
 }
 
 TEST_F(CameraTest, DISABLED_testCameraFrustumCullingOrthogonal) {
