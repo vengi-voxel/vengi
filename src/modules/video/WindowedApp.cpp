585b67711b4acedb16d80af92deee15de46294a2
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-09-28 19:56:47 +0200

    VIDEO: bump the shadow map size to 4096

--- src/modules/video/WindowedApp.cpp
+++ src/modules/video/WindowedApp.cpp
@@ -526,7 +526,7 @@ app::AppState WindowedApp::onConstruct() {
 	core::Var::get(cfg::RenderCheckerBoard, "false", core::CV_SHADER, _("Activate checkerboard rendering"), core::Var::boolValidator);
 	core::Var::get(cfg::ClientBloom, "true", _("Activate bloom post processing"), core::Var::boolValidator);
 	core::Var::get(cfg::ClientDebugShadow, "false", core::CV_SHADER, _("Activate shadow debug rendering"), core::Var::boolValidator);
-	core::Var::get(cfg::ClientShadowMapSize, "1024");
+	core::Var::get(cfg::ClientShadowMapSize, "4096");
 	core::Var::get(cfg::ClientDebugShadowMapCascade, "false", core::CV_SHADER, _("Activate cascade shadow map debug rendering"), core::Var::boolValidator);
 	core::Var::get(cfg::ClientGamma, "1.0", core::CV_SHADER, _("Gamma correction"));
 	core::Var::get(cfg::ClientWindowDisplay, 0);
