6d5273233d4ff5eba5aa0ed8a6132ff4fe2edbfb
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-10-04 17:09:29 +0200

    VOXCONVERT: expose the cvars to the web version, too

--- src/tools/voxconvert/Emscripten.cpp
+++ src/tools/voxconvert/Emscripten.cpp
@@ -26,8 +26,27 @@ extern "C" EMSCRIPTEN_KEEPALIVE void convert_file(char *input, char *output) {
 	char appnameArg[] = "voxconvert";
 	char inputArg[] = "--input";
 	char outputArg[] = "--output";
-	char *argv[] = {appnameArg, inputArg, input, outputArg, output};
+	char forceArg[] = "--force";
+	char *argv[] = {appnameArg, inputArg, input, outputArg, output, forceArg};
 	app.startMainLoop(lengthof(argv), argv);
+}
+
+extern "C" EMSCRIPTEN_KEEPALIVE const char *get_config_json() {
+	static core::String formats;
+	const io::FilesystemPtr &filesystem = core::make_shared<io::Filesystem>();
+	const core::TimeProviderPtr &timeProvider = core::make_shared<core::TimeProvider>();
+	VoxConvert app(filesystem, timeProvider);
+	char appnameArg[] = "voxconvert";
+	char versionArg[] = "--version";
+	char *argv[] = {appnameArg, versionArg};
+	app.setArgs(lengthof(argv), argv);
+	app.onConstruct();
+	io::BufferedReadWriteStream stream;
+	app::App::writeConfigJson(stream);
+	app.onCleanup();
+	app.onDestroy();
+	formats = core::String((const char *)stream.getBuffer(), stream.size());
+	return formats.c_str();
 }
 
 extern "C" EMSCRIPTEN_KEEPALIVE const char *get_supported_formats_json() {
