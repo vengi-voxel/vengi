36493c6638e6e580871fb0beb17145ab8a6dac28
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-10-24 19:56:20 +0200

    VOXELUTIL: added test for hollow() function

---
+++ src/modules/voxelutil/tests/HollowTest.cpp
@@ -0,0 +1,71 @@
+/**
+ * @file
+ */
+
+#include "voxelutil/Hollow.h"
+#include "app/tests/AbstractTest.h"
+#include "voxel/RawVolume.h"
+#include "voxelutil/VolumeVisitor.h"
+
+namespace voxelutil {
+
+class HollowTest : public app::AbstractTest {};
+
+TEST_F(HollowTest, testHollow1RemoveCenter) {
+	voxel::Region region(0, 2);
+	voxel::RawVolume volume(region);
+	const voxel::Voxel solid = voxel::createVoxel(voxel::VoxelType::Generic, 1);
+	// create a solid cube
+	for (int z = region.getLowerZ(); z <= region.getUpperZ(); ++z) {
+		for (int y = region.getLowerY(); y <= region.getUpperY(); ++y) {
+			for (int x = region.getLowerX(); x <= region.getUpperX(); ++x) {
+				volume.setVoxel(x, y, z, solid);
+			}
+		}
+	}
+	voxelutil::hollow(volume);
+	const int solidVoxels = visitVolumeParallel(volume, EmptyVisitor(), SkipEmpty());
+	const int cubeVoxels = region.voxels();
+	EXPECT_LT(solidVoxels, cubeVoxels);
+	EXPECT_EQ(cubeVoxels - 1, solidVoxels);
+}
+
+TEST_F(HollowTest, DISABLED_testHollow4RemoveCenter) {
+	voxel::Region region(0, 3);
+	voxel::RawVolume volume(region);
+	const voxel::Voxel solid = voxel::createVoxel(voxel::VoxelType::Generic, 1);
+	// create a solid cube
+	for (int z = region.getLowerZ(); z <= region.getUpperZ(); ++z) {
+		for (int y = region.getLowerY(); y <= region.getUpperY(); ++y) {
+			for (int x = region.getLowerX(); x <= region.getUpperX(); ++x) {
+				volume.setVoxel(x, y, z, solid);
+			}
+		}
+	}
+	voxelutil::hollow(volume);
+	const int solidVoxels = visitVolumeParallel(volume, EmptyVisitor(), SkipEmpty());
+	const int cubeVoxels = region.voxels();
+	EXPECT_LT(solidVoxels, cubeVoxels);
+	EXPECT_EQ(cubeVoxels - 8, solidVoxels);
+}
+
+TEST_F(HollowTest, DISABLED_testHollow27RemoveCenter) {
+	voxel::Region region(0, 4);
+	voxel::RawVolume volume(region);
+	const voxel::Voxel solid = voxel::createVoxel(voxel::VoxelType::Generic, 1);
+	// create a solid cube
+	for (int z = region.getLowerZ(); z <= region.getUpperZ(); ++z) {
+		for (int y = region.getLowerY(); y <= region.getUpperY(); ++y) {
+			for (int x = region.getLowerX(); x <= region.getUpperX(); ++x) {
+				volume.setVoxel(x, y, z, solid);
+			}
+		}
+	}
+	voxelutil::hollow(volume);
+	const int solidVoxels = visitVolumeParallel(volume, EmptyVisitor(), SkipEmpty());
+	const int cubeVoxels = region.voxels();
+	EXPECT_LT(solidVoxels, cubeVoxels);
+	EXPECT_EQ(cubeVoxels - 27, solidVoxels);
+}
+
+} // namespace voxelutil
