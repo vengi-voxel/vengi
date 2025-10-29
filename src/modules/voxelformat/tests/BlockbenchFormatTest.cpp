7db30925e5a076423c3a144a8f5469cb373b5836
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-10-29 18:03:37 +0100

    VOXELFORMAT: BLOCKBENCH: extended tests

--- src/modules/voxelformat/tests/BlockbenchFormatTest.cpp
+++ src/modules/voxelformat/tests/BlockbenchFormatTest.cpp
@@ -11,53 +11,92 @@
 
 namespace voxelformat {
 
-class BlockbenchFormatTest : public AbstractFormatTest {};
+class BlockbenchFormatTest : public AbstractFormatTest {
+protected:
+	// Loading the same file in different versions of Blockbench should yield the same results
+	// TODO: for gltf/glb some stuff is different in the hierarchy - need to investigate further
+	void test_loy_s_goodies_female_template(scenegraph::SceneGraph &sceneGraph, bool isbbmodel) {
+		// Test hierarchy structure
+		scenegraph::SceneGraphNode *hand_right = sceneGraph.findNodeByName("hand_right");
+		ASSERT_NE(hand_right, nullptr);
+		EXPECT_EQ(hand_right->type(), scenegraph::SceneGraphNodeType::Group);
+		ASSERT_GE(hand_right->children().size(), 3u);
+
+		// Verify children of hand_right
+		scenegraph::SceneGraphNode &firstCube = sceneGraph.node(hand_right->children()[0]);
+		EXPECT_EQ(firstCube.name(), "cube");
+		scenegraph::SceneGraphNode &secondCube = sceneGraph.node(hand_right->children()[1]);
+		EXPECT_EQ(secondCube.name(), "cube");
+		scenegraph::SceneGraphNode &fingers_right = sceneGraph.node(hand_right->children()[2]);
+		EXPECT_EQ(fingers_right.name(), "fingers_right");
+		EXPECT_EQ(fingers_right.type(), scenegraph::SceneGraphNodeType::Group);
+
+		// Test first cube dimensions
+		// In bbmodel: from=[12, 20.9, -1], to=[13, 22.9, 1], origin=[8, 21.9, 0]
+		EXPECT_EQ(firstCube.type(), scenegraph::SceneGraphNodeType::Model);
+		const voxel::Region &firstRegion = firstCube.region();
+		const glm::ivec3 &firstDim = firstRegion.getDimensionsInVoxels();
+		EXPECT_EQ(firstDim.x, 1);
+		EXPECT_EQ(firstDim.y, 2);
+		EXPECT_EQ(firstDim.z, 2);
+
+		// Test pivot calculation
+		// Currently: pivot = (origin - from) / (to - from) = ([8,21.9,0] - [12,20.9,-1]) / [1,2,2] = [-4, 0.5, 0.5]
+		// This is expected given the current implementation
+		// TODO: VOXELFORMAT: Review if element origin should be used differently
+		const glm::vec3 &firstPivot = firstCube.pivot();
+		const glm::vec3 expectedFirstPivot(-4.0f, 0.5f, 0.5f);
+		EXPECT_VEC_NEAR(firstPivot, expectedFirstPivot, 0.0001f);
+
+		// Test second cube dimensions
+		// In bbmodel: from=[13, 21.9, -1], to=[14, 22.9, 1]
+		ASSERT_EQ(secondCube.type(), scenegraph::SceneGraphNodeType::Model);
+		const voxel::Region &secondRegion = secondCube.region();
+		const glm::ivec3 &secondDim = secondRegion.getDimensionsInVoxels();
+		EXPECT_EQ(secondDim.x, 1);
+		EXPECT_EQ(secondDim.y, 1);
+		EXPECT_EQ(secondDim.z, 2); // Test group node
+		ASSERT_EQ(fingers_right.type(), scenegraph::SceneGraphNodeType::Group);
+		EXPECT_GT(fingers_right.children().size(), 0u);
+
+		// Test animations
+		ASSERT_EQ(sceneGraph.animations().size(), 5u);
+		EXPECT_TRUE(sceneGraph.hasAnimation("handsdown"));
+		EXPECT_TRUE(sceneGraph.hasAnimation("sit"));
+		EXPECT_TRUE(sceneGraph.hasAnimation("fist"));
+		EXPECT_TRUE(sceneGraph.hasAnimation("walk"));
+		EXPECT_TRUE(sceneGraph.hasAnimation("run"));
+
+		// Test that the main group was created correctly
+		scenegraph::SceneGraphNode *main = sceneGraph.findNodeByName("main");
+		ASSERT_NE(main, nullptr);
+		EXPECT_EQ(main->type(), scenegraph::SceneGraphNodeType::Group);
+	}
+};
 
 // the model comes from https://github.com/SL0ANE/Loy-s-Goodies/tree/main/models and was licensed under CC0
 // version 4.5 - includes animations and a full scene with a hierarchy of nodes
 TEST_F(BlockbenchFormatTest, testLoad_4_5) {
 	scenegraph::SceneGraph sceneGraph;
 	testLoad(sceneGraph, "loy_s_goodies_female_template.bbmodel", 53);
-	scenegraph::SceneGraphNode *hand_right = sceneGraph.findNodeByName("hand_right");
-	ASSERT_NE(hand_right, nullptr);
-	ASSERT_EQ(hand_right->children().size(), 3u);
-	scenegraph::SceneGraphNode &firstCube = sceneGraph.node(hand_right->children()[0]);
-	ASSERT_EQ(firstCube.name(), "cube");
-	scenegraph::SceneGraphNode &secondCube = sceneGraph.node(hand_right->children()[0]);
-	ASSERT_EQ(secondCube.name(), "cube");
-	scenegraph::SceneGraphNode &fingers_right = sceneGraph.node(hand_right->children()[2]);
-	ASSERT_EQ(fingers_right.name(), "fingers_right");
+	test_loy_s_goodies_female_template(sceneGraph, true);
+}
 
-	ASSERT_EQ(firstCube.type(), scenegraph::SceneGraphNodeType::Model);
-	const voxel::Region &firstRegion = firstCube.volume()->region();
-	const glm::ivec3 &firstDim = firstRegion.getDimensionsInVoxels();
-	EXPECT_EQ(firstDim.x, 1);
-	EXPECT_EQ(firstDim.y, 2);
-	EXPECT_EQ(firstDim.z, 2);
-	const glm::vec3 &firstPivot = firstCube.pivot();
-	const glm::vec3 expectedFirstPivot(-4.0f, 0.5f, 0.5f);
-	EXPECT_VEC_NEAR(firstPivot, expectedFirstPivot, 0.0001f);
-	// const glm::vec3 expectedFirstWorldPivot(8.0f, 21.9f, 0.0f);
-	// const glm::vec3 firstWorldPivot = firstCube.worldPivot();
-	// EXPECT_VEC_NEAR(firstWorldPivot, expectedFirstWorldPivot, 0.0001f);
-	// TODO: VOXELFORMAT: this is without pivot
-	// EXPECT_VEC_NEAR(firstCube.transform().worldTranslation(), glm::vec3(12.0f, 20.9f, -1.0f), 0.0001f);
-	// TODO: VOXELFORMAT: check voxels and colors
+// TODO: failing to load the new version
+TEST_F(BlockbenchFormatTest, DISABLED_testLoad_5_0_3) {
+	{
+		SCOPED_TRACE("loy_s_goodies_female_template_5_0_3.glb");
+		scenegraph::SceneGraph sceneGraphGLB;
+		testLoad(sceneGraphGLB, "loy_s_goodies_female_template_5_0_3.glb", 53);
+		test_loy_s_goodies_female_template(sceneGraphGLB, false);
+	}
 
-	ASSERT_EQ(secondCube.type(), scenegraph::SceneGraphNodeType::Model);
-	const voxel::Region &secondRegion = secondCube.volume()->region();
-	const glm::ivec3 &secondDim = secondRegion.getDimensionsInVoxels();
-	EXPECT_EQ(secondDim.x, 1);
-	EXPECT_EQ(secondDim.y, 2);
-	EXPECT_EQ(secondDim.z, 2);
-	ASSERT_EQ(fingers_right.type(), scenegraph::SceneGraphNodeType::Group);
-
-	ASSERT_EQ(sceneGraph.animations().size(), 5u);
-	EXPECT_TRUE(sceneGraph.hasAnimation("handsdown"));
-	EXPECT_TRUE(sceneGraph.hasAnimation("sit"));
-	EXPECT_TRUE(sceneGraph.hasAnimation("fist"));
-	EXPECT_TRUE(sceneGraph.hasAnimation("walk"));
-	EXPECT_TRUE(sceneGraph.hasAnimation("run"));
+	{
+		SCOPED_TRACE("loy_s_goodies_female_template_5_0_3.glb");
+		scenegraph::SceneGraph sceneGraph;
+		testLoad(sceneGraph, "loy_s_goodies_female_template_5_0_3.bbmodel", 53);
+		test_loy_s_goodies_female_template(sceneGraph, true);
+	}
 }
 
 // this model is based on a model from https://github.com/SL0ANE/Loy-s-Goodies/tree/main/models - but only one cube was
