0d6d395d9a971d1fe4440082329a8f01962d3486
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-11-04 20:09:40 +0100

    APP: implemented sort_parallel

--- src/modules/app/Async.h
+++ src/modules/app/Async.h
@@ -29,7 +29,46 @@ int for_parallel_size(int start, int end);
  */
 template<typename Iter, class Comparator>
 void sort_parallel(Iter first, Iter last, Comparator comp) {
-	core::sort(first, last, comp); // TODO: PERF: implement parallel sort
+	const int size = core::distance(first, last);
+	if (size <= 128) {
+		core::sort(first, last, comp);
+		return;
+	}
+
+	const int chunkCount = app::for_parallel_size(0, size);
+	if (chunkCount <= 1) {
+		core::sort(first, last, comp);
+		return;
+	}
+
+	const int chunkSize = (size + chunkCount - 1) / chunkCount;
+
+	app::for_parallel(0, chunkCount, [first, &comp, chunkSize, size](int chunkIdx, int chunkIdxEnd) {
+		for (int idx = chunkIdx; idx < chunkIdxEnd; ++idx) {
+			const int start = idx * chunkSize;
+			const int end = core_min(start + chunkSize, size);
+			if (start < size) {
+				const Iter chunkFirst = core::next(first, start);
+				const Iter chunkLast = core::next(first, end);
+				core::sort(chunkFirst, chunkLast, comp);
+			}
+		}
+	});
+
+	int currentChunkSize = chunkSize;
+	while (currentChunkSize < size) {
+		const int mergeSize = currentChunkSize * 2;
+
+		// Merge pairs of chunks
+		for (int i = 0; i + currentChunkSize < size; i += mergeSize) {
+			const Iter leftFirst = core::next(first, i);
+			const Iter middle = core::next(first, core_min(i + currentChunkSize, size));
+			const Iter rightLast = core::next(first, core_min(i + mergeSize, size));
+			core::inplace_merge(leftFirst, middle, rightLast, comp);
+		}
+
+		currentChunkSize = mergeSize;
+	}
 }
 
 } // namespace app
