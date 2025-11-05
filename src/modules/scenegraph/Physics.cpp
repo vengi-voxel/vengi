vengi-voxel/vengi

commit 1503fa386ffda9a8516b08377585df1b556d037d

SCENEGRAPH: use visitor paettern

src/modules/scenegraph/Physics.cpp
@@ -9,6 +9,7 @@
 #include "scenegraph/SceneGraphNode.h"
 #include "voxel/RawVolume.h"
 #include "voxel/Voxel.h"
+#include "voxelutil/VolumeVisitor.h"
 #include <glm/ext/scalar_constants.hpp>
 
 namespace scenegraph {
@@ -84,24 +85,9 @@ bool Physics::checkCollision(const CollisionNodes &nodes, const glm::vec3 &nextB
 			continue;
 		}
 
-		// Create a sampler for the volume of the collision node.
-		voxel::RawVolume::Sampler sampler(node.volume);
-		sampler.setPosition(mins);
-		// Iterate over all voxels in the AABB and check for collision.
-		for (int x = mins.x; x <= maxs.x; x++) {
-			voxel::RawVolume::Sampler sampler2 = sampler;
-			for (int y = mins.y; y <= maxs.y; y++) {
-				voxel::RawVolume::Sampler sampler3 = sampler2;
-				for (int z = mins.z; z <= maxs.z; z++) {
-					if (voxel::isBlocked(sampler3.voxel().getMaterial())) {
-						return true;
-					}
-					sampler3.movePositiveZ();
-				}
-				sampler2.movePositiveY();
-			}
-			sampler.movePositiveX();
-		}
+		voxelutil::visitVolume(*node.volume, {mins, maxs}, [&](int x, int y, int z, const voxel::Voxel &voxel) {
+			return true;
+		});
 	}
 	return false;
 }

commit e93e5a5b0c8785ba7d5126687a667817b0b62956

SCENEGRAPH: fixed missing return values handling and broken collision
introduced in 1503fa386ffda9a8516b08377585df1b556d037d

src/modules/scenegraph/Physics.cpp
@@ -85,9 +85,13 @@ bool Physics::checkCollision(const CollisionNodes &nodes, const glm::vec3 &nextB
 			continue;
 		}
 
-		voxelutil::visitVolume(*node.volume, {mins, maxs}, [&](int x, int y, int z, const voxel::Voxel &voxel) {
+		auto func = [&](int x, int y, int z, const voxel::Voxel &voxel) {
 			return true;
-		});
+		};
+		const voxel::Region collisionRegion{mins, maxs};
+		if (voxelutil::visitVolume(*node.volume, collisionRegion, func) == 1) {
+			return true;
+		}
 	}
 	return false;
 }