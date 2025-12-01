ac3ac25dc4784736e3f09823342a7f8404ed2e30
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-12-01 22:18:34 +0100

    IMAGE: cleanup capture tool test

--- src/modules/image/tests/CaptureToolTest.cpp
+++ src/modules/image/tests/CaptureToolTest.cpp
@@ -4,26 +4,30 @@
 
 #include "image/CaptureTool.h"
 #include "app/tests/AbstractTest.h"
+#include "core/StandardLib.h"
 #include "image/Image.h"
 #include "io/Filesystem.h"
-#include "core/StandardLib.h"
 
 namespace image {
 
 class CaptureToolTest : public app::AbstractTest {
+protected:
+	image::ImagePtr createImage() {
+		image::ImagePtr img = image::createEmptyImage("frame");
+		uint8_t buffer[64 * 64 * 4];
+		core_memset(buffer, 255, sizeof(buffer));
+		img->loadRGBA(buffer, 64, 64);
+		return img;
+	}
 };
 
 TEST_F(CaptureToolTest, testRecordAVI) {
 	CaptureTool tool(CaptureType::AVI);
-	const core::String filename = "test.avi";
-	ASSERT_TRUE(tool.startRecording(filename.c_str(), 64, 64));
+	const char *filename = "test.avi";
+	ASSERT_TRUE(tool.startRecording(filename, 64, 64));
 	EXPECT_TRUE(tool.isRecording());
 
-	image::ImagePtr img = image::createEmptyImage("frame");
-	uint8_t buffer[64 * 64 * 4];
-	core_memset(buffer, 255, sizeof(buffer));
-	img->loadRGBA(buffer, 64, 64);
-
+	const image::ImagePtr &img = createImage();
 	tool.enqueueFrame(img);
 	tool.enqueueFrame(img);
 
@@ -37,15 +41,11 @@ TEST_F(CaptureToolTest, testRecordAVI) {
 
 TEST_F(CaptureToolTest, testRecordMPEG2) {
 	CaptureTool tool(CaptureType::MPEG2);
-	const core::String filename = "test.mpg";
-	ASSERT_TRUE(tool.startRecording(filename.c_str(), 64, 64));
+	const char *filename = "test.mpg";
+	ASSERT_TRUE(tool.startRecording(filename, 64, 64));
 	EXPECT_TRUE(tool.isRecording());
 
-	image::ImagePtr img = image::createEmptyImage("frame");
-	uint8_t buffer[64 * 64 * 4];
-	core_memset(buffer, 255, sizeof(buffer));
-	img->loadRGBA(buffer, 64, 64);
-
+	const image::ImagePtr &img = createImage();
 	tool.enqueueFrame(img);
 	tool.enqueueFrame(img);
 
@@ -59,15 +59,11 @@ TEST_F(CaptureToolTest, testRecordMPEG2) {
 
 TEST_F(CaptureToolTest, testAbort) {
 	CaptureTool tool(CaptureType::AVI);
-	const core::String filename = "test_abort.avi";
-	ASSERT_TRUE(tool.startRecording(filename.c_str(), 64, 64));
+	const char *filename = "test_abort.avi";
+	ASSERT_TRUE(tool.startRecording(filename, 64, 64));
 	EXPECT_TRUE(tool.isRecording());
 
-	image::ImagePtr img = image::createEmptyImage("frame");
-	uint8_t buffer[64 * 64 * 4];
-	core_memset(buffer, 255, sizeof(buffer));
-	img->loadRGBA(buffer, 64, 64);
-
+	const image::ImagePtr &img = createImage();
 	tool.enqueueFrame(img);
 	tool.abort();
 	EXPECT_FALSE(tool.isRecording());
