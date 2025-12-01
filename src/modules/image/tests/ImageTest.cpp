26c0a762fd1e7741d5c8ee7dabb0826a2da98aa1
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-12-01 18:20:33 +0100

    IMAGE: added a few more image tests

--- src/modules/image/tests/ImageTest.cpp
+++ src/modules/image/tests/ImageTest.cpp
@@ -7,16 +7,16 @@
 #include "core/tests/TestColorHelper.h"
 #include "io/BufferedReadWriteStream.h"
 #include "io/File.h"
+#include "io/MemoryReadStream.h"
 #include "math/tests/TestMathHelper.h"
 
 namespace image {
 
 class ImageTest : public app::AbstractTest {
-private:
+protected:
 	static constexpr color::RGBA r = color::RGBA(255, 0, 0);
 	static constexpr color::RGBA b = color::RGBA(0, 0, 0);
 
-protected:
 	static constexpr color::RGBA img1[]{
 		r, r, b, b, b, b, r, r, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b,
 	};
@@ -134,6 +134,117 @@ TEST_F(ImageTest, testUVPixelConversion) {
 								   << ", h: " << img->height() << ")";
 		}
 	}
+}
+
+TEST_F(ImageTest, testIsGrayScale) {
+	image::ImagePtr img = image::createEmptyImage("gray");
+	img->loadRGBA((const uint8_t *)img1, 6, 6);
+	EXPECT_FALSE(img->isGrayScale());
+
+	// Create a grayscale image manually
+	image::ImagePtr grayImg = image::createEmptyImage("gray2");
+	grayImg->loadRGBA((const uint8_t *)img1, 6, 6);
+	for (int x = 0; x < 6; ++x) {
+		for (int y = 0; y < 6; ++y) {
+			grayImg->setColor(color::RGBA(128, 128, 128, 255), x, y);
+		}
+	}
+	EXPECT_TRUE(grayImg->isGrayScale());
+}
+
+TEST_F(ImageTest, testSetColor) {
+	image::ImagePtr img = image::createEmptyImage("setcolor");
+	img->loadRGBA((const uint8_t *)img1, 6, 6);
+	color::RGBA c(10, 20, 30, 40);
+	EXPECT_TRUE(img->setColor(c, 0, 0));
+	EXPECT_EQ(c, img->colorAt(0, 0));
+	EXPECT_FALSE(img->setColor(c, -1, 0));
+	EXPECT_FALSE(img->setColor(c, 6, 0));
+	EXPECT_FALSE(img->setColor(c, 0, -1));
+	EXPECT_FALSE(img->setColor(c, 0, 6));
+}
+
+TEST_F(ImageTest, testMakeOpaque) {
+	image::ImagePtr img = image::createEmptyImage("opaque");
+	img->loadRGBA((const uint8_t *)img1, 6, 6);
+	// Set a pixel with alpha < 255
+	img->setColor(color::RGBA(255, 0, 0, 128), 0, 0);
+	EXPECT_EQ(128, img->colorAt(0, 0).a);
+	img->makeOpaque();
+	EXPECT_EQ(255, img->colorAt(0, 0).a);
+}
+
+TEST_F(ImageTest, testFlipVerticalRGBA) {
+	uint8_t pixels[] = {
+		255, 0, 0, 255,  0, 255, 0, 255,
+		0, 0, 255, 255,  255, 255, 255, 255
+	};
+	// 2x2 image
+	// R G
+	// B W
+	image::Image::flipVerticalRGBA(pixels, 2, 2);
+	// Expected:
+	// B W
+	// R G
+	EXPECT_EQ(0, pixels[0]); EXPECT_EQ(0, pixels[1]); EXPECT_EQ(255, pixels[2]); EXPECT_EQ(255, pixels[3]); // B
+	EXPECT_EQ(255, pixels[4]); EXPECT_EQ(255, pixels[5]); EXPECT_EQ(255, pixels[6]); EXPECT_EQ(255, pixels[7]); // W
+	EXPECT_EQ(255, pixels[8]); EXPECT_EQ(0, pixels[9]); EXPECT_EQ(0, pixels[10]); EXPECT_EQ(255, pixels[11]); // R
+	EXPECT_EQ(0, pixels[12]); EXPECT_EQ(255, pixels[13]); EXPECT_EQ(0, pixels[14]); EXPECT_EQ(255, pixels[15]); // G
+}
+
+TEST_F(ImageTest, testResize) {
+	image::ImagePtr img = image::createEmptyImage("resize");
+	img->loadRGBA((const uint8_t *)img1, 6, 6);
+	EXPECT_TRUE(img->resize(12, 12));
+	EXPECT_EQ(12, img->width());
+	EXPECT_EQ(12, img->height());
+	// Check if content is preserved/scaled (basic check)
+	// img1 has red at 0,0
+	EXPECT_EQ(r, img->colorAt(0, 0));
+}
+
+TEST_F(ImageTest, testPngBase64) {
+	image::ImagePtr img = image::createEmptyImage("base64");
+	img->loadRGBA((const uint8_t *)img1, 6, 6);
+	core::String base64 = img->pngBase64();
+	EXPECT_FALSE(base64.empty());
+	// Basic check for PNG header in base64 (iVBORw0KGgo)
+	EXPECT_TRUE(base64.contains("iVBORw0KGgo"));
+}
+
+TEST_F(ImageTest, testLoadRGBA) {
+	image::ImagePtr img = image::createEmptyImage("loadrgba");
+	uint8_t data[] = {
+		255, 0, 0, 255,
+		0, 255, 0, 255
+	};
+	EXPECT_TRUE(img->loadRGBA(data, 2, 1));
+	EXPECT_EQ(2, img->width());
+	EXPECT_EQ(1, img->height());
+	EXPECT_EQ(color::RGBA(255, 0, 0, 255), img->colorAt(0, 0));
+	EXPECT_EQ(color::RGBA(0, 255, 0, 255), img->colorAt(1, 0));
+}
+
+TEST_F(ImageTest, testLoadBGRA) {
+	image::ImagePtr img = image::createEmptyImage("loadbgra");
+	uint8_t data[] = {
+		0, 0, 255, 255, // Red in BGRA (B G R A)
+		0, 255, 0, 255  // Green in BGRA
+	};
+	io::MemoryReadStream stream(data, sizeof(data));
+	EXPECT_TRUE(img->loadBGRA(stream, 2, 1));
+	EXPECT_EQ(2, img->width());
+	EXPECT_EQ(1, img->height());
+	EXPECT_EQ(color::RGBA(255, 0, 0, 255), img->colorAt(0, 0));
+	EXPECT_EQ(color::RGBA(0, 255, 0, 255), img->colorAt(1, 0));
+}
+
+TEST_F(ImageTest, testPrint) {
+	image::ImagePtr img = image::createEmptyImage("print");
+	img->loadRGBA((const uint8_t *)img1, 6, 6);
+	core::String output = image::print(img);
+	EXPECT_FALSE(output.empty());
+	EXPECT_TRUE(output.contains("w: 6, h: 6, d: 4"));
 }
 
 } // namespace image
