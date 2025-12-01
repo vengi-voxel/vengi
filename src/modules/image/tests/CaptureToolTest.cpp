aab7d890ec21a93ea5df10bda7d59382435cf286
Martin Gerhardy <martin.gerhardy@gmail.com>
2025-12-01 19:36:53 +0100

    IMAGE: added tests for CaptureTool

---
+++ src/modules/image/tests/CaptureToolTest.cpp
@@ -0,0 +1,76 @@
+/**
+ * @file
+ */
+
+#include "image/CaptureTool.h"
+#include "app/tests/AbstractTest.h"
+#include "image/Image.h"
+#include "io/Filesystem.h"
+#include "core/StandardLib.h"
+
+namespace image {
+
+class CaptureToolTest : public app::AbstractTest {
+};
+
+TEST_F(CaptureToolTest, DISABLED_testRecordAVI) {
+	CaptureTool tool(CaptureType::AVI);
+	const core::String filename = "test.avi";
+	ASSERT_TRUE(tool.startRecording(filename.c_str(), 64, 64));
+	EXPECT_TRUE(tool.isRecording());
+
+	image::ImagePtr img = image::createEmptyImage("frame");
+	uint8_t buffer[64 * 64 * 4];
+	SDL_memset(buffer, 255, sizeof(buffer));
+	img->loadRGBA(buffer, 64, 64);
+
+	tool.enqueueFrame(img);
+	tool.enqueueFrame(img);
+
+	EXPECT_TRUE(tool.stopRecording());
+	EXPECT_FALSE(tool.isRecording());
+	EXPECT_TRUE(tool.flush());
+	EXPECT_TRUE(tool.hasFinished());
+
+	EXPECT_TRUE(_testApp->filesystem()->exists(filename));
+}
+
+TEST_F(CaptureToolTest, DISABLED_testRecordMPEG2) {
+	CaptureTool tool(CaptureType::MPEG2);
+	const core::String filename = "test.mpg";
+	ASSERT_TRUE(tool.startRecording(filename.c_str(), 64, 64));
+	EXPECT_TRUE(tool.isRecording());
+
+	image::ImagePtr img = image::createEmptyImage("frame");
+	uint8_t buffer[64 * 64 * 4];
+	SDL_memset(buffer, 255, sizeof(buffer));
+	img->loadRGBA(buffer, 64, 64);
+
+	tool.enqueueFrame(img);
+	tool.enqueueFrame(img);
+
+	EXPECT_TRUE(tool.stopRecording());
+	EXPECT_FALSE(tool.isRecording());
+	EXPECT_TRUE(tool.flush());
+	EXPECT_TRUE(tool.hasFinished());
+
+	EXPECT_TRUE(_testApp->filesystem()->exists(filename));
+}
+
+TEST_F(CaptureToolTest, testAbort) {
+	CaptureTool tool(CaptureType::AVI);
+	const core::String filename = "test_abort.avi";
+	ASSERT_TRUE(tool.startRecording(filename.c_str(), 64, 64));
+	EXPECT_TRUE(tool.isRecording());
+
+	image::ImagePtr img = image::createEmptyImage("frame");
+	uint8_t buffer[64 * 64 * 4];
+	SDL_memset(buffer, 255, sizeof(buffer));
+	img->loadRGBA(buffer, 64, 64);
+
+	tool.enqueueFrame(img);
+	tool.abort();
+	EXPECT_FALSE(tool.isRecording());
+}
+
+} // namespace image
