cc2885b989aa760b75b6dae798aa91bdbe3ee7c6
Martin Gerhardy <martin.gerhardy@gmail.com>
2026-01-24 23:35:12 +0100

    IO: hopefully fixed c++11 compilation

--- src/modules/io/Base64Stream.h
+++ src/modules/io/Base64Stream.h
@@ -18,10 +18,7 @@ protected:
 	/**
 	 * This array is a LUT specified in RFC 4648
 	 */
-	static constexpr char LUT[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-								   'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
-								   'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-								   'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'};
+	static const char LUT[];
 
 	static CORE_FORCE_INLINE bool validbyte(const uint8_t c) {
 		return c == '+' || c == '/' || isalnum(c);
