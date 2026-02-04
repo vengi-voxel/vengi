8424af6e1163b487f2dc958eaa345bb657cbec02
Martin Gerhardy <martin.gerhardy@gmail.com>
2026-02-04 18:47:48 +0100

    MCP: ability to reduce the scene state json by fetching only single nodes

--- src/tools/voxedit/modules/voxedit-mcp/GetSceneStateTool.cpp
+++ src/tools/voxedit/modules/voxedit-mcp/GetSceneStateTool.cpp
@@ -3,8 +3,10 @@
  */
 
 #include "GetSceneStateTool.h"
+#include "core/UUID.h"
 #include "io/BufferedReadWriteStream.h"
 #include "scenegraph/JsonExporter.h"
+#include "scenegraph/SceneGraph.h"
 #include "voxedit-util/SceneManager.h"
 
 namespace voxedit {
@@ -12,16 +14,34 @@ namespace voxedit {
 GetSceneStateTool::GetSceneStateTool() : Tool("voxedit_get_scene_state") {
 	_tool["description"] =
 		"Get the current scene graph state. This should be your first action after connecting to the MCP server to get "
-		"the UUIDs of the existing nodes and their structure. Do this call from time to time to get an updated state.";
-	_tool["inputSchema"]["type"] = "object";
+		"the UUIDs of the existing nodes and their structure. Do this call from time to time to get an updated state. "
+		"If a node uuid is specified, only a single node is returned.";
+	nlohmann::json inputSchema;
+	inputSchema["type"] = "object";
+	inputSchema["properties"]["nodeUUID"] = propUUID();
+	// TODO: MCP: allow to specify flags to include/exclude palette, meshes, etc. (JsonExporterFlags)
+	_tool["inputSchema"] = core::move(inputSchema);
 }
 
 bool GetSceneStateTool::execute(const nlohmann::json &id, const nlohmann::json &args, ToolContext &ctx) {
-	if (ctx.sceneMgr->sceneGraph().empty()) {
+	const scenegraph::SceneGraph &sceneGraph = ctx.sceneMgr->sceneGraph();
+	if (sceneGraph.empty()) {
 		return ctx.result(id, "Scene graph is empty - not connected or no scene loaded", true);
 	}
+
 	io::BufferedReadWriteStream stream;
-	scenegraph::sceneGraphJson(ctx.sceneMgr->sceneGraph(), stream);
+	uint32_t flags = scenegraph::JSONEXPORTER_ALL;
+	core::UUID nodeUUID = argsUUID(args);
+	if (nodeUUID.isValid()) {
+		if (const scenegraph::SceneGraphNode *node = sceneGraph.findNodeByUUID(nodeUUID)) {
+			scenegraph::sceneGraphNodeJson(sceneGraph, node->id(), stream, flags);
+		} else {
+			scenegraph::sceneGraphJson(sceneGraph, stream, flags);
+		}
+	} else {
+		scenegraph::sceneGraphJson(sceneGraph, stream, flags);
+	}
+
 	core::String json((const char *)stream.getBuffer(), (size_t)stream.size());
 	return ctx.result(id, json, false);
 }
