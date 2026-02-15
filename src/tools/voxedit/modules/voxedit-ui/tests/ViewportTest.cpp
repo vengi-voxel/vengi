75d0513fccd2e691de7e54f83a714099c83aff95
Martin Gerhardy <martin.gerhardy@gmail.com>
2026-02-15 22:06:53 +0100

    VOXEDIT: extended viewport tests with toggling all the different rendering modes

--- src/tools/voxedit/modules/voxedit-ui/tests/ViewportTest.cpp
+++ src/tools/voxedit/modules/voxedit-ui/tests/ViewportTest.cpp
@@ -4,6 +4,8 @@
 
 #include "../Viewport.h"
 #include "command/CommandHandler.h"
+#include "core/ConfigVar.h"
+#include "core/Var.h"
 #include "util/VarUtil.h"
 #include "voxedit-util/Config.h"
 #include "voxedit-util/SceneManager.h"
@@ -30,8 +32,210 @@ void Viewport::registerUITests(ImGuiTestEngine *engine, const char *) {
 		IM_CHECK_EQ(1, voxelutil::countVoxels(*model->volume()));
 	};
 
-	// TODO: viewport menubar tests
-	// TODO: enable different rendering modes like bloom, bones, aabb, mirror axis, etc.
+	IM_REGISTER_TEST(engine, testCategory(), "toggle rendering options")->TestFunc = [=](ImGuiTestContext *ctx) {
+		IM_CHECK(activateViewportEditMode(ctx, _app));
+		const int viewportId = viewportEditMode(ctx, _app);
+		IM_CHECK_SILENT(viewportId != -1);
+		IM_CHECK(_sceneMgr->newScene(true, ctx->Test->Name, voxel::Region(0, 31)));
+		IM_CHECK(centerOnViewport(ctx, _sceneMgr, viewportId));
+		ctx->Yield();
+
+		const core::String vid = Viewport::viewportId(viewportId);
+
+		// toggle each rendering option on and off individually via the View menu
+		const char *checkboxItems[] = {"Grid", "Show gizmo", "Show locked axis", "Bounding box",
+									   "Bones", "Plane", "Outlines", "Checkerboard", "Bloom"};
+		const char *checkboxCvars[] = {cfg::VoxEditShowgrid, cfg::VoxEditShowaxis, cfg::VoxEditShowlockedaxis,
+									   cfg::VoxEditShowaabb, cfg::VoxEditShowBones, cfg::VoxEditShowPlane,
+									   cfg::RenderOutline, cfg::RenderCheckerBoard, cfg::ClientBloom};
+
+		for (int i = 0; i < (int)lengthof(checkboxItems); ++i) {
+			const core::VarPtr &var = core::Var::getSafe(checkboxCvars[i]);
+			const bool initialVal = var->boolVal();
+
+			// toggle on
+			IM_CHECK(focusWindow(ctx, vid.c_str()));
+			ctx->MenuClick(core::String::format("View/%s", checkboxItems[i]).c_str());
+			ctx->Yield(3);
+			IM_CHECK_EQ(var->boolVal(), !initialVal);
+
+			// toggle back
+			IM_CHECK(focusWindow(ctx, vid.c_str()));
+			ctx->MenuClick(core::String::format("View/%s", checkboxItems[i]).c_str());
+			ctx->Yield(3);
+			IM_CHECK_EQ(var->boolVal(), initialVal);
+		}
+	};
+
+	IM_REGISTER_TEST(engine, testCategory(), "toggle rendering combinations")->TestFunc = [=](ImGuiTestContext *ctx) {
+		IM_CHECK(activateViewportEditMode(ctx, _app));
+		const int viewportId = viewportEditMode(ctx, _app);
+		IM_CHECK_SILENT(viewportId != -1);
+		IM_CHECK(_sceneMgr->newScene(true, ctx->Test->Name, voxel::Region(0, 31)));
+		IM_CHECK(centerOnViewport(ctx, _sceneMgr, viewportId));
+		ctx->Yield();
+
+		const core::String vid = Viewport::viewportId(viewportId);
+
+		// enable grid + bounding box + bones together
+		const core::VarPtr &gridVar = core::Var::getSafe(cfg::VoxEditShowgrid);
+		const core::VarPtr &aabbVar = core::Var::getSafe(cfg::VoxEditShowaabb);
+		const core::VarPtr &bonesVar = core::Var::getSafe(cfg::VoxEditShowBones);
+		const core::VarPtr &bloomVar = core::Var::getSafe(cfg::ClientBloom);
+		const core::VarPtr &outlineVar = core::Var::getSafe(cfg::RenderOutline);
+		const core::VarPtr &checkerVar = core::Var::getSafe(cfg::RenderCheckerBoard);
+		const core::VarPtr &planeVar = core::Var::getSafe(cfg::VoxEditShowPlane);
+		const core::VarPtr &gizmoVar = core::Var::getSafe(cfg::VoxEditShowaxis);
+
+		// save initial states to restore later
+		const bool gridInit = gridVar->boolVal();
+		const bool aabbInit = aabbVar->boolVal();
+		const bool bonesInit = bonesVar->boolVal();
+		const bool bloomInit = bloomVar->boolVal();
+		const bool outlineInit = outlineVar->boolVal();
+		const bool checkerInit = checkerVar->boolVal();
+		const bool planeInit = planeVar->boolVal();
+		const bool gizmoInit = gizmoVar->boolVal();
+
+		// combination 1: enable grid + bounding box + bones
+		if (!gridInit) {
+			IM_CHECK(focusWindow(ctx, vid.c_str()));
+			ctx->MenuClick("View/Grid");
+			ctx->Yield();
+		}
+		if (!aabbInit) {
+			IM_CHECK(focusWindow(ctx, vid.c_str()));
+			ctx->MenuClick("View/Bounding box");
+			ctx->Yield();
+		}
+		if (!bonesInit) {
+			IM_CHECK(focusWindow(ctx, vid.c_str()));
+			ctx->MenuClick("View/Bones");
+			ctx->Yield();
+		}
+		IM_CHECK(gridVar->boolVal());
+		IM_CHECK(aabbVar->boolVal());
+		IM_CHECK(bonesVar->boolVal());
+		ctx->Yield(3);
+
+		// combination 2: add bloom + outlines + checkerboard on top
+		if (!bloomInit) {
+			IM_CHECK(focusWindow(ctx, vid.c_str()));
+			ctx->MenuClick("View/Bloom");
+			ctx->Yield();
+		}
+		if (!outlineInit) {
+			IM_CHECK(focusWindow(ctx, vid.c_str()));
+			ctx->MenuClick("View/Outlines");
+			ctx->Yield();
+		}
+		if (!checkerInit) {
+			IM_CHECK(focusWindow(ctx, vid.c_str()));
+			ctx->MenuClick("View/Checkerboard");
+			ctx->Yield();
+		}
+		IM_CHECK(bloomVar->boolVal());
+		IM_CHECK(outlineVar->boolVal());
+		IM_CHECK(checkerVar->boolVal());
+		ctx->Yield(3);
+
+		// combination 3: disable some while keeping others - toggle grid and bloom off
+		IM_CHECK(focusWindow(ctx, vid.c_str()));
+		ctx->MenuClick("View/Grid");
+		ctx->Yield();
+		IM_CHECK(focusWindow(ctx, vid.c_str()));
+		ctx->MenuClick("View/Bloom");
+		ctx->Yield(3);
+		IM_CHECK(!gridVar->boolVal());
+		IM_CHECK(!bloomVar->boolVal());
+		// others should still be enabled
+		IM_CHECK(aabbVar->boolVal());
+		IM_CHECK(bonesVar->boolVal());
+		IM_CHECK(outlineVar->boolVal());
+		IM_CHECK(checkerVar->boolVal());
+
+		// combination 4: enable plane + gizmo with the remaining options
+		if (!planeInit) {
+			IM_CHECK(focusWindow(ctx, vid.c_str()));
+			ctx->MenuClick("View/Plane");
+			ctx->Yield();
+		}
+		if (!gizmoInit) {
+			IM_CHECK(focusWindow(ctx, vid.c_str()));
+			ctx->MenuClick("View/Show gizmo");
+			ctx->Yield();
+		}
+		IM_CHECK(planeVar->boolVal());
+		IM_CHECK(gizmoVar->boolVal());
+		ctx->Yield(3);
+
+		// restore all to initial state
+		if (gridVar->boolVal() != gridInit) {
+			IM_CHECK(focusWindow(ctx, vid.c_str()));
+			ctx->MenuClick("View/Grid");
+			ctx->Yield();
+		}
+		if (aabbVar->boolVal() != aabbInit) {
+			IM_CHECK(focusWindow(ctx, vid.c_str()));
+			ctx->MenuClick("View/Bounding box");
+			ctx->Yield();
+		}
+		if (bonesVar->boolVal() != bonesInit) {
+			IM_CHECK(focusWindow(ctx, vid.c_str()));
+			ctx->MenuClick("View/Bones");
+			ctx->Yield();
+		}
+		if (bloomVar->boolVal() != bloomInit) {
+			IM_CHECK(focusWindow(ctx, vid.c_str()));
+			ctx->MenuClick("View/Bloom");
+			ctx->Yield();
+		}
+		if (outlineVar->boolVal() != outlineInit) {
+			IM_CHECK(focusWindow(ctx, vid.c_str()));
+			ctx->MenuClick("View/Outlines");
+			ctx->Yield();
+		}
+		if (checkerVar->boolVal() != checkerInit) {
+			IM_CHECK(focusWindow(ctx, vid.c_str()));
+			ctx->MenuClick("View/Checkerboard");
+			ctx->Yield();
+		}
+		if (planeVar->boolVal() != planeInit) {
+			IM_CHECK(focusWindow(ctx, vid.c_str()));
+			ctx->MenuClick("View/Plane");
+			ctx->Yield();
+		}
+		if (gizmoVar->boolVal() != gizmoInit) {
+			IM_CHECK(focusWindow(ctx, vid.c_str()));
+			ctx->MenuClick("View/Show gizmo");
+			ctx->Yield();
+		}
+	};
+
+	IM_REGISTER_TEST(engine, testCategory(), "toggle scene mode")->TestFunc = [=](ImGuiTestContext *ctx) {
+		IM_CHECK(_sceneMgr->newScene(true, ctx->Test->Name, voxel::Region(0, 31)));
+		ctx->Yield();
+
+		const core::String vid = Viewport::viewportId(_id);
+		const core::String viewportRef = vid + "/##MenuBar/Scene Mode";
+
+		// ensure we start in edit mode
+		if (isSceneMode()) {
+			ctx->ItemClick(viewportRef.c_str());
+			ctx->Yield(3);
+		}
+		IM_CHECK(!isSceneMode());
+
+		// switch to scene mode
+		ctx->ItemClick(viewportRef.c_str());
+		ctx->Yield(3);
+		IM_CHECK(isSceneMode());
+
+		// switch back to edit mode
+		ctx->ItemClick(viewportRef.c_str());
+		ctx->Yield(3);
+		IM_CHECK(!isSceneMode());
+	};
 
 #if 0
 	IM_REGISTER_TEST(engine, testCategory(), "select node")->TestFunc = [=](ImGuiTestContext *ctx) {
