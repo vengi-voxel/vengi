be973e63e966f3c302165490b19eac4d4231c8ab
Martin Gerhardy <martin.gerhardy@gmail.com>
2026-03-07 13:37:13 +0100

    VOXEDIT: extend OptionsPanel test to change rendering and display settings

--- src/tools/voxedit/modules/voxedit-ui/tests/OptionsPanelTest.cpp
+++ src/tools/voxedit/modules/voxedit-ui/tests/OptionsPanelTest.cpp
@@ -6,6 +6,7 @@
 #include "TestUtil.h"
 #include "core/ConfigVar.h"
 #include "core/Var.h"
+#include "voxedit-util/Config.h"
 #include "voxel/SurfaceExtractor.h"
 
 namespace voxedit {
@@ -58,6 +59,108 @@ void OptionsPanel::registerUITests(ImGuiTestEngine *engine, const char *id) {
 		ctx->ComboClick("Mesh mode/Binary");
 		ctx->Yield();
 		IM_CHECK_EQ(meshMode->intVal(), (int)voxel::SurfaceExtractionType::Binary);
+	};
+
+	IM_REGISTER_TEST(engine, testCategory(), "display toggles")->TestFunc = [=](ImGuiTestContext *ctx) {
+		setVisible(true);
+		_selectedCategory = OptionCategory::Display;
+		ctx->Yield();
+		IM_CHECK(focusWindow(ctx, id));
+		ctx->Yield();
+
+		ImGuiTestItemInfo info = ctx->WindowInfo("##optionscontent");
+		IM_CHECK(info.Window != nullptr);
+		ctx->SetRef(info.Window);
+
+		const core::VarPtr &showGrid = core::getVar(cfg::VoxEditShowgrid);
+		const core::VarPtr &showAxis = core::getVar(cfg::VoxEditShowaxis);
+		const core::VarPtr &showAabb = core::getVar(cfg::VoxEditShowaabb);
+		const core::VarPtr &showBones = core::getVar(cfg::VoxEditShowBones);
+
+		// Toggle grid off and on
+		const bool gridBefore = showGrid->boolVal();
+		ctx->ItemClick("Grid");
+		ctx->Yield();
+		IM_CHECK_EQ(showGrid->boolVal(), !gridBefore);
+		ctx->ItemClick("Grid");
+		ctx->Yield();
+		IM_CHECK_EQ(showGrid->boolVal(), gridBefore);
+
+		// Toggle axis
+		const bool axisBefore = showAxis->boolVal();
+		ctx->ItemClick("Show gizmo");
+		ctx->Yield();
+		IM_CHECK_EQ(showAxis->boolVal(), !axisBefore);
+		ctx->ItemClick("Show gizmo");
+		ctx->Yield();
+		IM_CHECK_EQ(showAxis->boolVal(), axisBefore);
+
+		// Toggle bounding box
+		const bool aabbBefore = showAabb->boolVal();
+		ctx->ItemClick("Bounding box");
+		ctx->Yield();
+		IM_CHECK_EQ(showAabb->boolVal(), !aabbBefore);
+		ctx->ItemClick("Bounding box");
+		ctx->Yield();
+		IM_CHECK_EQ(showAabb->boolVal(), aabbBefore);
+
+		// Toggle bones
+		const bool bonesBefore = showBones->boolVal();
+		ctx->ItemClick("Bones");
+		ctx->Yield();
+		IM_CHECK_EQ(showBones->boolVal(), !bonesBefore);
+		ctx->ItemClick("Bones");
+		ctx->Yield();
+		IM_CHECK_EQ(showBones->boolVal(), bonesBefore);
+	};
+
+	IM_REGISTER_TEST(engine, testCategory(), "rendering toggles")->TestFunc = [=](ImGuiTestContext *ctx) {
+		IM_CHECK(newTemplateScene(ctx, "##templates/##Knight"));
+
+		setVisible(true);
+		_selectedCategory = OptionCategory::Rendering;
+		ctx->Yield();
+		IM_CHECK(focusWindow(ctx, id));
+		ctx->Yield();
+
+		ImGuiTestItemInfo info = ctx->WindowInfo("##optionscontent");
+		IM_CHECK(info.Window != nullptr);
+		ctx->SetRef(info.Window);
+
+		// Set mesh mode to Cubic first so rendering checkboxes are not disabled
+		core::getVar(cfg::VoxelMeshMode)->setVal((int)voxel::SurfaceExtractionType::Cubic);
+		ctx->Yield();
+
+		const core::VarPtr &outline = core::getVar(cfg::RenderOutline);
+		const core::VarPtr &checkerBoard = core::getVar(cfg::RenderCheckerBoard);
+		const core::VarPtr &bloom = core::getVar(cfg::ClientBloom);
+
+		// Toggle outline
+		const bool outlineBefore = outline->boolVal();
+		ctx->ItemClick("Outlines");
+		ctx->Yield();
+		IM_CHECK_EQ(outline->boolVal(), !outlineBefore);
+		ctx->ItemClick("Outlines");
+		ctx->Yield();
+		IM_CHECK_EQ(outline->boolVal(), outlineBefore);
+
+		// Toggle checkerboard
+		const bool checkerBefore = checkerBoard->boolVal();
+		ctx->ItemClick("Checkerboard");
+		ctx->Yield();
+		IM_CHECK_EQ(checkerBoard->boolVal(), !checkerBefore);
+		ctx->ItemClick("Checkerboard");
+		ctx->Yield();
+		IM_CHECK_EQ(checkerBoard->boolVal(), checkerBefore);
+
+		// Toggle bloom
+		const bool bloomBefore = bloom->boolVal();
+		ctx->ItemClick("Bloom");
+		ctx->Yield();
+		IM_CHECK_EQ(bloom->boolVal(), !bloomBefore);
+		ctx->ItemClick("Bloom");
+		ctx->Yield();
+		IM_CHECK_EQ(bloom->boolVal(), bloomBefore);
 	};
 }
 
