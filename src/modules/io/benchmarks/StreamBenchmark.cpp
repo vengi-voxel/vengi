73a4d7c2e59ca2d9c53ee7cb7da04c48e9cc7501
Martin Gerhardy <martin.gerhardy@gmail.com>
2026-03-08 21:16:23 +0100

    IO: extend std stream benchmarks

--- src/modules/io/benchmarks/StreamBenchmark.cpp
+++ src/modules/io/benchmarks/StreamBenchmark.cpp
@@ -7,8 +7,11 @@
 #include "io/Base64ReadStream.h"
 #include "io/Base64WriteStream.h"
 #include "io/BufferedReadWriteStream.h"
+#include "io/StdStreamBuf.h"
 #include "io/ZipReadStream.h"
 #include "io/ZipWriteStream.h"
+#include <istream>
+#include <ostream>
 
 class StreamBenchmark : public app::AbstractBenchmark {
 protected:
@@ -100,4 +103,55 @@ BENCHMARK_REGISTER_F(StreamBenchmark, ZipStreamWrite);
 BENCHMARK_REGISTER_F(StreamBenchmark, Base64StreamWrite);
 BENCHMARK_REGISTER_F(StreamBenchmark, Base64StreamRead);
 BENCHMARK_REGISTER_F(StreamBenchmark, BufferedStream);
+
+BENCHMARK_DEFINE_F(StreamBenchmark, StdOStreamBufWrite)(benchmark::State &state) {
+	for (auto _ : state) {
+		io::BufferedReadWriteStream target;
+		target.reserve(1024);
+		io::StdOStreamBuf buf(target);
+		std::ostream ostream(&buf);
+		for (int i = 0; i < 1024; ++i) {
+			ostream.put((char)1);
+		}
+		ostream.flush();
+		benchmark::DoNotOptimize(target.pos());
+	}
+}
+
+BENCHMARK_DEFINE_F(StreamBenchmark, StdOStreamBufBulkWrite)(benchmark::State &state) {
+	const char block[1024] = {};
+	for (auto _ : state) {
+		io::BufferedReadWriteStream target;
+		target.reserve(sizeof(block));
+		io::StdOStreamBuf buf(target);
+		std::ostream ostream(&buf);
+		ostream.write(block, sizeof(block));
+		ostream.flush();
+		benchmark::DoNotOptimize(target.pos());
+	}
+}
+
+BENCHMARK_DEFINE_F(StreamBenchmark, StdIStreamBufRead)(benchmark::State &state) {
+	io::BufferedReadWriteStream source;
+	source.reserve(1024);
+	for (int i = 0; i < 1024; ++i) {
+		source.writeUInt8(1);
+	}
+	for (auto _ : state) {
+		source.seek(0);
+		io::StdIStreamBuf buf(source);
+		std::istream istream(&buf);
+		char c;
+		int count = 0;
+		while (istream.get(c)) {
+			++count;
+		}
+		benchmark::DoNotOptimize(count);
+	}
+}
+
+BENCHMARK_REGISTER_F(StreamBenchmark, StdOStreamBufWrite);
+BENCHMARK_REGISTER_F(StreamBenchmark, StdOStreamBufBulkWrite);
+BENCHMARK_REGISTER_F(StreamBenchmark, StdIStreamBufRead);
+
 BENCHMARK_MAIN();
