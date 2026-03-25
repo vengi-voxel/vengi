3d2d674d01d708b58047788e85f4d3f74d08604e
Jozsa Istvan <jozsa.istvan@hudl.com>
2026-03-25 12:00:27 +0200

    VOXEDIT: add icons to shape, transform, sculpt, and select mode dropdowns

--- src/tools/voxedit/modules/voxedit-util/modifier/ShapeType.h
+++ src/tools/voxedit/modules/voxedit-util/modifier/ShapeType.h
@@ -5,6 +5,7 @@
 #pragma once
 
 #include "core/ArrayLength.h"
+#include "ui/IconsLucide.h"
 
 namespace voxedit {
 
@@ -31,7 +32,18 @@ static constexpr const char *ShapeTypeStr[ShapeType::Max]{
 	"Ellipse",
 	"Circle"
 };
+
+static constexpr const char *ShapeTypeIcons[ShapeType::Max]{
+	ICON_LC_BOX,
+	ICON_LC_TORUS,
+	ICON_LC_CYLINDER,
+	ICON_LC_CONE,
+	ICON_LC_GLOBE,
+	ICON_LC_EGG,
+	ICON_LC_CIRCLE_DASHED
+};
 // clang-format on
 static_assert(lengthof(ShapeTypeStr) == (int)ShapeType::Max, "Array size mismatch");
+static_assert(lengthof(ShapeTypeIcons) == (int)ShapeType::Max, "Array size mismatch");
 
 } // namespace voxedit
