8e433226a3c8fd3e7eca2f5e5441a6c35019628f
Martin Gerhardy <martin.gerhardy@gmail.com>
2026-03-30 12:55:06 +0200

    VOXELRENDER: RenderState move support

--- src/modules/voxelrender/RenderState.h
+++ src/modules/voxelrender/RenderState.h
@@ -11,6 +11,41 @@
 namespace voxelrender {
 
 struct RenderState : public core::NonCopyable {
+	RenderState() = default;
+	RenderState(RenderState &&other) noexcept
+		: _culled(other._culled), _empty(other._empty), _dirtyNormals(other._dirtyNormals),
+		  _normalPreviewBufferIndex(other._normalPreviewBufferIndex) {
+		for (int i = 0; i < voxel::MeshType_Max; ++i) {
+			_vertexBufferIndex[i] = other._vertexBufferIndex[i];
+			_normalBufferIndex[i] = other._normalBufferIndex[i];
+			_indexBufferIndex[i] = other._indexBufferIndex[i];
+			_vertexBuffer[i] = core::move(other._vertexBuffer[i]);
+			other._vertexBufferIndex[i] = -1;
+			other._normalBufferIndex[i] = -1;
+			other._indexBufferIndex[i] = -1;
+		}
+		other._normalPreviewBufferIndex = -1;
+	}
+	RenderState &operator=(RenderState &&other) noexcept {
+		if (this != &other) {
+			_culled = other._culled;
+			_empty = other._empty;
+			_dirtyNormals = other._dirtyNormals;
+			_normalPreviewBufferIndex = other._normalPreviewBufferIndex;
+			for (int i = 0; i < voxel::MeshType_Max; ++i) {
+				_vertexBufferIndex[i] = other._vertexBufferIndex[i];
+				_normalBufferIndex[i] = other._normalBufferIndex[i];
+				_indexBufferIndex[i] = other._indexBufferIndex[i];
+				_vertexBuffer[i] = core::move(other._vertexBuffer[i]);
+				other._vertexBufferIndex[i] = -1;
+				other._normalBufferIndex[i] = -1;
+				other._indexBufferIndex[i] = -1;
+			}
+			other._normalPreviewBufferIndex = -1;
+		}
+		return *this;
+	}
+
 	bool _culled = false;
 	bool _empty = false; // this is only updated for non hidden nodes
 	bool _dirtyNormals = false;
