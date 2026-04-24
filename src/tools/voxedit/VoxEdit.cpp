100f8828699078c31e9650d479d09783803d91f4
Martin Gerhardy <martin.gerhardy@gmail.com>
2026-04-24 21:23:50 +0200

    UI: fixed binding context not taking WantCaptureKeyboard into account
    this could send key strokes to the viewport and the console, file dialog or text editor (scripts) - and maybe others

--- src/tools/voxedit/VoxEdit.cpp
+++ src/tools/voxedit/VoxEdit.cpp
@@ -626,6 +626,9 @@ app::AppState VoxEdit::onRunning() {
 	}
 
 	_collectionMgr->update(_nowSeconds);
+	if (ImGui::GetIO().WantCaptureKeyboard) {
+		return state;
+	}
 	const voxedit::Viewport *viewport = _mainWindow->hoveredViewport();
 	if (viewport) {
 		if (viewport->isGameMode()) {
