vengi-voxel/vengi

commit bfb27a2a8df278d56f4c55b12fd7dacaca2e8c30

VIDEO: removed opengl runtime reflection handling

src/tools/shadertool/Generator.cpp
@@ -58,18 +58,9 @@ bool generateSrc(const core::String& templateHeader, const core::String& templat
 	core::String uniformArrayInfo;
 	const int uniformSize = int(shaderStruct.uniforms.size());
 	if (uniformSize > 0) {
-		uniforms += "checkUniforms({";
-		int i = 0;
-		for (const Variable& uniform : shaderStruct.uniforms) {
-			uniforms += "\"";
-			uniforms += uniform.name;
-			uniforms += "\"";
-			if (i < uniformSize - 1) {
-				uniforms += ", ";
-			}
-			++i;
-		}
-		uniforms += "});";
+		uniforms += "// ";
+		uniforms += core::string::toString(uniformSize);
+		uniforms += " uniforms";
 	} else {
 		uniforms += "// no uniforms";
 	}
@@ -77,18 +68,9 @@ bool generateSrc(const core::String& templateHeader, const core::String& templat
 	core::String attributes;
 	const int attributeSize = int(shaderStruct.attributes.size());
 	if (attributeSize > 0) {
-		attributes += "checkAttributes({";
-		int i = 0;
-		for (const Variable& v : shaderStruct.attributes) {
-			attributes += "\"";
-			attributes += v.name;
-			attributes += "\"";
-			if (i < attributeSize - 1) {
-				attributes += ", ";
-			}
-			++i;
-		}
-		attributes += "});\n";
+		attributes += "// ";
+		attributes += core::string::toString(attributeSize);
+		attributes += " attributes";
 	} else {
 		attributes += "// no attributes";
 	}
@@ -208,13 +190,53 @@ bool generateSrc(const core::String& templateHeader, const core::String& templat
 		if (layout.location != -1) {
 			methods += core::string::toString(layout.location);
 			methods += ";\n";
-		} else {
-			methods += "getUniformLocation(\"";
+		} else if ((v.isSampler() || v.isImage()) && layout.binding != -1) {
+			// layout(binding=X) already sets the texture unit at compile time in GLSL 4.2+
+			methods += "0; (void)location; (void)";
 			methods += v.name;
-			methods += "\");\n";
-			methods += "\tif (location == -1) {\n";
-			methods += "\t\treturn false;\n";
-			methods += "\t}\n";
+			methods += ";\n";
+			methods += "\treturn true;\n";
+			methods += "}\n";
+
+			// skip the rest of the setter generation for this uniform
+			if (v.isSampler()) {
+				if (layout.binding != -1) {
+					prototypes += "\n\tvideo::TextureUnit getBound";
+					prototypes += uniformName;
+					prototypes += "TexUnit() const;\n";
+					methods += "\n\nvideo::TextureUnit ";
+					methods += filename;
+					methods += "::getBound";
+					methods += uniformName;
+					methods += "TexUnit() const {\n";
+					methods += "\treturn video::TextureUnit::";
+					methods += convertToTexUnit(layout.binding);
+					methods += ";\n}\n";
+				}
+			}
+			if (v.isSampler() || v.isImage()) {
+				if (layout.imageFormat != video::ImageFormat::Max) {
+					prototypes += "\n\tvideo::ImageFormat getImageFormat";
+					prototypes += uniformName;
+					prototypes += "() const;\n";
+					methods += "\nvideo::ImageFormat ";
+					methods += filename;
+					methods += "::getImageFormat";
+					methods += uniformName;
+					methods += "() const {\n";
+					methods += "\treturn video::ImageFormat::";
+					methods += util::getImageFormatTypeString(layout.imageFormat);
+					methods += ";\n}\n";
+				}
+			}
+			if (n < uniformSize - 2) {
+				methods += "\n";
+			}
+			++n;
+			continue;
+		} else {
+			Log::error("Uniform '%s' has no layout(location=X) or layout(binding=X) qualifier", v.name.c_str());
+			return false;
 		}
 		methods += "\tsetUniform";
 		methods += util::uniformSetterPostfix(v.type, v.arraySize == -1 ? 2 : v.arraySize);
@@ -302,11 +324,9 @@ bool generateSrc(const core::String& templateHeader, const core::String& templat
 			methods += ", ";
 			methods += core::string::toString(v.arraySize);
 			methods += ">& var) const {\n";
-			methods += "\tconst int location = getUniformLocation(\"";
-			methods += v.name;
-			methods += "\");\n\tif (location == -1) {\n";
-			methods += "\t\treturn false;\n";
-			methods += "\t}\n";
+			methods += "\tconst int location = ";
+			methods += core::string::toString(layout.location);
+			methods += ";\n";
 			methods += "\tsetUniform";
 			methods += util::uniformSetterPostfix(v.type, v.arraySize);
 			methods += "(location, &var[0], var.size());\n";
@@ -321,6 +341,15 @@ bool generateSrc(const core::String& templateHeader, const core::String& templat
 	int i = 0;
 	for (const Variable& v : shaderStruct.attributes) {
 		const core::String& attributeName = util::convertName(v.name, true);
+		auto attrLayoutIter = shaderStruct.layouts.find(v.name);
+		Layout attrLayout;
+		if (attrLayoutIter != shaderStruct.layouts.end()) {
+			attrLayout = attrLayoutIter->second;
+		}
+		if (attrLayout.location == -1) {
+			Log::error("Attribute '%s' has no layout(location=X) qualifier", v.name.c_str());
+			return false;
+		}
 
 		prototypes += "\n\t/**\n";
 		prototypes += "\t * @brief This version takes the c++ data type as a reference\n";
@@ -357,7 +386,6 @@ bool generateSrc(const core::String& templateHeader, const core::String& templat
 		prototypes += "\t\tattribute";
 		prototypes += attributeName;
 		prototypes += ".type = video::mapType<TYPE>();\n";
-		// TODO: add validation that the given c++ data type fits the specified glsl type.
 		prototypes += "\t\treturn attribute";
 		prototypes += attributeName;
 		prototypes += ";\n";
@@ -366,12 +394,12 @@ bool generateSrc(const core::String& templateHeader, const core::String& templat
 		prototypes += "\n\t/**\n\t * @brief Return the binding location of the shader attribute @c ";
 		prototypes += attributeName;
 		prototypes += "\n\t */\n";
-		prototypes += "\tinline int getLocation";
+		prototypes += "\tstatic inline int getLocation";
 		prototypes += attributeName;
-		prototypes += "() const {\n";
-		prototypes += "\t\treturn getAttributeLocation(\"";
-		prototypes += v.name;
-		prototypes += "\");\n";
+		prototypes += "() {\n";
+		prototypes += "\t\treturn ";
+		prototypes += core::string::toString(attrLayout.location);
+		prototypes += ";\n";
 		prototypes += "\t}\n";
 
 		prototypes += "\n\t/**\n\t * @brief Return the components if the attribute @c ";
@@ -477,27 +505,6 @@ bool generateSrc(const core::String& templateHeader, const core::String& templat
 			ub += core::string::toString(align);
 			ub += "\n";
 
-			uniforms += "\n\tif (";
-			uniforms += core::string::toString((uint32_t)(offset * 4));
-			uniforms += " != getUniformBufferOffset(\"";
-			uniforms += v.name;
-			if (v.arraySize > 0) {
-				uniforms += "[0]";
-			}
-			uniforms += "\")) {\n";
-			uniforms += "\t\tLog::error(\"Invalid offset found for uniform ";
-			uniforms += v.name;
-			if (v.arraySize > 0) {
-				uniforms += "[0]";
-			}
-			uniforms += " %i - expected ";
-			uniforms += core::string::toString((uint32_t)(offset * 4));
-			uniforms += "\", getUniformBufferOffset(\"";
-			uniforms += v.name;
-			uniforms += "\"));\n";
-			//uniforms += "\t\treturn false;\n";
-			uniforms += "\t}\n";
-
 			if (offsetsIndex > 0) {
 				offsets += ", ";
 			}
@@ -575,9 +582,13 @@ bool generateSrc(const core::String& templateHeader, const core::String& templat
 		prototypes += "\tinline bool set";
 		prototypes += uniformBufferStructName;
 		prototypes += "(const video::UniformBuffer& buf) {\n";
-		prototypes += "\t\treturn setUniformBuffer(\"";
-		prototypes += ubuf.name;
-		prototypes += "\", buf);\n";
+		if (ubuf.layout.binding == -1) {
+			Log::error("Uniform block '%s' has no layout(binding=X) qualifier", ubuf.name.c_str());
+			return false;
+		}
+		prototypes += "\t\treturn buf.bind(";
+		prototypes += core::string::toString(ubuf.layout.binding);
+		prototypes += ");\n";
 		prototypes += "\t}\n";
 
 		core::String generatedUb = core::string::replaceAll(templateUniformBuffer, "$name$", uniformBufferClassName);

src/modules/video/RendererInterface.h
@@ -7,7 +7,8 @@
 #pragma once
 
 #include "RenderBuffer.h"
-#include "ShaderTypes.h"
+#include "Types.h"
+#include "core/String.h"
 #include "core/SharedPtr.h"
 #include "video/RendererState.h"
 #include <glm/vec3.hpp>
@@ -552,14 +553,6 @@ bool runShader(Id program, const glm::uvec3 &workGroups, MemoryBarrierType wait 
  * @brief Wait for the execution of a compute shader
  */
 void waitShader(MemoryBarrierType wait);
-/**
- * @brief Fetch all uniforms in a shader
- */
-int fetchUniforms(Id program, ShaderUniforms &uniforms, const core::String &name = "unknown-shader");
-/**
- * @brief Fetch all attributes in a shader
- */
-int fetchAttributes(Id program, ShaderAttributes &attributes, const core::String &name = "unknown-shader");
 
 /**
  * @brief Flush queued rendering commands to the GPU driver.
@@ -587,10 +580,5 @@ void syncPendingState();
  * Typically implemented with glFinish() on OpenGL backends.
  */
 void finish();
-
-// shader stuff
-void setUniformBufferBinding(Id program, uint32_t blockIndex, uint32_t blockBinding);
-// only for validation purposes
-int32_t getUniformBufferOffset(Id program, const char *name);
 
 } // namespace video

src/modules/video/RendererState.h
@@ -109,8 +109,6 @@ struct RendererState {
 	float pendingLineWidth = 1.0f;
 	core::BitSet<core::enumVal(Vendor::Max)> vendor;
 	int drawCalls = 0;
-	// Cache for uniform buffer bindings: maps (program << 32 | blockIndex) to blockBinding
-	core::Map<uint64_t, uint32_t, 64> uniformBufferBindings;
 	// Cache for buffer base bindings: maps (type << 32 | index) to buffer handle
 	core::Map<uint64_t, Id, 64> bufferBaseBindings;
 	// Pending integer uniforms: maps location to value (deferred until draw calls)

src/modules/video/Shader.cpp
@@ -16,7 +16,6 @@
 #include "core/Enum.h"
 #include "core/Singleton.h"
 #include "core/StringUtil.h"
-#include "UniformBuffer.h"
 #include "video/Renderer.h"
 #include "util/IncludeUtil.h"
 #include <glm/gtc/type_ptr.hpp>
@@ -102,50 +101,6 @@ Shader::Shader() {
 
 Shader::~Shader() {
 	Shader::shutdown();
-}
-
-bool Shader::hasAttribute(const core::String& name) const {
-	return _attributes.hasKey(name);
-}
-
-bool Shader::hasUniform(const core::String& name) const {
-	return _uniforms.hasKey(name);
-}
-
-bool Shader::isUniformBlock(const core::String& name) const {
-	auto i = _uniforms.find(name);
-	if (i == _uniforms.end()) {
-		return false;
-	}
-	return i->second.block;
-}
-
-void Shader::checkAttribute(const core::String& attribute) {
-	if (!hasAttribute(attribute)) {
-		Log::warn("Attribute %s missing for shader %s", attribute.c_str(), _name.c_str());
-	} else {
-		Log::debug("Found attribute %s for shader %s", attribute.c_str(), _name.c_str());
-	}
-}
-
-void Shader::checkUniform(const core::String& uniform) {
-	if (!hasUniform(uniform)) {
-		Log::warn("Uniform %s missing for shader %s", uniform.c_str(), _name.c_str());
-	} else {
-		Log::debug("Found uniform %s for shader %s", uniform.c_str(), _name.c_str());
-	}
-}
-
-void Shader::checkAttributes(std::initializer_list<core::String> attributes) {
-	for (const core::String& attribute : attributes) {
-		checkAttribute(attribute);
-	}
-}
-
-void Shader::checkUniforms(std::initializer_list<core::String> uniforms) {
-	for (const core::String& uniform : uniforms) {
-		checkUniform(uniform);
-	}
 }
 
 void Shader::shutdown() {
@@ -237,10 +192,6 @@ bool Shader::init() {
 	createProgramFromShaders();
 	const bool success = _program != InvalidId;
 	_initialized = success;
-	if (_initialized) {
-		fetchAttributes();
-		fetchUniforms();
-	}
 	return success;
 }
 
@@ -270,13 +221,6 @@ bool Shader::deactivate() const {
 
 	_active = false;
 	_time = 0;
-	if (_recordUsedUniforms) {
-		for (const auto& e : _uniforms) {
-			if (_usedUniforms.find(e->value.location) == _usedUniforms.end()) {
-				Log::error("Didn't set the uniform %s (shader: %s)", e->key.c_str(), _name.c_str());
-			}
-		}
-	}
 
 	return _active;
 }
@@ -284,22 +228,6 @@ bool Shader::deactivate() const {
 void Shader::addDefine(const core::String& name, const core::String& value) {
 	core_assert_msg(!_initialized, "Shader is already initialized");
 	_defines.put(name, value);
-}
-
-int Shader::getAttributeLocation(const core::String& name) const {
-	const int location = checkAttributeLocation(name);
-	if (location == -1) {
-		Log::debug("can't find attribute %s in shader %s", name.c_str(), _name.c_str());
-	}
-	return location;
-}
-
-int Shader::checkAttributeLocation(const core::String& name) const {
-	auto i = _attributes.find(name);
-	if (i == _attributes.end()) {
-		return -1;
-	}
-	return i->second;
 }
 
 bool Shader::checkUniformCache(int location, const void* value, int length) const {
@@ -319,38 +247,6 @@ bool Shader::checkUniformCache(int location, const void* value, int length) cons
 	_uniformStateMap.put(location, hash);
 	return true;
 #endif
-}
-
-int Shader::getUniformLocation(const core::String& name) const {
-	const Uniform* uniform = getUniform(name);
-	if (uniform == nullptr) {
-		return -1;
-	}
-	return uniform->location;
-}
-
-const Uniform* Shader::getUniform(const core::String& name) const {
-	auto i = _uniforms.find(name);
-	if (i == _uniforms.end()) {
-		Log::debug("can't find uniform %s in shader %s", name.c_str(), _name.c_str());
-		for (const auto& uniformEntry : _uniforms) {
-			Log::trace("uniform %s", uniformEntry->key.c_str());
-		}
-		return nullptr;
-	}
-	return &i->second;
-}
-
-int Shader::fetchUniforms() {
-	_uniforms.clear();
-	Log::debug("Fetch uniforms");
-	return video::fetchUniforms(_program, _uniforms, _name);
-}
-
-int Shader::fetchAttributes() {
-	_attributes.clear();
-	Log::debug("Fetch attributes");
-	return video::fetchAttributes(_program, _attributes, _name);
 }
 
 core::String Shader::validPreprocessorName(const core::String& name) {
@@ -520,36 +416,10 @@ ScopedShader::~ScopedShader() {
 	useProgram(_oldShader);
 }
 
-bool Shader::setUniformBuffer(const core::String& name, const UniformBuffer& buffer) {
-	const Uniform* uniform = getUniform(name);
-	if (uniform == nullptr) {
-		Log::error("%s is no uniform", name.c_str());
-		return false;
-	}
-	if (!uniform->block) {
-		Log::error("%s is no uniform buffer", name.c_str());
-		return false;
-	}
-
-	if (uniform->size != (int)buffer.size()) {
-		Log::error("Uniform buffer %s: size %i differs from uploaded structure size %i", name.c_str(), uniform->size, (int)buffer.size());
-		return false;
-	}
-
-	video::setUniformBufferBinding(_program, uniform->blockIndex, uniform->blockBinding);
-	addUsedUniform(uniform->location);
-	return buffer.bind(uniform->blockIndex);
-}
-
 void Shader::setUniformi(int location, int value) const {
 	if (checkUniformCache(location, &value, sizeof(value))) {
 		video::setUniformi(location, value);
 	}
-	addUsedUniform(location);
-}
-
-int32_t Shader::getUniformBufferOffset(const char *name) {
-	return video::getUniformBufferOffset(_program, name);
 }
 
 }

src/modules/video/Shader.h
@@ -11,11 +11,9 @@
 #include <stdint.h>
 #include <string.h>
 #include <glm/fwd.hpp>
-#include "ShaderTypes.h"
+#include "Types.h"
 
 namespace video {
-
-class UniformBuffer;
 
 #ifndef VERTEX_POSTFIX
 #define VERTEX_POSTFIX ".vert"
@@ -32,9 +30,6 @@ class UniformBuffer;
 #ifndef COMPUTE_POSTFIX
 #define COMPUTE_POSTFIX ".comp"
 #endif
-
-// activate this to validate that every uniform was set
-#define VALIDATE_UNIFORMS 0
 
 /**
  * @brief Shader wrapper for GLSL. See shadertool for autogenerated shader wrapper code
@@ -57,24 +52,9 @@ protected:
 	typedef core::DynamicStringMap<core::String> ShaderDefines;
 	ShaderDefines _defines;
 
-	ShaderUniforms _uniforms;
-
-	// can be used to validate that every uniform was set. The value type is the location index
-	mutable core::DynamicMap<int, bool, 4> _usedUniforms;
-	bool _recordUsedUniforms = false;
-	void addUsedUniform(int location) const;
-
-	ShaderAttributes _attributes;
-
 	mutable uint32_t _time = 0u;
 
 	core::String _name;
-
-	const Uniform* getUniform(const core::String& name) const;
-
-	int fetchUniforms();
-
-	int fetchAttributes();
 
 	bool createProgramFromShaders();
 
@@ -136,10 +116,6 @@ public:
 		return false;
 	}
 
-	void recordUsedUniforms(bool state);
-
-	void clearUsedUniforms();
-
 	bool loadFromFile(const core::String& filename, ShaderType shaderType);
 
 	/**
@@ -180,36 +156,18 @@ public:
 	 */
 	bool run(const glm::uvec3& workGroups, MemoryBarrierType wait = MemoryBarrierType::None);
 
-	void checkAttribute(const core::String& attribute);
-	void checkUniform(const core::String& uniform);
-	void checkAttributes(std::initializer_list<core::String> attributes);
-	void checkUniforms(std::initializer_list<core::String> uniforms);
-
 	/**
 	 * @brief Adds a new define in the form '#define value' to the shader source code
 	 */
 	void addDefine(const core::String& name, const core::String& value);
 
-	int checkAttributeLocation(const core::String& name) const;
-	int getAttributeLocation(const core::String& name) const;
-
-	int getUniformLocation(const core::String& name) const;
-
 	void setUniform(int location, TextureUnit value) const;
-	bool hasAttribute(const core::String& name) const;
-	bool hasUniform(const core::String& name) const;
-	bool isUniformBlock(const core::String& name) const;
 
 	// particular renderer api must implement this
 
 private:
 	// only called from setting the texture units
 	void setUniformi(int location, int value) const;
-public:
-	// validate that the offset is correct for the given uniform buffer name
-	// this happens at shader setup time and is part of the validation layer of a shader
-	int32_t getUniformBufferOffset(const char *name);
-	bool setUniformBuffer(const core::String& name, const UniformBuffer& buffer);
 };
 
 inline bool Shader::isDirty() const {
@@ -218,18 +176,6 @@ inline bool Shader::isDirty() const {
 
 inline bool Shader::isInitialized() const {
 	return _initialized;
-}
-
-inline void Shader::addUsedUniform(int location) const {
-	_usedUniforms.put(location, true);
-}
-
-inline void Shader::recordUsedUniforms(bool state) {
-	_recordUsedUniforms = state;
-}
-
-inline void Shader::clearUsedUniforms() {
-	_usedUniforms.clear();
 }
 
 inline void Shader::setUniform(int location, TextureUnit value) const {
@@ -253,7 +199,5 @@ public:
 	ScopedShader(const Shader& shader);
 	~ScopedShader();
 };
-
-#define shaderSetUniformIf(shader, func, var, ...) if (shader.hasUniform(var)) { shader.func(var, __VA_ARGS__); }
 
 }

commit 950b12a7f2dcd1165c6fc4f3dd45f1fd0ad1a9c1

RENDER: fixed wrong texture units are refactoring
bfb27a2a8df278d56f4c55b12fd7dacaca2e8c30 broke it

src/modules/render/BloomRenderer.cpp
@@ -123,7 +123,7 @@ void BloomRenderer::blur(const video::TexturePtr &source, video::FrameBuffer &de
 	_convolutionFragData.coefficients2 = 0.25f;
 	core_assert_always(_convolutionData.update(_convolutionFragData));
 	core_assert_always(_convolutionShader.setConv(_convolutionData.getConvUniformBuffer()));
-	core_assert_always(video::bindTexture(video::TextureUnit::Zero, source));
+	core_assert_always(video::bindTexture(_convolutionShader.getBoundImageTexUnit(), source));
 	dest.bind(true);
 	video::viewport(0, 0, source->width(), source->height());
 	video::drawArrays(video::Primitive::Triangles, 6);
@@ -135,8 +135,6 @@ void BloomRenderer::apply(video::FrameBuffer *sources, video::FrameBuffer *dests
 		{
 			video::ScopedShader scoped(_combine2Shader);
 			dests[l].bind(true);
-			core_assert_always(_combine2Shader.setTexture0(video::TextureUnit::Zero));
-			core_assert_always(_combine2Shader.setTexture1(video::TextureUnit::One));
 			const video::TexturePtr &srcTex = sources[l].texture();
 			if (i != 0) {
 				const video::TexturePtr &destTex = dests[l + 1].texture();
@@ -170,7 +168,6 @@ void BloomRenderer::render(const video::TexturePtr& srcTexture, const video::Tex
 
 	{
 		video::ScopedShader scoped(_convolutionShader);
-		core_assert_always(_convolutionShader.setImage(video::TextureUnit::Zero));
 		core_assert_always(_vbo.bind());
 		blur(glowTexture, _bloom[0], false);
 		blur(_bloom[0].texture(), _bloom[1], true);
@@ -179,7 +176,6 @@ void BloomRenderer::render(const video::TexturePtr& srcTexture, const video::Tex
 	// prepare the first source buffer by rendering the glow texture into it.
 	{
 		video::ScopedShader scoped(_textureShader);
-		core_assert_always(_textureShader.setTexture(video::TextureUnit::Zero));
 		_frameBuffers0[0].bind(true);
 		video::bindTexture(video::TextureUnit::Zero, _bloom[1].texture());
 		video::drawArrays(video::Primitive::Triangles, 6);

src/modules/testcore/TextureRenderer.cpp
@@ -21,7 +21,6 @@ bool TextureRenderer::init(bool yFlipped) {
 
 void TextureRenderer::render(video::TextureUnit texUnit) {
 	video::ScopedShader scoped(_shader);
-	_shader.setTexture(texUnit);
 	video::ScopedBuffer scopedBuf(_vbo);
 	const int elements = (int)_vbo.elements(0, _shader.getComponentsPos());
 	core_assert(elements == 6);

src/tools/shadertool/Generator.cpp
@@ -125,16 +125,49 @@ bool generateSrc(const core::String& templateHeader, const core::String& templat
 		}
 		const bool isInteger = v.isSingleInteger();
 		const core::String& uniformName = util::convertName(v.name, true);
-		core::String mproto;
-		mproto += "set";
-		mproto += uniformName;
-		mproto += "(";
 		const Types& cType = util::resolveTypes(v.type);
 		auto layoutIter = shaderStruct.layouts.find(v.name);
 		Layout layout;
 		if (layoutIter != shaderStruct.layouts.end()) {
 			layout = layoutIter->second;
 		}
+
+		// samplers/images with layout(binding=X) need no setter - the binding is set at compile time
+		if ((v.isSampler() || v.isImage()) && layout.binding != -1 && layout.location == -1) {
+			if (v.isSampler()) {
+				prototypes += "\n\tvideo::TextureUnit getBound";
+				prototypes += uniformName;
+				prototypes += "TexUnit() const;\n";
+				methods += "\nvideo::TextureUnit ";
+				methods += filename;
+				methods += "::getBound";
+				methods += uniformName;
+				methods += "TexUnit() const {\n";
+				methods += "\treturn video::TextureUnit::";
+				methods += convertToTexUnit(layout.binding);
+				methods += ";\n}\n";
+			}
+			if (layout.imageFormat != video::ImageFormat::Max) {
+				prototypes += "\n\tvideo::ImageFormat getImageFormat";
+				prototypes += uniformName;
+				prototypes += "() const;\n";
+				methods += "\nvideo::ImageFormat ";
+				methods += filename;
+				methods += "::getImageFormat";
+				methods += uniformName;
+				methods += "() const {\n";
+				methods += "\treturn video::ImageFormat::";
+				methods += util::getImageFormatTypeString(layout.imageFormat);
+				methods += ";\n}\n";
+			}
+			++n;
+			continue;
+		}
+
+		core::String mproto;
+		mproto += "set";
+		mproto += uniformName;
+		mproto += "(";
 
 		if ((v.arraySize > 0 && isInteger) || cType.passBy == PassBy::Reference) {
 			mproto += "const ";
@@ -164,11 +197,6 @@ bool generateSrc(const core::String& templateHeader, const core::String& templat
 		methods += "::";
 		methods += mproto;
 
-		if (v.isSampler() && layout.binding != -1) {
-			mproto += " = video::TextureUnit::";
-			mproto += convertToTexUnit(layout.binding);
-		}
-
 		if (v.arraySize == -1) {
 			mproto += ", int amount";
 			methods += ", int amount";
@@ -190,50 +218,6 @@ bool generateSrc(const core::String& templateHeader, const core::String& templat
 		if (layout.location != -1) {
 			methods += core::string::toString(layout.location);
 			methods += ";\n";
-		} else if ((v.isSampler() || v.isImage()) && layout.binding != -1) {
-			// layout(binding=X) already sets the texture unit at compile time in GLSL 4.2+
-			methods += "0; (void)location; (void)";
-			methods += v.name;
-			methods += ";\n";
-			methods += "\treturn true;\n";
-			methods += "}\n";
-
-			// skip the rest of the setter generation for this uniform
-			if (v.isSampler()) {
-				if (layout.binding != -1) {
-					prototypes += "\n\tvideo::TextureUnit getBound";
-					prototypes += uniformName;
-					prototypes += "TexUnit() const;\n";
-					methods += "\n\nvideo::TextureUnit ";
-					methods += filename;
-					methods += "::getBound";
-					methods += uniformName;
-					methods += "TexUnit() const {\n";
-					methods += "\treturn video::TextureUnit::";
-					methods += convertToTexUnit(layout.binding);
-					methods += ";\n}\n";
-				}
-			}
-			if (v.isSampler() || v.isImage()) {
-				if (layout.imageFormat != video::ImageFormat::Max) {
-					prototypes += "\n\tvideo::ImageFormat getImageFormat";
-					prototypes += uniformName;
-					prototypes += "() const;\n";
-					methods += "\nvideo::ImageFormat ";
-					methods += filename;
-					methods += "::getImageFormat";
-					methods += uniformName;
-					methods += "() const {\n";
-					methods += "\treturn video::ImageFormat::";
-					methods += util::getImageFormatTypeString(layout.imageFormat);
-					methods += ";\n}\n";
-				}
-			}
-			if (n < uniformSize - 2) {
-				methods += "\n";
-			}
-			++n;
-			continue;
 		} else {
 			Log::error("Uniform '%s' has no layout(location=X) or layout(binding=X) qualifier", v.name.c_str());
 			return false;