5035ae456585c37ce45fa0923c9215a3d849c23d
Martin Gerhardy <martin.gerhardy@gmail.com>
2026-05-13 17:23:55 +0200

    APP: removed outdated todo comment - we have core::Future now

--- src/modules/app/Async.h
+++ src/modules/app/Async.h
@@ -13,8 +13,6 @@
 namespace app {
 
 // add new work item to the pool
-// TODO: get rid of the futures here - use schedule or for_parallel - this would allow us to hide the thread pool
-// implementation and make it easier to switch to a different implementation that might also not rely on the STL
 template<class F>
 auto async(F &&f) -> core::Future<core::invoke_result_t<F>> {
 	return app::App::getInstance()->threadPool()->enqueue(core::forward<F>(f));
