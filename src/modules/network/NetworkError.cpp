424c2f974e02c283a2827f39030225c6762c553c
Martin Gerhardy <martin.gerhardy@gmail.com>
2026-07-17 17:48:53 +0200

    NETWORK: fixed warning

--- src/modules/network/NetworkError.cpp
+++ src/modules/network/NetworkError.cpp
@@ -4,6 +4,10 @@
 
 #include "NetworkError.h"
 #ifdef _WIN32
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <winsock2.h>
 #include <windows.h>
 #include <stdio.h>
 #endif
